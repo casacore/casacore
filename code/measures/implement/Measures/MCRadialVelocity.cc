a47f5cd9e5c1a8640798f34417488d2abeb4e907
Wim Brouw <brouw@astron.nl>
1997-04-21 03:42:23 +0000

    MRadialVelocity conversion routines

---
+++ code/measures/implement/Measures/MCRadialVelocity.cc
@@ -0,0 +1,347 @@
+//# MCRadialVelocity.cc: MRadialVelocity conversion routines 
+//# Copyright (C) 1995,1996,1997
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//# $Id$
+
+//# Includes
+#ifdef __GNUG__
+#include <aips/Measures/Quantum.h>
+typedef Quantum<Double> gpp_mepoch_bug1;
+#endif
+#include <aips/Mathematics/Constants.h>
+#include <aips/Measures/QMath.h>
+#include <aips/Measures/MCRadialVelocity.h>
+#include <aips/Measures/MCFrame.h>
+#include <aips/Measures/MVPosition.h>
+#include <aips/Measures/MVDirection.h>
+#include <aips/Measures/Aberration.h>
+#include <aips/Measures/MeasTable.h>
+#include <aips/Measures/MDoppler.h>
+
+//# Constructors
+MCRadialVelocity::MCRadialVelocity() :
+  MVPOS1(0), MVDIR1(0), ABERTO(0), ABERFROM(0) {}
+
+//# Destructor
+MCRadialVelocity::~MCRadialVelocity() {
+  clearConvert();
+}
+
+//# Operators
+
+//# Member functions
+
+void MCRadialVelocity::getConvert(MConvertBase &mc,
+				  const MRBase &inref, 
+				  const MRBase &outref) {
+// Array of conversion routines to call
+  static const MCRadialVelocity::Routes 
+    FromTo[MRadialVelocity::N_Types][MRadialVelocity::N_Types] = {
+      { MCRadialVelocity::N_Routes,
+	MCRadialVelocity::LSR_BARY,
+	MCRadialVelocity::LSR_BARY,
+	MCRadialVelocity::LSR_BARY,
+	MCRadialVelocity::LSR_BARY,
+	MCRadialVelocity::LSR_GALACTO},
+      { MCRadialVelocity::LSRK_BARY,
+	MCRadialVelocity::N_Routes,
+	MCRadialVelocity::LSRK_BARY,
+	MCRadialVelocity::LSRK_BARY,
+	MCRadialVelocity::LSRK_BARY,
+	MCRadialVelocity::LSRK_BARY},
+      { MCRadialVelocity::BARY_LSR,
+	MCRadialVelocity::BARY_LSRK,
+	MCRadialVelocity::N_Routes,
+	MCRadialVelocity::BARY_GEO,
+	MCRadialVelocity::BARY_GEO,
+	MCRadialVelocity::BARY_LSR},
+      { MCRadialVelocity::GEO_BARY,
+	MCRadialVelocity::GEO_BARY,
+	MCRadialVelocity::GEO_BARY,
+	MCRadialVelocity::N_Routes,
+	MCRadialVelocity::GEO_TOPO,
+	MCRadialVelocity::GEO_BARY},
+      { MCRadialVelocity::TOPO_GEO,
+	MCRadialVelocity::TOPO_GEO,
+	MCRadialVelocity::TOPO_GEO,
+	MCRadialVelocity::TOPO_GEO,
+	MCRadialVelocity::N_Routes,
+	MCRadialVelocity::TOPO_GEO},
+      { MCRadialVelocity::GALACTO_LSR,
+	MCRadialVelocity::GALACTO_LSR,
+	MCRadialVelocity::GALACTO_LSR,
+	MCRadialVelocity::GALACTO_LSR,
+	MCRadialVelocity::GALACTO_LSR,
+	MCRadialVelocity::N_Routes}
+    };
+    
+// List of codes converted to
+    static const MRadialVelocity::Types ToRef[MCRadialVelocity::N_Routes] = {
+      MRadialVelocity::BARY,
+      MRadialVelocity::LSR,
+      MRadialVelocity::GEO,
+      MRadialVelocity::TOPO,
+      MRadialVelocity::BARY,
+      MRadialVelocity::GEO,
+      MRadialVelocity::GALACTO,
+      MRadialVelocity::LSR,
+      MRadialVelocity::BARY,
+      MRadialVelocity::LSRK
+    };
+
+    Int iin  = inref.getType();
+    Int iout = outref.getType();
+    Int tmp;
+    while (iin != iout) {
+	tmp = FromTo[iin][iout];
+	iin = ToRef[tmp];
+	mc.addMethod(tmp);
+	initConvert(tmp, mc);
+    }
+}
+
+void MCRadialVelocity::clearConvert() {
+  delete MVPOS1; MVPOS1 = 0;
+  delete MVDIR1; MVDIR1 = 0;
+  delete ABERFROM; ABERFROM = 0;
+  delete ABERTO; ABERTO = 0;
+}
+
+//# Conversion routines
+void MCRadialVelocity::initConvert(uInt which, MConvertBase &mc) {
+  if (!MVPOS1) {
+    MVPOS1 = new MVPosition();
+  };
+  if (!MVDIR1) {
+    MVDIR1 = new MVDirection();
+  };
+
+  switch (which) {
+      
+  case LSR_BARY:
+    break;
+
+  case BARY_LSR:
+    break;
+
+  case BARY_GEO:
+    if (ABERFROM) delete ABERFROM;
+    ABERFROM = new Aberration(Aberration::STANDARD);
+    break;
+
+  case GEO_TOPO:
+    break;
+
+  case GEO_BARY:
+    if (ABERTO) delete ABERTO;
+    ABERTO = new Aberration(Aberration::STANDARD);
+    break;
+
+  case TOPO_GEO:
+    break;
+
+  case LSR_GALACTO:
+    break;
+
+  case GALACTO_LSR:
+    break;
+
+  case LSRK_BARY:
+    break;
+
+  case BARY_LSRK:
+    break;
+
+  default:
+    break;
+  }
+}
+
+void MCRadialVelocity::doConvert(MeasValue &in,
+				 MRBase &inref,
+				 MRBase &outref,
+				 const MConvertBase &mc) {
+  doConvert(*(MVRadialVelocity*)&in,
+	    inref, outref, mc);
+}
+
+void MCRadialVelocity::doConvert(MVRadialVelocity &in,
+				 MRBase &inref,
+				 MRBase &outref,
+				 const MConvertBase &mc) {
+  Double g1, g2, g3, lengthE, tdbTime;
+
+  MCFrame::make(inref.getFrame());
+  MCFrame::make(outref.getFrame());
+
+  for (Int i=0; i<mc.nMethod(); i++) {
+
+    switch (mc.getMethod(i)) {
+
+    case LSR_BARY: {
+      *MVPOS1 = MVPosition(MeasTable::velocityLSR(0));
+      ((MCFrame *)(MRadialVelocity::Ref::frameDirection(outref, inref).
+		   getMCFramePoint()))->
+	getJ2000(*MVDIR1);
+      g1 = (*MVPOS1 * *MVDIR1) / C::c;
+      g2 = in.getValue()/C::c;
+      in = (g2 - g1)/(1 - g2 * g1) * C::c;
+    }
+    break;
+
+    case BARY_LSR: {
+      *MVPOS1 = MVPosition(MeasTable::velocityLSR(0));
+      ((MCFrame *)(MRadialVelocity::Ref::frameDirection(inref, outref).
+		   getMCFramePoint()))->
+	getJ2000(*MVDIR1);
+      g1 = (*MVPOS1 * *MVDIR1) / C::c;
+      g2 = in.getValue()/C::c;
+      in = (g2 + g1)/(1 + g2 * g1) * C::c;
+    }
+    break;
+
+    case BARY_GEO: {
+      ((MCFrame *)(MRadialVelocity::Ref::frameEpoch(outref, inref).
+		   getMCFramePoint()))->
+	getTDB(tdbTime);
+      *MVPOS1 = ABERFROM->operator()(tdbTime);
+      ((MCFrame *)(MRadialVelocity::Ref::frameDirection(outref, inref).
+		   getMCFramePoint()))->
+	getJ2000(*MVDIR1);
+      g1 = *MVPOS1 * *MVDIR1;
+      g2 = in.getValue()/C::c;
+      in = (g2 - g1)/(1 - g2 * g1) * C::c;
+    }	
+    break;
+
+    case GEO_TOPO: {
+      ((MCFrame *)(MRadialVelocity::Ref::frameEpoch(outref, inref).
+		   getMCFramePoint()))->
+	getLASTr(g1);
+      ((MCFrame *)(MRadialVelocity::Ref::framePosition(outref, inref).
+		   getMCFramePoint()))->
+	getRadius(lengthE);
+      ((MCFrame *)(MRadialVelocity::Ref::frameEpoch(outref, inref).
+		   getMCFramePoint()))->
+	getTDB(tdbTime);
+      ((MCFrame *)(MRadialVelocity::Ref::framePosition(outref, inref).
+		   getMCFramePoint()))->
+	getLat(g3);
+      g2 = MeasTable::diurnalAber(lengthE, tdbTime);
+      *MVPOS1 = MVDirection(C::pi_2 + g1, 0.0);
+      MVPOS1->readjust(g2 * cos(g3));
+      ((MCFrame *)(MRadialVelocity::Ref::frameDirection(outref, inref).
+		   getMCFramePoint()))->
+	getApp(*MVDIR1);
+      g1 = *MVPOS1 * *MVDIR1;
+      g2 = in.getValue()/C::c;
+      in = (g2 - g1)/(1 - g2 * g1) * C::c;
+    }
+    break;
+
+    case GEO_BARY: {
+      ((MCFrame *)(MRadialVelocity::Ref::frameEpoch(inref, outref).
+		   getMCFramePoint()))->
+	getTDB(tdbTime);
+      *MVPOS1 = ABERTO->operator()(tdbTime);
+      ((MCFrame *)(MRadialVelocity::Ref::frameDirection(outref, inref).
+		   getMCFramePoint()))->
+	getApp(*MVDIR1);
+      g1 = *MVPOS1 * *MVDIR1;
+      g2 = in.getValue()/C::c;
+      in = (g2 + g1)/(1 + g2 * g1) * C::c;
+    }	
+    break;
+
+    case TOPO_GEO: {
+      ((MCFrame *)(MRadialVelocity::Ref::frameEpoch(inref, outref).
+		   getMCFramePoint()))->
+	getLASTr(g1);
+      ((MCFrame *)(MRadialVelocity::Ref::framePosition(inref, outref).
+		   getMCFramePoint()))->
+	getRadius(lengthE);
+      ((MCFrame *)(MRadialVelocity::Ref::frameEpoch(inref, outref).
+		   getMCFramePoint()))->
+	getTDB(tdbTime);
+      ((MCFrame *)(MRadialVelocity::Ref::framePosition(inref, outref).
+		   getMCFramePoint()))->
+	getLat(g3);
+      g2 = MeasTable::diurnalAber(lengthE, tdbTime);
+      *MVPOS1 = MVDirection(C::pi_2 + g1, 0.0);
+      MVPOS1->readjust(g2 * cos(g3));
+      ((MCFrame *)(MRadialVelocity::Ref::frameDirection(outref, inref).
+		   getMCFramePoint()))->
+	getApp(*MVDIR1);
+      g1 = *MVPOS1 * *MVDIR1;
+      g2 = in.getValue()/C::c;
+      in = (g2 + g1)/(1 + g2 * g1) * C::c;
+    }
+    break;
+
+    case LSR_GALACTO:
+      *MVPOS1 = MVPosition(MeasTable::velocityLSRGal(0));
+      ((MCFrame *)(MRadialVelocity::Ref::frameDirection(inref, outref).
+		   getMCFramePoint()))->
+	getJ2000(*MVDIR1);
+      g1 = (*MVPOS1 * *MVDIR1) / C::c;
+      g2 = in.getValue()/C::c;
+      in = (g2 + g1)/(1 + g2 * g1) * C::c;
+      break;
+
+    case GALACTO_LSR:
+      *MVPOS1 = MVPosition(MeasTable::velocityLSRGal(0));
+      ((MCFrame *)(MRadialVelocity::Ref::frameDirection(outref, inref).
+		   getMCFramePoint()))->
+	getJ2000(*MVDIR1);
+      g1 = (*MVPOS1 * *MVDIR1) / C::c;
+      g2 = in.getValue()/C::c;
+      in = (g2 - g1)/(1 - g2 * g1) * C::c;
+      break;
+
+    case LSRK_BARY:
+      *MVPOS1 = MVPosition(MeasTable::velocityLSRK(0));
+      ((MCFrame *)(MRadialVelocity::Ref::frameDirection(outref, inref).
+		   getMCFramePoint()))->
+	getJ2000(*MVDIR1);
+      g1 = (*MVPOS1 * *MVDIR1) / C::c;
+      g2 = in.getValue()/C::c;
+      in = (g2 - g1)/(1 - g2 * g1) * C::c;
+      break;
+
+    case BARY_LSRK:
+      *MVPOS1 = MVPosition(MeasTable::velocityLSRK(0));
+      ((MCFrame *)(MRadialVelocity::Ref::frameDirection(inref, outref).
+		   getMCFramePoint()))->
+	getJ2000(*MVDIR1);
+      g1 = (*MVPOS1 * *MVDIR1) / C::c;
+      g2 = in.getValue()/C::c;
+      in = (g2 + g1)/(1 + g2 * g1) * C::c;
+      break;
+
+    default:
+      break;
+    }
+  }
+}
---
+++ code/measures/implement/Measures/MCRadialVelocity.h
@@ -0,0 +1,174 @@
+//# MCRadialVelocity.h: MRadialVelocity conversion routines 
+//# Copyright (C) 1995,1996,1997
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//#
+//# $Id$
+
+#if !defined(AIPS_MCRADIALVELOCITY_H)
+#define AIPS_MCRADIALVELOCITY_H
+
+#if defined(_AIX)
+#pragma implementation ("MCRadialVelocity.cc")
+#endif
+
+//# Includes
+#include <aips/aips.h>
+#include <aips/Measures/MeasBase.h>
+#include <aips/Measures/MeasRef.h>
+#include <aips/Measures/MCBase.h>
+#include <aips/Measures/MConvertBase.h>
+#include <aips/Measures/MRadialVelocity.h>
+
+//# Forward Declarations
+class MCRadialVelocity;
+class MDoppler;
+class MVPosition;
+class MVDirection;
+class Aberration;
+
+//# Typedefs
+
+// <summary>
+// MRadialVelocity conversion routines 
+// </summary>
+
+// <use visibility=local>
+
+// <reviewed reviewer="" date="yyyy/mm/dd" tests="tMeasure" demos="">
+// </reviewed>
+
+// <prerequisite>
+//   <li> <linkto class=Measure>Measure</linkto> class 
+//   <li> <linkto class=MCBase>MCBase</linkto> base class
+//   <li> <linkto class=MConvertBase</linkto> overall conversion class
+// </prerequisite>
+//
+// <etymology>
+// Measure, Convert and RadialVelocity
+// </etymology>
+//
+// <synopsis>
+// Contains state machinery and cashing for actual conversions
+//
+// <example>
+// Get the Doppler shift for an oberved HI RadialVelocity of 100 km/s
+// <srcblock>
+//	#include <aips/Measures.h>
+//	#include <aips/Measures/MRadialVelocity.h>
+//	#include <aips/Measures/MDoppler.h>
+//	cout << "Redshift for 100 km/s: " <<
+//		MDoppler::Convert( MRadialVelocity( Quantity(100., "km/s"),
+//				      MRadialVelocity::TOPO).toDoppler(QC::HI),
+//				   MDoppler::Z)() << endl;
+// </srcblock>				   
+// </example>
+//
+// <motivation>
+// </motivation>
+//
+// <todo asof="1997/04/14">
+//   <li> look at relativistic effects
+// </todo>
+
+class MCRadialVelocity : public MCBase {
+  
+public:
+  //# Friends
+  // Conversion of data
+  friend class MeasConvert<MRadialVelocity,MVRadialVelocity,MCRadialVelocity>;
+  
+  //# Constructors
+  // Default constructor
+  MCRadialVelocity();
+  
+  //# Destructor
+  ~MCRadialVelocity();
+  
+private:
+  //# Enumerations
+  // The list of actual routines provided.
+  // <note role=warning> Each <src>AA_BB</src> in the list points to routine
+  // that can be used in the FromTo list in the getConvert routine.
+  // In addition the type to which each is converted should be in the
+  // ToRef array, again in the proper order. </note>
+  enum Routes {
+    LSR_BARY,
+    BARY_LSR,
+    BARY_GEO,
+    GEO_TOPO,
+    GEO_BARY,
+    TOPO_GEO,
+    LSR_GALACTO,
+    GALACTO_LSR,
+    LSRK_BARY,
+    BARY_LSRK,
+    N_Routes };
+  
+  //# Typedefs
+  
+  //# Operators
+  
+  //# General Member Functions
+  
+  //# Enumerations
+  
+  //# Cached Data
+  MVPosition *MVPOS1;
+  MVDirection *MVDIR1;
+  Aberration *ABERFROM;
+  Aberration *ABERTO;
+  
+  //# Constructors
+  // Copy constructor (not implemented)
+  MCRadialVelocity(const MCRadialVelocity &other);
+  // Assignment (not implemented)
+  MCRadialVelocity &operator=(const MCRadialVelocity &other);
+  
+  //# Member functions
+  // Create conversion function pointer
+  virtual void getConvert(MConvertBase &mc,
+			  const MRBase &inref,
+			  const MRBase &outref);
+  
+  // Create help structures for Measure conversion routines
+  virtual void initConvert(uInt which, MConvertBase &mc);
+  
+  // Delete the pointers used in the MeasConvert help structure cache
+  virtual void clearConvert();
+  
+  // Routine to convert RadialVelocity from one reference frame to another
+  virtual void doConvert(MeasValue &in,
+			 MRBase &inref,
+			 MRBase &outref,
+			 const MConvertBase &mc);
+  // Conversion routine to cater for inheritance question
+  void doConvert(MVRadialVelocity &in,
+		 MRBase &inref,
+		 MRBase &outref,
+		 const MConvertBase &mc);
+  
+};
+
+#endif
