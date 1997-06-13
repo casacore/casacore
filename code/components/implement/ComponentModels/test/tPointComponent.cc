47a52b378cbf1f49ea02c7a5dbcbb8a798e49a12
Ralph Marson <rmarson@nrao.edu>
1997-06-13 00:13:45 +0000

    Made to work again

--- code/components/implement/ComponentModels/test/tPointComponent.cc
+++ code/components/implement/ComponentModels/test/tPointComponent.cc
@@ -1,5 +1,5 @@
 //# tPointComponent.cc:  this defines tPointComponent.cc
-//# Copyright (C) 1996
+//# Copyright (C) 1996,1997
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
@@ -24,249 +24,158 @@
 //#                        Charlottesville, VA 22903-2475 USA
 //#
 //# $Id$
+
 #include <aips/aips.h>
-#include <aips/Measures/Quantum.h>
-#include <aips/Measures/MDirection.h>
-#include <aips/Measures/MVDirection.h>
 #include <trial/ComponentModels/PointComponent.h>
-#include <aips/Arrays/Vector.h>
+#include <trial/Coordinates/CoordinateUtil.h>
 #include <trial/Images/PagedImage.h>
-#include <aips/Lattices/IPosition.h>
-#include <aips/Utilities/String.h>
-#include <aips/Mathematics/Constants.h>
-#include <aips/Utilities/String.h>
 #include <trial/MeasurementEquations/StokesVector.h>
 #include <trial/MeasurementEquations/StokesUtil.h>
+#include <aips/Arrays/Vector.h>
+#include <aips/Exceptions/Error.h>
+#include <aips/Lattices/IPosition.h>
+#include <aips/Mathematics/Constants.h>
+#include <aips/Measures/Quantum.h>
+#include <aips/Measures/MeasConvert.h>
+#include <aips/Measures/MCDirection.h>
+#include <aips/Measures/MDirection.h>
+#include <aips/Measures/MVDirection.h>
+#include <aips/Utilities/String.h>
+#include <aips/Utilities/Assert.h>
 
-#include <trial/Measures/SkyPosition.h>
-#include <trial/ImgCrdSys/ImageCoordinate.h>
-ImageCoordinate defaultCoords2D(){
-    // Default object position is at RA=0, Dec=0
-  Vector<Double> defaultPosition(2); defaultPosition = 0;
-  // Default Earth position is for the centre of the VLA at 0:00 UT on 1/1/96
-  //EarthPosition defaultObs(0.0, 1, 1, 1996, 107.61773, 34.07875, 2124.0);
-  // Default Earth position is for the centre of the earth at 0:00 UT on 1/1/96
-  EarthPosition defaultObs(0.0, 1, 1, 1996, 0.0, 0.0, 0.0);
-  // Default Reference Pixel is the first pixel
-  Vector<Double> refPixel(2); refPixel = 1;
-  // Default step size is 1 arc-min/pixel
-  Vector<Double> defaultStep(2); defaultStep = 1./60*C::pi/180; 
-  ProjectedPosition defaultRAandDec(ProjectedPosition::SIN, 
-                                    SkyPosition::EQUATORIAL,
-                                    SkyPosition::J2000,
-                                    defaultPosition, 
-                                    defaultObs,
-                                    0.0, // Rotation
-                                    refPixel,
-                                    defaultStep); 
-  ImageCoordinate defaultAxes;
-  defaultAxes.addAxis(defaultRAandDec);
-  return defaultAxes;
-}
-ImageCoordinate defaultCoords3D(){
-  ImageCoordinate defaultAxes(defaultCoords2D());
-  LinearAxis polAxis(MeasuredValue::POLARIZATION, 0.0, 
-                            ReferenceValue(ReferenceValue::POLARIZATION, 1.0),
-                            1, 1.0);
-  defaultAxes.addAxis(polAxis);
-  return defaultAxes;
-}
-ImageCoordinate defaultCoords4D(){
-  ImageCoordinate defaultAxes(defaultCoords3D());
-  LinearAxis freqAxis(MeasuredValue::FREQUENCY, 1.415E9, 
-                            ReferenceValue(ReferenceValue::FREQUENCY, 0.0),
-                            10E6, 1.0);
-
-  defaultAxes.addAxis(freqAxis);
-  return defaultAxes;
-}
+#ifdef __GNUG__
+typedef MeasConvert<MDirection,MVDirection,MCDirection> gpp_complist_bug1;
+#endif
 
 int main() {
-  Bool anyFailures = False;
-  {
-    Bool Failed = False;
-    // Create a point component at the default position
-    MVDirection defMVdir;
-    MDirection defDirJ2000(defMVdir);
-    MDirection defDirB1950(defMVdir, MDirection::B1950);
-    PointComponent defPoint;
-    StokesVector flux(1.0f, 0.0f, 0.0f, 0.0f);
-    StokesVector zero(0.0f);
-    if (!near(defPoint(defDirJ2000), flux) || 
-	!near(defPoint(defDirB1950), zero))
-      Failed = True;
-    if (Failed) {
-      cout << "Failed"; 
-      anyFailures = True;
+  try {
+    {
+      // Create a point component at the default position
+      MVDirection defMVdir;
+      MDirection defDirJ2000(defMVdir);
+      MDirection defDirB1950(defMVdir, MDirection::B1950);
+      PointComponent defPoint;
+      StokesVector flux(1.0f, 0.0f, 0.0f, 0.0f);
+      StokesVector zero(0.0f);
+      AlwaysAssert(near(defPoint(defDirJ2000), flux), AipsError);
+      AlwaysAssert(near(defPoint(defDirB1950), zero), AipsError);
+      cout << "Passed the default Point component test" << endl;
     }
-    else
-      cout << "Passed";
-    cout << " the default Point component test" << endl;
-  }
-  {
-    Bool Failed = False;
-    // Create a point component at a defined non-J2000 position
-    MVDirection dir1934(Quantity(293.5,"deg"),
-			Quantity(-63, "deg") + Quantity(43, "'"));
-    MDirection coord1934J2000(dir1934, MDirection::J2000);
-    MDirection coord1934B1950(dir1934, MDirection::B1950);
-    StokesVector flux1934(6.3f, 0.0f, 0.0f, 0.0f);
-    PointComponent B1934(flux1934, coord1934B1950);
-    Vector<MDirection> directions(2);
-    directions(0) = coord1934B1950;
-    directions(1) = coord1934J2000;
-    Vector<StokesVector> fluxes(2);
-    fluxes = B1934(directions);
-    if (!near(fluxes(0), flux1934) ||
-	!near(fluxes(1), StokesVector(0.0f)))
-      Failed = True;
-    if (Failed){
-      cout << "Failed";
-      anyFailures = True;
+    {
+      // Create a point component at a defined non-J2000 position
+      MVDirection dir1934(Quantity(293.5,"deg"),
+			  Quantity(-63, "deg") + Quantity(43, "'"));
+      MDirection coord1934J2000(dir1934, MDirection::J2000);
+      MDirection coord1934B1950(dir1934, MDirection::B1950);
+      StokesVector flux1934(6.3f, 0.0f, 0.0f, 0.0f);
+      PointComponent B1934(flux1934, coord1934B1950);
+      Vector<MDirection> directions(2);
+      directions(0) = coord1934B1950;
+      directions(1) = coord1934J2000;
+      Vector<StokesVector> fluxes(2);
+      fluxes = B1934(directions);
+      AlwaysAssert(near(fluxes(0), flux1934), AipsError);
+      AlwaysAssert(near(fluxes(1), StokesVector(0.0f)), AipsError);
+      cout << "Passed the arbitrary Point component test" << endl;
     }
-    else
-      cout << "Passed";
-    cout << " the arbitrary Point component test" << endl;
-  }
-  {
-    Bool Failed = False;
+    {
+      // Create a default point component
+      PointComponent B1934;
+      // Set and verify  the flux of the point component.
+      StokesVector flux1934(6.3f, 0.0f, 0.0f, 0.0f);
+      B1934.setFlux(flux1934);
+      AlwaysAssert(near(B1934.flux(), flux1934), AipsError);
 
-    // Create a default point component
-    PointComponent B1934;
-
-    // Set and verify  the flux of the point component.
-    StokesVector flux1934(6.3f, 0.0f, 0.0f, 0.0f);
-    B1934.setFlux(flux1934);
-    if (!near(B1934.flux(), flux1934))
-      Failed = True;
-
-    // Set and verify the position of the point component. It is internally
-    // converted to a J2000 reference frame
-    MVDirection dir1934(Quantity(293.5,"deg"),Quantity(-63.8,"deg"));
-    MDirection coord1934B1950(dir1934, MDirection::B1950);
-    B1934.setPosition(coord1934B1950);
-    MDirection coord1934J2000 = B1934.position();
-    if (coord1934J2000.getRef().getType() != MDirection::J2000)
-      Failed = True;
-    if (!coord1934J2000.getValue().near(
-         MDirection::Convert(coord1934B1950,MDirection::J2000)().getValue()))
-      Failed = True;
-    // Check this is a point component
-    if (B1934.type().matches("Point") != 1)
-      Failed = True;
-    if (Failed) {
-      cout << "Failed"; 
-      anyFailures = True;
+      // Set and verify the position of the point component. It is internally
+      // converted to a J2000 reference frame
+      MVDirection dir1934(Quantity(293.5,"deg"),Quantity(-63.8,"deg"));
+      MDirection coord1934B1950(dir1934, MDirection::B1950);
+      B1934.setPosition(coord1934B1950);
+      MDirection coord1934J2000 = B1934.position();
+      AlwaysAssert(coord1934J2000.getRef().getType() == MDirection::J2000,
+		   AipsError); 
+      AlwaysAssert(coord1934J2000.getValue().near(MDirection::Convert(
+                   coord1934B1950,MDirection::J2000)().getValue()),AipsError);
+      
+      // Check this is a point component
+      AlwaysAssert(B1934.type().matches("Point") == 1, AipsError);
+      cout << "Passed the set/get parameters test for point components" 
+	   << endl;
     }
-    else
-      cout << "Passed";
-    cout << " the set/get parameters test for point components" << endl;
-  }
-  {
-    Bool Failed = False;
-
+    {
     uInt nx=6, ny=nx;
     uInt nPol = 4;
     uInt nFreq = 2;
     PagedImage<Float> image(IPosition(4,nx,ny,nPol,nFreq), defaultCoords4D(), 
-			    String("test.image"));
-    image.set(Float(0));
+			    "tPointComponent_tmp.image");
+    image.set(0.0f);
     PointComponent defComp;
     MVDirection ra0dec0(Quantity(2, "'"), Quantity(1, "'"));
     MDirection coord00(ra0dec0, MDirection::J2000);
     defComp.setPosition(coord00);
     defComp(image);
-
-    if (!near(image(IPosition(4, 2, 1, 0, 0)), Float(1)))
-      Failed = True;
-    else
-      image(IPosition(4, 2, 1, 0, 0)) = Float(0);
-    if (!near(image(IPosition(4, 2, 1, 0, 1)), Float(1)))
-      Failed = True;
-    else
-      image(IPosition(4, 2, 1, 0, 1)) = Float(0);
+    AlwaysAssert(near(image(IPosition(4, 2, 1, 0, 0)), 1.0f), AipsError);
+    image(IPosition(4, 2, 1, 0, 0)) = 0.0f;
+    AlwaysAssert(near(image(IPosition(4, 2, 1, 0, 1)), 1.0f), AipsError);
+    image(IPosition(4, 2, 1, 0, 1)) = 0.0f;
 
     for (uInt f = 0; f < nFreq; f++)
       for (uInt p = 0; p < nPol; p++)
-	for (uInt i = 0; i < 6; i++)
-	  for (uInt j = 0; j < 6; j++)
-	    if (!near(image(IPosition(4, i, j, p, f)), Float(0)))
-	      Failed = True;
-
-    if (Failed) {
-      cout << "Failed"; 
-      anyFailures = True;
+ 	for (uInt i = 0; i < 6; i++)
+ 	  for (uInt j = 0; j < 6; j++)
+ 	    AlwaysAssert(near(image(IPosition(4, i, j, p, f)), 0.0f), 
+			 AipsError);
+    cout << "Passed the projection to an image test" << endl;
     }
-    else
-      cout << "Passed";
-    cout << " the projection to an image test" << endl;
-  }
-  {
-    Bool Failed = False;
-
-    uInt nx=6, ny=nx;
-    uInt nPol = 1;
-    PagedImage<Float> image(IPosition(3,nx, ny, nPol), defaultCoords3D(), 
-			    String("test.image"));
-    PagedImage<Float> psf(IPosition(2,4), defaultCoords2D(), 
-			  String("psf.image"));
-    image.set(Float(0)); psf.set(Float(0));
-    psf(IPosition(2, 2, 2)) = Float(1);
-    psf(IPosition(2, 1, 2)) = Float(.5);
-    psf(IPosition(2, 3, 2)) = Float(.5);
-    psf(IPosition(2, 2, 1)) = Float(.5);
-    psf(IPosition(2, 2, 3)) = Float(.5);
+    {
+      uInt nx=6, ny=nx;
+      uInt nPol = 1;
+      CoordinateSystem imcoord(defaultCoords2D());
+      addIAxis(imcoord);
+      PagedImage<Float> image(IPosition(3,nx, ny, nPol), imcoord, 
+			      String("test.image"));
+      PagedImage<Float> psf(IPosition(2,4), defaultCoords2D(), 
+			    String("psf.image"));
+      image.set(Float(0)); psf.set(Float(0));
+      psf(IPosition(2, 2, 2)) = Float(1);
+      psf(IPosition(2, 1, 2)) = Float(.5);
+      psf(IPosition(2, 3, 2)) = Float(.5);
+      psf(IPosition(2, 2, 1)) = Float(.5);
+      psf(IPosition(2, 2, 3)) = Float(.5);
     
-    PointComponent defComp;
-    MVDirection ra0dec0(Quantity(2, "'"), Quantity(1, "'"));
-    MDirection coord00(ra0dec0, MDirection::J2000);
-    defComp.setPosition(coord00);
-    defComp(image, psf);
+      PointComponent defComp;
+      MVDirection ra0dec0(Quantity(2, "'"), Quantity(1, "'"));
+      MDirection coord00(ra0dec0, MDirection::J2000);
+      defComp.setPosition(coord00);
+      defComp(image, psf);
 
-    if (!near(image(IPosition(3, 2, 1, 0)), Float(1)))
-      Failed = True;
-    else
-      image(IPosition(3, 2, 1, 0)) = Float(0);
+      AlwaysAssert(near(image(IPosition(3, 2, 1, 0)), 1.0f), AipsError);
+      image(IPosition(3, 2, 1, 0)) = 0.0f;
+      AlwaysAssert(near(image(IPosition(3, 1, 1, 0)), 0.5f), AipsError);
+      image(IPosition(3, 1, 1, 0)) = 0.0f;
+      AlwaysAssert(near(image(IPosition(3, 3, 1, 0)), 0.5f), AipsError);
+      image(IPosition(3, 3, 1, 0)) = 0.0f;
+      AlwaysAssert(near(image(IPosition(3, 2, 0, 0)), 0.5f), AipsError);
+      image(IPosition(3, 2, 0, 0)) = 0.0f;
+      AlwaysAssert(near(image(IPosition(3, 2, 2, 0)), 0.5f), AipsError);
+      image(IPosition(3, 2, 2, 0)) = 0.0f;
 
-    if (!near(image(IPosition(3, 1, 1, 0)), Float(0.5)))
-      Failed = True;
-    else
-      image(IPosition(3, 1, 1, 0)) = Float(0);
+      for (uInt i = 0; i < 6; i++)
+        for (uInt j = 0; j < 6; j++)
+	  AlwaysAssert(nearAbs(image(IPosition(3, i, j, 0)), 0.0f), AipsError);
 
-    if (!near(image(IPosition(3, 3, 1, 0)), Float(0.5)))
-      Failed = True;
-    else
-      image(IPosition(3, 3, 1, 0)) = Float(0);
-
-    if (!near(image(IPosition(3, 2, 0, 0)), Float(0.5)))
-      Failed = True;
-    else
-      image(IPosition(3, 2, 0, 0)) = Float(0);
-
-    if (!near(image(IPosition(3, 2, 2, 0)), Float(0.5)))
-      Failed = True;
-    else
-      image(IPosition(3, 2, 2, 0)) = Float(0);
-
-     for (uInt i = 0; i < 6; i++)
-       for (uInt j = 0; j < 6; j++)
-	 if (!nearAbs(image(IPosition(3, i, j, 0)), Float(0)))
-	   Failed = True;
-
-    if (Failed) {
-      cout << "Failed"; 
-      anyFailures = True;
+      cout << "Passed the projection to an image (with convolution) test" 
+	   << endl;
     }
-    else
-      cout << "Passed";
-    cout << " the projection to an image (with convolution) test" << endl;
   }
-  if (anyFailures) {
+  catch (AipsError x) {
+    cerr << x.getMesg() << endl;
     cout << "FAIL" << endl;
     return 1;
-  }
-  else {
-    cout << "OK" << endl;
-    return 0;
-  }
+  } end_try;
+  cout << "OK" << endl;
+  return 0;
 }
+// Local Variables: 
+// compile-command: "gmake OPTLIB=1 tPointComponent"
+// End: 
