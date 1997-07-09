fc9924d0f619f53b160f3b8df242efa87e01bb0d
Ralph Marson <rmarson@nrao.edu>
1997-07-09 22:47:53 +0000

    New heavily modified version. Hardly debugged at all.

--- code/components/implement/ComponentModels/ComponentList.cc
+++ code/components/implement/ComponentModels/ComponentList.cc
@@ -26,118 +26,93 @@
 //# $Id$
 
 #include <trial/ComponentModels/ComponentList.h>
-#include <trial/MeasurementEquations/StokesVector.h>
+#include <trial/ComponentModels/ComponentType.h>
 #include <trial/Images/ImageInterface.h>
-#include <aips/Measures.h>
-#include <aips/Measures/MCDirection.h>
-#include <aips/Tables/TableDesc.h>
-#include <aips/Tables/ScaColDesc.h>
-#include <aips/Tables/ArrColDesc.h>
-#include <aips/Tables/SetupNewTab.h>
-#include <aips/Tables/TableRecord.h>
-#include <aips/Tables/ScalarColumn.h>
-#include <aips/Tables/ArrayColumn.h>
-#include <aips/Mathematics/Constants.h>
-#include <trial/ComponentModels/GaussianComponent.h>
-#include <trial/ComponentModels/PointComponent.h>
-#include <trial/MeasurementEquations/StokesVector.h>
+
+#include <aips/Arrays/Array.h>
+#include <aips/Arrays/ArrayMath.h>
+#include <aips/Arrays/Vector.h>
 #include <aips/Exceptions/Error.h>
-#include <iostream.h>
+#include <aips/Measures/MCDirection.h>
+#include <aips/Measures/MVDirection.h>
+#include <aips/Measures/MDirection.h>
+#include <aips/Measures/MeasConvert.h>
+#include <aips/Measures/Quantum.h>
+#include <aips/OS/Directory.h>
+#include <aips/OS/File.h>
+#include <aips/Tables/ArrColDesc.h>
+#include <aips/Tables/ArrayColumn.h>
+#include <aips/Tables/ScaColDesc.h>
+#include <aips/Tables/ScalarColumn.h>
+#include <aips/Tables/SetupNewTab.h>
+#include <aips/Tables/Table.h>
+#include <aips/Tables/TableDesc.h>
+#include <aips/Tables/TableRecord.h>
+#include <aips/Utilities/Assert.h>
+#include <aips/Utilities/String.h>
+// #include <iostream.h>
+
 #ifdef __GNUG__
 typedef MeasConvert<MDirection,MVDirection,MCDirection> gpp_complist_bug1;
 #endif
 
-ComponentList::
-ComponentList()
-  :theFileName(), 
-   theList(new List<CountedPtr<SkyComponent> >(), True)
+ComponentList::ComponentList()
+  :theList(),
+   theNelements(0),
+   theFileName("defaultComponentList")
 {
-};
+}
 
-ComponentList::
-ComponentList(SkyComponent & component)
-  :theFileName(), 
-   theList(new List<CountedPtr<SkyComponent> >(), True)
+ComponentList::ComponentList(const File & fileName, const Bool readOnly)
+  :theList(),
+   theNelements(0),
+   theFileName("defaultComponentList") 
 {
-     insertComp(component);
-};
+  {
+    // The table is always opened readonly
+    const Table table(fileName.path().expandedName(), Table::Old);
+    const ROScalarColumn<String> typeCol(table, "Type");
+    const ROArrayColumn<Double> fluxCol(table, "Flux");
+    const ROArrayColumn<Double> dirCol(table, "Direction");
+    const TableRecord dirKeywords(dirCol.keywordSet());
+    const ROArrayColumn<Double> parmCol(table, "Parameters");
+    MDirection compDir;
+    {
+      MDirection::Ref refType;
+      String frame;
+      dirKeywords.get("Frame", frame);
+      AlwaysAssert(compDir.giveMe(frame, refType), AipsError);
+      compDir.set(refType);
+    }
+    Vector<Double> dir(2);
+    Quantum<Vector<Double> > qdir(dir);
+    {
+      String angleUnit;
+      dirKeywords.get("Unit", angleUnit);
+      qdir.setUnit(angleUnit);
+    }
+    const uInt nComp = typeCol.nrow();
+    Vector<Double> flux, parameters;
+    String componentName;
+    SkyComponent currentComp;
+    for (uInt i = 0; i < nComp; i++) {
+      typeCol.get(i, componentName);
+      currentComp = SkyComponent(ComponentType::type(componentName));
+      fluxCol.get(i, flux); currentComp.setFlux(flux);
+      dirCol.get(i, dir); qdir.setValue(dir); compDir.set(qdir);
+      currentComp.setPosition(compDir);
+      parmCol.get(i, parameters); currentComp.setParameters(parameters);
+      add(currentComp);
+    }
+  }
+  if (!readOnly)
+    setName(fileName);
+}
 
-ComponentList::
-ComponentList(CountedPtr<SkyComponent> ptrComponent)
-  :theFileName(), 
-   theList(new List<CountedPtr<SkyComponent> >(), True)
-{
-     insertComp(ptrComponent);
-};
-
-ComponentList::
-ComponentList(SkyComponent * ptrComponent)
-  :theFileName(), 
-   theList(new List<CountedPtr<SkyComponent> >(), True)
-{
-     insertComp(ptrComponent);
-};
-
-ComponentList::
-ComponentList(const String & filename, Bool readOnly)  
-  :theFileName(filename), 
-   theList(new List<CountedPtr<SkyComponent> >(), True)
-{
-     if (readOnly)
-       theFileName = "";
-     // The table is always opened readonly
-     Table table(filename);
-     ROScalarColumn<String> typeCol(table, "Type");
-     ROArrayColumn<Float> fluxCol(table, "Flux");
-     ROArrayColumn<Double> dirCol(table, "Direction");
-     ROArrayColumn<Double> parmCol(table, "Parameters");
-     TableRecord dirKeywords(dirCol.keywordSet());
-     String angleUnit, frame;
-     dirKeywords.get("Unit", angleUnit);
-     dirKeywords.get("Frame", frame);
-
-     String componentType;
-     //
-     Vector<Float> flux(4);
-     StokesVector sflux;
-     //
-     Vector<Double> dir(2);
-     Quantum<Vector<Double> > qdir(dir, angleUnit);
-     MDirection compDir;
-     {
-       MDirection::Ref refType;
-       if (compDir.giveMe(frame, refType) == False)
-	 throw(AipsError("ComponentList::ComponentList(const String &, Bool)"
-			 " - Could not translate the reference frame"));
-       compDir.set(refType);
-     }
-     //
-     uInt nComponents = typeCol.nrow();
-     for (uInt i = 0; i < nComponents; i++){
-       typeCol.get(i, componentType);
-       if (componentType.matches("Point"))
-	 addComp(new PointComponent());
-       else if (componentType.matches("Gaussian"))
-	 addComp(new GaussianComponent());
-       else
-	 throw(AipsError("ComponentList::ComponentList(const String &, Bool)"
-			 " - Unknown component type"));
-
-       fluxCol.get(i, flux);
-       sflux = flux;
-       getComp()->setFlux(sflux);
-       //       cout << " Flux = " << flux.ac();
-       dirCol.get(i, dir);
-       qdir.setValue(dir);
-       compDir.set(qdir);
-       //       cout << compDir << endl;
-       getComp()->setPosition(compDir);
-     }
-};
-
-ComponentList::
-~ComponentList(){
-  if (theFileName.empty() == 0){
+ComponentList::~ComponentList() {
+  if (theFileName.path().expandedName() != "defaultComponentList"){
+    // The setName function must have been called. 
+    // Assume the file can be created.
     // These two constants define the units and frame of the output list
     const String angleUnits("deg");
     const String refFrame("J2000");
@@ -149,197 +124,108 @@ ComponentList::
       td.addColumn (typeCol);
 
       ArrayColumnDesc<Float> fluxCol("Flux" ,"Stokes I,Q,U,V flux in Jy",
-				     IPosition(1,4),  ColumnDesc::Direct);
+ 				     IPosition(1,4),  ColumnDesc::Direct);
       fluxCol.rwKeywordSet().define ("Unit", "Jy");
       td.addColumn(fluxCol);
 
       ArrayColumnDesc<Double> dirCol("Direction" ,"RA/Dec in "
-				     + angleUnits + " ("+refFrame+")",
-				     IPosition(1,2),  ColumnDesc::Direct);
+ 				     + angleUnits + " ("+refFrame+")",
+ 				     IPosition(1,2),  ColumnDesc::Direct);
       dirCol.rwKeywordSet().define ("Unit", angleUnits);
       dirCol.rwKeywordSet().define ("Frame", refFrame);
       td.addColumn(dirCol);
       
       ArrayColumnDesc<Double> 
-	parmCol("Parameters", "Parameters specific to this component type", 1);
+ 	parmCol("Parameters", "Parameters specific to this component type", 1);
       td.addColumn(parmCol);
     }
-    SetupNewTable newTable(theFileName, td, Table::New);
-    Table table(newTable, nComponents(), True);
+    Table::TableOption tableStatus;
+    if (theFileName.exists()) {
+      AlwaysAssert(theFileName.isWritable(), AipsError);
+      tableStatus = Table::Update;
+    }
+    else {
+      Directory dirName(theFileName);
+      AlwaysAssert(dirName.isWritable(), AipsError);
+      tableStatus = Table::New;
+    }
+    SetupNewTable newTable(theFileName.path().expandedName(), td, tableStatus);
+    Table table(newTable, nelements(), True);
     ScalarColumn<String> typeCol(table, "Type");
-    ArrayColumn<Float> fluxCol(table, "Flux");
+    ArrayColumn<Double> fluxCol(table, "Flux");
     ArrayColumn<Double> dirCol(table, "Direction");
     ArrayColumn<Double> parmCol(table, "Parameters");
 
-    StokesVector flux;
     MDirection compDir;
     Vector<Double> dirn;
     uInt refNum;
-    Vector<Double> parameters(3); parameters = 1;
     {
       MDirection::Ref refType;
-      if (compDir.giveMe(refFrame, refType) == False)
-	throw(AipsError("ComponentList::~ComponentList()"
-			" - Could not translate the default reference frame"));
+      AlwaysAssert(compDir.giveMe(refFrame, refType), AipsError);
       refNum = refType.getType();
     }
-    
-    gotoStart();
-    for (uInt i = 0; i < nComponents(); i++){
-      typeCol.put(i, getComp()->type());
-      //
-      flux = getComp()->flux();
-      fluxCol.put(i, flux.vector());
-      //
-      compDir = getComp()->position();
+    Vector<Double> compFlux(4), compParms;
+    for (uInt i = 0; i < nelements(); i++) {
+      typeCol.put(i, component(i).type());
+      component(i).flux(compFlux);
+      fluxCol.put(i, compFlux);
+      component(i).position(compDir);
       if (compDir.getRef().getType() != refNum)
-	compDir = MDirection::Convert(compDir, refNum)();
+ 	compDir = MDirection::Convert(compDir, refNum)();
       dirn = compDir.getAngle().getValue(angleUnits);
       dirCol.put(i, dirn);
-      //
-      // parameters.resize(0);
-      // parameters = getComp()->parameters()
-      parmCol.put(i, parameters);
-      //
-      nextComp();
+      compParms.resize(component(i).nParameters());
+      component(i).parameters(compParms);
+      parmCol.put(i, compParms);
     }
   }
-};
+}
 
-StokesVector ComponentList::
-operator()(const MDirection & samplePos) {
-  StokesVector result(0.0, 0.0, 0.0, 0.0);
-  for (uInt i = 0; i < nComponents(); i++) {
-    if (curPosition() == nComponents()) 
-      gotoStart();
-    else
-      nextComp();
-    result += getComp()->operator()(samplePos);
+void ComponentList::sample(Vector<Double> & result, const MDirection & samplePos) const {
+  result = 0.0;
+  Vector<Double> compResult(4);
+  for (uInt i = 0; i < nelements(); i++) {
+    component(i).sample(compResult, samplePos);
+    result.ac() += compResult.ac();
   }
-  return result;
-};
+}
 
-Vector<StokesVector> ComponentList::
-operator()(const Vector<MDirection> & samplePos) {
-  Vector<StokesVector> result(samplePos.nelements());
-  result = StokesVector(0.0, 0.0, 0.0, 0.0);
-  for (uInt i = 0; i < samplePos.nelements(); i++) {
-    result(i) = operator()(samplePos(i));
+void ComponentList::project(ImageInterface<Float> & plane) const {
+  for (uInt i = 0; i < nelements(); i++)
+    component(i).project(plane);
+}
+
+void ComponentList::add(SkyComponent & component) {
+  uInt blockSize = theList.nelements();
+  if (theNelements == blockSize) {
+    uInt newSize = (blockSize < 50) ? 2 * blockSize + 1 : blockSize + 100;
+    theList.resize(newSize);
   }
-  return result;
-};
+  theList[theNelements] = component;
+  theNelements++;
+}
 
-void ComponentList::
-operator()(ImageInterface<Float> & plane){
-  for (uInt i = 0; i < nComponents(); i++) {
-    if (curPosition() == nComponents()) 
-      gotoStart();
-    else
-      nextComp();
-    getComp()->operator()(plane);
-  }
-};
+void ComponentList::remove(uInt index) {
+  theList.remove(index, False);
+  theNelements--;
+}
 
-void ComponentList::
-operator()(ImageInterface<Float> & plane, const ImageInterface<Float> & psf) {
-  for (uInt i = 0; i < nComponents(); i++) {
-    if (curPosition() == nComponents()) 
-      gotoStart();
-    else
-      nextComp();
-    getComp()->operator()(plane, psf);
-  }
-};
+uInt ComponentList::nelements() const {
+  return theNelements;
+}
 
-void ComponentList::
-insertComp(SkyComponent & component){
-  CountedPtr<SkyComponent> countedPtr(&component, False);
-  insertComp(countedPtr);
-};
+SkyComponent & ComponentList::component(uInt index) {
+  return theList[index];
+}
 
-void ComponentList::
-insertComp(CountedPtr<SkyComponent> & countedPtr){
-  theList.addRight(countedPtr);
-};
+const SkyComponent & ComponentList::component(uInt index) const {
+  return theList[index];
+}
 
-void ComponentList::
-insertComp(SkyComponent * ptrComponent){
-  CountedPtr<SkyComponent> countedPtr(ptrComponent);
-  insertComp(countedPtr);
-};
+void ComponentList::setName(const File & fileName) {
+  theFileName = fileName;
+}
 
-void ComponentList::
-addComp(SkyComponent & component){
-  CountedPtr<SkyComponent> countedPtr(&component, False);
-  addComp(countedPtr);
-};
-
-void ComponentList::
-addComp(CountedPtr<SkyComponent> & countedPtr){
-  if (nComponents() != 0) theList++;
-  insertComp(countedPtr);
-};
-
-void ComponentList::
-addComp(SkyComponent * ptrComponent){
-  CountedPtr<SkyComponent> countedPtr(ptrComponent);
-  addComp(countedPtr);
-};
-
-void ComponentList::
-removeComp(){
-  theList.removeRight();
-  if ((curPosition() > nComponents()) && (nComponents() != 0))
-    theList--;
-};
-
-void ComponentList::
-gotoStart() {
-  theList.toStart();
-};
-
-void ComponentList::
-gotoEnd() {
-  theList.toEnd();
-  theList--;
-};
-
-void ComponentList::
-gotoPosition(uInt index) {
-  theList.pos(index-1);
-};
-
-void ComponentList::
-nextComp() {
-  if (curPosition() < nComponents())
-    theList++;
-};
-
-void ComponentList::
-prevComp() {
-  if (curPosition() > 1)
-    theList--;
-};
-
-uInt ComponentList::
-nComponents() const {
-  return theList.len();
-};
-
-uInt ComponentList::
-curPosition() const {
-  return theList.pos() + 1;
-};
-
-CountedPtr<SkyComponent> ComponentList::
-getComp(){
-  return theList.getRight();
-};
-
-void ComponentList::
-setListName(const String & filename){
-  theFileName = filename;
-};
 // Local Variables: 
 // compile-command: "gmake OPTLIB=1 ComponentList"
 // End: 
