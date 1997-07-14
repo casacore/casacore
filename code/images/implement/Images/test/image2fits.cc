9a8884642fbc71d90c0dba4444312b7d25d8440f
Brian Glendenning <bglenden@naro.edu>
1997-07-14 23:38:36 +0000

    Works with ImageFITSConverter.h now.

--- code/images/implement/Images/test/image2fits.cc
+++ code/images/implement/Images/test/image2fits.cc
@@ -30,8 +30,8 @@
 //-----------------------------------------------------------------------------
 #include <aips/aips.h>
 
-#include <trial/ImgCrdSys/ImageCoordinate.h>
 #include <trial/Images/PagedImage.h>
+#include <trial/Images/ImageFITSConverter.h>
 
 #include <aips/Arrays/Array.h>
 #include <aips/Containers/MapIO.h>
@@ -48,104 +48,44 @@
 
 Int main(int argc, char *argv[])
 {
-  try {
-    // Inputs
-    Input inp(1);
-    inp.Version(" ");
-    inp.Create("in", " ", "Input AIPS++ Image name", "string");
-    inp.Create("out", " ", "Output FITS file name", "string");
-    inp.Create("verbose", "False", "Verbose?", "Bool");
-    inp.ReadArguments(argc, argv);
+    try {
+	// Inputs
+	Input inp(1);
+	inp.Version(" ");
+	inp.Create("in", "in.image", "Input AIPS++ Image name", "string");
+	inp.Create("out", "out.fits", "Output FITS file name", "string");
+	inp.Create("verbose", "False", "Verbose?", "Bool");
+	inp.ReadArguments(argc, argv);
     
-    Bool verbose=inp.GetBool("verbose");
+	Bool verbose=inp.GetBool("verbose");
     
-    File inputFile(inp.GetString("in"));
-    if (!inputFile.isReadable()) 
-      throw (AipsError ("input file unreadable"));
+	File inputFile(inp.GetString("in"));
+	if (!inputFile.isReadable()) 
+	    throw (AipsError ("input file unreadable"));
     
+	String out(inp.GetString("out"));
+	if (out == "") out = "out.fits";
     
-    // Get the image from disk
-    PagedImage<Float> image(inputFile.path().originalName());
-    IPosition imageShape(image.shape());
-    if(verbose) {
-      cout << "Read input image " << inputFile.path().originalName()
-	  << " successfully" << endl;
-      cout << "Shape is " << imageShape << endl;
-    };
+	// Get the image from disk
+	PagedImage<Float> image(inputFile.path().originalName());
+	IPosition imageShape(image.shape());
+	if(verbose) {
+	    cout << "Read input image " << inputFile.path().originalName()
+		 << " successfully" << endl;
+	    cout << "Shape is " << imageShape << endl;
+	}
 
-    // Use getSlice to get the whole thing at once
-    Array<Float> limage(image.shape());
-    uInt numberOfAxes = imageShape.nelements();
-    IPosition ipStart(numberOfAxes,0);
-    IPosition ipStride(numberOfAxes,1);
-    image.getSlice(limage,ipStart,imageShape,ipStride);
+	String error;
+	Bool ok = ImageFITSConverter::ImageToFITS(error, image, out);
+	if (!ok) {
+	    cout << "Error writing FITS file: " << error << endl;
+	    return 1;
+	}
 
-    // Units are not defined in Image yet
-    String units = image.units().getName();;
-
-    // Fix once axis names are tranlated properly
-    Block<String> axisNames = image.coordinates().axisNames();
-    Vector<Double> referenceValuesD = image.coordinates().referenceValues();
-    Vector<Double> referencePixelsD = image.coordinates().referencePixels();
-    Vector<Double> deltasD = image.coordinates().deltas();
-    Vector<String> names(numberOfAxes);
-    Vector<Float> referenceValues(numberOfAxes);
-    Vector<Float> referencePixels(numberOfAxes);
-    Vector<Float> deltas(numberOfAxes);
-
-    // Copy values and output if required
-    for (Int axis=0;axis<numberOfAxes;axis++) {
-      names(axis) = axisNames[axis];
-      referenceValues(axis)=referenceValuesD(axis);
-      referencePixels(axis)=referencePixelsD(axis);
-      deltas(axis)=deltasD(axis);
-      if(names(axis)=="SIN Right Ascension") {
-         names(axis)="RA---SIN";
-         referenceValues(axis)=referenceValuesD(axis)/C::degree;
-         deltas(axis)=deltasD(axis)/C::degree;
-      }
-      if(names(axis)=="SIN Declination") {
-         names(axis)="DEC--SIN";
-         referenceValues(axis)=referenceValuesD(axis)/C::degree;
-         deltas(axis)=deltasD(axis)/C::degree;
-      }
-      if(names(axis)=="Frequency") {
-         names(axis)="FREQ";
-      }
-      if(verbose) {
-	cout<<"Output Axis "<<axis<<endl;
-	cout<<"   name    = "<<names(axis)<<endl;
-	cout<<"   ref val = "<<referenceValues(axis)<<endl;
-	cout<<"   ref pix = "<<referencePixels(axis)<<endl;
-	cout<<"   delta= "<<deltas(axis)<<endl;
-      };
-    }
-    //OrderedMap <String, Double> keywords (0.0);
-
-    String outputFilename = inp.GetString("out");
-    if(outputFilename==" ") {
-       outputFilename=inputFile.path().originalName()+".FITS";
-    };
-    String errorMessage;
-    Bool success = WriteFITS(outputFilename, limage, errorMessage,
-			     units.chars(), &names, &referencePixels,
-			     &referenceValues, &deltas);
-    if (!success) {
-      cout << "  WriteFITS error message: " << errorMessage << endl;
-      return 1;
-    }
-  
-    cout << "Write complete of " << outputFilename << " complete" << endl;
-    } // try
-  catch (AipsError x) {
-    cout << "Exception: " << x.getMesg() << endl;
-    return 1;
-  } end_try;
-
-  return 0;  
+    } catch (AipsError x) {
+	cout << "Exception: " << x.getMesg() << endl;
+	return 1;
+    } end_try;
+    
+    return 0;  
 }
-
-
-
-
-
