0ce2f0af4d87248f32ffe5c5d68ad5cbcd4a5daa
Mark Wieringa <Mark.Wieringa@csiro.au>
1997-10-13 08:08:53 +0000

    initial version - doesn't handle missing correlations very well

---
+++ code/ms/implement/MeasurementSets/StokesConverter.cc
@@ -0,0 +1,310 @@
+//# StokesConverter.cc: convert polarizations from one frame to another
+//# Copyright (C) 1997
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//#
+//# $Id$
+
+
+#include <trial/MeasurementComponents/StokesConverter.h>
+#include <trial/Arrays/SquareMatrix.h>
+#include <aips/Arrays/ArrayMath.h>
+#include <aips/Arrays/MatrixMath.h>
+#include <aips/Exceptions/Error.h>
+#include <aips/Mathematics/Math.h>
+#include <aips/Utilities/Assert.h>
+
+StokesConverter::StokesConverter() {}
+
+StokesConverter::StokesConverter(const Vector<Int>& out, const Vector<Int>& in)
+{
+  setConversion(out,in);
+}
+
+StokesConverter& StokesConverter::operator=(const StokesConverter& other)
+{
+  setConversion(other.out_p,other.in_p);
+}
+
+void StokesConverter::setConversion(const Vector<Int>& out, 
+				    const Vector<Int>& in)
+{
+  doIQUV_p=False;
+  initConvMatrix();
+  Int nIn=in.nelements();
+  Int nOut=out.nelements();
+  out_p.resize(nOut);
+  out_p=out;
+  in_p.resize(nIn);
+  in_p=in;
+  conv_p.resize(nOut,nIn);
+  // analyze the input: for now we assume it will either be 
+  // some linears (XX, XY, YX, YY) or some circulars (RR, RL, LR, RR)
+  // other cases are not yet handled.
+  Bool linear=False, circular=False, iquv=False, circlin=False, lincirc=False;
+  Int count=0;
+  {for (Int i=0; i<nIn; i++) {
+    if (in(i)>=Stokes::I && in(i) <=Stokes::V) {
+      if (!iquv) count++;
+      iquv=True;
+    }
+    if (in(i)>=Stokes::XX && in(i) <=Stokes::YY) {
+      if (!linear) count++;
+      linear=True;
+    }
+    if (in(i)>=Stokes::RR && in(i) <=Stokes::LL) {
+      if (!circular) count++;
+      circular=True;
+    }
+    if (in(i)>=Stokes::RX && in(i) <=Stokes::LY) {
+      if (!circlin) count++;
+      circlin=True;
+    }
+    if (in(i)>=Stokes::XR && in(i) <=Stokes::YL) {
+      if (!lincirc) count++;
+      lincirc=True;
+    }
+  }}
+  if (count==0) {
+    throw(AipsError("StokesConverter::setConversion - input polarization"
+		    " frame not supported"));
+  }
+  if (count>1) {
+    throw(AipsError("StokesConverter::setConversion - input polarizations"
+		    " cannot be in a mixture of frames"));
+  }
+  // set up the conversion matrix
+  for (Int i=0; i<nOut; i++) {
+    if (out(i)<Stokes::PP) {
+      for (Int j=0; j<nIn; j++) { 
+	conv_p(i,j)=polConv_p(out(i)-1,in(j)-1);
+      }
+    } else {
+      // if output has Ptotal, Plinear or Pangle (or PFtotal, PFlinear), we
+      // also setup the matrix for conversion to Stokes.
+      if (out(i)>=Stokes::Ptotal && out(i)<=Stokes::Pangle && !doIQUV_p) {
+	doIQUV_p=True;
+	iquvconv_p.resize(4,nIn);
+	for (Int j=0; j<nIn; j++) {
+	  for (Int k=0; k<4; k++) {
+	    iquvconv_p(k,j)=polConv_p(k,in(j)-1);
+	  }
+	}
+      }
+    }
+  }
+}
+
+void StokesConverter::initConvMatrix()
+{
+  Complex Slin[4][4] = 
+  { 0.5, 0.5, 0.0, 0.0,
+    0.0, 0.0, 0.5, Complex(0.0,0.5),
+    0.0, 0.0, 0.5, Complex(0.0,-0.5),
+    0.5, -0.5, 0.0, 0.0
+  };
+  Complex H[2][2] =
+  { 1.0, Complex(0.0,1.0),
+    1.0, Complex(0.0,-1.0)
+  };
+  SquareMatrix<Complex,4> Slinear(Slin);
+  SquareMatrix<Complex,2> h(H),hconj;
+  h*=(1/sqrt(2.0));
+  hconj=h;
+  hconj.conj();
+  SquareMatrix<Complex,4> Scirc;
+  directProduct(Scirc,h,hconj);
+  Scirc*=Slinear;
+
+  SquareMatrix<Complex,4> Slincirc;
+  SquareMatrix<Complex,2> I2;
+  directProduct(Slincirc,I2,hconj);
+  Slincirc*=Slinear;
+
+  SquareMatrix<Complex,4> Scirclin;
+  directProduct(Scirclin,h,I2);
+  Scirclin*=Slinear;
+
+  polConv_p.resize(20,20);
+  polConv_p.set(0.0);
+  polConv_p.diagonal().set(1.0);
+  polConv_p(Slice(4,4),Slice(0,4))=Scirc.matrix();
+  polConv_p(Slice(8,4),Slice(0,4))=Slinear.matrix();
+  polConv_p(Slice(12,4),Slice(0,4))=Scirclin.matrix();
+  polConv_p(Slice(16,4),Slice(0,4))=Slincirc.matrix();
+  polConv_p(Slice(0,4),Slice(4,4))=Scirc.inverse().matrix();
+  polConv_p(Slice(0,4),Slice(8,4))=Slinear.inverse().matrix();
+  polConv_p(Slice(0,4),Slice(12,4))=Scirclin.inverse().matrix();
+  polConv_p(Slice(0,4),Slice(16,4))=Slincirc.inverse().matrix();
+  SquareMatrix<Complex,4> tmp;
+  // circ -> lin
+  tmp=Slinear; tmp*=Scirc.inverse();
+  polConv_p(Slice(8,4),Slice(4,4))=tmp.matrix();
+  // circ -> circlin
+  tmp=Scirclin; tmp*=Scirc.inverse();
+  polConv_p(Slice(12,4),Slice(4,4))=tmp.matrix();
+  // circ -> lincirc
+  tmp=Slincirc; tmp*=Scirc.inverse();
+  polConv_p(Slice(16,4),Slice(4,4))=tmp.matrix();
+  // lin -> circ
+  tmp=Scirc; tmp*=Slinear.inverse();
+  polConv_p(Slice(4,4),Slice(8,4))=tmp.matrix();
+  // lin -> circlin
+  tmp=Scirclin; tmp*=Slinear.inverse();
+  polConv_p(Slice(12,4),Slice(8,4))=tmp.matrix();
+  // lin -> lincirc
+  tmp=Slincirc; tmp*=Slinear.inverse();
+  polConv_p(Slice(16,4),Slice(8,4))=tmp.matrix();
+  // circlin -> circ
+  tmp=Scirc; tmp*=Scirclin.inverse();
+  polConv_p(Slice(4,4),Slice(12,4))=tmp.matrix();
+  // circlin -> lin
+  tmp=Slinear; tmp*=Scirclin.inverse();
+  polConv_p(Slice(8,4),Slice(12,4))=tmp.matrix();
+  // circlin -> lincirc
+  tmp=Slincirc; tmp*=Scirclin.inverse();
+  polConv_p(Slice(16,4),Slice(12,4))=tmp.matrix();
+  // lincirc -> circ
+  tmp=Scirc; tmp*=Slincirc.inverse();
+  polConv_p(Slice(4,4),Slice(16,4))=tmp.matrix();
+  // lincirc -> lin
+  tmp=Slinear; tmp*=Slincirc.inverse();
+  polConv_p(Slice(8,4),Slice(16,4))=tmp.matrix();
+  // lincirc -> circlin
+  tmp=Scirclin; tmp*=Slincirc.inverse();
+  polConv_p(Slice(12,4),Slice(16,4))=tmp.matrix();
+
+  // remove roundoff
+  for (Int i=0; i<20; i++) {
+    for (Int j=0; j<20; j++) {
+      if (nearAbs(polConv_p(i,j),Complex(0.,0.),1.e-4)) 
+	polConv_p(i,j)=Complex(0.,0.);
+      if (nearAbs(polConv_p(i,j),Complex(1.,0.),1.e-4)) 
+	polConv_p(i,j)=Complex(1.,0.);
+      if (nearAbs(polConv_p(i,j),Complex(-1.,0.),1.e-4)) 
+	polConv_p(i,j)=Complex(-1.,0.);
+      if (nearAbs(polConv_p(i,j),Complex(0.,1.),1.e-4)) 
+	polConv_p(i,j)=Complex(0.,1.);
+      if (nearAbs(polConv_p(i,j),Complex(0.,-1.),1.e-4)) 
+	polConv_p(i,j)=Complex(0.,-1.);
+      if (nearAbs(polConv_p(i,j),Complex(0.5,0.),1.e-4)) 
+	polConv_p(i,j)=Complex(0.5,0.);
+      if (nearAbs(polConv_p(i,j),Complex(-0.5,0.),1.e-4)) 
+	polConv_p(i,j)=Complex(-0.5,0.);
+      if (nearAbs(polConv_p(i,j),Complex(0.,0.5),1.e-4)) 
+	polConv_p(i,j)=Complex(0.,0.5);
+      if (nearAbs(polConv_p(i,j),Complex(0.,-0.5),1.e-4)) 
+	polConv_p(i,j)=Complex(0.,-0.5);
+    }
+  }
+}
+
+void StokesConverter::convert(Array<Complex>& out, const Array<Complex>& in)
+{
+  IPosition outShape(in.shape()); outShape(0)=out_p.nelements();
+  Int nDim=in.ndim();
+  out.resize(outShape);
+  Int nCorrIn=in.shape()(0);
+  DebugAssert(nCorrIn==in_p.nelements(),AipsError);
+  Matrix<Complex> inMat=in.reform(IPosition(2,nCorrIn,in.nelements()/nCorrIn));
+
+  Matrix<Complex> outMat=out.reform(IPosition(2,outShape(0),
+					      out.nelements()/outShape(0)));
+  IPosition iquvShape(outMat.shape()); iquvShape(0)=4;
+  Matrix<Complex> iquv;
+  if (doIQUV_p) iquv.resize(iquvShape);
+  IPosition outStart(nDim,0),outEnd(outShape-1);
+
+  for (Int i=0; i<out_p.nelements(); i++) {
+    Int pol = out_p(i);
+    if (pol<Stokes::PP) {
+      // linear conversion
+      outMat(Slice(i,1),Slice())=product(conv_p(Slice(i,1),Slice()),inMat);
+    } else if (pol >= Stokes::Ptotal && pol<= Stokes::Pangle) {
+      // first convert to IQUV
+      for (Int j=0; j<4; j++) {
+	iquv(Slice(j,1),Slice())=product(iquvconv_p(Slice(j,1),Slice()),inMat);
+      }
+      // now calculate required parameter
+      // todo: there are some possible large temporaries to be optimized here
+      switch (pol) {
+      case Stokes::Ptotal:
+      case Stokes::PFtotal:
+	{
+	  Array<Complex> tmp;
+	  Vector<Float> outf;
+	  for (Int j=1; j<=3; j++) {
+	    tmp=iquv.row(j);
+	    tmp*=conj(tmp);
+	    if (j==1) outf=real(tmp);
+	    else outf.ac()+=real(tmp);
+	  }
+	  outf.apply(sqrtf);
+	  if (pol==Stokes::PFtotal) {
+	    outf.ac()/=amplitude(iquv.row(0).ac());
+	  }
+	  for (Int k=0; k<outf.nelements(); k++) outMat(i,k)=outf(k);
+	}
+	break;
+      case Stokes::Plinear:
+      case Stokes::PFlinear:
+	{
+	  Array<Complex> tmp;
+	  Vector<Float> outf;
+	  for (Int j=1; j<=2; j++) {
+	    tmp=iquv.row(j);
+	    tmp*=conj(tmp);
+	    if (j==1) outf=real(tmp);
+	    else outf.ac()+=real(tmp);
+	  }
+	  outf.apply(sqrtf);
+	  if (pol==Stokes::PFlinear) {
+	    outf.ac()/=amplitude(iquv.row(0).ac());
+	  }
+	  for (Int k=0; k<outf.nelements(); k++) outMat(i,k)=outf(k);
+	}
+	break;
+      case Stokes::Pangle:
+	{
+	  // note: angle is not well defined for complex quantities
+	  // only makes sense if Q and U phase differs by 0 or 180 degrees.
+	  Vector<Float> outf=atan2(real(iquv.row(2)),real(iquv.row(1)));
+	  outf.ac()/=2.0f;
+      	  // convertArray(outMat.row(i).ac(),outf.ac());
+	  // convertArray is broken 1997/10/09, spell it out
+	  for (Int k=0; k<outf.nelements(); k++) outMat(i,k)=outf(k);
+	}
+	break;
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
+
---
+++ code/ms/implement/MeasurementSets/StokesConverter.h
@@ -0,0 +1,149 @@
+//# StokesConverter.h: convert any set of polarizations into any other one
+//# Copyright (C) 1997
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//#
+//# $Id$
+
+#if !defined(TRIAL_STOKESCONVERTER_H)
+#define TRIAL_STOKESCONVERTER_H
+
+#include <aips/aips.h>
+#include <aips/Arrays/Vector.h>
+#include <aips/Arrays/Matrix.h>
+#include <aips/Mathematics/Complex.h>
+#include <aips/Measures/Stokes.h>
+
+// <summary>
+// StokesConverter converts any set of polarizations into any other one
+// </summary>
+
+// <use visibility=export>
+
+// <reviewed reviewer="" date="yyyy/mm/dd" tests="tStokesConverter" demos="">
+// </reviewed>
+
+// <prerequisite>
+//   <li> <a href="aips/Measures/Stokes.html"> Stokes </a>
+// </prerequisite>
+//
+// <etymology>
+// StokesConverter is a class that converts Stokes Parameters
+// </etymology>
+//
+// <synopsis>
+// This class is used to convert polarizations from one system to
+// another.
+// First the conversion wanted is specified and then large blocks of data
+// can be converted.
+// StokesConverter StokesConverter(myMS);
+// <example>
+// <srcblock>
+// // create converter
+//    StokesConverter sc;
+//    Vector<Int> out(7),in(4);
+// // set the input polarizations   
+//    in(0)=Stokes::RR;
+//    in(1)=Stokes::LL;
+//    in(2)=Stokes::RL;
+//    in(3)=Stokes::LR;
+// // set the required output  
+//    out(0)=Stokes::I;
+//    out(1)=Stokes::Q;
+//    out(2)=Stokes::U;
+//    out(3)=Stokes::V;
+//    out(4)=Stokes::Ptotal;
+//    out(5)=Stokes::Pangle;
+//    out(6)=Stokes::PFlinear;
+// // initialize the conversion engine   
+//    sc.setConversion(out,in);
+// // set up some test data   
+//    Vector<Complex> datain(4),dataout(7);
+//    datain(0)=1.0;
+//    datain(1)=0.9;
+//    datain(2)=0.3;
+//    datain(3)=0.2;
+// // convert the data   
+//    sc.convert(dataout,datain);
+// </srcblock>
+// </example>
+// </synopsis>
+//
+// <motivation>
+// Polarization conversion is needed in various places. It makes sense to
+// provide all conversion in one place.
+// </motivation>
+//
+// <thrown>
+//    <li>
+//    <li>
+// </thrown>
+//
+// <todo asof="1997/10/09">
+//   <li> cope with incomplete input polarizations sensibly
+//   <li> decide what to do about factor 2 between I and RR/LL,XX/YY etc.
+// </todo>
+
+class StokesConverter
+{
+public:
+
+  // default constructor, does not set up a conversion
+  StokesConverter();
+  
+  // Set up a conversion from in to out.
+  // The in and out vectors contain a list of polarization present/wanted
+  // in that order. The in vector should match the data to convert.
+  // (CORR_TYPE column in SPECTRAL_WINDOW table contains this info)
+  StokesConverter(const Vector<Int>& out, const Vector<Int>& in);
+  
+  // Copy constructor
+  StokesConverter(const StokesConverter& other);
+  
+  // Assignment, 
+  StokesConverter& operator=(const StokesConverter& other);
+  
+  // Change or Set the conversion. Arguments are the same as for
+  // constructor above.
+  void setConversion(const Vector<Int>& out, const Vector<Int>& in);
+  
+  // convert data, first dimension of input must match
+  // that of the conversion vector used to set up the conversion.
+  // Output is resized as needed.
+  void convert(Array<Complex>& out, const Array<Complex>& in);
+
+protected:
+
+  // initialize the polarization conversion matrix
+  void initConvMatrix();
+
+private:
+  Vector<Int> in_p,out_p;
+  Matrix<Complex> conv_p;
+  Matrix<Complex> iquvconv_p;
+  Bool doIQUV_p;
+  Matrix<Complex> polConv_p;
+};
+
+#endif
