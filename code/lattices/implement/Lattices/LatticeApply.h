84ab87b32386445d3f418efa0894923558e1938f
Ger van Diepen <gervandiepen@gmail.com>
1997-10-13 12:05:56 +0000

    Added some doc

--- code/lattices/implement/Lattices/LatticeApply.h
+++ code/lattices/implement/Lattices/LatticeApply.h
@@ -1,4 +1,4 @@
-//# LatticeApply.h: optimally iterate through Lattcies and apply provided function
+//# LatticeApply.h: Optimally iterate through Lattices and apply provided function
 //# Copyright (C) 1996,1997
 //# Associated Universities, Inc. Washington DC, USA.
 //#
@@ -31,87 +31,161 @@
 #pragma implementation ("LatticeApply.cc")
 #endif
 
-// <summary> Classes to asists computation with optimal lattice iteration </summary>
+//# Includes
+#include <aips/aips.h>
+#include <aips/Containers/Block.h>
+
+//# Forward Declarations
+template <class T> class VectorCollapser;
+template <class T> class Lattice;
+class IPosition;
+
+
+// <summary>
+// Optimally iterate through Lattices and apply provided function
+// </summary>
+
 // <use visibility=export>
-//
+
 // <reviewed reviewer="" date="yyyy/mm/dd" tests="" demos="">
 // </reviewed>
-//
+
 // <prerequisite>
 //   <li> Lattice
 //   <li> LatticeIterator
 //   <li> LatticeStepper
 //   <li> TiledStepper
 // </prerequisite>
-//
-// <etymology>
-// </etymology>
-//
+
 // <synopsis>
 // Optimally iterate though a Lattice and apply the supplied function
 // to the Lattice data.
 // </synopsis>
-//
+
 // <example>
-// <srcBlock>
-// </srcBlock>
+// <srcblock>
+// </srcblock>
 // </example>
-// 
-// <note role=caution>
-// </note>
-//      
-// <note role=tip>
-// </note>
-//      
+
 // <motivation>
 // </motivation>
-//      
-// <todo asof="1997/08/01">   
-//   <li> 
-// </todo>
-//
- 
-#include <aips/aips.h>
-#include <aips/Containers/Block.h>
-template <class T> class VectorCollapser;
-template <class T> class Lattice;
-class IPosition;
 
+//# <todo asof="1997/08/01">   
+//#   <li> 
+//# </todo>
+
+ 
 // Provided by the experts to push the data through the algorithm as
 // efficiently as possible
 
 template <class T> class LatticeApply
 {
 public:
-  static void vectorApply(Lattice<T>& latticeOut, 
-                          Lattice<T>& latticeIn,
-                          VectorCollapser<T>& operation,
-                          const Int profileAxis,
-                          const IPosition blc,
-                          const IPosition trc,
-                          const Bool dropAxis,
-                          const Bool showProgress,
-                          const String progressTitle);
+// This function iterates through a Lattice and applies a user given
+// function to profiles along the specified axis.  The result of the
+// user supplied function is written into the output Lattice at the
+// location of the collapsed profile.  The output lattice must be supplied
+// with the correct shape; it must have the shape of the supplied region
+// 
+// Inputs
+//  latticeIn  The input Lattice
+//  collapser  The user supplies an object of a class derived from
+//             the base class VectorCollapser.  The user provides the
+//             implementation of the method "collapse" which takes
+//             a profile and returns a value from it.
+//  profileAxis     
+//             The profile axis
+//  blc,trc    The region of interest of the input Lattice.  Will be
+//             filled in (0 & shape-1)if values not given or illegal
+//  dropAxis   If False, then the output Lattice must have as many 
+//             dimensions as the input Lattice, but its shape for
+//             axis "index" must be unity.   E.g. if
+//               in.shape() = [2,4,6]  and profileAxis=1,  then
+//              out.shape() = [2,1,6]
+//          
+//             If True, then the output Lattice must have one less 
+//             dimension than the input Lattice; the axis "index" is 
+//             thus dropped from the output Lattice.    E.g. if
+//               in.shape() = [2,4,6]  and profileAxis=1,  then
+//              out.shape() = [2,6]
+//  showProgress 
+//             Show ProgressMeter
+//  progressTitle
+//             title for progress meter
+// Output
+//  latticeOut The output lattice.  It must be the correct shape to account
+//             for the desired region (i.e. shape = trc - blc + 1)
 
-  static void vectorMultiApply(PtrBlock<Lattice<T> *> &latticeOut, 
-                               Lattice<T>& latticeIn,
-                               VectorCollapser<T>& collapser,
-                               const Int VectorAxis,
-                               const IPosition blc,
-                               const IPosition trc,
-                               const Bool dropAxis,
-                               const Bool showProgress,
-                               const String progressTitle);
+    static void vectorApply (Lattice<T>& latticeOut, 
+			     const Lattice<T>& latticeIn,
+			     VectorCollapser<T>& operation,
+			     const Int profileAxis,
+			     const IPosition blc,
+			     const IPosition trc,
+////			     const IPosition inc,
+			     const Bool dropAxis,
+			     const Bool showProgress,
+			     const String progressTitle);
+    
+// This function iterates through a Lattice and applies a user given
+// function to profiles along the specified axis.  The result of the
+// user supplied function is written into the output Lattices at the
+// location of the collapsed profile.  The output lattices must be supplied
+// with the correct shape; it must have the shape of the supplied region
+//
+// The collapser member function "collapse" must return a vector of numbers.
+// These are assigned to the output lattices, one to one.
+// 
+// Inputs
+//  latticeIn  The input Lattice
+//  collapser  The user supplies an object of a class derived from
+//             the base class VectorCollapser.  The user provides the
+//             implementation of the method "multiCollapse" which takes
+//             a profile and returns a vector of values from it.
+//  profileAxis     
+//             The profile axis
+//  blc,trc    The region of interest of the input Lattice.  Will be
+//             filled in (0 & shape-1)if values not given or illegal
+//  dropAxis   If False, then the output Lattice must have as many 
+//             dimensions as the input Lattice, but its shape for
+//             axis "index" must be unity.   E.g. if
+//               in.shape() = [2,4,6]  and profileAxis=1,  then
+//              out.shape() = [2,1,6]
+//          
+//             If True, then the output Lattice must have one less 
+//             dimension than the input Lattice; the axis "index" is 
+//             thus dropped from the output Lattice.    E.g. if
+//               in.shape() = [2,4,6]  and profileAxis=1,  then
+//              out.shape() = [2,6]
+//  showProgress 
+//             Show ProgressMeter
+//  progressTitle
+//             title for progress meter
+// Output
+//  latticeOut The output lattices.  They must be the correct shape to account
+//             for the desired region (i.e. shape = trc - blc + 1)
 
+    static void vectorMultiApply (PtrBlock<Lattice<T> *> &latticeOut, 
+				  const Lattice<T>& latticeIn,
+				  VectorCollapser<T>& collapser,
+				  const Int VectorAxis,
+				  const IPosition blc,
+				  const IPosition trc,
+////				  const IPosition inc,
+				  const Bool dropAxis,
+				  const Bool showProgress,
+				  const String progressTitle);
+    
 private:
 
-   static void prepare(IPosition& ioMap,
-                       IPosition& inBlc,
-                       IPosition& inTrc,
-                       Lattice<T>& latticeIn,
-                       Lattice<T>& latticeOut,
-                       const Int VectorAxis,
-                       const Bool dropAxis);
+   static void prepare (IPosition& ioMap,
+			IPosition& inBlc,
+			IPosition& inTrc,
+			IPosition& inInc,
+			const Lattice<T>& latticeIn,
+			Lattice<T>& latticeOut,
+			const Int VectorAxis,
+			const Bool dropAxis);
 
 };
 
