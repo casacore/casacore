a971eae6c2f03be354b373e4e633648794b92daf
Wes Young <wyoung@nrao.edu>
1997-10-14 16:45:51 +0000

    Changed copyright noticed.

--- code/casa/implement/BasicMath/Math.h
+++ code/casa/implement/BasicMath/Math.h
@@ -1,5 +1,5 @@
 //# Math.h: AIPS++ interface to <src><math.h></src> and other general functions
-//# Copyright (C) 1993,1994,1995,1996
+//# Copyright (C) 1993,1994,1995,1996,1997
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
