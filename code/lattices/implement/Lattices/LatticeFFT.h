bb86d04b128aba672f095b51c50a5ff212c05b7a
Ralph Marson <rmarson@nrao.edu>
1997-10-22 20:11:49 +0000

    First implementation of this class is reworkrd from selected functions in LatticeUtil

---
+++ code/lattices/implement/Lattices/LatticeFFT.h
@@ -0,0 +1,87 @@
+//# LatticeFFT.h: Definitions for Lattice FFT functions
+//# Copyright (C) 1996,1997
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be adressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//#
+//# $Id$
+
+#if !defined(AIPS_LATTICEFFT_H)
+#define AIPS_LATTICEFFT_H
+
+#include <aips/aips.h>
+#include <aips/Mathematics/Complex.h>
+
+template <class T> class Vector;
+template <class T> class Lattice;
+
+// <summary>Functions for Fourier transforming Lattices</summary>
+
+// <reviewed reviewer="" date="" tests="" demos="">
+
+// <prerequisite>
+// </prerequisite>
+//
+// <etymology>
+// </etymology>
+//
+// <synopsis> 
+// </synopsis> 
+//
+// <example>
+// <srcblock>
+// </srcblock>
+// </example>
+//
+// <motivation>
+// </motivation>
+//
+// <todo asof="">
+// </todo>
+
+class LatticeFFT
+{
+public: 
+  // 2-D in-place complex->complex FFT. Transforms over the first two
+  // dimensions and iterates over all the others. The Lattice must have two or
+  // more dimensions otherwise and AipsError is thrown.
+  static void fft2d(Lattice<Complex> & cLattice, const Bool toFrequency=True);
+  
+  // N-D in-place complex->complex FFT. Only transform over selected
+  // dimensions. Iterate over the others. whichAxes must be the same length as
+  // the number of dimensions in the Lattice otherwise an AipsError is thrown.
+  static void fft(Lattice<Complex> & cLattice,
+  		  const Vector<Bool> & whichAxes, const Bool toFrequency=True);
+
+  // N-D real->complex FFT. Only one half of the Hermition result is
+  // returned. Transforms are only done on selected dimensions.
+//   static void fft(Lattice<Complex> & out, const Lattice<Float> & in, 
+//  		  const Vector<Bool> & whichAxes);
+
+  // FFT a complex Lattice that is assumed to be Hermition (so that only half
+  // of the data is represented) to produce a Real one. Transforms are only
+  // done on selected dimensions.
+//   static void fft(Lattice<Float> & out, const Lattice<Complex> & in, 
+//  		  const Vector<Bool> & whichAxes);
+};
+#endif
