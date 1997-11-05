0870aaa90f36b1fedee78a03c805a7485663d637
Ralph Marson <rmarson@nrao.edu>
1997-11-05 23:17:07 +0000

    First go at this file only tests one function!

---
+++ code/lattices/implement/Lattices/test/tTempLattice.cc
@@ -0,0 +1,50 @@
+//# tTempLattice.cc
+//# Copyright (C) 1996,1997
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This program is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU General Public License as published by the Free
+//# Software Foundation; either version 2 of the License, or (at your option)
+//# any later version.
+//#
+//# This program is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+//# more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with this program; if not, write to the Free Software Foundation, Inc.,
+//# 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//# $Id$
+
+#include <aips/aips.h>
+#include <aips/Exceptions/Error.h>
+#include <aips/Exceptions/Excp.h>
+#include <aips/Lattices/IPosition.h>
+#include <aips/Utilities/Assert.h>
+#include <trial/Lattices/TempLattice.h>
+#include <iostream.h>
+
+int main() {
+  try {
+    TempLattice<Float> small(IPosition(3,10,10,10));
+    AlwaysAssert(small.ok() == True, AipsError);
+  } catch (AipsError x) {
+    cerr << x.getMesg() << endl;
+    cout << "FAIL" << endl;
+    return 1;
+  } end_try;
+  cout << "OK" << endl;
+  return 0;
+}
+// Local Variables: 
+// compile-command: "gmake OPTLIB=1 tTempLattice"
+// End: 
