fa268dd8ae5d87c0ddf00012fe7ebe0abe33c32b
Brian Glendenning <bglenden@naro.edu>
1997-11-12 20:07:14 +0000

    *** empty log message ***

---
+++ code/casa/implement/OS/Memory.cc
@@ -0,0 +1,53 @@
+//# Memory.cc: Memory related information and utilities.
+//# Copyright (C) 1997
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//#
+//# $Id$
+
+#include <aips/OS/Memory.h>
+#include <malloc.h>
+
+size_t Memory::allocatedMemoryInBytes()
+{
+    struct mallinfo m = mallinfo();
+    return m.hblkhd + m.usmblks + m.uordblks;
+}
+
+size_t Memory::assignedMemoryInBytes()
+{
+    struct mallinfo m = mallinfo();
+    return m.arena;
+}
+
+void Memory::releaseMemory()
+{
+#if defined(AIPS_RELEASEMEM)
+    AIPS_RELEASEMEM;
+#else if defined(AIPS_LINUX)
+    malloc_trim(0);
+#endif
+}
+
+
