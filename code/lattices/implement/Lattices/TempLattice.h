7cd8b3dad1529f54c3dc55ca8a07a7f2787c0b1c
Ger van Diepen <gervandiepen@gmail.com>
1997-11-24 08:01:40 +0000

    added functions clone, isWritable

--- code/lattices/implement/Lattices/TempLattice.h
+++ code/lattices/implement/Lattices/TempLattice.h
@@ -29,14 +29,13 @@
 #if !defined(AIPS_TEMPLATTICE_H)
 #define AIPS_TEMPLATTICE_H
 
-#if defined(_AIX)
-#pragma implementation ("TempLattice.cc")
-#endif
 
+//# Includes
 #include <aips/aips.h>
 #include <aips/Utilities/CountedPtr.h>
 #include <trial/Lattices/Lattice.h>
 
+//# Forward Declarations
 template <class T> class Array;
 template <class T> class COWPtr;
 template <class Domain, class Range> class Functional;
@@ -45,7 +44,10 @@ class IPosition;
 class Slicer;
 class LatticeNavigator;
 
-// <summary>A Lattice that can be used for temporary storage</summary>
+
+// <summary>
+// A Lattice that can be used for temporary storage
+// </summary>
 
 // <use visibility=export>
 
@@ -57,14 +59,14 @@ class LatticeNavigator;
 //   <li> <linkto class="ArrayLattice">ArrayLattice</linkto>
 //   <li> <linkto class="PagedArray">PagedArray</linkto>
 // </prerequisite>
-//
+
 // <etymology>
 // A TempLattice disappears from both memory and disk when it goes out of
 // scope. Hence it is only useful for temporary storage of data. Unlike Arrays
 // however TempLattices can get huge and not place excessive demands on the
 // computers paging system. This is because 
 // </etymology>
-//
+
 // <synopsis>
 // Lattice classes are designed to allow the memory efficient handling of large
 // amounts of data. But they can also used with much smaller arrays. With
@@ -74,7 +76,7 @@ class LatticeNavigator;
 // data the <linkto class="ArrayLattice">ArrayLattice</linkto> class should be
 // used as all the data is always in memory avoiding the I/O associated with
 // PagedArrays.
-
+//
 // However applications often cannot predict until run time whether they will
 // be dealing with a large or small amount of data. So that the use of a
 // PagedArray or an ArrayLattice cannot be made until the size of the arrays
@@ -82,7 +84,7 @@ class LatticeNavigator;
 // help in making a good choice the TempLattice class also examines how much
 // memory the operating system has and compares it with the size of the
 // requested Arrays.
-
+//
 // The algorithm currently used is to create an ArrayLattice if the size of the
 // array is less than a quarter of the total system memory otherwise a
 // PagedArray is created. The PagedArray is currently stored in the current
@@ -92,27 +94,22 @@ class LatticeNavigator;
 // they are used by a process, the PagedArrays created by the TempArray class
 // are always scratch arrays.
 // </synopsis>
-//
+
 // <example>
 // <srcblock>
 // </srcblock>
 // </example>
-//
+
 // <motivation>
 // I needed a temporaryt Lattice when converting the Convolver class to using
 // Lattices. This was to store the Transfre function.
 // </motivation>
-//
+
 // <templating arg=T>
 // Any type that can be used by the Tables System can also be used by
 // this class.
 // </templating>
-//
-// <thrown>
-//    <li>
-//    <li>
-// </thrown>
-//
+
 // <todo asof="yyyy/mm/dd">
 //   <li> add this feature
 //   <li> fix this bug
@@ -132,20 +129,26 @@ public:
   // positive value to the maxMemoryinMB argument. Otherwise it will assume
   // it can use up to 25% of the memory on your machine (this algorithm may
   // change).
-  TempLattice(const IPosition & shape, Int maxMemoryInMB=-1);
+  TempLattice (const IPosition& shape, Int maxMemoryInMB=-1);
   
   // The copy constructor uses reference semantics. ie modifying data in the
   // coipied TempLattice also modifies the data in the original TempLattice.
   // Passing by value doesn't make sense, because it may require the creation
   // of a temporary (but possibly huge) file on disk.
-  TempLattice(const TempLattice<T> & other) ;
+  TempLattice (const TempLattice<T>& other) ;
     
   // The destructor removes the Lattice from memory and if necessary disk.
   virtual ~TempLattice();
 
   // the assignment operator with reference semantics. As with the copy
   // constructor assigning by value does not make sense.
-  TempLattice<T> & operator=(const TempLattice<T> & other);
+  TempLattice<T>& operator= (const TempLattice<T>& other);
+
+  // Make a copy of the object (reference semantics).
+  virtual Lattice<T>* clone() const;
+
+  // Is the TempLattice writable? It should be.
+  virtual Bool isWritable() const;
 
   // returns the shape of the Lattice including all degenerate axes
   // (ie. axes with a length of one)
@@ -160,7 +163,7 @@ public:
   
   // returns a value of "True" if this instance of Lattice and 'other' have 
   // the same shape, otherwise returns a value of "False".
-  virtual Bool conform(const Lattice<T> & other) const;
+  virtual Bool conform (const Lattice<T>& other) const;
   
   // Functions which extract an Array of values from a Lattice. All the
   // IPosition arguments must have the same number of axes as the underlying
@@ -197,16 +200,16 @@ public:
   // These functions return 'True' if "buffer" is a reference to Lattice data
   // and 'False' if it is a copy.
   // <group>   
-  virtual Bool getSlice(COWPtr<Array<T> > & buffer, const IPosition & start,
-			const IPosition & shape, const IPosition & stride,
-			Bool removeDegenerateAxes=False) const;
-  virtual Bool getSlice(COWPtr<Array<T> > & buffer, const Slicer & section,
-			Bool removeDegenerateAxes=False) const;
-  virtual Bool getSlice(Array<T> & buffer, const IPosition & start, 
-			const IPosition & shape, const IPosition & stride, 
-			Bool removeDegenerateAxes=False);
-  virtual Bool getSlice(Array<T> & buffer, const Slicer & section, 
-			Bool removeDegenerateAxes=False);
+  virtual Bool getSlice (COWPtr<Array<T> >& buffer, const IPosition& start,
+			 const IPosition& shape, const IPosition& stride,
+			 Bool removeDegenerateAxes=False) const;
+  virtual Bool getSlice (COWPtr<Array<T> >& buffer, const Slicer& section,
+			 Bool removeDegenerateAxes=False) const;
+  virtual Bool getSlice (Array<T>& buffer, const IPosition& start, 
+			 const IPosition& shape, const IPosition& stride, 
+			 Bool removeDegenerateAxes=False);
+  virtual Bool getSlice (Array<T>& buffer, const Slicer& section, 
+			 Bool removeDegenerateAxes=False);
   // </group>   
   
   // A function which places an Array of values within this instance of the
@@ -216,14 +219,13 @@ public:
   // will) have less axes than the Lattice. The stride defaults to one if
   // not specified. 
   // <group>   
-  virtual void putSlice(const Array<T> & sourceBuffer, const IPosition & where,
-			const IPosition & stride);
-  virtual void putSlice(const Array<T> & sourceBuffer, const IPosition &
-			where);
+  virtual void putSlice (const Array<T>& sourceBuffer, const IPosition& where,
+			 const IPosition& stride);
+  virtual void putSlice (const Array<T>& sourceBuffer, const IPosition& where);
   // </group>   
   
   // function which sets all of the elements in the Lattice to a value.
-  virtual void set(const T & value);
+  virtual void set (const T& value);
 
   // replace every element, x, of the Lattice with the result of f(x).  You
   // must pass in the address of the function -- so the function must be
@@ -237,9 +239,9 @@ public:
   // issue for large Lattices stored in memory, where disk access is not an
   // issue.
   // <group>
-  virtual void apply(T (*function)(T));
-  virtual void apply(T (*function)(const T &));
-  virtual void apply(const Functional<T,T> & function);
+  virtual void apply (T (*function)(T));
+  virtual void apply (T (*function)(const T&));
+  virtual void apply (const Functional<T,T>& function);
   // </group>
 
   // This function returns the recommended maximum number of pixels to
@@ -249,12 +251,12 @@ public:
   // Returns a recommended cursor shape for iterating through all the pixels in
   // the Lattice.  Usually, this function will be called with
   // <src>maxPixels()</src> as its argument.
-  virtual IPosition niceCursorShape(uInt maxPixels) const;
+  virtual IPosition niceCursorShape (uInt maxPixels) const;
 
   // These are the true implementations of the parentheses operator.
   // <group>
-  virtual T getAt(const IPosition & where) const;
-  virtual void putAt(const T & value, const IPosition & where);
+  virtual T getAt (const IPosition& where) const;
+  virtual void putAt (const T& value, const IPosition& where);
   // </group>
   
   // Check class internals - used for debugging. Should always return True
@@ -263,9 +265,12 @@ public:
   // This function is used by the LatticeIterator class to generate an
   // iterator of the correct type for this Lattice. Not recommended
   // for general use. 
-  virtual LatticeIterInterface<T> * makeIter(
-				 const LatticeNavigator & navigator) const;
+  virtual LatticeIterInterface<T>*
+                      makeIter (const LatticeNavigator& navigator) const;
+
 private:
-  CountedPtr<Lattice<T> > theLatticePtr;
+  CountedPtr<Lattice<T> > itsLatticePtr;
 };
+
+
 #endif
