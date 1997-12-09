7247bf4943732e71f8cbcbd4a63840668acc0a3d
Ger van Diepen <gervandiepen@gmail.com>
1997-12-09 09:14:26 +0000

    Initial check in.

---
+++ code/measures/implement/TableMeasures/TableMeasOffsetDesc.cc
@@ -0,0 +1,194 @@
+//# TableMeasOffsetDesc.cc: Definition of a offset measure in a Table.
+//# Copyright (C) 1997
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//# $Id$
+
+
+//# Includes
+#include <aips/Exceptions.h>
+#include <aips/Measures/MDirection.h>
+#include <aips/Measures/MDoppler.h>
+#include <aips/Measures/MEpoch.h>
+#include <aips/Measures/MFrequency.h>
+#include <aips/Measures/MPosition.h>
+#include <aips/Measures/MRadialVelocity.h>
+#include <aips/Measures/Measure.h>
+#include <aips/Tables/Table.h>
+#include <aips/Tables/TableRecord.h>
+#include <trial/TableMeasures/TableMeasOffsetDesc.h>
+#include <trial/TableMeasures/TableMeasDescBase.h>
+#include <trial/TableMeasures/TableMeasDesc.h>
+
+TableMeasOffsetDesc::TableMeasOffsetDesc(const TableMeasDescBase& column)
+: itsTMDesc(column.clone()),
+  itsMeasure(0)
+{}
+
+TableMeasOffsetDesc::TableMeasOffsetDesc(const Measure& measure)
+: itsTMDesc(0),
+  itsMeasure(measure.clone())
+{}
+
+TableMeasOffsetDesc::TableMeasOffsetDesc(const TableMeasOffsetDesc& that)
+: itsTMDesc(0),
+  itsMeasure(0)
+{
+    *this = that;
+}
+    
+TableMeasOffsetDesc::~TableMeasOffsetDesc()
+{
+    delete itsTMDesc;
+    delete itsMeasure;
+}
+
+TableMeasOffsetDesc* TableMeasOffsetDesc::reconstruct(
+	    	    	    	    	    const TableRecord& measInfo,
+				    	    const String& prefix,
+				    	    const Table& tab)
+{
+    TableMeasOffsetDesc* P = 0;
+    
+    if ((measInfo.fieldNumber(prefix + "Msr") >= 0) || 
+    	(measInfo.fieldNumber(prefix + "Col") >= 0)) {
+   	P = new TableMeasOffsetDesc(measInfo, prefix, tab);
+    }
+    
+    return P;
+}
+
+TableMeasOffsetDesc::TableMeasOffsetDesc(const TableRecord& measInfo,
+				     	 const String& prefix,
+				     	 const Table& tab)
+: itsTMDesc(0),
+  itsMeasure(0)
+{
+    Int fnr;
+    fnr = measInfo.fieldNumber(prefix + "Msr");
+    if (fnr >= 0) {
+    	const TableRecord& measRec = measInfo.subRecord(fnr);
+	fnr = measRec.fieldNumber("mRef");
+	uInt measRef;
+	if (fnr >= 0) {
+	    measRef = measRec.asuInt(fnr);
+	} else {
+    	    throw(AipsError("TableMeasOffsetDesc::TableMeasOffsetDesc() "
+    	    	"measure reference type unknown: " +
+    	    	measRec.fieldNumber("mRef")));
+	}
+	fnr = measRec.fieldNumber("mVal");
+    	Vector<Double> measVal;
+	if (fnr >= 0) {
+	    measVal = measRec.asArrayDouble(fnr);
+	} else {
+    	    throw(AipsError("TableMeasOffsetDesc::TableMeasOffsetDesc() "
+    	    	"measure value not found: " +
+    	    	measRec.fieldNumber("mVal")));
+	}
+    	fnr = measRec.fieldNumber("mType");
+    	if (measRec.asString(fnr) == "Epoch") {
+	    MVEpoch meval;
+	    meval.putVector(measVal);
+    	    itsMeasure = new MEpoch(meval, measRef);
+    	} else if (measRec.asString(fnr) == "Position") {
+	    MVPosition meval;
+	    meval.putVector(measVal);
+    	    itsMeasure = new MPosition(meval, measRef);
+    	} else if (measRec.asString(fnr) == "Direction") {
+	    MVDirection meval;
+	    meval.putVector(measVal);
+    	    itsMeasure = new MDirection(meval, measRef);
+    	} else if (measRec.asString(fnr) == "Frequency") {
+	    MVFrequency meval;
+	    meval.putVector(measVal);
+    	    itsMeasure = new MFrequency(meval, measRef);
+    	} else if (measRec.asString(fnr) == "Doppler") {
+	    MVDoppler meval;
+	    meval.putVector(measVal);
+    	    itsMeasure = new MDoppler(meval, measRef);
+    	} else if (measRec.asString(fnr) == "RadialVelocity") {
+	    MVRadialVelocity meval;
+	    meval.putVector(measVal);
+    	    itsMeasure = new MRadialVelocity(meval, measRef);
+    	} else {
+    	    throw(AipsError("TableMeasOffsetDesc::TableMeasOffsetDesc() "
+    	    	"measure type unknown: " + measRec.fieldNumber("mType")));
+    	}
+    }
+    fnr = measInfo.fieldNumber(prefix + "Col");
+    if (fnr >= 0) {
+	itsTMDesc = TableMeasDescBase::reconstruct(tab, 
+	    	    	    	    	    	   measInfo.asString(fnr));
+	itsVarColName = itsTMDesc->columnName();
+    }
+}
+
+TableMeasOffsetDesc& TableMeasOffsetDesc::operator=(
+    const TableMeasOffsetDesc& that)
+{
+    if (this != &that) {
+	delete itsTMDesc;
+	delete itsMeasure;
+	itsTMDesc  = that.itsTMDesc;
+	itsMeasure = that.itsMeasure;
+	if (itsTMDesc != 0) {
+	    itsTMDesc = itsTMDesc->clone();
+	}
+	if (itsMeasure != 0) {
+	    itsMeasure = itsMeasure->clone();
+	}
+    }
+    return *this;
+}
+
+const Measure& TableMeasOffsetDesc::getOffset() const
+{
+    if (isVariable()) {
+    	throw(AipsError("TableMeasOffsetDesc::getOffset() "
+    	    "attempt to reference undefined measure offset."));
+    }
+    return *itsMeasure;
+}
+
+const String& TableMeasOffsetDesc::columnName() const 
+{
+    return itsVarColName;
+}
+
+void TableMeasOffsetDesc::write(TableRecord& measInfo, const String& prefix)
+{
+
+    if (itsMeasure != 0) {
+    	TableRecord measRec;
+    	measRec.define("mType", itsMeasure->tellMe());
+	measRec.define("mRef", itsMeasure->getRefPtr()->getType());
+	measRec.define("mVal", itsMeasure->getData()->getVector());
+        measInfo.defineRecord(prefix + "Msr", measRec);
+    }
+    if (itsTMDesc != 0) {
+	measInfo.define(prefix + "Col", itsTMDesc->columnName());
+	itsTMDesc->write();
+    }
+}
---
+++ code/measures/implement/TableMeasures/TableMeasOffsetDesc.h
@@ -0,0 +1,121 @@
+//# TableMeasOffseDesc.h: Definition of aOffset measure in a Table.
+//# Copyright (C) 1997
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//# $Id$
+
+#if !defined(AIPS_TABLEMEASOFFSETDESC_H)
+#define AIPS_TABLEMEASOFFSETDESC_H
+
+//# Includes
+
+//# Forward Declarations
+class Measure;
+class String;
+class Table;
+class TableMeasDescBase;
+class TableRecord;
+
+// <summary>
+// Definition of a Measure Offset in a Table.
+// </summary>
+
+// <use visibility=export>
+
+// <reviewed reviewer="" date="" tests="tTableMeasure.cc">
+// </reviewed>
+
+// <prerequisite>
+//# Classes you should understand before using this one.
+//   <li> <linkto module=Measures>Measures</linkto>
+//   <li> <linkto module=Tables>Tables</linkto>
+//   <li> TableMeasDesc
+// </prerequisite>
+
+// <synopsis>
+// The measure offset can be static for an entire column or it can vary per
+// row.
+// </synopsis>
+
+// <example>
+// See class <linkto class="TableMeasDesc">TableMeasDesc</linkto>.
+// </example>
+
+// <motivation>
+// Creating the required keyword for the definition of a Measure
+// in a Table is somewhat complicated. This class assists in that
+// process.
+// </motivation>
+
+//# <todo asof="$DATE:$">
+//# A List of bugs, limitations, extensions or planned refinements.
+//# </todo>
+
+class TableMeasOffsetDesc
+{
+public:
+    // Constructor which defines a constant (non-variable) offset.
+    TableMeasOffsetDesc(const Measure& offset);
+
+    // Constructor for defining a variable offset.  
+    TableMeasOffsetDesc(const TableMeasDescBase& offsetColumn);
+
+    // Copy constructor.
+    TableMeasOffsetDesc(const TableMeasOffsetDesc& that);
+
+    ~TableMeasOffsetDesc();
+
+    // Assignment operator.
+    TableMeasOffsetDesc& operator=(const TableMeasOffsetDesc& that);
+    
+    // Reconstructs the TableMeasOffsetDesc from the measInfo TableRecord.
+    static TableMeasOffsetDesc* reconstruct(const TableRecord& measInfo,
+				    	    const String& prefix,
+		    	    	            const Table& tab);
+    
+    // Get the (non-variable) measure offset for this column.  If it doesn't
+    // exist (the offset is variable) and exception is thrown.
+    const Measure& getOffset() const;
+    
+    // Returns True is the offset varies per row.
+    Bool isVariable() const { return (itsTMDesc != 0 ? True : False); }
+    
+    // Gets the name of the column which stores the variable offset. "" is
+    // returned if the offset is not variable.
+    const String& columnName() const;
+    
+    // Write the information into the record.
+    void write(TableRecord& measInfo, const String& prefix);
+
+private:
+    // Constructor which uses the measInfo TableRecord.
+    TableMeasOffsetDesc(const TableRecord& measInfo, const String& prefix,
+		      	const Table&);
+
+    TableMeasDescBase* itsTMDesc;   //# Stores variable offset if applicable
+    Measure* itsMeasure;    	    //# The offset if non-variable.
+    String itsVarColName;   	    //# "" if offset non-variable.
+};
+
+#endif
