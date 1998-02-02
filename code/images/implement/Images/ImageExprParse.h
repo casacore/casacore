70fb2d1d9a6c9b956fee63764710d9c63d803e5e
Ger van Diepen <gervandiepen@gmail.com>
1998-02-02 12:59:41 +0000

    renamed from LatticeParse.h

--- code/images/implement/Images/ImageExprParse.h
+++ code/images/implement/Images/ImageExprParse.h
@@ -1,4 +1,4 @@
-//# LatticeParse.h: Classes to hold results from lattice expression parser
+//# ImageExprParse.h: Classes to hold results from image expression parser
 //# Copyright (C) 1998
 //# Associated Universities, Inc. Washington DC, USA.
 //#
@@ -25,8 +25,8 @@
 //#
 //# $Id$
 
-#if !defined(AIPS_LATTICEPARSE_H)
-#define AIPS_LATTICEPARSE_H
+#if !defined(AIPS_IMAGEEXPRPARSE_H)
+#define AIPS_IMAGEEXPRPARSE_H
 
 
 //# Includes
@@ -39,7 +39,7 @@
 
 
 // <summary>
-// Class to hold values from lattice expression parser
+// Class to hold values from image expression parser
 // </summary>
 
 // <use visibility=export>
@@ -53,21 +53,21 @@
 // </prerequisite>
 
 // <etymology>
-// LatticeParse is the class used to parse a lattice command.
+// ImageExprParse is the class used to parse an image expression command.
 // </etymology>
 
 // <synopsis> 
-// LatticeParse is used by the parser of lattice expression statements.
-// The parser is written in Bison and Flex in files LatticeGram.y and .l.
+// ImageExprParse is used by the parser of image expression statements.
+// The parser is written in Bison and Flex in files ImageExprGram.y and .l.
 // The statements in there use the routines in this file to act
 // upon a reduced rule.
 // <p>
 // The class contains some values used by the parsing process.
 // The only function to be used by a user is the static
-// function LatticeParse::command which parses an expression.
+// function ImageExprParse::command which parses an expression.
 // It returns a <linkto class=LatticeExprNode>LatticeExprNode</linkto>
 // object containing the expression represented as a tree.
-// The object can be used as a <src>Lattice(Expr)</src> in other operations.
+// The object can be used as a <src>Lattice(Expr)<T></src> in other operations.
 // </synopsis> 
 
 // <example>
@@ -84,7 +84,7 @@
 // </example>
 
 // <motivation>
-// It is necessary to be able to give a lattice expression command in ASCII.
+// It is necessary to be able to give an image expression command in ASCII.
 // This can be used in glish to operate on lattices/images.
 // </motivation>
 
@@ -93,23 +93,24 @@
 //# </todo>
 
 
-class LatticeParse
+class ImageExprParse
 {
 public:
 
     // Parse the given command.
     // It will open all lattices needed.
-    // It returns the resulting lattice expression.
+    // It returns the resulting image expression.
     static LatticeExprNode command (const String& str);
 
     // Construct a literal object for the given type.
     // <group>
-    LatticeParse (Int value);
-    LatticeParse (Float value);
-    LatticeParse (Double value);
-    LatticeParse (const Complex& value);
-    LatticeParse (const DComplex& value);
-    LatticeParse (const String& value);
+    ImageExprParse (Bool value);
+    ImageExprParse (Int value);
+    ImageExprParse (Float value);
+    ImageExprParse (Double value);
+    ImageExprParse (const Complex& value);
+    ImageExprParse (const DComplex& value);
+    ImageExprParse (const String& value);
     // </group>
 
     // Make a LatticeExprNode for a function.
@@ -120,11 +121,14 @@ public:
 				  const LatticeExprNode& arg2) const;
     // </group>
 
-    // Make a LatticeExprNode object for the lattice.
+    // Make a LatticeExprNode object for the lattice name.
     LatticeExprNode makeLatticeNode() const;
 
-    // Make a LatticeExprNode object for the literal.
+    // Make a LatticeExprNode object for the literal value.
     LatticeExprNode makeLiteralNode() const;
+
+    // Make a LatticeExprNode object for the name of constant or lattice.
+    LatticeExprNode makeLitLattNode() const;
 
     // Set the static node object (used by the .y file).
     static void setNode (const LatticeExprNode& node)
@@ -135,6 +139,7 @@ private:
     static LatticeExprNode theirNode;
 
     DataType itsType;
+    Bool     itsBval;              //# boolean literal
     Int      itsIval;              //# integer literal
     Float    itsFval;              //# Float literal
     Double   itsDval;              //# Double literal
