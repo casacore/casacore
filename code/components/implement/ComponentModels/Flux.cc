0f170f8108d4d93bbedad2fe11077b11c9addee8
Ralph Marson <rmarson@nrao.edu>
1998-02-03 19:09:01 +0000

    Hacked for g++. This means that many functions where moved to the .h file.

--- code/components/implement/ComponentModels/Flux.cc
+++ code/components/implement/ComponentModels/Flux.cc
@@ -26,11 +26,7 @@
 //# $Id$
 
 #include <trial/ComponentModels/ComponentFlux.h>
-#include <aips/Exceptions/Error.h>
-#include <aips/Utilities/Assert.h>
 #include <aips/Utilities/String.h>
-#include <aips/Measures/Quantum.h>
-#include <aips/Mathematics/Complex.h>
 
 template<class T> ComponentFlux<T>::
 ComponentFlux()
@@ -63,22 +59,6 @@ ComponentFlux(T i, T q, T u, T v)
 }
 
 template<class T> ComponentFlux<T>::
-ComponentFlux(NumericTraits<T>::ConjugateType xx,
- 	      NumericTraits<T>::ConjugateType xy,
- 	      NumericTraits<T>::ConjugateType yx,
- 	      NumericTraits<T>::ConjugateType yy, 
-	      FluxEnums::PolType rep)
-  :itsFlux(4),
-   itsRep(rep),
-   itsUnit("Jy") 
-{
-  itsFlux(0) = xx;
-  itsFlux(1) = xy;
-  itsFlux(2) = yx;
-  itsFlux(3) = yy;
-}
-
-template<class T> ComponentFlux<T>::
 ComponentFlux(const Vector<T> & flux)
   :itsFlux(4, NumericTraits<T>::ConjugateType(0,0)),
    itsRep(FluxEnums::STOKES),
@@ -102,27 +82,6 @@ ComponentFlux(const Quantum<Vector<T> > & flux)
   for (uInt s = 0; s < 4; s++) {
     itsFlux(s).re = fluxVal(s);
   }
-}
-
-template<class T> ComponentFlux<T>::
-ComponentFlux(const Vector<NumericTraits<T>::ConjugateType> & flux,
-	      const FluxEnums::PolType & rep) 
-  :itsFlux(flux.copy()),
-   itsRep(rep),
-   itsUnit("Jy")
-{
-  AlwaysAssert(itsFlux.nelements() == 4, AipsError);
-}
-
-template<class T> ComponentFlux<T>::
-ComponentFlux(const Quantum<Vector<NumericTraits<T>::ConjugateType> > & flux,
-	      const FluxEnums::PolType & rep) 
-  :itsFlux(flux.getValue().copy()),
-   itsRep(rep),
-   itsUnit(flux.getFullUnit())
-{
-  AlwaysAssert(itsFlux.nelements() == 4, AipsError);
-  AlwaysAssert(itsUnit == Unit("Jy"), AipsError);
 }
 
 template<class T> ComponentFlux<T>::
@@ -196,15 +155,6 @@ flux(Vector<T> & value) {
 }
 
 template<class T> void ComponentFlux<T>::
-flux(Vector<NumericTraits<T>::ConjugateType> & value, 
- 	    const FluxEnums::PolType & rep) {
-  uInt len = value.nelements();
-  AlwaysAssert (len == 4 || len == 0, AipsError);
-  convertRep(rep);
-  value = itsFlux;
-}
-
-template<class T> void ComponentFlux<T>::
 flux(Quantum<Vector<T> > & value) {
   uInt len = value.getValue().nelements();
   AlwaysAssert(len == 4 || len == 0, AipsError);
@@ -214,16 +164,6 @@ flux(Quantum<Vector<T> > & value) {
   for (uInt s = 0 ; s < 4; s++) {
     newValue(s) = itsFlux(s).re;
   }
-}
-
-template<class T> void ComponentFlux<T>::
-flux(Quantum<Vector<NumericTraits<T>::ConjugateType> > & value,
-     const FluxEnums::PolType & rep) {
-  uInt len = value.getValue().nelements();
-  AlwaysAssert(len == 4 || len == 0, AipsError);
-  convertUnit(value.getFullUnit());
-  convertRep(rep);
-  value.setValue(itsFlux);
 }
 
 template<class T> void ComponentFlux<T>::
@@ -246,14 +186,6 @@ setFlux(const Vector<T> & value) {
 }
 
 template<class T> void ComponentFlux<T>::
-setFlux(const Vector<NumericTraits<T>::ConjugateType> & value, 
-	const FluxEnums::PolType & rep) {
-  AlwaysAssert (value.nelements() == 4, AipsError);
-  itsFlux = value;
-  itsRep = rep;
-}
-
-template<class T> void ComponentFlux<T>::
 setFlux(const Quantum<Vector<T> > & value) {
   AlwaysAssert (value.getValue().nelements() == 4, AipsError);
   const Vector<T> & val = value.getValue();
@@ -263,129 +195,6 @@ setFlux(const Quantum<Vector<T> > & value) {
   }
   itsUnit = value.getFullUnit();
   itsRep = FluxEnums::STOKES;
-}
-
-template<class T> void ComponentFlux<T>::
-setFlux(const Quantum<Vector<NumericTraits<T>::ConjugateType> > & value,
-	const FluxEnums::PolType & rep) {
-  AlwaysAssert (value.getValue().nelements() == 4, AipsError);
-  itsFlux = value.getValue();
-  itsUnit = value.getFullUnit();
-  itsRep = rep;
-}
-
-// template<class T> void ComponentFlux<T>::
-// stokesToCircular(Vector<NumericTraits<T>::ConjugateType> & out,
-// 		 const Vector<T> & in) {
-//   const T i = in(0);
-//   const T q = in(1);
-//   const T u = in(2);
-//   const T v = in(3);
-//   out(0).re = i + v; out(0).im = T(0);
-//   out(1).re = q;     out(1).im = u;
-//   out(2).re = q;     out(2).im = -u;
-//   out(3).re = i - v; out(3).im = T(0);
-// }
-
-template<class T> void ComponentFlux<T>::
-stokesToCircular(Vector<NumericTraits<T>::ConjugateType> & out, 
-		 const Vector<NumericTraits<T>::ConjugateType> & in) {
-  const NumericTraits<T>::ConjugateType i = in(0);
-  const NumericTraits<T>::ConjugateType q = in(1);
-  const NumericTraits<T>::ConjugateType & u = in(2);
-  const NumericTraits<T>::ConjugateType ju(-u.im, u.re);
-  const NumericTraits<T>::ConjugateType v = in(3);
-  out(0) = i + v;
-  out(1) = q + ju;
-  out(2) = q - ju;
-  out(3) = i - v;
-}
-
-// template<class T> void ComponentFlux<T>::
-// circularToStokes(Vector<T> & out,
-// 		 const Vector<NumericTraits<T>::ConjugateType> & in) {
-//   const T rr = in(0).re;
-//   const NumericTraits<T>::ConjugateType rl = in(1);
-//   const NumericTraits<T>::ConjugateType lr = in(2);
-//   const T ll = in(3).re;
-//   out(0) = (rr + ll)/T(2);
-//   out(1) = (rl.re + lr.re)/T(2);
-//   out(2) = (rl.im - lr.im)/T(2);
-//   out(3) = (rr - ll)/T(2);
-// }
-
-template<class T> void ComponentFlux<T>::
-circularToStokes(Vector<NumericTraits<T>::ConjugateType> & out,
-		 const Vector<NumericTraits<T>::ConjugateType> & in) {
-  const NumericTraits<T>::ConjugateType rr = in(0);
-  const NumericTraits<T>::ConjugateType rl = in(1);
-  const NumericTraits<T>::ConjugateType lr = in(2);
-  const NumericTraits<T>::ConjugateType ll = in(3);
-  out(0) = (rr + ll)/T(2);
-  out(1) = (rl + lr)/T(2);
-  NumericTraits<T>::ConjugateType & u = out(2);
-  u.re = (rl.im-lr.im)/T(2);
-  u.im = (lr.re-rl.re)/T(2);
-  out(3) = (rr - ll)/T(2);
-}
-
-template<class T> void ComponentFlux<T>::
-stokesToLinear(Vector<NumericTraits<T>::ConjugateType> & out, 
-	       const Vector<NumericTraits<T>::ConjugateType> & in) {
-  const NumericTraits<T>::ConjugateType i = in(0);
-  const NumericTraits<T>::ConjugateType q = in(1);
-  const NumericTraits<T>::ConjugateType u = in(2);
-  const NumericTraits<T>::ConjugateType & v = in(3);
-  const NumericTraits<T>::ConjugateType jv(-v.im, v.re);
-  out(0) = i + q;
-  out(1) = u + jv;
-  out(2) = u - jv;
-  out(3) = i - q;
-}
-
-template<class T> void ComponentFlux<T>::
-linearToStokes(Vector<NumericTraits<T>::ConjugateType> & out, 
-	       const Vector<NumericTraits<T>::ConjugateType> & in) {
-  const NumericTraits<T>::ConjugateType xx = in(0);
-  const NumericTraits<T>::ConjugateType xy = in(1);
-  const NumericTraits<T>::ConjugateType yx = in(2);
-  const NumericTraits<T>::ConjugateType yy = in(3);
-  out(0) = (xx + yy)/T(2);
-  out(1) = (xx - yy)/T(2);
-  out(2) = (xy + yx)/T(2);
-  NumericTraits<T>::ConjugateType & v = out(3);
-  v.re = (-xy.re-yx.im)/T(2);
-  v.im = (yx.re-xy.im)/T(2);
-}
-
-template<class T> void ComponentFlux<T>::
-linearToCircular(Vector<NumericTraits<T>::ConjugateType> & out, 
-		 const Vector<NumericTraits<T>::ConjugateType> & in) {
-  const NumericTraits<T>::ConjugateType xx = in(0);
-  const NumericTraits<T>::ConjugateType & xy = in(1);
-  const NumericTraits<T>::ConjugateType jxy(-xy.im, xy.re);
-  const NumericTraits<T>::ConjugateType & yx = in(2);
-  const NumericTraits<T>::ConjugateType jyx(-yx.im, yx.re);
-  const NumericTraits<T>::ConjugateType yy = in(3);
-  out(0) = (xx - jxy + jyx + yy)/T(2);
-  out(1) = (xx + jxy + jyx - yy)/T(2);
-  out(2) = (xx - jxy - jyx - yy)/T(2);
-  out(3) = (xx + jxy - jyx + yy)/T(2);
-}
-
-template<class T> void ComponentFlux<T>::
-circularToLinear(Vector<NumericTraits<T>::ConjugateType> & out, 
-		 const Vector<NumericTraits<T>::ConjugateType> & in) {
-  const NumericTraits<T>::ConjugateType rr = in(0);
-  const NumericTraits<T>::ConjugateType rl = in(1);
-  const NumericTraits<T>::ConjugateType lr = in(2);
-  const NumericTraits<T>::ConjugateType ll = in(3);
-  out(0) = (rr + rl + lr + ll)/T(2);
-  out(1).re = (-rr.im + rl.im - lr.im + ll.im)/T(2);
-  out(1).im = ( rr.re - rl.re + lr.re - ll.re)/T(2);
-  out(2).re = (-rr.im - rl.im + lr.im + ll.im)/T(2);
-  out(2).im = (-rr.re - rl.re + lr.re + ll.re)/T(2);
-  out(3) = (rr - rl - lr + ll)/T(2);
 }
 
 template<class T> void ComponentFlux<T>::
