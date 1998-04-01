26c62dad880326e31cde4f6fbdeed79e174c435d
Ger van Diepen <gervandiepen@gmail.com>
1998-04-01 10:34:45 +0000

    *** empty log message ***

---
+++ code/lattices/implement/Lattices/LatticeRegion.h
@@ -0,0 +1,205 @@
+//# LatticeRegion.h: An optionally strided region in a lattice
+//# Copyright (C) 1998
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//#
+//# $Id$
+
+#if !defined(AIPS_LATTICEREGION_H)
+#define AIPS_LATTICEREGION_H
+
+
+//# Includes
+#include <trial/Lattices/LCRegion.h>
+#include <aips/Lattices/Slicer.h>
+
+//# Forward Declarations
+
+
+// <summary>
+// An optionally strided  region in a Lattice
+// </summary>
+
+// <use visibility=local>
+
+// <reviewed reviewer="" date="yyyy/mm/dd" tests="" demos="">
+// </reviewed>
+
+// <prerequisite>
+//   <li> <linkto class="LCRegion">LCRegion</linkto>
+// </prerequisite>
+
+// <synopsis>
+// A LatticeRegion is a lattice referencing a subset of another lattice
+// by means of a <linkto class="Slicer">Slicer</linkto> object.
+// <br>It is useful when only a subset of a lattice needs to be accessed.
+// <p>
+// When the LatticeRegion is created from a const <src>Lattice</src> object,
+// it is not writable, thus it can only be used as an rvalue.
+// </synopsis>
+
+// <example>
+// <srcblock>
+// </srcblock>
+// </example>
+
+// <templating arg=T>
+// Any type that can be used by the Tables System can also be used by
+// this class.
+// </templating>
+
+//# <todo asof="yyyy/mm/dd">
+//# </todo>
+
+class LatticeRegion: public Lattice<Bool>
+{
+public:
+  // The default constructor creates a LatticeRegion that is useless for just
+  // about everything, except that it can be assigned to with the assignment
+  // operator.
+  LatticeRegion();
+
+  // Create from the given region.
+  // The pointer to the parent can be 0.
+  LatticeRegion (const LCRegion& region, const LatticeRegion* parent = 0);
+
+  // Construct from the given slicer. The lattice shape has to be
+  // the lattice shape of the lattice where the region is taken from.
+  LatticeRegion (const Slicer& slicer, const IPosition& latticeShape);
+
+  // Construct from the given slicer. The pointer to the parent
+  // should not be 0.
+  LatticeRegion (const Slicer& slicer, const LatticeRegion* parent);
+
+  // Copy constructor (reference semantics).
+  LatticeRegion (const LatticeRegion& other);
+    
+  virtual ~LatticeRegion();
+
+  // Assignment (reference semantics).
+  LatticeRegion& operator= (const LatticeRegion& other);
+
+  // Set the region and/or pointer to the parent.
+  void setParent (const LatticeRegion* parent);
+
+  // Make a copy of the object (reference semantics).
+  virtual Lattice<Bool>* clone() const;
+
+  // Is the LatticeRegion writable?
+  virtual Bool isWritable() const;
+
+  // Has the object really a mask?
+  Bool hasMask() const;
+
+  // Get the LCRegion object describing the region.
+  // Note that it does not contain strides, even if this LatticeRegion
+  // object was constructed from a Slicer with strides. In that case
+  // the region only defines the resulting shape.
+  const LCRegion& region() const;
+
+  // Get the Slicer object describing the region.
+  // Note that it may contain strides.
+  const Slicer& slicer() const;
+
+  // Returns the shape of the LatticeRegion including all degenerate axes
+  // (i.e. axes with a length of one).
+  virtual IPosition shape() const;
+  
+  // Returns the number of axes in this LatticeRegion. This includes all
+  // degenerate axes.
+  virtual uInt ndim() const;
+  
+  // Returns the total number of elements in this LatticeRegion.
+  virtual uInt nelements() const;
+  
+  // Check class internals - used for debugging. Should always return True
+  virtual Bool ok() const;
+
+  // This function is used by the LatticeIterator class to generate an
+  // iterator of the correct type for this Lattice. Not recommended
+  // for general use. 
+  virtual LatticeIterInterface<Bool>*  makeIter
+                           (const LatticeNavigator& navigator) const;
+
+    // The following "put" functions are described in detail in class
+    // <linkto class=Lattice>Lattice</linkto>.
+    // They'll throw an exception is no mask is available or if
+    // the mask is not writable. In practice this means that only
+    // regions LCMask and LCPagedMask can in principle be written.
+    // <group>
+    virtual void set (const Bool& value);
+    virtual void apply (Bool (*function)(Bool));
+    virtual void apply (Bool (*function)(const Bool&));
+    virtual void apply (const Functional<Bool,Bool>& function);
+    virtual void putAt (const Bool& value, const IPosition& where);
+    virtual void copyData (const Lattice<Bool>& from);
+    // </group>
+
+    // Convert positions to positions in the parent object.
+    // <group>
+    Slicer convert (const Slicer& slicer) const;
+    IPosition convert (const IPosition& position) const;
+    // </group>
+
+protected:
+    // Do the actual getting of the mask.
+    virtual Bool doGetSlice (Array<Bool>& buffer, const Slicer& section);
+
+    // Do the actual putting of the mask. Only possible if region is writable.
+    virtual void doPutSlice (const Array<Bool>& sourceBuffer,
+			     const IPosition& where,
+			     const IPosition& stride);
+
+    // Get the best cursor shape.
+    virtual IPosition doNiceCursorShape (uInt maxPixels) const;
+
+
+private:
+  LCRegion*  itsRegion;
+  Slicer     itsSlicer;
+  const LatticeRegion* itsParent;
+  Bool       itsHasRegionMask;
+  Bool       itsHasParentMask;
+};
+
+
+inline void LatticeRegion::setParent (const LatticeRegion* parent)
+{
+    itsParent = parent;
+}
+inline Bool LatticeRegion::hasMask() const
+{
+    return ToBool (itsHasParentMask || itsHasRegionMask);
+}
+inline const LCRegion& LatticeRegion::region() const
+{
+    return *itsRegion;
+}
+inline const Slicer& LatticeRegion::slicer() const
+{
+    return itsSlicer;
+}
+
+
+#endif
---
+++ code/lattices/implement/Lattices/MaskedLattice.h
@@ -0,0 +1,287 @@
+//# MaskedLattice.h: Abstract base class for array-like classes with masks
+//# Copyright (C) 1998
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//# $Id$
+
+#if !defined(AIPS_MASKEDLATTICE_H)
+#define AIPS_MASKEDLATTICE_H
+
+
+//# Includes
+#include <trial/Lattices/Lattice.h>
+
+//# Forward Declarations
+class LatticeRegion;
+
+
+// <summary>
+// A templated, abstract base class for array-like objects with masks.
+// </summary>
+
+// <use visibility=export>
+
+// <reviewed reviewer="" date="yyyy/mm/dd" tests="" demos="dLattice.cc">
+// </reviewed>
+
+// <prerequisite>
+//   <li> <linkto class="IPosition"> IPosition </linkto>
+//   <li> Abstract Base class Inheritance - try "Advanced C++" by James
+//        O. Coplien, Ch. 5.
+// </prerequisite>
+
+// <etymology>
+// Lattice: "A regular, periodic configuration of points, particles, 
+// or objects, throughout an area of a space..." (American Heritage Directory)
+// This definition matches our own: an n-dimensional arrangement of items,
+// on regular orthogonal axes.
+// </etymology>
+
+// <synopsis>
+// This pure abstract base class defines the operations which may be performed
+// on any concrete class derived from it.  It has only a few non-pure virtual 
+// member functions.
+// The fundamental contribution of this class, therefore, is that it 
+// defines the operations derived classes must provide:
+// <ul>
+//    <li> how to extract a "slice" (or sub-array, or subsection) from
+//         a Lattice.
+//    <li> how to copy a slice in.
+//    <li> how to get and put a single element 
+//    <li> how to apply a function to all elements
+//    <li> various shape related functions.
+// </ul>
+// <note role=tip> Lattices are always zero origined. </note>
+// </synopsis> 
+
+// <example>
+// Because Lattice is an abstract base class, an actual instance of this
+// class cannot be constructed. However the interface it defines can be used
+// inside a function. This is always recommended as it allows Functions
+// which have Lattices as arguments to work for any derived class.
+//
+// I will give a few examples here and then refer the reader to the 
+// <linkto class="ArrayLattice">ArrayLattice</linkto> class (a memory resident
+// Lattice) and the <linkto class="PagedArray">PagedArray</linkto> class (a
+// disk based Lattice) which contain further examples with concrete
+// classes (rather than an abstract one). All the examples shown below are used
+// in the <src>dLattice.cc</src> demo program.
+//
+// <h4>Example 1:</h4>
+// This example calculates the mean of the Lattice. Because Lattices can be too
+// large to fit into physical memory it is not good enough to simply use
+// <src>getSlice</src> to read all the elements into an Array. Instead the
+// Lattice is accessed in chunks which can fit into memory (the size is
+// determined by the <src>maxPixels</src> and <src>niceCursorShape</src>
+// functions). The <src>LatticeIterator::cursor()</src> function then returns
+// each of these chunks as an Array and the standard Array based functions are
+// used to calculate the mean on each of these chunks. Functions like this one
+// are the recommended way to access Lattices as the 
+// <linkto class="LatticeIterator">LatticeIterator</linkto> will correctly
+// setup any required caches.
+//
+// <srcblock>
+// Complex latMean(const Lattice<Complex>& lat) {
+//   const uInt cursorSize = lat.maxPixels();
+//   const IPosition cursorShape = lat.niceCursorShape(cursorSize);
+//   const IPosition latticeShape = lat.shape();
+//   Complex currentSum = 0.0f;
+//   uInt nPixels = 0u;
+//   RO_LatticeIterator<Complex> iter(lat, 
+// 				   LatticeStepper(latticeShape, cursorShape));
+//   for (iter.reset(); !iter.atEnd(); iter++){
+//     currentSum += sum(iter.cursor());
+//     nPixels += iter.cursor().nelements();
+//   }
+//   return currentSum/nPixels;
+// }
+// </srcblock>
+//
+// <h4>Example 2:</h4>
+// Sometimes it will be neccesary to access slices of a Lattice in a nearly
+// random way. Often this can be done using the subSection commands in the
+// <linkto class="LatticeStepper">LatticeStepper</linkto> class. But it is also
+// possible to use the getSlice and putSlice functions. The following example
+// does a two-dimensional Real to Complex Fourier transform. This example is
+// restricted to four-dimensional Arrays (unlike the previous example) and does
+// not set up any caches (caching is currently only used with PagedArrays).  So
+// only use getSlice and putSlice when things cannot be done using
+// LatticeIterators.
+//
+// <srcblock>
+// void FFT2DReal2Complex(Lattice<Complex>& result, 
+// 		       const Lattice<Float>& input){
+//   AlwaysAssert(input.ndim() == 4, AipsError);
+//   const IPosition shape = input.shape();
+//   const uInt nx = shape(0);
+//   AlwaysAssert (nx > 1, AipsError);
+//   const uInt ny = shape(1);
+//   AlwaysAssert (ny > 1, AipsError);
+//   const uInt npol = shape(2);
+//   const uInt nchan = shape(3); 
+//   const IPosition resultShape = result.shape();
+//   AlwaysAssert(resultShape.nelements() == 4, AipsError);
+//   AlwaysAssert(resultShape(3) == nchan, AipsError);
+//   AlwaysAssert(resultShape(2) == npol, AipsError);
+//   AlwaysAssert(resultShape(1) == ny, AipsError);
+//   AlwaysAssert(resultShape(0) == nx/2 + 1, AipsError);
+//
+//   const IPosition inputSliceShape(4,nx,ny,1,1);
+//   const IPosition resultSliceShape(4,nx/2+1,ny,1,1);
+//   COWPtr<Array<Float> > 
+//     inputArrPtr(new Array<Float>(inputSliceShape.nonDegenerate()));
+//   Array<Complex> resultArray(resultSliceShape.nonDegenerate());
+//   FFTServer<Float, Complex> FFT2D(inputSliceShape.nonDegenerate());
+//  
+//   IPosition start(4,0);
+//   Bool isARef;
+//   for (uInt c = 0; c < nchan; c++){
+//     for (uInt p = 0; p < npol; p++){
+//       isARef = input.getSlice(inputArrPtr,
+//                               Slicer(start,inputSliceShape), True);
+//       FFT2D.fft(resultArray, *inputArrPtr);
+//       result.putSlice(resultArray, start);
+//       start(2) += 1;
+//     }
+//     start(2) = 0;
+//     start(3) += 1;
+//   }
+// }
+// </srcblock>
+//
+// <h4>Example 3:</h4>
+// Occasionally you may want to access a few elements of a Lattice without
+// all the difficulty involved in setting up Iterators or calling getSlice
+// and putSlice. This is demonstrated in the example below and uses the
+// parenthesis operator, along with the LatticeValueRef companion
+// class. Using these functions to access many elements of a Lattice is not
+// recommended as this is the slowest access method.
+//
+// In this example an ideal point spread function will be inserted into an
+// empty Lattice. As with the previous examples all the action occurs
+// inside a function because Lattice is an interface (abstract) class.
+//
+// <srcblock>
+// void makePsf(Lattice<Float>& psf) {
+//   const IPosition centrePos = psf.shape()/2;
+//   psf.set(0.0f);       // this sets all the elements to zero
+//                        // As it uses a LatticeIterator it is efficient
+//   psf(centrePos) = 1;  // This sets just the centre element to one
+//   AlwaysAssert(near(psf(centrePos), 1.0f, 1E-6), AipsError);
+//   AlwaysAssert(near(psf(centrePos*0), 0.0f, 1E-6), AipsError);
+// }
+// </srcblock>
+// </example>
+
+// <motivation>
+// Creating an abstract base class which provides a common interface between
+// memory and disk based arrays has a number of advantages.
+// <ul>
+// <li> It allows functions common to all arrays to be written independent
+// of the way the data is stored. This is illustrated in the three examples
+// above. 
+// <li> It reduces the learning curve for new users who only have to become
+// familiar with one interface (ie. Lattice) rather than distinct interfaces
+// for different array types. 
+// </ul>
+// </motivation>
+
+// <todo asof="1996/07/01">
+//   <li> Remove the latticeCast member when the GNU compiler is 
+//        sufficiently robust.
+// </todo>
+
+template <class T> class MaskedLattice : virtual public Lattice<T>
+{
+public: 
+  // a virtual destructor is needed so that it will use the actual destructor
+  // in the derived class
+  virtual ~MaskedLattice();
+
+  // Make a copy of the object (reference semantics).
+  virtual MaskedLattice<T>* cloneML() const = 0;
+
+  // Has the object really a mask?
+  virtual Bool isMasked() const = 0;
+
+  // Get the region used.
+  virtual const LatticeRegion& region() const = 0;
+
+  // <group>   
+  Bool getMask (COWPtr<Array<Bool> >& buffer,
+		Bool removeDegenerateAxes=False) const;
+  Bool getMaskSlice (COWPtr<Array<Bool> >& buffer, const Slicer& section,
+		     Bool removeDegenerateAxes=False) const;
+  Bool getMaskSlice (COWPtr<Array<Bool> >& buffer, const IPosition& start, 
+		     const IPosition& shape,
+		     Bool removeDegenerateAxes=False) const;
+  Bool getMaskSlice (COWPtr<Array<Bool> >& buffer, const IPosition& start, 
+		     const IPosition& shape, const IPosition& stride,
+		     Bool removeDegenerateAxes=False) const;
+  Bool getMask (Array<Bool>& buffer,
+		Bool removeDegenerateAxes=False);
+  Bool getMaskSlice (Array<Bool>& buffer, const Slicer& section,
+		     Bool removeDegenerateAxes=False);
+  Bool getMaskSlice (Array<Bool>& buffer, const IPosition& start,
+		     const IPosition& shape,
+		     Bool removeDegenerateAxes=False);
+  Bool getMaskSlice (Array<Bool>& buffer, const IPosition& start,
+		     const IPosition& shape, const IPosition& stride,
+		     Bool removeDegenerateAxes=False);
+  Array<Bool> getMask (Bool removeDegenerateAxes=False) const;
+  Array<Bool> getMaskSlice (const Slicer& section,
+			    Bool removeDegenerateAxes=False) const;
+  Array<Bool> getMaskSlice (const IPosition& start,
+			    const IPosition& shape,
+			    Bool removeDegenerateAxes=False) const;
+  Array<Bool> getMaskSlice (const IPosition& start,
+			    const IPosition& shape, const IPosition& stride,
+			    Bool removeDegenerateAxes=False) const;
+  // </group>
+  
+  // These functions were put in for the Gnu compiler which presently
+  // (version 2.7.2.1) is unable to automatically cast a derived class to a
+  // base class in a templated global function.
+  // <group>
+  MaskedLattice<T>& maskedLatticeCast() {return *this;}
+  const MaskedLattice<T>& maskedLatticeCast() const {return *this;}
+  // </group>
+
+  // Short-hand for <src>maskedLatticeCast()</src>
+  // <group>
+  MaskedLattice<T>& mlc() {return *this;}
+  const MaskedLattice<T>& mlc() const {return *this;}
+  // </group>
+
+protected:
+  // Assignment can only be used by derived classes.
+  MaskedLattice<T>& operator= (const MaskedLattice<T>&)
+    { return *this; }
+
+  // Get a section of the mask.
+  virtual Bool doGetMaskSlice (Array<Bool>& buffer, const Slicer& section) = 0;
+};
+
+
+#endif
