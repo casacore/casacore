2111675a72ed2cf3c4b1a98970ea5deedb7baa67
Jeff Uphoff <juphoff@nrao.edu>
1998-04-07 16:03:14 +0000

    Diddled comment.

--- code/install/codemgmt/sai_master.c
+++ code/install/codemgmt/sai_master.c
@@ -36,7 +36,7 @@
 * for further details.
 *
 * Like ai_master, this program is not indended for direct execution by
-* users.
+* users.  It's only experimental anyway.
 *
 * Options:
 *    See the documentation for ai_master.
