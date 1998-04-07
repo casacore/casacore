a6d4fbc43a3a6bb2d4631f687e82c306188392e9
Jeff Uphoff <juphoff@nrao.edu>
1998-04-07 18:17:02 +0000

    Oh, bother.

--- code/install/codemgmt/sai_master.c
+++ code/install/codemgmt/sai_master.c
@@ -36,7 +36,7 @@
 * for further details.
 *
 * Like ai_master, this program is not indended for direct execution by
-* users.  It's only experimental anyway.  La la la.
+* users.  It's only experimental anyway.  La la la.  Do re mi.
 *
 * Options:
 *    See the documentation for ai_master.
