7c0947465ced793fbb11fd3a797796c4db248320
Neil Killeen <nkilleen@unimelb.edu.au>
1998-04-15 12:52:09 +0000

    *** empty log message ***

--- code/images/implement/Images/test/dImageMoments.cc
+++ code/images/implement/Images/test/dImageMoments.cc
@@ -1,5 +1,5 @@
 //# imoment.cc: generate image moments
-//# Copyright (C) 1996,1997
+//# Copyright (C) 1996,1997,1998
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
