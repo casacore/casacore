c689a30d9fc4cae5cd9662010a7be5fd0203f012
Wim Brouw <brouw@astron.nl>
1998-04-16 03:16:02 +0000

    Copyright only

--- code/measures/implement/Measures/MRBase.h
+++ code/measures/implement/Measures/MRBase.h
@@ -1,5 +1,5 @@
 //# MRBase.h: Base for Reference frame for physical measures
-//# Copyright (C) 1995,1996,1997
+//# Copyright (C) 1995,1996,1997,1998
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
