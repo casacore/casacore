8764a1ad9621e000d0ac05a60c5bb2603ef05968
Ralph Marson <rmarson@nrao.edu>
1998-05-14 14:38:53 +0000

    Implemented functions to parse the reference frequency

--- code/components/implement/ComponentModels/SpectralModel.cc
+++ code/components/implement/ComponentModels/SpectralModel.cc
@@ -26,13 +26,16 @@
 //# $Id$
 
 #include <trial/ComponentModels/SpectralModel.h>
+#include <trial/Measures/MeasureHolder.h>
+#include <aips/Containers/Record.h>
 #include <aips/Containers/RecordFieldId.h>
 #include <aips/Containers/RecordInterface.h>
 #include <aips/Exceptions/Error.h>
-#include <aips/Exceptions/Excp.h>
 #include <aips/Lattices/IPosition.h>
+#include <aips/Measures/MFrequency.h>
 #include <aips/Utilities/Assert.h>
 #include <aips/Utilities/String.h>
+#include <aips/Utilities/DataType.h>
 
 SpectralModel::~SpectralModel() {
 }
@@ -46,19 +49,56 @@ getType(String & errorMessage, const RecordInterface & record) {
     return ComponentType::UNKNOWN_SPECTRAL_SHAPE;
   }
   const RecordFieldId type(typeString);
-  if (record.shape(type) != IPosition(1,1)) {
-    errorMessage += String("\nThe 'type' field must have only 1 element");
+  if (record.dataType(type) != TpString) {
+    errorMessage += String("\nThe 'type' field in the spectrum record,")
+      + String("must be a String");
     return ComponentType::UNKNOWN_SPECTRAL_SHAPE;
   }      
-  String typeVal;
-  try {
-    typeVal = record.asString(type);
-  }
-  catch (AipsError x) {
-    errorMessage += String("\nThe 'type' field must be a String");
+  if (record.shape(type) != IPosition(1,1)) {
+    errorMessage += String("The 'type' field, in the spectrum record,") + 
+      String(" must have only 1 element\n");
     return ComponentType::UNKNOWN_SPECTRAL_SHAPE;
-  } end_try;
+  }      
+  const String & typeVal = record.asString(type);
   return ComponentType::spectralShape(typeVal);
+}
+
+Bool SpectralModel::readFreq(String & errorMessage,
+			     const RecordInterface & record) {
+  const String freqString("frequency");
+  if (!record.isDefined(freqString)) {
+    errorMessage += "The 'frequency' field does not exist\n";
+    return False;
+  }
+  const RecordFieldId frequency(freqString);
+  if (record.dataType(frequency) != TpRecord) {
+    errorMessage += "The 'frequency' field must be a record\n";
+    return False;
+  }
+  const Record & freqRecord = record.asRecord(frequency);
+  MeasureHolder mh;
+  if (!mh.fromRecord(errorMessage, freqRecord)) {
+    errorMessage += "Could not parse the reference frequency\n";
+    return False;
+  }
+  if (!mh.isMFrequency()) {
+    errorMessage += "The reference frequency is not a frequency measure\n";
+    return False;
+  }
+  setRefFrequency(mh.asMFrequency());
+  return True;
+}
+
+Bool SpectralModel::addFreq(String & errorMessage, 
+			    RecordInterface & record) const {
+  Record freqRecord;
+  const MeasureHolder mh(refFrequency());
+  if (!mh.toRecord(errorMessage, freqRecord)) {
+    errorMessage += "Could not convert the reference frequency to a record\n";
+    return False;
+  }
+  record.defineRecord(RecordFieldId("frequency"), freqRecord);
+  return True;
 }
 // Local Variables: 
 // compile-command: "gmake OPTLIB=1 SpectralModel"
