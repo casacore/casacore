0de20ce1a561866dd236e51ec87a66d97d0b1908
Wim Brouw <brouw@astron.nl>
1998-05-15 00:55:00 +0000

    Added tests for all methods

--- code/measures/implement/Measures/test/tMBaseline.cc
+++ code/measures/implement/Measures/test/tMBaseline.cc
@@ -29,6 +29,8 @@
 #include <aips/aips.h>
 #include <aips/Exceptions/Error.h>
 #include <aips/Measures.h>
+#include <aips/Measures/RotMatrix.h>
+#include <aips/Measures/Euler.h>
 #include <aips/Measures/Aberration.h>
 #include <aips/Measures/MEpoch.h>
 #include <aips/Measures/MDirection.h>
@@ -51,7 +53,6 @@ main()
 		      MPosition::ITRF);
 	MeasFrame mf(tbm, pos);
 	MVBaseline mvb0(100 ,10, 0);
-	Double cadj = sqrt(10000+100);
 	cout << "Baseline: " << mvb0 << endl;
 
 	MBaseline::Ref mbref0(MBaseline::ITRF, mf);
@@ -75,6 +76,9 @@ main()
 	cout << "Back      " << bconv() << endl <<
 	  " as " << bconvb(bconv()) << endl;
 
+	cout << "--------------------------------------" << endl;
+	cout << "Testing all conversions forward/backward" << endl;
+
 	Vector<Double> tvec(3);
 	tvec = 0.0;
 	for (uInt i=MBaseline::ITRF; i<MBaseline::N_Types; i++) {
@@ -94,8 +98,12 @@ main()
 	    };
 	  };
 	};
-	cout << "========================================================" <<
-	  endl;
+
+	cout << "All forward/backward conversions: ok" << endl;
+	cout << "------------------------------------" << endl;
+
+	cout << "Testing all MDirection conversions forward/backward" << endl;
+
  	{
 	  MVDirection mvd0(0.5, 0.5, 0.5);
 	  Double tp;
@@ -122,7 +130,97 @@ main()
 	    };
 	  };
 	}
-       
+
+	cout << "All forward/backward Direction conversions: ok" << endl;
+	cout << "----------------------------------------------" << endl;
+	cout << "Exercise all MVBaseline function" << endl;
+	{
+	  MVBaseline x(mvb0);
+	  if (x != mvb0) cout << "Copy constructor error" << endl;
+	  x = mvb0;
+	  if (x != mvb0) cout << "Assignment error" << endl;
+	  Vector<Quantum<Double> > vq(3);
+	  vq = Quantity(23, "m");
+	  x.putValue(vq);
+	  cout << "putValue:       " << vq.ac() << ", " << x << endl;
+	  cout << "BaselineAngle:  " << x.BaselineAngle(mvb0) << endl;
+	  cout << "BaselineAngle:  " << x.BaselineAngle(mvb0, "deg") << endl;
+	  cout << "get:            " << x.get().ac() << endl;
+	  cout << "getRecordValue: " << x.getRecordValue().ac() << endl;
+	  cout << "separation:     " << x.separation(mvb0) << endl;
+	  cout << "separation:     " << x.separation(mvb0, "deg") << endl;
+	  cout << "crossProduct:   " << x.crossProduct(mvb0) << endl;
+	  cout << "getAngle:       " << x.getAngle() << endl;
+	  cout << "getAngle:       " << x.getAngle("deg") << endl;
+	  cout << "getlength:      " << x.getLength("cm") << endl;
+	  cout << "radius:         " << x.radius() << endl;
+	  cout << "getXRecordValue:" << x.getXRecordValue().ac() << endl;
+	  Vector<Double> x1(3);
+	  x1(0) = 30;
+	  x1(1) = 40;
+	  x1(2) = 0;
+	  x.putVector(x1);
+	  cout << "putVector:      " << x1.ac() << ", " << x << endl;
+	  MVBaseline x2(vq);
+	  cout << "VQ constructor: " << x2 << endl;
+	  cout << "Pos constructor:" << MVBaseline(x, x2) << endl;
+	  cout << "Q constructor:  " << MVBaseline(Quantity(50, "m")) << endl;
+	  cout << "QV constructor: " << MVBaseline(x2.getAngle()) << endl;
+	  cout << "QV constructor: " << MVBaseline(Quantity(34,"m"),
+						   x2.getAngle()) << endl;
+	  cout << "V constructor:  " << MVBaseline(x1) << endl;
+	  cout << "D constructor:  " << MVBaseline(Double(78)) << endl;
+	  cout << "operator+:      " << x+x2 << endl;
+	  cout << "operator-:      " << x-x2 << endl;
+	  cout << "operator-pre-:  " << -x2 << endl;
+	  RotMatrix rm(Euler(25, 1, 0, 0));
+	  cout << "operator*:      " << x2*rm << endl;
+	  cout << "operator*:      " << x2*2 << endl;
+	  MVBaseline::assert(x);
+	  cout << "assert:         " << "ok" << endl;
+	  cout << "getLength:      " << x.getLength() << endl;
+	  cout << "operator*:      " << x*x1 << endl;
+	  cout << "operator*       " << x*x2 << endl;
+	  cout << "operator*:      " << x1*x << endl;
+	  cout << "clone:          " << *(x.clone()) << endl;
+	  cout << "getVector:      " << x.getVector().ac() << endl;
+	  cout << "near:           " << x.near(x2) << endl;
+	  cout << "near:           " << x.near(x2, Quantity(1, "deg")) << endl;
+	  cout << "nearAbs:        " << x.nearAbs(x2) << endl;
+	  cout << "!=:             " << (x != x2) << endl;
+	  cout << "==:             " << (x == x2) << endl;
+	  cout << "type:           " << x.type() << endl;
+
+	  cout << "All MVBaseline functions: ok" << endl;
+	  cout << "----------------------------" << endl;
+	}
+
+ 	cout << "Exercise all MBaseline function" << endl;
+	{
+	  MBaseline mb(mvb0, MBaseline::B1950);
+	  String s0("azel");
+	  MBaseline::Types tp;
+	  MBaseline::Ref mr;
+	  cout << "getType:        " << MBaseline::getType(tp, s0) << ", ";
+	  cout << tp << endl;
+	  cout << "giveMe:         " << mb.giveMe(mr, s0) << ", ";
+	  cout << mr << endl;
+	  cout << "setRefString:   " << mb.setRefString("hadec") << ", ";
+	  cout << mb << endl;
+	  MBaseline::assert(mb);
+	  cout << "assert:         " << "ok" << endl;
+	  cout << "clone:          " << *(mb.clone()) << endl;
+	  cout << "get:            " << mb.get("cm") << endl;
+	  cout << "getAngle:       " << mb.getAngle("deg") << endl;
+	  cout << "getDefaultType: " << mb.getDefaultType() << endl;
+	  cout << "getRefString:   " << mb.getRefString() << endl;
+	  cout << "myType:         " << mb.myType() << endl;
+	  cout << "type:           " << mb.type() << endl;
+
+	  cout << "All MBaseline functions: ok" << endl;
+	  cout << "---------------------------" << endl;
+	}
+      
     } catch (AipsError x) {
 	cout << x.getMesg() << endl;
     } end_try;
