ef051129f726804a9053792c60eba1a0a8ae47d0
Ger van Diepen <gervandiepen@gmail.com>
1998-06-18 12:03:29 +0000

    *** empty log message ***

---
+++ code/lattices/implement/Lattices/RegionType.h
@@ -0,0 +1,54 @@
+//# RegionType.h: Define the various region types in an enum.
+//# Copyright (C) 1998
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//# $Id$
+
+#if !defined(AIPS_REGIONTYPE_H)
+#define AIPS_REGIONTYPE_H
+
+
+// <summary>
+// Define the various region types.
+// </summary>
+
+// <use visibility=export>
+
+// <reviewed reviewer="" date="" tests="">
+// </reviewed>
+
+
+class RegionType
+{
+public:
+    enum Type {
+	Invalid = -1,
+	Other = 0,
+	LC = 1,
+	WC = 2
+    };
+};
+
+
+#endif
