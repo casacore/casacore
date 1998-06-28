d13b1813586265e1b1056de4652efddb09eeb214
Neil Killeen <nkilleen@unimelb.edu.au>
1998-06-28 05:07:02 +0000

    Add comparison operators

--- code/lattices/implement/Lattices/LCPolygon.h
+++ code/lattices/implement/Lattices/LCPolygon.h
@@ -115,13 +115,19 @@ public:
 	       const IPosition& latticeShape);
     // </group>
 
-    // Copy constructor (partly reference semantics).
+    // Copy constructor (reference semantics).
     LCPolygon (const LCPolygon& other);
 
     virtual ~LCPolygon();
 
     // Assignment (copy semantics).
     LCPolygon& operator= (const LCPolygon& other);
+
+    // Comparison
+    // <group>
+    virtual Bool operator== (const LCRegion& other) const;
+    virtual Bool operator!= (const LCRegion& other) const;
+    // </group>
 
     // Make a copy of the derived object.
     virtual LCRegion* cloneRegion() const;
@@ -140,6 +146,9 @@ public:
 
     // Get the class name (to store in the record).
     static String className();
+
+    // Get the region type.  Returns className()
+    virtual String type() const;
 
     // Convert the (derived) object to a record.
     virtual TableRecord toRecord (const String& tableName) const;
