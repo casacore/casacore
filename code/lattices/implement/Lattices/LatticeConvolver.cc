0ce91394961c207916930b7459d1285543596c4b
Ralph Marson <rmarson@nrao.edu>
1998-07-31 22:51:20 +0000

    Implemented linear convolution

--- code/lattices/implement/Lattices/LatticeConvolver.cc
+++ code/lattices/implement/Lattices/LatticeConvolver.cc
@@ -27,23 +27,58 @@
 
 #include <trial/Lattices/LatticeConvolver.h>
 #include <trial/Lattices/LatticeFFT.h>
-#include <aips/Lattices/IPosition.h>
+#include <trial/Lattices/LatticeExpr.h>
+#include <trial/Lattices/SubLattice.h>
+#include <aips/Lattices/Slicer.h>
+#include <aips/Utilities/Assert.h>
 
 template<class T> LatticeConvolver<T>::
 LatticeConvolver()
-  :itsXfr(IPosition(1,1))
+  :itsFFTShape(IPosition(1,1)),
+   itsXfr(itsFFTShape)
 {
-  itsXfr.putAt(NumericTraits<T>::ConjugateType(1), IPosition(1,1));
+  itsXfr.set(NumericTraits<T>::ConjugateType(0));
+  IPosition XFRShape = itsFFTShape/2;
+  XFRShape(0) = 0;
+  itsXfr.putAt(NumericTraits<T>::ConjugateType(1), XFRShape);
 } 
 
 template<class T> LatticeConvolver<T>::
 LatticeConvolver(const Lattice<T> & psf)
+  :itsFFTShape(psf.shape())
 {
-  IPosition shape = psf.shape();
-  shape(0) = (shape(0) + 2)/2;
-  itsXfr = TempLattice<NumericTraits<T>::ConjugateType>(shape);
+  IPosition XFRShape = itsFFTShape;
+  XFRShape(0) = (XFRShape(0) + 2)/2;
+  itsXfr = TempLattice<NumericTraits<T>::ConjugateType>(XFRShape);
   LatticeFFT::rcfft(itsXfr, psf);
 } 
+
+template<class T> LatticeConvolver<T>::
+LatticeConvolver(const Lattice<T> & psf, const IPosition & imageShape) {
+  const uInt ndim = psf.ndim();
+  const IPosition & psfShape = psf.shape();
+  // Check that everything is the same dimension and that none of the
+  // dimensions is zero length.
+  AlwaysAssert(ndim == imageShape.nelements(), AipsError);
+  AlwaysAssert(psfShape.product() != 0, AipsError);
+  AlwaysAssert(imageShape.product() != 0, AipsError);
+  // Now calculate the minimum fft size required. I spent a fair of of time
+  // working out this formulae (empirically)!
+  itsFFTShape = imageShape + psfShape/2;
+  for (uInt i = 0; i < ndim; i++) {
+    if (itsFFTShape(i) < psfShape(i)) {
+      itsFFTShape(i) = 2 * imageShape(i) - 1;
+    }
+  }
+  // Now copy the psf into the padded psf, prior to calculating the transfer
+  // function. 
+  TempLattice<T> paddedPsf;
+  pad(paddedPsf, psf);
+  IPosition XFRShape(itsFFTShape);
+  XFRShape(0) = (XFRShape(0)+2)/2;
+  itsXfr = TempLattice<NumericTraits<T>::ConjugateType>(XFRShape);
+  LatticeFFT::rcfft(itsXfr, paddedPsf);
+}
 
 template<class T> LatticeConvolver<T>::
 ~LatticeConvolver()
@@ -55,6 +90,48 @@ getPsf(Lattice<T> & psf) const {
   LatticeFFT::crfft(psf, itsXfr);
 }
 
+template<class T> void LatticeConvolver<T>::
+linear(Lattice<T> & result, const Lattice<T> & model) {
+  TempLattice<T> padded;
+  pad(padded, model);
+  IPosition XFRShape(itsFFTShape);
+  XFRShape(0) = (XFRShape(0)+2)/2;
+  TempLattice<NumericTraits<T>::ConjugateType> fftModel(XFRShape);
+  LatticeFFT::rcfft(fftModel, padded);
+  fftModel.copyData(fftModel*itsXfr);
+  LatticeFFT::crfft(padded.lc(), fftModel.lc());
+  unpad(result, padded);
+}
+
+template<class T> void LatticeConvolver<T>::
+pad(TempLattice<T> & paddedModel, const Lattice<T> & model) const {
+  const uInt ndim = model.ndim();
+  const IPosition & modelShape = model.shape();
+  IPosition inBlc(ndim, 0), patchShape(modelShape);
+  for (uInt k = 0; k < ndim; k++) {
+    if (itsFFTShape(k) < modelShape(k)) {
+      inBlc(k) = modelShape(k)/2 - itsFFTShape(k)/2;
+      patchShape(k) = itsFFTShape(k);
+    }
+  }
+  const Slicer modelSlice(inBlc, patchShape);
+  const SubLattice<T> modelPatch(model, modelSlice); 
+  const IPosition outBlc = itsFFTShape/2 - patchShape/2;
+  const Slicer paddedSlice(outBlc, patchShape);
+  paddedModel = TempLattice<T>(itsFFTShape);
+  paddedModel.set(0.0f);
+  SubLattice<T> paddedPatch(paddedModel, paddedSlice, True); 
+  paddedPatch.copyData(modelPatch);
+}
+
+template<class T> void LatticeConvolver<T>::
+unpad(Lattice<T> & result, const TempLattice<T> & paddedResult) const {
+  const IPosition & resultShape = result.shape();
+  const IPosition inBlc = itsFFTShape/2 - resultShape/2;
+  const Slicer paddedSlice(inBlc, resultShape);
+  const SubLattice<T> resultPatch(paddedResult, paddedSlice); 
+  result.copyData(resultPatch);
+}
 // Local Variables: 
-// compile-command: "cd test; gmake OPTLIB=1 inst"
+// compile-command: "(cd test; gmake OPTLIB=1 tLatticeConvolver)"
 // End: 
