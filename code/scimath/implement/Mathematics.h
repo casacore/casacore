103abbd500c93f73ba2af59ee6d1808f9f13d22c
Ralph Marson <rmarson@nrao.edu>
1998-09-24 20:52:12 +0000

    Updated for the beta release and fixed a bad reference

--- code/scimath/implement/Mathematics.h
+++ code/scimath/implement/Mathematics.h
@@ -1,5 +1,5 @@
 //# Mathematics.h: Module header for Mathematical operations
-//# Copyright (C) 1995,1996,1997
+//# Copyright (C) 1995,1996,1997,1998
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
@@ -28,22 +28,23 @@
 #if !defined (AIPS_MATHEMATICS_H)
 #define AIPS_MATHEMATICS_H
 
- #include <aips/Mathematics/Complex.h>
- #include <aips/Mathematics/Constants.h>
-#include <aips/Mathematics/ConvVector.h>
- #include <aips/Mathematics/Convolver.h>
- #include <aips/Mathematics/DComplex.h>
- #include <aips/Mathematics/DFTServer.h>
- #include <aips/Mathematics/FFTServer.h>
- #include <aips/Mathematics/FourierTool.h>
- #include <aips/Mathematics/GridTool.h>
+#include <aips/Mathematics/Complex.h>
+#include <aips/Mathematics/Constants.h>
+#include <aips/Mathematics/Convolver.h>
+#include <aips/Mathematics/FFTServer.h>
 #include <aips/Mathematics/Math.h>
- #include <aips/Mathematics/MathFunc.h>
 #include <aips/Mathematics/NumericTraits.h>
 //# No need to include NumericTraits2.h as it is included by NumericTraits.h
- #include <aips/Mathematics/Primes.h>
- #include <aips/Mathematics/Random.h>
- #include <aips/Mathematics/extern_fft.h>
+#include <aips/Mathematics/Primes.h>
+#include <aips/Mathematics/Random.h>
+//# The following will be renamed FFTPack in a future release
+#include <aips/Mathematics/extern_fft.h>
+// The following are scheduled to be removed in a future AIPS++ release
+#include <aips/Mathematics/ConvVector.h>
+#include <aips/Mathematics/DFTServer.h>
+#include <aips/Mathematics/FourierTool.h>
+#include <aips/Mathematics/GridTool.h>
+#include <aips/Mathematics/MathFunc.h>
 
 // <module>
 
@@ -72,38 +73,45 @@
 // <linkto module="Arrays">Arrays</linkto> module).
 //
 // The classes presently in this module fall into the following categories:
-// <ol>
-//    <li> <linkto file="Math.h">Generic</linkto> mathematical functions
-//         (including the system math.h functions) 
-//    <li> <linkto file="Complex.h">Complex</linkto> numbers
-//    <li> Mathematical, numerical and physical 
-//         <linkto file="Constants.h">constants </linkto> like 
-//         pi, FLT_MAX and h (Planks constant). Many of the Physical 
-//         constants are also available with units in the 
-//         <linkto class="QC">Measures</linkto> Module.
-//    <li> <linkto file=NumericTraits.h>Relationships</linkto>
-//         between different numerical data types.
-//    <li> Numerical Fourier transforms, both 
-//         <linkto class="DFTServer">Direct Transforms</linkto>
-//         (for use with ungridded data) and 
-//         <linkto class="FFTServer">Fast Transforms</linkto> 
-//         (for gridded data)
-//    <li> Numerical <linkto class="Convolver">Convolution</linkto> 
-//         (both linear and circular) of multi-dimensional Arrays.
-//    <li> <linkto class="GridTool">Gridding</linkto> of irregular data 
-//         (this class is tagged for an update in the future)
-//    <li> <linkto file="Random.h">Random</linkto> numbers in a wide 
-//         variety of distributions. 
-//    <li> <linkto class="Primes">Prime</linkto> numbers
-//    <li> <linkto class="Interp">Interpolation</linkto> in one dimension
-//         This class is largely superseded by the 
-//         <linkto class="Interpolate1D">Interpolate1D</linkto> class in 
-//         the <linkto module="Functionals">Functionals</linkto> module
-//    <li> ``Special'' <linkto file="MathFunc.h">function</linkto> classes
-//         (which will be replaced by the 
-//         <linkto module="Functionals">Functional</linkto> 
-//         classes eventually).
-// </ol>
+// <ul>
+// <li> <linkto file="Math.h">Generic</linkto> mathematical functions
+//      (including the system math.h functions) 
+// <li> <linkto file="Complex.h">Complex</linkto> numbers
+// <li> Mathematical, numerical and physical 
+//      <linkto file="Constants.h">constants </linkto> like 
+//      pi, FLT_MAX and h (Planks constant). Many of the Physical 
+//      constants are also available with units in the 
+//      <linkto class="QC">Measures</linkto> Module.
+// <li> <linkto file=NumericTraits.h>Relationships</linkto>
+//      between different numerical data types.
+// <li> Numerical Fourier transforms, both 
+//      <linkto class="DFTServer">Direct Transforms</linkto>
+//      (for use with ungridded data) and 
+//      <linkto class="FFTServer">Fast Transforms</linkto> 
+//      (for gridded data)
+// <li> Numerical <linkto class="Convolver">Convolution</linkto> 
+//      (both linear and circular) of multi-dimensional Arrays.
+// <li> <linkto class="GridTool">Gridding</linkto> of irregular data 
+// <li> <linkto file="Random.h">Random</linkto> numbers in a wide 
+//      variety of distributions. 
+// <li> <linkto class="Primes">Prime</linkto> numbers
+// <li> Interpolation in one dimension is performed by the 
+//      <linkto class="Interpolate1D">Interpolate1D</linkto> class in 
+//      the <linkto module="Functionals">Functionals</linkto> module
+// <li> ``Special'' <linkto file="MathFunc.h">function</linkto> classes
+// </ul>
+// A number of classes in this module are very old and are either no longer
+// required or have been superceeded by newer classes. The following classes
+// will be removed in future AIPS++ releases.
+// <ul>
+// <li> ConvVector (replaced by Convolver)
+// <li> DFTServer (ungridded Fourier Transforms are not required)
+// <li> FourierTool (replaced by the updated FFTServer class)
+// <li> GridTool (replaced by classes in the trial package)
+// <li> MathFunc (this code will move into the Functionals module)
+// </ul>
+// In addition the extern_fft class will be renamed FFTPack and the Constants
+// class will move into the OS Module in a future AIPS++ release.
 // </synopsis>
 
 //# // <example>
