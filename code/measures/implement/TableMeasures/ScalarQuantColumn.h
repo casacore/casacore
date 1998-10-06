9d33d42daccd4d4d5f0da2704083e6bbccebceb9
Michael Haller <haller@astron.nl>
1998-10-06 15:42:26 +0000

    Update to documentation.

--- code/measures/implement/TableMeasures/ScalarQuantColumn.h
+++ code/measures/implement/TableMeasures/ScalarQuantColumn.h
@@ -1,5 +1,5 @@
 //# ScalarQuantColumn.h: Access to a Scalar Quantum Column in a table.
-//# Copyright (C) 1997
+//# Copyright (C) 1997,1998
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
@@ -39,7 +39,7 @@ template <class T> class ScalarColumn;
 template <class Qtype> class Quantum;
 
 // <summary>
-// A class for storing and retrieving Quantums in Tables.
+// Provides read-only access to Scalar Quantum Columns in Tables.
 // </summary>
 
 // <use visibility=export>
@@ -49,27 +49,41 @@ template <class Qtype> class Quantum;
 
 // <prerequisite>
 //# Classes you should understand before using this one.
-//   <li> <linkto class=Quantum.h>Quantum</linkto>
-//   <li> <linkto class=TableQuantumDesc>TableQuantumDesc</linkto>
+//   <li> <linkto class=Table>Table</linkto>
+//   <li> <linkto class=ROScalarColumn>ROScalarColumn</linkto>
+//   <li> <linkto class=Quantum>Quantum</linkto>
 // </prerequisite>
 
 // <synopsis>
-// The ROScalarQuantColumn class provides read only access to the Quantums 
-// stored in a Scalar Quantum Table column.  The column should previously have 
-// been defined as a Quantum column by means of the 
-// <linkto class=TableQuantumDesc>TableQuantumDesc class</linkto>.  
-// (Read/write access is provided by the ScalarQuantColumn class.)<br>
-// The class supports a subset (i.e., not all) of the operations possible
-// with a ScalarColumn object.<br>
-// The Quantums retreived from the ROScalarQuantColumn will have the units as
-// specified when the TableQuantumDesc was used to create the Quantum column.
-// If the unit is fixed, all Quantums retrieved will have this unit.
-// Alternatively the units are variable in which case the unit of the quantum
-// retrieve will be the same as when it was stored.
-// This default unit value can be overriden by supplying a value for units 
-// when constructing the ROScalarQuantColumn.<br>
-// Quantums can also be converted to new units while retrieving in a way
-// which mimics the conversion facilities offered by the Quantum class.
+// The ROScalarQuantColumn class provides read-only access to Quanta
+// stored in a scalar Quantum Table column.  The Quantum column should 
+// already exist in the table and would have been defined by means of a 
+// <linkto class=TableQuantumDesc>TableQuantumDesc object</linkto>.  
+// In addition,
+// for a ROScalarQuantColumn object to be useful the column should
+// contain Quanta.  Inserting Quanta into column requires the use of a
+// <linkto class=ScalarQuantColumn">ScalarQuantColumn</A> 
+// object.<br>
+//
+// A ScalarQuantColumn object is used much in the same way as a 
+// <linkto class=ROScalarColumn>ROScalarColumn</linkto> object.
+//
+// <note role=warning> This class supports a subset (i.e., not all) of the 
+// operations possible with a ROScalarColumn object.
+//
+// <h3>Quantum Units</h3></A>
+// Quanta retrieved from the column will normally have the Unit that was
+// specified 
+// when the Quantum column was defined.  However, it is possible to override
+// the default column Unit by supplying a Unit in a ROScalarQuantColumn
+// constructor which exists for this purpose.  When constructed in this
+// fashion the retrieved Quanta will have the newly specified Unit but their
+// values will be the same as when originally stored in the column.  That is,
+// no Unit conversion is done.
+// If Unit conversion is desired it can be achieved by
+// supplying a Unit argument to one of retrieval members (either get() or
+// operator()). (Quanta can of course be converted after they have been
+// retrieved from a column.)
 // </synopsis>
 
 // <example>
@@ -77,14 +91,14 @@ template <class Qtype> class Quantum;
 //     Quantum<Double> q(5.3, "keV");
 //     // "QuantScalar" has previously been defined as a Quantum column
 //     // by means of a TableQuantumDesc.  This example assumes the column
-//     // already contains quantums.
+//     // already contains quanta.
 //     ROScalarQuantColumn<Double> qCol(qtab, "QuantScalar");
-//     // return and print quantums as stored in the column
+//     // return and print quanta as stored in the column
 //     for (i = 0; i < qtab.nrow(); i++) {
 //         cout << qCol(i) << endl;
 //     }
-//     // The following retrieves and converts the quantums to GHz.  They
-//     // are then devided by the Quantum constant QC::h (Planck).
+//     // The following retrieves and converts the quanta to GHz.  They
+//     // are then divided by the Quantum constant QC::h (Planck).
 //     for (i=0; i < qtab.nrow(); i++) {
 //         cout << (qCol(i, "GHz"))/(QC::h);
 //     }
@@ -92,34 +106,36 @@ template <class Qtype> class Quantum;
 // </example>
 
 // <motivation>
-// Add support for Quantums in the Tables system.
+// Add support for Quanta in the Tables system.
 // </motivation>
-
+//
+// <thrown>
+//    <li>TableInvOper if the Table column is null.
+// </thrown>
+//
 //# <todo asof="$DATE:$">
 //# A List of bugs, limitations, extensions or planned refinements.
 //# </todo>
 
-template<class Qtype>
-class ROScalarQuantColumn
+template<class Qtype> class ROScalarQuantColumn
 {
 public:
     // The default constructor creates a null object.  Useful for creating
     // arrays of ROScalarQuantColumn objects.  Attempting to use a null object
     // will produce a segmentation fault so care needs to be taken to
     // initialise the objects by using the attach() member before any attempt
-    // is made to use the object.  A ROScalarQuantColumn object can be tested
-    // for nullnes by using the isNull() member.
+    // is made to use the object.  The isNull() member can be used to test
+    // if a ROScalarQuantColumn object is null.
     ROScalarQuantColumn();
 
     // Create the ROScalarQuantColumn from the specified table and column name.
     ROScalarQuantColumn(const Table& tab, const String& columnName);
 
     // Create the ROScalarQuantColumn from the specified table and column name.
-    // Units for the Quantum are specified in u and over-rides the default
-    // units for the column.  Note that retreived quantums are not converted
-    // to 'u', they are simply returned with 'u' units as if that was how 
-    // they were stored. This could be useful when Quantum column are created 
-    // with an inappropriate or undefined value for units.
+    // Units for the Quanta are specified in u.  This over-rides the default
+    // Unit for the column.  Note that specifying a Unit does not do Unit
+    // conversion.  Unit conversion is possible with one of the get() 
+    // members.
     ROScalarQuantColumn(const Table& tab, const String& columnName, 
 	    	        const Unit& u);
 
@@ -160,7 +176,7 @@ public:
     // Test whether the Quantum column has variable units
     Bool isUnitVariable() const { return (itsUnitsCol != 0 ? True : False); }
     
-    // Returns the column's units as a string.
+    // Returns the column's value for Units as a string.
     const String& getUnits() const { return itsUnit.getName(); }
     
     // Test if the object is null.
@@ -184,14 +200,14 @@ private:
     // Declaring this operator private makes it unusable.
     ROScalarQuantColumn& operator= (const ROScalarQuantColumn& that);
     
-    // Deletes allocated memory etc. Called by ~tor and any member which needs
-    // to reallocate data.
+    // Deletes allocated memory etc. Called by destructor and any member 
+    // which needs to reallocate data.
     void cleanUp();
 
 };
 
 // <summary>
-// A class for providing read/write access to Quantum columns in Tables.
+// Provides read/write access to Scalar Quantum Columns in Tables.
 // </summary>
 
 // <use visibility=export>
@@ -200,25 +216,32 @@ private:
 // </reviewed>
 
 // <prerequisite>
-//   <li> <linkto class=Quantum.h>Quantum</linkto>
-//   <li> TableQuantumDesc
+//   <li> <linkto class=Table>Table</linkto>
+//   <li> <linkto class=ROScalarColumn>ROScalarColumn</linkto>
+//   <li> <linkto class=Quantum>Quantum</linkto>
 // </prerequisite>
 
 // <synopsis>
-// The ScalarQuantColumn class provides read/write access to the Quantums
+// The ScalarQuantColumn class provides read/write access to Quanta
 // stored in a Quantum Table column.  The column should previously have 
 // been defined as a Quantum column by means of the 
-// <linkto class=TableQuantumDesc>TableQuantumDesc class</linkto>.  
-// (Read only access is provided by the ROScalarQuantColumn class.)<br>
-// The underlying Quantum column can have fixed or variable units which was
-// specified when the TableQuantumDesc was used to create the quantum column.
-// If the Quantum Column's Unit is fixed then the unit component of quantums
-// being added to a column is disregarded, that is, quantums are assumed to
-// have the correct unit and no attempt is made to convert the quantum to the
-// correct unit before it is stored.  In this case it is not an error to add
+// <linkto class=TableQuantumDesc>TableQuantumDesc</linkto> object.
+// In addition to the operations provided by its read-only partner,
+// <linkto class=ROScalarQuantColumn>ROScalarQuantColumn</linkto>, use 
+// of a ScalarQuantColumn object allows the insertion of Quanta into a column.
+// <br>
+//
+// <h3>Quantum Units</h3></A>
+// The underlying Quantum column will have been defined to have either
+// variable or fixed Units
+// If the Quantum Column's Unit is fixed then the unit component of quanta
+// being added to a column is disregarded, that is, quanta are assumed to
+// have the correct unit and no attempt is made to convert the quanta to the
+// correct unit before they are stored.  Therefore, it is not an error to add
 // a quantum with the wrong unit but the user should be aware that the unit
-// information will be lost.  On the otherhand, if the Quantum Column has
-// variable units the quantum's unit is stored as well as its value.<br>
+// information will be lost.  On the other-hand, if the Quantum Column was
+// defined with variable units the unit of every Quantum stored is stored
+// along with the Quantum's value.
 // </synopsis>
 
 // <example>
@@ -234,33 +257,38 @@ private:
 // </example>
 
 // <motivation>
-// Add support for Quantums in the Tables system.
+// Add support for Quanta in the Tables system.
 // </motivation>
-
+//
+// <thrown>
+//    <li>TableInvOper if the Table column is null.
+// </thrown>
+//
 // <todo asof="$DATE:$">
 // Is there a better way of dealing with fixed unit columns?  When putting a
-// Quantum into a column should the unit be checked and converted or should
-// it be illegal to added quantums with incorrect units?
+// Quantum into a column should the unit be checked and converted or perhaps
+// it should be illegal to add quanta with incorrect units?
 // </todo>
 
-template<class Qtype>
-class ScalarQuantColumn : public ROScalarQuantColumn<Qtype>
+
+template<class Qtype> class ScalarQuantColumn : 
+    public ROScalarQuantColumn<Qtype>
 {
 public:
     // The default constructor creates a null object.  Useful for creating
     // arrays of ScalarQuantColumn objects.  Attempting to use a null object
     // will produce a segmentation fault so care needs to be taken to
     // initialise the objects by using the attach() member before any attempt
-    // is made to use the object.  A ScalarQuantColumn object can be tested
-    // for nullnes by using the isNull() member.
+    // is made to use the object.  The isNull() member can be used to test
+    // if a ScalarQuantColumn object is null.
     ScalarQuantColumn();
 
     // Create the ScalarQuantColumn from the specified table and column name.
     ScalarQuantColumn(const Table& tab, const String& columnName);
 
     // Create the ScalarQuantColumn from the specified table and column name.
-    // Units for the Quantum are specified in u and over-rides the default
-    // units for the column.  Note that retreived quantums are not converted
+    // Units for the Quantum are specified in u. This over-rides the default
+    // units for the column.  Note that retrieved quanta are not converted
     // to 'u', they are simply returned with 'u' units as if that was how 
     // they were stored. This could be useful when Quantum column are created 
     // with an inappropriate or undefined value for units.
@@ -281,7 +309,9 @@ public:
     void attach(const Table& tab, const String& columnName, const Unit& u);
     // </group>
  
-    // Add a quantum to the table.  
+    // Add a quantum to the table.  If the column supports variable units
+    // the q's Unit is stored with q.  If Units are fixed for the column
+    // q's Unit is disregarded (i.e., it is assumed to conform).
     void put(uInt rownr, const Quantum<Qtype>& q);
     
 private:
