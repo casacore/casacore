5b4ed3a63d9a721d1f819eb356b09fd6be286c06
Michael Haller <haller@astron.nl>
1998-10-06 15:10:42 +0000

    Additional tests for more complete coverage.

--- code/measures/implement/TableMeasures/test/tTableQuantum.cc
+++ code/measures/implement/TableMeasures/test/tTableQuantum.cc
@@ -47,9 +47,10 @@
 #include <aips/Tables/TableDesc.h>
 #include <iostream.h>
 
-int main(void)
+int main(int argc)
 {
   try {
+    Bool doExcep = ToBool(argc<2);
 
     cout << "Begin tTableQuantum.cc.\n";
     
@@ -68,31 +69,84 @@ int main(void)
 	"A scalar column Quantum<Complex> with variable units.");
     ScalarColumnDesc<String> scdStr("varUnitsColumn", 
 	"Units columns for column ScaQuantComplex");
+
     
     // Also create ArrayQuantum columns for the test
     ArrayColumnDesc<Double> acdQD("ArrQuantDouble", 
 	"A Quantum<double> array column");
+    ArrayColumnDesc<Double> acdQD3("ArrQuantDoubleNonVar", 
+	"A Quantum<double> array column");
+    ArrayColumnDesc<Double> acdQD2("ArrQuantScaUnits", 
+	"A Quantum<double> array column");
     ArrayColumnDesc<String> acdStr("varArrUnitsColumn", 
 	"String column for array of units");
+    ScalarColumnDesc<String> ascdStr("varArrScaUnitsColumn", 
+	"Scalar string column for variable units per row");
     
     // These must be added to the table descriptor
     cout << "Adding column descriptors to the table...\n";
     td.addColumn(scdQD);
     td.addColumn(scdQC);
     td.addColumn(acdQD);
+    td.addColumn(acdQD2);
+    td.addColumn(acdQD3);
     td.addColumn(scdStr);
     td.addColumn(acdStr);
+    td.addColumn(ascdStr);
     
-    // Now create the Table Quantum Descriptors...
-    TableQuantumDesc tqdSQD(td, "ScaQuantDouble", Unit("deg"));
+    // Now create the Table Quantum Descriptors.  Three are used below but
+    // a couple of dummy objects are created to test assignment and the copy
+    // constructor.  The object we finally want is tqdSQD.
+    TableQuantumDesc tqddummy(td, "ScaQuantDouble", Unit("deg"));
+    // test copy constructor
+    TableQuantumDesc tqddummy2 = tqddummy;
+    // test empty unit
+    TableQuantumDesc tqdSQD(td, "ScaQuantDouble");
+    // test assignment
+    tqdSQD = tqddummy2;
+
     TableQuantumDesc tqdSQC(td, "ScaQuantComplex", "varUnitsColumn");
     TableQuantumDesc tqdAQC(td, "ArrQuantDouble", "varArrUnitsColumn");
+    TableQuantumDesc tqdAQC2(td, "ArrQuantScaUnits", "varArrScaUnitsColumn");
+    TableQuantumDesc tqdAQC3(td, "ArrQuantDoubleNonVar", Unit("deg"));
+
+    // test the exceptions
+    if (doExcep) {
+	cout << "Testing TableQuantumDesc constructor exceptions...\n";
+	try {
+	    // no such column
+	    TableQuantumDesc taexcep(td, "SillyName");
+	} catch (AipsError x) {
+	    cout << "A no such column message should follow\n";
+	    cout << x.getMesg() << endl;
+	} end_try;
     
+	try {
+	    // variable unit's column doesn't exist.
+	    TableQuantumDesc taexcep(td, "ScaQuantComplex", "SillyName");
+	} catch (AipsError x) {
+	    cout << "A no such unit's column message should follow\n";
+	    cout << x.getMesg() << endl;
+	} end_try;
+    
+	try {
+	    // The vaiable unit's column exists but the units type isn't String
+	    ScalarColumnDesc<Int> eucol("testvarcolumn", 
+		  "variable units column with incorrect type");
+	    td.addColumn(eucol);
+	    TableQuantumDesc taexcep(td, "ScaQuantComplex", "testvarcolumn");
+	} catch (AipsError x) {
+	    cout << "A message about an incorrect variable unit's type...\n";
+	    cout << x.getMesg() << endl;
+	} end_try;
+    }
     // ...and make them persistant.
     tqdSQD.write(td);
     tqdSQC.write(td);
     tqdAQC.write(td);
-    
+    tqdAQC2.write(td);
+    tqdAQC3.write(td);
+
     cout << "Column's name is: " + tqdSQD.columnName() << endl;
     if (tqdSQD.isUnitVariable()) {
 	cout << "Quantum column " + tqdSQD.columnName()
@@ -103,7 +157,7 @@ int main(void)
     
     cout << "Column's name is: " + tqdSQC.columnName() << endl;
     
-    // create the table with 5 rows.
+    // create a table with 5 rows.
     SetupNewTable newtab("TestTableMeasures", td, Table::New);
     Table qtab(newtab, 5);
     
@@ -114,6 +168,15 @@ int main(void)
 
 	cout << "Check if isNull and then throwIfNull\n";
 	if (sqCol.isNull()) {
+	    if (doExcep) {
+		// test isnull exception
+		try {
+		    sqCol.throwIfNull();
+		} catch (AipsError x) {
+		    cout << "Catch an AipsError. Column is null...\n";
+		    cout << x.getMesg() << endl;
+		} end_try;
+	    }
 	    cout << "Object says it is null...attach a column\n";
 	    sqCol.attach(qtab, "ScaQuantDouble");
 	    if (sqCol.isNull()) {
@@ -140,7 +203,12 @@ int main(void)
     {
 	// Could also read values from sqCol but instead a ROScalarQuantCol 
 	// is created here to do that.
-	ROScalarQuantColumn<Double> rosqCol(qtab, "ScaQuantDouble");
+	// test attach member for this first
+	ROScalarQuantColumn<Double> rosqCol;
+	if (rosqCol.isNull()) {
+	    rosqCol.attach(qtab, "ScaQuantDouble");
+	}
+	rosqCol.throwIfNull();
 	cout << "Column's quantum units are: " << rosqCol.getUnits() << endl;
 	for (uInt i=0; i<qtab.nrow(); i++) {
 	    cout << "Quantum " << i << ": " << rosqCol(i) << endl;
@@ -193,13 +261,24 @@ int main(void)
 	// default units.  Creating a ROScalarQuantum object with default
 	// units doesn't do conversion.  It simply replaces the value for
 	// unit which may or may not exist for this column.
-	ROScalarQuantColumn<Double> rosqCol(qtab, "ScaQuantDouble", "m/s");
+	// Create a null column and attach a legitament one.
+	ROScalarQuantColumn<Double> rosqCol;
+	rosqCol.attach(qtab, "ScaQuantDouble", "m/s");
 	Quantum<Double> q;
 	// Retrieve 2 quantums one with conversion to GHz.
 	rosqCol.get(q, 1);
 	cout << "Quantum<Double> (units (m/s)) " << q << endl;
 	rosqCol.get(q, 2, "GHz");
 	cout << "Quantum<Double> (units (GHz)) " << q << endl;
+    }
+
+    {
+	// Cover the functions we haven't so far tested
+	// ScalarQuantColumn copy constructor
+	ScalarQuantColumn<Double> sqcol(qtab, "ScaQuantDouble", "m/s");
+	ScalarQuantColumn<Double> sqcolCpy = sqcol;
+	ScalarQuantColumn<Double> sqcolNull;
+	sqcolNull.attach(qtab, "ScaQuantDouble", "m/s");
     }
 
     cout << "\nFinished test of Quantum scalar column...........\n";
@@ -209,6 +288,14 @@ int main(void)
 	// Now for array columns.  This set up a Quant Array column with
 	// variable units where the units vary per array element.
 	ArrayQuantColumn<Double> aqCol;
+	if (doExcep) {
+	  try {
+	      aqCol.throwIfNull();
+	  } catch (AipsError x) {
+	      cout << "Catch an AipsError. Array column is null...\n";
+	      cout << x.getMesg() << endl;
+	  } end_try;
+	}
 	if (aqCol.isNull()) {
 	    cout << "Array Quantum Column is initially null.\n";
 	    aqCol.attach(qtab, "ArrQuantDouble");
@@ -253,10 +340,79 @@ int main(void)
 	cout << roaqCol(0, "m/s") << endl;
     }    
     {
+	// A second ArrayQuantColumn with variable units but in this case
+	// the units only vary once per row as opposed to per array element
+	// per row as in the example above.  This can be done because the
+	// TableQuantDesc for the row specified a ScalarColumn as the
+	// units column.
+	ArrayQuantColumn<Double> aqCol(qtab, "ArrQuantScaUnits");
+	if (aqCol.isUnitVariable()) {
+	    cout << "Array quantum column: units are variable.\n";
+	} else {
+	    cout << "Array quantum column units: " << aqCol.getUnits() << endl;
+	}
+	
+	IPosition shape(2, 3, 2);
+	Array<Quantum<Double> > qArr1(shape);
+	Array<Quantum<Double> > qArr2(shape);
+	Bool deleteIt;
+	Quantum<Double>* q_p1 = qArr1.getStorage(deleteIt);
+	// when these are eventually stored in the column all will be stored
+	// with the common unit of "GHz" as this is the unit value of the 
+	// first Quantum in the array
+	q_p1->setValue(1.41212);
+	q_p1->setUnit("GHz");
+	q_p1++;
+	q_p1->setValue(1.4921);
+	q_p1->setUnit("deg");
+	q_p1++;    
+	q_p1->setValue(1.4111);
+	q_p1->setUnit("ms-1");
+	q_p1++;    
+	q_p1->setValue(1.4003);
+	q_p1->setUnit("Jy");
+	q_p1++;    
+	q_p1->setValue(1.22);
+	q_p1->setUnit("GHz");
+	q_p1++;    
+	q_p1->setValue(1.090909);
+	q_p1->setUnit("g");	
+	qArr1.putStorage(q_p1, deleteIt);
+	
+	Quantum<Double>* q_p2 = qArr2.getStorage(deleteIt);
+	// For row 2 the unit will be "deg"
+	q_p2->setValue(1.41212);
+	q_p2->setUnit("deg");
+	q_p2++;
+	q_p2->setValue(1.4921);
+	q_p2->setUnit("deg");
+	q_p2++;    
+	q_p2->setValue(1.4111);
+	q_p2->setUnit("ms-1");
+	q_p2++;    
+	q_p2->setValue(1.4003);
+	q_p2->setUnit("Jy");
+	q_p2++;    
+	q_p2->setValue(1.22);
+	q_p2->setUnit("GHz");
+	q_p2++;    
+	q_p2->setValue(1.090909);
+	q_p2->setUnit("g");	
+	qArr2.putStorage(q_p2, deleteIt);
+	
+	// put the quantum array in the column
+	aqCol.put(0, qArr1);
+	aqCol.put(1, qArr2);
+    }
+    {
 	// another way of creating the object
 	ROArrayQuantColumn<Double> roaqCol(qtab, "ArrQuantDouble", "deg");
 	cout << roaqCol(0) << endl;	
 	cout << roaqCol(0, "m/s") << endl;
+
+	
+	Quantum<Double> q(0.21, "deg");
+	cout << roaqCol(0, q);
     }    
     {
 	// another way of creating the object
@@ -264,6 +420,27 @@ int main(void)
 	roaqCol.attach(qtab, "ArrQuantDouble", "m/s");
 	cout << roaqCol(0) << endl;	
 	cout << roaqCol(0, "deg") << endl;
+
+	// the copy constructor
+	ROArrayQuantColumn<Double> roCpyCol = roaqCol;
+
+	// another test of attach
+	ROArrayQuantColumn<Double> roNullCol;
+	roNullCol.attach(qtab, "ArrQuantDouble");
+    }
+    {
+	// These should complete the coverage of the class
+	// contructor
+	ArrayQuantColumn<Double> aqc(qtab, "ArrQuantDouble", "m/s");
+	// copy constructor
+	ArrayQuantColumn<Double> aqc1 = aqc;
+	// attach
+	ArrayQuantColumn<Double> aqc2;
+	aqc2.attach(qtab, "ArrQuantDouble", "m/s");
+
+	// non-variable units column
+	ROArrayQuantColumn<Double> aqc3(qtab, "ArrQuantDoubleNonVar");
+	cout << aqc3.getUnits() << endl;
     }
     
     cout << "End tTableQuantum.cc.\n";
