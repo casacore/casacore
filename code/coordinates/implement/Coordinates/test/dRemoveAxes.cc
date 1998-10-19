d5be7a2d510fc8667e40a1136eea96448186f96f
Neil Killeen <nkilleen@unimelb.edu.au>
1998-10-19 03:50:44 +0000

    Track chnages to CoordinateUtil::removeAxes caused by chnage to CoordinateSYstem::removeWorldAxis

--- code/coordinates/implement/Coordinates/test/dRemoveAxes.cc
+++ code/coordinates/implement/Coordinates/test/dRemoveAxes.cc
@@ -10,8 +10,7 @@
 
 
 void listIt (const CoordinateSystem& cSys,
-             const Vector<Double>& worldReplacement,
-             const Vector<Double>& pixelReplacement);
+             const Vector<Double>& worldReplacement);
 
 
 main (int argc, char **argv)
@@ -19,70 +18,67 @@ main (int argc, char **argv)
 try {
 
    Vector<Double> worldReplacement;
-   Vector<Double> pixelReplacement;   
    {
-     cout << "remove all axes = [0, 1] from [ra, dec, freq]" << endl;
+     cout << "remove world axes = [0, 1] and associated pixel axes from [ra, dec, freq]" << endl;
      CoordinateSystem cSys = CoordinateUtil::defaultCoords3D();
      Vector<uInt> list(1);
      list(0) = 2;
      Bool remove = False;
      if (CoordinateUtil::removeAxes(cSys, worldReplacement,
-                                    pixelReplacement, list, 
-                                    remove, True)) {
-       listIt (cSys, worldReplacement, pixelReplacement);
+                                    list,  remove)) {
+       listIt (cSys, worldReplacement);
      } else {
        cout << "failed" << endl;
      }
    }   
    cout << endl << endl;
    {
-     cout << "remove all axes = [2] from [ra, dec, freq]" << endl;
+     cout << "remove world axes = [2] and associated pixel axes from [ra, dec, freq]" << endl;
      CoordinateSystem cSys = CoordinateUtil::defaultCoords3D();
+
      Vector<uInt> list(1);
      list(0) = 2;
      Bool remove = True;
      if (CoordinateUtil::removeAxes(cSys, worldReplacement,
-                                    pixelReplacement, list, 
-                                    remove, True)) {
-        listIt (cSys, worldReplacement, pixelReplacement);
+                                    list, remove)) {
+        listIt (cSys, worldReplacement);
      } else {
        cout << "failed" << endl;
      }
    }   
    cout << endl << endl;
    {
-     cout << "remove all axes = [0, 2] from [ra,dec,freq]" << endl;
+     cout << "remove world axes = [0, 2] and associated pixel axes from [ra,dec,freq]" << endl;
      CoordinateSystem cSys = CoordinateUtil::defaultCoords3D();
+
      Vector<uInt> list(2);
      list(0) = 0;
      list(1) = 2;
      Bool remove = True;
      if (CoordinateUtil::removeAxes(cSys, worldReplacement,
-                                    pixelReplacement, list, 
-                                    remove, True)) {
-        listIt (cSys, worldReplacement, pixelReplacement);
+                                    list, remove)) {
+        listIt (cSys, worldReplacement);
      } else {
        cout << "failed" << endl;
      }
    }   
    cout << endl << endl;
    {
-     cout << "remove all axes = [0] from [ra, dec, freq]" << endl;
-     cout << "and then all axes = [0, 1]" << endl;
+     cout << "remove world axes = [0] and associated pixel axes from [ra, dec, freq]" << endl;
+     cout << "and then world axes = [0, 1] and associated pixel axes " << endl;
      CoordinateSystem cSys = CoordinateUtil::defaultCoords3D();
+
      Vector<uInt> list(1);
      list(0) = 0;
      Bool remove = True;
      if (CoordinateUtil::removeAxes(cSys, worldReplacement,
-                                    pixelReplacement, list, 
-                                    remove, True)) {
+                                    list, remove)) {
         list.resize(2);
         list(0) = 0;
         list(1) = 1;
         if (CoordinateUtil::removeAxes(cSys, worldReplacement,
-                                       pixelReplacement, list, 
-                                       remove, True)) {
-           listIt (cSys, worldReplacement, pixelReplacement);
+                                       list, remove)) {
+           listIt (cSys, worldReplacement);
         } else {
           cout << "failed" << endl;
         }
@@ -93,69 +89,48 @@ try {
    }   
    cout << endl << endl;
    {
-     cout << "remove axes = [0, 2] from [ra,dec,freq]" << endl;
-     cout << "specified world replacement values = [1.0,2e9]" << endl;
-     cout << "specified pixel replacement values = [10.0,20.0]" << endl;
-     CoordinateSystem cSys = CoordinateUtil::defaultCoords3D();
-     Vector<uInt> list(2);
-     list(0) = 0;
-     list(1) = 2;
-     Bool remove = True;
-     worldReplacement.resize(2);
-     worldReplacement(0) = 1.0;
-     worldReplacement(1) = 2e9;
-     pixelReplacement.resize(2);
-     pixelReplacement(0) = 10.0;
-     pixelReplacement(1) = 20.0;
-     
-     if (CoordinateUtil::removeAxes(cSys, worldReplacement,
-                                    pixelReplacement, list, 
-                                    remove, False)) {
-       listIt (cSys, worldReplacement, pixelReplacement);
-     } else {
-       cout << "failed" << endl;
-     }
-   }   
-   cout << endl << endl;
-   {
-     cout << "remove world axes = [0, 2] from [ra,dec,freq]" << endl;
-     CoordinateSystem cSys = CoordinateUtil::defaultCoords3D();
-     Vector<uInt> list(2);
-     list(0) = 0;
-     list(1) = 2;
-     Bool remove = True;
-     if (CoordinateUtil::removeAxes(cSys, worldReplacement,
-                                    pixelReplacement, list, 
-                                    remove, False)) {
-       listIt (cSys, worldReplacement, pixelReplacement);
-     } else {
-       cout << "failed" << endl;
-     }
-   }   
-   cout << endl << endl;
-   {
-     cout << "remove world axes = [0, 2] from [ra,dec,freq]" << endl;
-     cout << "specified replacement values = [1.0,2e9]" << endl;
-     CoordinateSystem cSys = CoordinateUtil::defaultCoords3D();
-     Vector<uInt> list(2);
-     list(0) = 0;
-     list(1) = 2;
-     Bool remove = True;
-     worldReplacement.resize(2);
-     worldReplacement(0) = 1.0;
-     worldReplacement(1) = 2e9;
-     pixelReplacement.resize(0);
-     
-     if (CoordinateUtil::removeAxes(cSys, worldReplacement,
-                                    pixelReplacement, list, 
-                                    remove, False)) {
-       listIt (cSys, worldReplacement, pixelReplacement);
-     } else {
-       cout << "failed" << endl;
-     }
-   }   
-   cout << endl << endl;
+     cout << "remove world axes = [0, 2] and associated pixel axes from [ra,dec,freq]" << endl;
 
+     CoordinateSystem cSys = CoordinateUtil::defaultCoords3D();
+
+     Vector<uInt> list(2);
+     list(0) = 0;
+     list(1) = 2;
+     Bool remove = True;
+     Vector<Double> incr = cSys.increment();
+     Vector<Double> refVal = cSys.referenceValue();
+     Vector<Double> refPix = cSys.referencePixel();
+
+     worldReplacement.resize(2);
+     worldReplacement(0) = (-1 - refPix(list(0)))*incr(list(0)) + refVal(list(0));
+     worldReplacement(1) = (10 - refPix(list(1)))*incr(list(1)) + refVal(list(1));
+
+     cout << "specified world replacement values = " << worldReplacement.ac() << endl;
+     
+     if (CoordinateUtil::removeAxes(cSys, worldReplacement,
+                                    list, remove)) {
+       listIt (cSys, worldReplacement);
+     } else {
+       cout << "failed" << endl;
+     }
+   }   
+   cout << endl << endl;
+   {
+     cout << "remove world axes = [0, 2] and associated pixel axes from [ra,dec,freq]" << endl;
+     CoordinateSystem cSys = CoordinateUtil::defaultCoords3D();
+
+     Vector<uInt> list(2);
+     list(0) = 0;
+     list(1) = 2;
+     Bool remove = True;
+     if (CoordinateUtil::removeAxes(cSys, worldReplacement,
+                                    list, remove)) {
+       listIt (cSys, worldReplacement);
+     } else {
+       cout << "failed" << endl;
+     }
+   }   
+   cout << endl << endl;
 
 
 }
@@ -170,14 +145,12 @@ try {
 
 
 void listIt (const CoordinateSystem& cSys,
-             const Vector<Double>& worldReplacement,
-             const Vector<Double>& pixelReplacement)
+             const Vector<Double>& worldReplacement)
 {
     cout << "nWorldAxes = " << cSys.nWorldAxes() << endl;
     cout << "nPixelAxes = " << cSys.nPixelAxes() << endl;
     cout << "world axis names = " << cSys.worldAxisNames().ac() << endl;
     cout << "reference pixels = " << cSys.referencePixel().ac() << endl;
     cout << "replacement world values = " << worldReplacement.ac() << endl;
-    cout << "replacement pixel values = " << pixelReplacement.ac() << endl;
 }
 
