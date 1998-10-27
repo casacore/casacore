10035a04c0c3804222a90db00bfb9b5d065a08bb
Ger van Diepen <gervandiepen@gmail.com>
1998-10-27 08:08:53 +0000

    added function to make a description of the world axes added comment functions fromRecord is split off to a separate .cc file

--- code/images/implement/Images/WCRegion.cc
+++ code/images/implement/Images/WCRegion.cc
@@ -26,29 +26,33 @@
 //# $Id$
 
 #include <trial/Images/WCRegion.h>
-
-#include <aips/Exceptions/Error.h>
-#include <aips/Tables/TableRecord.h>
-#include <trial/Images/WCBox.h>
-#include <trial/Images/WCPolygon.h>
 #include <trial/Lattices/RegionType.h>
+#include <trial/Lattices/LCExtension.h>
+#include <trial/Lattices/LCBox.h>
+#include <trial/Coordinates/CoordinateSystem.h>
+#include <trial/Coordinates/Coordinate.h>
+#include <trial/Coordinates/DirectionCoordinate.h>
+#include <trial/Coordinates/SpectralCoordinate.h>
+#include <aips/Tables/TableRecord.h>
+#include <aips/Utilities/GenSort.h>
+#include <aips/Utilities/Assert.h>
+#include <aips/Exceptions/Error.h>
+
 
 WCRegion::WCRegion()
 {}
 
-WCRegion::WCRegion (const WCRegion&)
-// 
-// There is no private data in WCRegion so
-// nothing to do
-//
+WCRegion::WCRegion (const WCRegion& other)
+: itsComment  (other.itsComment),
+  itsAxesDesc (other.itsAxesDesc)
 {}
  
-WCRegion& WCRegion::operator= (const WCRegion&)
-//
-// There is no private data in WCRegion so
-// there is nothing to assign
-//
+WCRegion& WCRegion::operator= (const WCRegion& other)
 {
+    if (this != &other) {
+	itsComment  = other.itsComment;
+	itsAxesDesc = other.itsAxesDesc;
+    }
     return *this;
 }
 
@@ -56,26 +60,191 @@ WCRegion::~WCRegion()
 {}
 
 
-WCRegion* WCRegion::fromRecord (const TableRecord& rec,
-                                const String& tableName)
+Bool WCRegion::operator== (const WCRegion& other) const
 {
-    const String& name = rec.asString ("name");
-    if (name == WCBox::className()) {
-        return WCBox::fromRecord (rec, tableName);
-    } else if (name == WCPolygon::className()) {
-        return WCPolygon::fromRecord (rec, tableName);
-    } else {
-        throw (AipsError ("WCRegion::fromRecord - " + name +
-                          " is unknown derived WCRegion class"));
-    }
-    return 0;
+    // Type check.
+    return ToBool (type() == other.type());
 }
-
 
 void WCRegion::defineRecordFields (RecordInterface& record,
                                    const String& className) const
 {
     record.define ("isRegion", Int(RegionType::WC));
     record.define ("name", className);
+    record.define ("comment", itsComment);
 }
 
+
+const Record& WCRegion::getAxisDesc (uInt axis) const
+{
+    AlwaysAssert (axis < itsAxesDesc.nfields(), AipsError);
+    return itsAxesDesc.subRecord (axis);
+}
+
+Int WCRegion::axisNr (const Record& desc, const Record& axesDesc) const
+{
+    uInt nf = axesDesc.nfields();
+    for (uInt i=0; i<nf; i++) {
+	if (isAxisDescEqual (desc, axesDesc.subRecord(i))) {
+	    return i;
+	}
+    }
+    return -1;
+}
+
+Bool WCRegion::isAxisDescEqual (const Record& desc1, const Record& desc2) const
+{
+    uInt nf = desc1.nfields();
+    if (desc2.nfields() != nf) {
+	return False;
+    }
+    for (uInt j=0; j<nf; j++) {
+	Int fld = desc1.fieldNumber (desc2.name(j));
+	if (fld < 0) {
+	    return False;                // field does not exist
+	}
+	if (desc2.dataType(j) != desc1.dataType(fld)) {
+	    return False;
+	}
+	switch (desc2.dataType(j)) {
+	case TpInt:
+	    if (desc2.asInt(j) != desc1.asInt(fld)) {
+		return False;
+	    }
+	    break;
+	case TpString:
+	    if (desc2.asString(j) != desc1.asString(fld)) {
+		return False;
+	    }
+	    break;
+	default:
+	    throw (AipsError ("WCRegion::isAxisEqual - "
+			      "cannot handle data type"));
+	}
+    }
+    return True;
+}
+
+void WCRegion::addAxisDesc (const Record& desc)
+{
+    itsAxesDesc.defineRecord (itsAxesDesc.nfields(), desc);
+}
+
+Record WCRegion::makeAxisDesc (const CoordinateSystem& cSys, uInt axis) const
+{
+    Int coord, axisInCoord;
+    Record axisrec;
+    AlwaysAssert (axis <cSys.nPixelAxes(), AipsError);
+    cSys.findPixelAxis (coord, axisInCoord, axis);
+    Int type = cSys.type (coord);
+    axisrec.define ("type", type);
+    axisrec.define ("axis", axisInCoord);
+    switch (type) {
+    case Coordinate::DIRECTION:
+    {
+	Int type = cSys.directionCoordinate(coord).directionType();
+	axisrec.define ("dirtype", type);
+	break;
+    }
+    case Coordinate::SPECTRAL:
+    {
+	Int type = cSys.spectralCoordinate(coord).frequencySystem();
+	axisrec.define ("freqtype", type);
+	break;
+    }
+    default:
+	Vector<String> names = cSys.coordinate(coord).worldAxisNames();
+	axisrec.define ("name", names(axisInCoord));
+    }
+    return axisrec;
+}
+
+Record WCRegion::makeAxesDesc (const CoordinateSystem& cSys) const
+{
+    Record desc;
+    for (uInt i=0; i<cSys.nPixelAxes(); i++) {
+	desc.defineRecord (i, makeAxisDesc (cSys, i));
+    }
+    return desc;
+}
+
+Bool WCRegion::canExtend() const
+{
+    return False;
+}
+
+LCRegion* WCRegion::toLCRegion (const CoordinateSystem& cSys,
+				const IPosition& shape) const
+{
+    uInt i,n;
+    // Make sure shape length matches number of pixel axes.
+    AlwaysAssert (shape.nelements() == cSys.nPixelAxes(), AipsError);
+    // Make pixel axes description of coordinate system.
+    Record desc = makeAxesDesc (cSys);
+    // Find the mapping of the axesDesc of the region to the axesDesc
+    // of the new coordinate system.
+    // An exception is thrown if a region axis is not used in the
+    // coordinate system.
+    uInt ndout = shape.nelements();
+    uInt ndreg = ndim();
+    IPosition pixelAxesMap(ndout);
+    IPosition axisUsed(ndout, 0);
+    n = 0;
+    for (i=0; i<ndreg; i++) {
+        Int axis = axisNr (getAxisDesc(i), desc);
+	if (axis < 0) {
+	    throw (AipsError ("WCRegion::toLCRegion - "
+			      "a region axis is unknown in target "
+			      "coordinate system"));
+	}
+	pixelAxesMap(n++) = axis;
+	axisUsed(axis) = True;
+    }
+    for (i=0; i<ndout; i++) {
+        if (axisUsed(i) == 0) {
+	    pixelAxesMap(n++) = i;
+	}
+    }
+    return toLCRegion (cSys, shape, pixelAxesMap, pixelAxesMap);
+}
+
+LCRegion* WCRegion::toLCRegion (const CoordinateSystem& cSys,
+				const IPosition& shape,
+				const IPosition& pixelAxesMap,
+				const IPosition& outOrder) const
+{
+    uInt i;
+    // We have an nD region which is used for an mD image (m>=n).
+    // outOrder(i) gives output axis of axis i.
+    // pixelAxesMap(i) gives cSys/shape axis of axis i.
+    // First determine along which axes the region has to be extended.
+    uInt ndreg = ndim();
+    uInt ndout = pixelAxesMap.nelements();
+    DebugAssert (ndout>=ndreg, AipsError);
+    // If no extension is needed or if the region can extend itself,
+    // life is simple.
+    if (ndout == ndreg  ||  canExtend()) {
+        return doToLCRegion (cSys, shape, pixelAxesMap, outOrder);
+    }
+    // We have to make the extension here.
+    // So split the IPositions into the region part and the extension part.
+    IPosition pixAxesMap(ndreg);
+    IPosition outOrd(ndreg);
+    IPosition extendAxes(ndout-ndreg);
+    IPosition extendShape(ndout-ndreg);
+    Vector<uInt> inx(ndreg);
+    GenSortIndirect<Int>::sort (inx, outOrder.storage(), ndreg);
+    for (i=0; i<ndreg; i++) {
+        pixAxesMap(i) = pixelAxesMap(i);
+	outOrd(inx(i)) = i;
+    }
+    for (i=ndreg; i<ndout; i++) {
+        extendAxes(i-ndreg) = outOrder(i);
+	extendShape(i-ndreg) = shape(pixelAxesMap(i));
+    }
+    LCRegion* regPtr = doToLCRegion (cSys, shape, pixAxesMap, outOrd);
+    LCRegion* extPtr = new LCExtension (*regPtr, extendAxes,
+					LCBox(extendShape));
+    delete regPtr;
+    return extPtr;
+}
