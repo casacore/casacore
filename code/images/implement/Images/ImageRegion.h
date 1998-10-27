c9246147bc9df689676c28ba1842afd960b192ea
Ger van Diepen <gervandiepen@gmail.com>
1998-10-27 08:01:23 +0000

    Construct from an LCSlicer or a WCRegion added function toLatticeRegion

--- code/images/implement/Images/ImageRegion.h
+++ code/images/implement/Images/ImageRegion.h
@@ -29,15 +29,16 @@
 #define AIPS_IMAGEREGION_H
 
 //# Includes
-#include <aips/aips.h>
+#include <trial/Lattices/LatticeRegion.h>
 
 //# Forward Declarations
 class CoordinateSystem;
 class IPosition;
 class LCRegion;
+class LCSlicer;
+class WCRegion;
 class String;
 class TableRecord;
-class WCRegion;
 
 
 // <summary>
@@ -50,14 +51,15 @@ class WCRegion;
 // </reviewed>
 
 // <prerequisite>
-//   <li> <linkto class=Slicer>Slicer</linkto>
+//   <li> <linkto class=LCSlicer>LCSlicer</linkto>
+//   <li> <linkto class=WCRegion>WCRegion</linkto>
 // </prerequisite>
 
 // <synopsis> 
 // The only purpose of ImageRegion is to have a single object for
-// the various kinds of regions (e.g. LCRegion and WCRegion).
-// So far, it is only used to hold an LCRegion or WCRegion object.
-// In the future it can also be useful to hold other types of regions.
+// the various kinds of regions
+// <linkto class=LCSlicer>LCSlicer</linkto> and
+// <linkto class=WCRegion>WCRegion</linkto>.
 // </synopsis> 
 
 // <example>
@@ -80,11 +82,19 @@ class WCRegion;
 class ImageRegion
 {
 public:
-    // Construct from a region based on lattice coordinates.
-    ImageRegion (const LCRegion&);
+    // Construct from a slicer based on lattice coordinates.
+    ImageRegion (const LCSlicer&);
 
     // Construct from a region based on world coordinates.
     ImageRegion (const WCRegion&);
+
+    // Similar constructors as above, but using a pointer.
+    // It takes over the pointer, so the user should not delete the
+    // object. It is deleted by the ImageRegion destructor.
+    // <group>
+    ImageRegion (LCSlicer*);
+    ImageRegion (WCRegion*);
+    // </group>
 
     // Copy constructor (copy semantics).
     ImageRegion (const ImageRegion& other);
@@ -100,38 +110,68 @@ public:
     Bool operator!=(const ImageRegion& other) const;
     // </group>
 
-    // Test if the underlying region is a WCRegion.
-    Bool isWCRegion() const;
-
-    // Get the region as an LCRegion or WCRegion.
-    // An exception is thrown if the region is not the correct type.
-    // Function <src>isWCRegion()</src> can be used to test the type.
+    // Test if the underlying region is an WCRegion, etc..
     // <group>
-    const LCRegion& asLCRegion() const;
+    Bool isLCSlicer() const;
+    Bool isWCRegion() const;
+    // </group>
+
+    // Get the region as an LCSlicer or WCRegion.
+    // An exception is thrown if the region is not the correct type.
+    // Functions <src>isWCRegion()</src>, etc. can be used to test the type.
+    // <group>
+    const LCSlicer& asLCSlicer() const;
     const WCRegion& asWCRegion() const;
     // </group>
 
-    // Convert to an LCRegion using the given coordinate system.
-    const LCRegion& toLCRegion (const CoordinateSystem& cSys,
-                                const IPosition& shape) const;
+    // Get the dimensionality.
+    uInt ndim() const;
+
+    // Convert to a LatticeRegion using the given coordinate system
+    // (with reference pixel) and shape.
+    // It will also make the region complete (absolute and non-fractional).
+    LatticeRegion toLatticeRegion (const CoordinateSystem& cSys,
+				   const IPosition& shape) const;
+
+    // Convert to an LCRegion using the given coordinate system
+    // (with reference pixel) and shape.
+    // It will also make the region complete (absolute and non-fractional).
+    // An exception is thrown if the region type is a LCSlicer.
+    // The axes argument tells which axes to use from the coordinate
+    // system and shape.
+    LCRegion* toLCRegion (const CoordinateSystem& cSys,
+			  const IPosition& shape) const;
 
     // Convert the (derived) object to a record.
     // The record can be used to make the object persistent.
     TableRecord toRecord (const String& tableName) const;
 
     // Convert correct object from a record.
-    static ImageRegion fromRecord (const TableRecord&,
-				   const String& tableName);
+    static ImageRegion* fromRecord (const TableRecord&,
+				    const String& tableName);
 
 private:
-    LCRegion* itsLC;
-    WCRegion* itsWC;
+    LCSlicer*   itsSlicer;
+    WCRegion*   itsWC;
+    uInt        itsNdim;
 };
 
 
+inline Bool ImageRegion::isLCSlicer() const
+{
+    return ToBool (itsSlicer != 0);
+}
 inline Bool ImageRegion::isWCRegion() const
 {
     return ToBool (itsWC != 0);
+}
+inline Bool ImageRegion::operator!= (const ImageRegion& other) const
+{
+    return ToBool (! ImageRegion::operator== (other));
+}
+inline uInt ImageRegion::ndim() const
+{
+    return itsNdim;
 }
 
 
