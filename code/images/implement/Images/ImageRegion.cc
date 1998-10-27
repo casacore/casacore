e4c6be125ff0ada6a8cec30da3c5ed89d22f52a3
Ger van Diepen <gervandiepen@gmail.com>
1998-10-27 08:05:58 +0000

    Construct from an LCSlicer or a WCRegion

--- code/images/implement/Images/ImageRegion.cc
+++ code/images/implement/Images/ImageRegion.cc
@@ -27,48 +27,70 @@
 
 #include <trial/Images/ImageRegion.h>
 #include <trial/Images/WCRegion.h>
-#include <trial/Lattices/LCRegion.h>
+#include <trial/Lattices/LCSlicer.h>
+#include <trial/Lattices/LCExtension.h>
 #include <trial/Lattices/RegionType.h>
+#include <trial/Coordinates/CoordinateSystem.h>
 #include <aips/Tables/TableRecord.h>
+#include <aips/Arrays/Vector.h>
+#include <aips/Utilities/String.h>
 #include <aips/Utilities/Assert.h>
 #include <aips/Exceptions/Error.h>
-#include <aips/Utilities/String.h>
+
+typedef Vector<Int> imageregion_gppbug1;
+typedef Vector<Double> imageregion_gppbug2;
 
 
-ImageRegion::ImageRegion (const LCRegion& region)
-: itsLC (region.cloneRegion()),
-  itsWC (0)
+ImageRegion::ImageRegion (const LCSlicer& slicer)
+: itsSlicer (new LCSlicer(slicer)),
+  itsWC     (0),
+  itsNdim   (slicer.ndim())
 {}
 
 ImageRegion::ImageRegion (const WCRegion& region)
-: itsLC (0),
-  itsWC (region.cloneRegion())
+: itsSlicer (0),
+  itsWC     (region.cloneRegion()),
+  itsNdim   (region.ndim())
+{}
+
+ImageRegion::ImageRegion (LCSlicer* slicer)
+: itsSlicer (slicer),
+  itsWC     (0),
+  itsNdim   (slicer->ndim())
+{}
+
+ImageRegion::ImageRegion (WCRegion* region)
+: itsSlicer (0),
+  itsWC     (region),
+  itsNdim   (region->ndim())
 {}
 
 ImageRegion::ImageRegion (const ImageRegion& other)
-: itsLC (0),
-  itsWC (0)
+: itsSlicer (0),
+  itsWC     (0)
 {
     operator= (other);
 }
 
 ImageRegion::~ImageRegion()
 {
-    delete itsLC;
+    delete itsSlicer;
     delete itsWC;
 }
 
 ImageRegion& ImageRegion::operator= (const ImageRegion& other)
 {
     if (this != &other) {
-        delete itsLC;
+	delete itsSlicer;
         delete itsWC;
-	itsLC = 0;
+	itsSlicer = other.itsSlicer;
 	itsWC = other.itsWC;
-	if (isWCRegion()) {
+	itsNdim = other.itsNdim;
+	if (itsSlicer != 0) {
+	    itsSlicer = new LCSlicer(*itsSlicer);
+	}
+	if (itsWC != 0) {
 	    itsWC = itsWC->cloneRegion();
-	} else {
-	    itsLC = other.itsLC->cloneRegion();
 	}
     }
     return *this;
@@ -76,29 +98,23 @@ ImageRegion& ImageRegion::operator= (const ImageRegion& other)
 
 Bool ImageRegion::operator== (const ImageRegion& other) const
 {
-   if ( isWCRegion()  && !other.isWCRegion()) return False;
-   if (!isWCRegion()  &&  other.isWCRegion()) return False;
-
-   Bool match;
-   if (isWCRegion()) {
-      match = (*itsWC == other.asWCRegion());
-   } else {
-      match = (*itsLC == other.asLCRegion());
-   }
-
-   return match;
+    if (isWCRegion()   != other.isWCRegion()
+    ||  isLCSlicer()   != other.isLCSlicer()) {
+	return False;
+    }
+    Bool match;
+    if (isLCSlicer()) {
+	match = (*itsSlicer == other.asLCSlicer());
+    } else {
+	match = (*itsWC == other.asWCRegion());
+    }
+    return match;
 }
 
-Bool ImageRegion::operator!= (const ImageRegion& other) const
+const LCSlicer& ImageRegion::asLCSlicer() const
 {
-   if (ImageRegion::operator==(other)) return False;
-   return True;
-}
-
-const LCRegion& ImageRegion::asLCRegion() const
-{
-    AlwaysAssert (! isWCRegion(), AipsError);
-    return *itsLC;
+    AlwaysAssert (isLCSlicer(), AipsError);
+    return *itsSlicer;
 }
 
 const WCRegion& ImageRegion::asWCRegion() const
@@ -107,54 +123,60 @@ const WCRegion& ImageRegion::asWCRegion() const
     return *itsWC;
 }
 
-const LCRegion& ImageRegion::toLCRegion (const CoordinateSystem& cSys,
-                                         const IPosition& shape) const
+LatticeRegion ImageRegion::toLatticeRegion (const CoordinateSystem& cSys,
+					    const IPosition& shape) const
 {
-    if (isWCRegion()) {
-        ImageRegion* This = (ImageRegion*)this;
-        This->itsLC = itsWC->toLCRegion (cSys, shape);
+    if (isLCSlicer()) {
+	return LatticeRegion (itsSlicer->toSlicer (cSys.referencePixel(),
+						   shape),
+			      shape);
     }
-    return *itsLC;
+    // LatticeRegion takes over the LCRegion pointer,
+    // so it does not need to be deleted.
+    // This is the top conversion, so use all axes.
+    return LatticeRegion (toLCRegion (cSys, shape));
+}
+
+LCRegion* ImageRegion::toLCRegion (const CoordinateSystem& cSys,
+				   const IPosition& shape) const
+{
+    // Convert the region to an LCRegion.
+    LCRegion* region = 0;
+    if (isWCRegion()) {
+        region = itsWC->toLCRegion (cSys, shape);
+    } else {
+	throw (AipsError ("ImageRegion::toLCRegion - "
+			  " cannot convert its LCSlicer object to LCRegion"));
+    }
+    return region;
 }
 
 TableRecord ImageRegion::toRecord (const String& tableName) const
 {
     TableRecord record;
     if (isWCRegion()) {
-        return itsWC->toRecord(tableName);
-    } else {
-        return itsLC->toRecord(tableName);
+        return itsWC->toRecord (tableName);
     }
+    return itsSlicer->toRecord (tableName);
 }
 
-ImageRegion ImageRegion::fromRecord (const TableRecord& record,
-				     const String& tableName)
+ImageRegion* ImageRegion::fromRecord (const TableRecord& record,
+				      const String& tableName)
 {
-
-// See if this is a valid LC or WC region record
-
-
-   if (!record.isDefined("isRegion")) {
-      throw (AipsError ("ImageRegion::fromRecord - record does not define a region"));
-   }
-  
-// Convert to correct region type
-
-   Int regionType = record.asInt("isRegion");       
-   ImageRegion* region = 0;
-   if (regionType == Int(RegionType::WC)) {
-      WCRegion* ptr = WCRegion::fromRecord (record, tableName);
-      region = new ImageRegion(*ptr);
-      delete ptr;
-   } else if (regionType == Int(RegionType::LC)) {
-       LCRegion* ptr = LCRegion::fromRecord (record, tableName);
-       region = new ImageRegion(*ptr);
-       delete ptr;
-   } else {
-      throw (AipsError ("ImageRegion::fromRecord - record is neither an LC nor WC region"));
-   }
-
-   ImageRegion tmp(*region);
-   delete region;
-   return tmp;
+    // See if this is a region record.
+    if (! record.isDefined ("isRegion")) {
+	throw (AipsError ("ImageRegion::fromRecord - "
+			  "record does not define a region"));
+    }
+    // Convert to correct region object.
+    // Note that in the following the ImageRegion constructors take
+    // over the pointer returned by fromRecord.
+    Int regionType = record.asInt ("isRegion");       
+    if (regionType == RegionType::WC) {
+	return new ImageRegion (WCRegion::fromRecord (record, tableName));
+    } else if (regionType != RegionType::Slicer) {
+	throw (AipsError ("ImageRegion::fromRecord - "
+			  "record has an unknown region type"));
+    }
+    return new ImageRegion (LCSlicer::fromRecord (record, tableName));
 }
