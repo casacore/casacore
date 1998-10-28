a909255c1f45d926e353e43ca9bd92bc0ff982b2
Wes Young <wyoung@nrao.edu>
1998-10-28 16:22:32 +0000

    Copyright change.

--- code/casa/implement/OS/test/tPath.cc
+++ code/casa/implement/OS/test/tPath.cc
@@ -1,5 +1,5 @@
 //# tPath.cc: Test program for class Path
-//# Copyright (C) 1993,1994,1995,1996
+//# Copyright (C) 1993,1994,1995,1996,1998
 //# Associated Universities, Inc. Washington DC, USA.
 //# 
 //# This library is free software; you can redistribute it and/or modify it
