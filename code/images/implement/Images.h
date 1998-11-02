076ebe946e33de25449ac0c9a7c47e226b7085ad
Ger van Diepen <gervandiepen@gmail.com>
1998-11-02 08:12:24 +0000

    Defined it as a module

---
+++ code/images/implement/Images.h
@@ -0,0 +1,91 @@
+//# Images.h: N-dimensional astronomical images
+//# Copyright (C) 1998
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//# $Id$
+
+#if !defined (AIPS_IMAGES_H)
+#define AIPS_IMAGES_H
+
+
+#include <aips/Lattices/IPosition.h>
+#include <aips/Lattices/Slice.h>
+#include <aips/Lattices/Slicer.h>
+
+// #include <aips/Lattices/ArrayLattice.h>
+// #include <aips/Lattices/PagedArray.h>
+// #include <aips/Lattices/TempLattice.h>
+// #include <aips/Lattices/TiledShape.h>
+// #include <aips/Lattices/LatticeIterator.h>
+// #include <aips/Lattices/LatticeStepper.h>
+// #include <aips/Lattices/TileStepper.h>
+// #include <aips/Lattices/TiledLineStepper.h>
+
+// #include <trial/Lattices/SubLattice.h>
+// #include <trial/Lattices/LatticeExpr.h>
+
+// #include <trial/Lattices/LatticeRegion.h>
+// #include <trial/Lattices/LCSlicer.h>
+// #include <trial/Lattices/LCBox.h>
+// #include <trial/Lattices/LCEllipsoid.h>
+// #include <trial/Lattices/LCPolygon.h>
+// #include <trial/Lattices/LCUnion.h>
+// #include <trial/Lattices/LCIntersection.h>
+// #include <trial/Lattices/LCDifference.h>
+// #include <trial/Lattices/LCConcatenation.h>
+// #include <trial/Lattices/LCComplement.h>
+// #include <trial/Lattices/LCExtension.h>
+
+
+// <module>
+
+// <summary>
+// N-dimensional astronomical images
+// </summary>
+
+// <prerequisite>
+//   <li> Programmers of new Image classes should understand Inheritance
+//   <li> Users of the Image classes should understand Polymorphism.
+//   <li> class <linkto name=Lattice>Lattice</linkto>
+//   <li> class <linkto name=CoordinateSystem>CoordinateSystem</linkto>
+// </prerequisite>
+
+// <reviewed reviewer="" date="" demos="">
+// </reviewed>
+
+// <etymology>
+// </etymology>
+
+// <synopsis>
+// </synopsis>
+
+// <motivation>
+// </motivation>
+
+// <todo asof="1998/11/02">
+// </todo>
+
+// </module>
+
+#endif
