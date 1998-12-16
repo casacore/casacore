de37302e6543d88e5208414acdda845fb0494125
Neil Killeen <nkilleen@unimelb.edu.au>
1998-12-16 03:16:24 +0000

    Copyright notice update only

--- code/coordinates/implement/Coordinates/SpectralCoordinate.cc
+++ code/coordinates/implement/Coordinates/SpectralCoordinate.cc
@@ -1,5 +1,5 @@
 //# <ClassFileName.h>: this defines <ClassName>, which ...
-//# Copyright (C) 1997
+//# Copyright (C) 1997,1998
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
