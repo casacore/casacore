1e8ce8486256a816dd7328b6729c8568d6052c0d
Neil Killeen <nkilleen@unimelb.edu.au>
1999-01-05 04:39:25 +0000

    Add Id line

--- code/lattices/implement/LatticeMath/Fit2D.cc
+++ code/lattices/implement/LatticeMath/Fit2D.cc
@@ -1,5 +1,5 @@
 //# Fit2D.cc: Class to fit 2D objects to a Lattice or Array
-//# Copyright (C) 1997,1998
+//# Copyright (C) 1997,1998,1999
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
@@ -23,6 +23,7 @@
 //#                        520 Edgemont Road
 //#                        Charlottesville, VA 22903-2475 USA
 //#
+//#   $Id$
 
 #include <trial/Fitting/Fit2D.h>
 
--- code/lattices/implement/LatticeMath/Fit2D.h
+++ code/lattices/implement/LatticeMath/Fit2D.h
@@ -1,5 +1,5 @@
 //# Fit2D.h: Class to fit 2-D objects to Lattices or Arrays
-//# Copyright (C) 1997,1998
+//# Copyright (C) 1997,1998,1999
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
@@ -23,6 +23,7 @@
 //#                        520 Edgemont Road
 //#                        Charlottesville, VA 22903-2475 USA
 //#
+//#   $Id$
 
 #if !defined(AIPS_FIT2D_H)
 #define AIPS_FIT2D_H
