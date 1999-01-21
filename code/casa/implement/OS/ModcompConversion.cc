0f999d8c8fb5ef4590a1565b04f5aeae2b1d53d6
Ralph Marson <rmarson@nrao.edu>
1999-01-21 00:45:12 +0000

    Added double precision conversions

--- code/casa/implement/OS/ModcompConversion.cc
+++ code/casa/implement/OS/ModcompConversion.cc
@@ -39,7 +39,7 @@ void ModcompConversion::toLocal (Float* to, const void* from, uInt nr)
 
   const uChar* data = (const uChar*) from;
   for (const Float* const last = to + nr; to < last; to++) {
-    // Copy the data to the union
+    // Copy the data to the temporary buffer
     for (uShort i = 0; i < SIZE_MODCOMP_FLOAT; i++) {
       asByte[i] = *data;
       data++;
@@ -199,44 +199,108 @@ void ModcompConversion::toLocal (Float* to, const void* from, uInt nr)
 // special numbers like NaN or Infinity. The Modcomp is big-endian (like Sun's)
 void ModcompConversion::toLocal (Double* to, const void* from, uInt nr)
 {
-}
-// // Modcomp D-float has format SEEEEEEE EFFFFFFF ...
-// // IEEE double has format SEEEEEEE EEEEFFFF ...
-// // The Modcomp exponent is 2 higher.
-// // The Modcomp byte order is b1b0b3b2b5b4b7b6.
-// void ModcompConversion::toLocal (double* to, const void* from,
-// 			     uInt nr)
-// {
-//     assert (sizeof(uInt) == 4);
-//     assert (sizeof(double) == 8);
-//     const char* data = (const char*)from;
-//     double* last = to + nr;
-//     uInt value, rest;
-//     while (to < last) {
-// 	moveFloat (&value, data);
-// 	moveFloat (&rest, data+4);
-// 	data += 8;
-// 	uInt exponent = (value & 0x7f800000) >> 23;
-// 	if (exponent == 0) {
-// 	    value = 0;
-// 	    rest  = 0;
-// 	}else{
-// 	    exponent = (exponent + (1022-128)) << 20;
-// 	    rest  = (value << 29) | (rest >> 3);
-// 	    value = (value & 0x80000000) | exponent
-// 	            | ((value >> 3) & 0x000fffff);
-// 	}
-// #if defined(AIPS_LITTLE_ENDIAN)
-// 	((uInt*)to)[0] = rest;
-// 	((uInt*)to)[1] = value;
-// #else
-// 	((uInt*)to)[0] = value;
-// 	((uInt*)to)[1] = rest;
-// #endif
-// 	to++;
-//     }
-// }
+  DebugAssert(sizeof(Short) >= 2, AipsError);
+  uChar asByte[SIZE_MODCOMP_DOUBLE];
 
+  const uChar* data = (const uChar*) from;
+  for (const Double* const last = to + nr; to < last; to++) {
+    // Copy the data to temporary buffer
+    for (uShort i = 0; i < SIZE_MODCOMP_DOUBLE; i++) {
+      asByte[i] = *data;
+      data++;
+    }
+    // If the number is negative then its positive value is the twos complement
+    const Bool isNegative = ((asByte[0] & 0x80) > 0) ? True : False;
+    if (isNegative) { // This code takes the twos complement
+      uShort i = 0;
+      while (i < SIZE_MODCOMP_DOUBLE) {
+ 	asByte[i] = ~asByte[i];
+ 	i++;
+      }
+      i--;
+      asByte[i]++;
+      while (asByte[i] == 0 && i > 0) {
+ 	i--;
+ 	asByte[i]++;
+      }
+    }
+
+    Bool isZero = (asByte[1] & 0x3f) == 0x00;
+    for (uShort i = 2; i < SIZE_MODCOMP_DOUBLE; i++) {
+      isZero = (asByte[i] == 0x00) && isZero;
+    }
+
+    if (isZero) { // Early exit if the number is zero. 
+      // I am told this is common in the data so it should speed things up.
+      for (uShort k = 1; k < SIZE_MODCOMP_DOUBLE; k++) {
+	asByte[k] = 0x00;
+      }
+      if (isNegative) { // Return a signed zero
+	asByte[0] = 0x80;
+      } else {
+	asByte[0] = 0x00;
+      }
+    } else { // Number is not zero
+      Short exponent = ((asByte[0] & 0x7f) << 2) | ((asByte[1] & 0xc0) >> 6);
+      while ((asByte[1] & 0x20) == 0) { // See if the number is unnormalised
+ 	{ // If so try to normalise it. 
+ 	  // This code does a byte by byte left shift by one bit (painful).
+ 	  uShort i = SIZE_MODCOMP_DOUBLE-1; 
+ 	  Bool msbIsSet = (asByte[i] & (0x80)) > 0 ? True : False;
+ 	  asByte[i] <<= 1;
+ 	  i--;
+ 	  while (i > 1) {
+ 	    Bool prevMsbIsSet = msbIsSet;
+ 	    msbIsSet = (asByte[i] & (0x80)) > 0 ? True : False;
+ 	    asByte[i] <<= 1;
+ 	    if (prevMsbIsSet) asByte[i] |= 0x01;
+ 	    i--;
+ 	  }
+ 	  asByte[1] = ((asByte[1] & 0x3f) << 1) | asByte[1] & 0xc0;
+ 	  if (msbIsSet) asByte[1] |= 0x01;
+ 	}
+ 	exponent--; // Because the exponent can go negative it must be signed
+      }
+      // In IEEE double precision the exponent is 11 bits, whereas the
+      // Modcomp's is only 9 bits. So we never have to worry about:
+      // * Numbers that are too big (overflow). Set to infinity.
+      // * Numbers that are too small (underflow). Set to zero or generate
+      //   a subnormal number.
+      // Hence, unlike the single precision code, every Modcomp double
+      // precision number can be represented as a 'normal' IEEE double
+      // precision number.
+
+      // The mantissa of the Modcomp double precision number is 54 bits,
+      // whereas it is 52 bits in the IEEE format. But only one bit of
+      // precision is lost in the conversion because the IEEE format has an
+      // implicit one at the beginning of the mantissa whereas the Modcomp has
+      // an explicit one (that wastes a bit).
+
+      // The next few lines assumes mantissa is normalised (as is done above)
+      exponent += 766;
+      { // This code does a byte by byte right shift by 1-bit (painful)
+	for (uShort i = SIZE_MODCOMP_DOUBLE-1; i > 1; i--) {
+	  asByte[i] >>= 1;
+	  asByte[i] |= (asByte[i-1] & 0x01) << 7;
+	}
+	asByte[1] >>= 1;
+      }
+      asByte[1] &= 0x0f;
+      asByte[1] |= uChar(exponent & 0x000f) << 4;
+      asByte[0] = uChar(exponent >> 4);
+      if (isNegative) {
+	asByte[0] |= 0x80;
+      } else {
+	asByte[0] &= 0x7f;
+      }
+      // The IEEE format effectively has two more bits in the mantissa than
+      // the Modcomp format. The least significant bits are always set to
+      // zero. Numerically it would be better to set them randomly to zero or
+      // one but I do not think the performance degradation warrents this.
+    }
+    CanonicalConversion::toLocal(*to, asByte);
+  }
+}
 
 // void ModcompConversion::fromLocal (void* to, const float* from,
 // 			       uInt nr)
