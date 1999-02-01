52f0191eb886611251feceea1cf4f9f6f9f93a7e
Kate Weatherall <kweather@nrao.edu>
1999-02-01 18:39:18 +0000

    nothing to change in this file

--- code/casa/implement/Arrays/LogiVector.h
+++ code/casa/implement/Arrays/LogiVector.h
@@ -1,5 +1,5 @@
 //# LogiVector.h:  Logical valued Vectors.
-//# Copyright (C) 1994,1995
+//# Copyright (C) 1994,1995,1999
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
