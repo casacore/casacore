216371250dd21173e8485dfa69ba52ca3a97ee06
Kate Weatherall <kweather@nrao.edu>
1999-02-01 18:16:43 +0000

    nothing to change here

--- code/casa/implement/Arrays/ArrayLogical.h
+++ code/casa/implement/Arrays/ArrayLogical.h
@@ -1,5 +1,5 @@
 //# ArrayLogical.h: Element by element logical operations on arrays.
-//# Copyright (C) 1993,1994,1995
+//# Copyright (C) 1993,1994,1995,1999
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
