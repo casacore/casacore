9f193b410f13341157ed06d20a98fc85a6709bdc
Neil Killeen <nkilleen@unimelb.edu.au>
1999-02-01 08:42:02 +0000

    use privaet instead of static temporary

--- code/coordinates/implement/Coordinates/Direction2Coordinate.cc
+++ code/coordinates/implement/Coordinates/Direction2Coordinate.cc
@@ -1,5 +1,5 @@
 //# <ClassFileName.h>: this defines <ClassName>, which ...
-//# Copyright (C) 1997
+//# Copyright (C) 1997,1999
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
@@ -31,18 +31,16 @@
 // A different file so that apps which don't need measures don't link them all
 // in (measures bring in tables and lots of other stuff)
 
-    static Vector<Double> xy(2);
 Bool DirectionCoordinate::toWorld(MDirection &world, 
 				  const Vector<Double> &pixel) const
 {
-    Bool ok = toWorld(xy, pixel);
+    Bool ok = toWorld(world_tmp_p, pixel);
 
-    if (ok) {
-	// We could cache much of this for efficiency
-	Quantity longi(xy(0), units_p(0));
-	Quantity lati(xy(1), units_p(1));
-	world = MDirection(longi, lati, type_p);
-    }
+// We could cache much of this for efficiency
+
+    Quantity longi(world_tmp_p(0), units_p(0));
+    Quantity lati(world_tmp_p(1), units_p(1));
+    world = MDirection(longi, lati, type_p);
 
     return ok;
 }
