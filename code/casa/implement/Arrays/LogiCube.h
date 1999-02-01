0a26366b4bdb28af6df4a1ce48877d19b3d2ebd9
Kate Weatherall <kweather@nrao.edu>
1999-02-01 18:37:57 +0000

    nothing to change here

--- code/casa/implement/Arrays/LogiCube.h
+++ code/casa/implement/Arrays/LogiCube.h
@@ -1,5 +1,5 @@
 //# LogiCube.h:  Logical valued Cubes.
-//# Copyright (C) 1994,1995
+//# Copyright (C) 1994,1995,1999
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
--- code/casa/implement/Arrays/LogiMatrix.h
+++ code/casa/implement/Arrays/LogiMatrix.h
@@ -1,5 +1,5 @@
 //# LogiMatrix.h:  Logical valued Matrices.
-//# Copyright (C) 1994,1995
+//# Copyright (C) 1994,1995,1999
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
