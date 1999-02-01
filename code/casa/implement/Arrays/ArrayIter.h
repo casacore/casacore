4256085c2520cfc54d3eb13784f889fd522fa5b8
Kate Weatherall <kweather@nrao.edu>
1999-02-01 18:12:36 +0000

    nothing to change in this file (i.e., object nor method appears)

--- code/casa/implement/Arrays/ArrayIter.h
+++ code/casa/implement/Arrays/ArrayIter.h
@@ -1,5 +1,5 @@
 //# ArrayIter.h: Iterate an Array cursor through another Array.
-//# Copyright (C) 1993,1994,1995,1996
+//# Copyright (C) 1993,1994,1995,1996,1999
 //# Associated Universities, Inc. Washington DC, USA.
 //# 
 //# This library is free software; you can redistribute it and/or modify it
