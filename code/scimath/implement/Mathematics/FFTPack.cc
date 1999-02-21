0194cada089303cc8539cf528a100287b05f200d
Ralph Marson <rmarson@nrao.edu>
1999-02-21 18:28:38 +0000

    Changed all the functions from global to static ones and put them in the FFTPack class

--- code/scimath/implement/Mathematics/FFTPack.cc
+++ code/scimath/implement/Mathematics/FFTPack.cc
@@ -1,5 +1,5 @@
-//# extern_fft.cc: C++ wrapper functions for FORTRAN FFT code
-//# Copyright (C) 1993,1994,1995,1997
+//# FFTPack.cc: C++ wrapper functions for Fortran FFTPACK code
+//# Copyright (C) 1993,1994,1995,1997,1999
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
@@ -25,204 +25,214 @@
 //#
 //# $Id$
 
-#include <aips/Mathematics/extern_fft.h>
+#include <aips/Mathematics/FFTPack.h>
+#include <aips/Mathematics/Complex.h>
+#include <aips/Utilities/Assert.h>
+#include <aips/Exceptions/Error.h>
 
 extern "C" {
-  void cffti_(int *, float *);
-  void dcffti_(int *, double *);
-  void cfftf_(int *, float *, float *);
-  void dcfftf_(int *, double *, double *);
-  void cfftb_(int *, float *, float *);
-  void dcfftb_(int *, double *, double *);
-}
-
-extern "C" {
-  void rfftf_(int *, float *, float *);
-  void rfftb_(int *, float *, float *);
-  void rffti_(int *, float *);
-  void drfftf_(int *, double *, double *);
-  void drfftb_(int *, double *, double *);
-  void drffti_(int *, double *);
+  void cffti_(int*, float*);
+  void dcffti_(int*, double*);
+  void cfftf_(int*, float*, float*);
+  void dcfftf_(int*, double*, double*);
+  void cfftb_(int*, float*, float*);
+  void dcfftb_(int*, double*, double*);
 }
 
 extern "C" {
-  void ezffti_(int *, float *);
-  void ezfftf_(int *, float *, float *, float *, float *, float *);
-  void ezfftb_(int * n, float *, float *, float *, float *, float *);
+  void rfftf_(int*, float*, float*);
+  void rfftb_(int*, float*, float*);
+  void rffti_(int*, float*);
+  void drfftf_(int*, double*, double*);
+  void drfftb_(int*, double*, double*);
+  void drffti_(int*, double*);
 }
 
 extern "C" {
-  void sinti_(int *, float *);
-  void dsinti_(int *, double *);
-  void sint_(int *, float *, float *);
-  void dsint_(int *, double *, double *);
+  void ezffti_(int*, float*);
+  void ezfftf_(int*, float*, float*, float*, float*, float*);
+  void ezfftb_(int* n, float*, float*, float*, float*, float*);
 }
 
 extern "C" {
-  void costi_(int *, float *);
-  void dcosti_(int *, double *);
-  void cost_(int *, float *, float *);
-  void dcost_(int *, double *, double *);
+  void sinti_(int*, float*);
+  void dsinti_(int*, double*);
+  void sint_(int*, float*, float*);
+  void dsint_(int*, double*, double*);
 }
 
 extern "C" {
-  void sinqi_(int *, float *);
-  void dsinqi_(int *, double *);
-  void sinqf_(int *, float *, float *);
-  void dsinqf_(int *, double *, double *);
-  void sinqb_(int *, float *, float *);
-  void dsinqb_(int *, double *, double *);
+  void costi_(int*, float*);
+  void dcosti_(int*, double*);
+  void cost_(int*, float*, float*);
+  void dcost_(int*, double*, double*);
 }
 
 extern "C" {
-  void cosqi_(int *, float *);
-  void dcosqi_(int *, double *);
-  void cosqf_(int *, float *, float *);
-  void dcosqf_(int *, double *, double *);
-  void cosqb_(int *, float *, float *);
-  void dcosqb_(int *, double *, double *);
+  void sinqi_(int*, float*);
+  void dsinqi_(int*, double*);
+  void sinqf_(int*, float*, float*);
+  void dsinqf_(int*, double*, double*);
+  void sinqb_(int*, float*, float*);
+  void dsinqb_(int*, double*, double*);
 }
 
-void cffti(Int n, Float * work) {
-  cffti_((int *) &n, (float *) work);
+extern "C" {
+  void cosqi_(int*, float*);
+  void dcosqi_(int*, double*);
+  void cosqf_(int*, float*, float*);
+  void dcosqf_(int*, double*, double*);
+  void cosqb_(int*, float*, float*);
+  void dcosqb_(int*, double*, double*);
 }
 
-void cffti(Int n, Double * work) {
-  dcffti_((int *) &n, (double *) work);
+void FFTPack::cffti(Int n, Float* work) {
+  cffti_((int*) &n, (float*) work);
 }
 
-void  cfftf(Int n, Complex * rdata, Float * work) {
-  cfftf_((int *) &n, (float *) rdata, (float *) work);
+void FFTPack::cffti(Int n, Double* work) {
+  dcffti_((int*) &n, (double*) work);
 }
 
-void cfftf(Int n, DComplex * rdata, Double * work) {
-  dcfftf_((int *) &n, (double *) rdata, (double *) work);
+void FFTPack::cfftf(Int n, Complex* rdata, Float* work) {
+  DebugAssert(sizeof(Complex) == 2*sizeof(float), AipsError);
+  cfftf_((int*) &n, (float*) rdata, (float*) work);
 }
 
-void cfftb(Int n, Complex * rdata, Float * work) {
-  cfftb_((int *) &n, (float *) rdata, (float *) work);
+void FFTPack::cfftf(Int n, DComplex* rdata, Double* work) {
+  DebugAssert(sizeof(DComplex) == 2*sizeof(double), AipsError);
+  dcfftf_((int*) &n, (double*) rdata, (double*) work);
 }
 
-void cfftb(Int n, DComplex * rdata, Double * work) {
-  dcfftb_((int *) &n, (double *) rdata, (double *) work);
+void FFTPack::cfftb(Int n, Complex* rdata, Float* work) {
+  DebugAssert(sizeof(Complex) == 2*sizeof(float), AipsError);
+  cfftb_((int*) &n, (float*) rdata, (float*) work);
 }
 
-void rffti(Int n, Float * work) {
-  rffti_((int *) &n, (float *) work);
+void FFTPack::cfftb(Int n, DComplex* rdata, Double* work) {
+  DebugAssert(sizeof(DComplex) == 2*sizeof(double), AipsError);
+  dcfftb_((int*) &n, (double*) rdata, (double*) work);
 }
 
-void rffti(Int n, Double * work) {
-  drffti_((int *) &n, (double *) work);
+void FFTPack::rffti(Int n, Float* work) {
+  rffti_((int*) &n, (float*) work);
 }
 
-void rfftf(Int n, Float * rdata, Float * work) {
-  rfftf_((int *) &n, (float *) rdata, (float *) work);
+void FFTPack::rffti(Int n, Double* work) {
+  drffti_((int*) &n, (double*) work);
 }
 
-void rfftf(Int n, Double * rdata, Double * work) {
-  drfftf_((int *) &n, (double *) rdata, (double *) work);
+void FFTPack::rfftf(Int n, Float* rdata, Float* work) {
+  rfftf_((int*) &n, (float*) rdata, (float*) work);
 }
 
-void rfftb(Int n, Float * rdata, Float * work) {
-  rfftb_((int *) &n, (float *) rdata, (float *) work);
+void FFTPack::rfftf(Int n, Double* rdata, Double* work) {
+  drfftf_((int*) &n, (double*) rdata, (double*) work);
 }
 
-void rfftb(Int n, Double * rdata, Double * work) {
-  drfftb_((int *) &n, (double *) rdata, (double *) work);
+void FFTPack::rfftb(Int n, Float* rdata, Float* work) {
+  rfftb_((int*) &n, (float*) rdata, (float*) work);
 }
 
-void ezffti(Int n, Float * wsave) {
-  ezffti_((int *) &n, (float *) wsave);
+void FFTPack::rfftb(Int n, Double* rdata, Double* work) {
+  drfftb_((int*) &n, (double*) rdata, (double*) work);
 }
 
-void ezfftf(Int n, Float * r, Float * azero, Float * a, Float * b, 
-	    Float * wsave) {
-  ezfftf_((int *) &n, (float *) r, (float *) azero, (float *) a, (float *) b,
-	  (float *) wsave);
+void FFTPack::ezffti(Int n, Float* wsave) {
+  ezffti_((int*) &n, (float*) wsave);
 }
 
-void ezfftb(Int n, Float * r, Float * azero, Float * a, Float * b, 
-	    Float * wsave) {
-  ezfftb_((int *) &n, (float *) r, (float *) azero, (float *) a, (float *) b,
-	  (float *) wsave);
+void FFTPack::ezfftf(Int n, Float* r, Float* azero, Float* a, Float* b, 
+	    Float* wsave) {
+  ezfftf_((int*) &n, (float*) r, (float*) azero, (float*) a, (float*) b,
+	  (float*) wsave);
 }
 
-void sinti(Int n, Float * wsave) {
-  sinti_((int *) &n, (float *) wsave);
+void FFTPack::ezfftb(Int n, Float* r, Float* azero, Float* a, Float* b, 
+	    Float* wsave) {
+  ezfftb_((int*) &n, (float*) r, (float*) azero, (float*) a, (float*) b,
+	  (float*) wsave);
 }
 
-void sinti(Int n, Double * wsave) {
-  dsinti_((int *) &n, (double *) wsave);
+void FFTPack::sinti(Int n, Float* wsave) {
+  sinti_((int*) &n, (float*) wsave);
 }
 
-void sint(Int n, Float * x, Float * wsave) {
-  sint_((int *) &n, (float *) x, (float *) wsave);
+void FFTPack::sinti(Int n, Double* wsave) {
+  dsinti_((int*) &n, (double*) wsave);
 }
 
-void sint(Int n, Double * x, Double * wsave) {
-   dsint_((int *) &n, (double *) x, (double *) wsave);
+void FFTPack::sint(Int n, Float* x, Float* wsave) {
+  sint_((int*) &n, (float*) x, (float*) wsave);
 }
 
-void costi(Int n, Float * wsave) {
-  costi_((int *) &n, (float *) wsave);
+void FFTPack::sint(Int n, Double* x, Double* wsave) {
+   dsint_((int*) &n, (double*) x, (double*) wsave);
 }
 
-void costi(Int n, Double * wsave) {
-  dcosti_((int *) &n, (double *) wsave);
+void FFTPack::costi(Int n, Float* wsave) {
+  costi_((int*) &n, (float*) wsave);
 }
 
-void cost(Int n, Float * x, Float * wsave) {
-  cost_((int *) &n, (float *) x, (float *) wsave);
+void FFTPack::costi(Int n, Double* wsave) {
+  dcosti_((int*) &n, (double*) wsave);
 }
 
-void cost(Int n, Double * x, Double * wsave) {
-  dcost_((int *) &n, (double *) x, (double *) wsave);
+void FFTPack::cost(Int n, Float* x, Float* wsave) {
+  cost_((int*) &n, (float*) x, (float*) wsave);
 }
 
-void sinqi(Int n, Float * wsave) {
-  sinqi_((int *) &n, (float *) wsave);
+void FFTPack::cost(Int n, Double* x, Double* wsave) {
+  dcost_((int*) &n, (double*) x, (double*) wsave);
 }
 
-void sinqi(Int n, Double * wsave) {
-  dsinqi_((int *) &n, (double *) wsave);
+void FFTPack::sinqi(Int n, Float* wsave) {
+  sinqi_((int*) &n, (float*) wsave);
 }
 
-void sinqf(Int n, Float * x, Float * wsave) {
-  sinqf_((int *) &n, (float *) x, (float *) wsave);
+void FFTPack::sinqi(Int n, Double* wsave) {
+  dsinqi_((int*) &n, (double*) wsave);
 }
 
-void sinqf(Int n, Double * x, Double * wsave) {
-  dsinqf_((int *) &n, (double *) x, (double *) wsave);
+void FFTPack::sinqf(Int n, Float* x, Float* wsave) {
+  sinqf_((int*) &n, (float*) x, (float*) wsave);
 }
 
-void sinqb(Int n, Float * x, Float * wsave) {
-  sinqb_((int *) &n, (float *) x, (float *) wsave);
+void FFTPack::sinqf(Int n, Double* x, Double* wsave) {
+  dsinqf_((int*) &n, (double*) x, (double*) wsave);
 }
 
-void sinqb(Int n, Double * x, Double * wsave) {
-  dsinqb_((int *) &n, (double *) x, (double *) wsave);
+void FFTPack::sinqb(Int n, Float* x, Float* wsave) {
+  sinqb_((int*) &n, (float*) x, (float*) wsave);
 }
 
-void cosqi(Int n, Float * wsave) {
-  cosqi_((int *) &n, (float *) wsave);
+void FFTPack::sinqb(Int n, Double* x, Double* wsave) {
+  dsinqb_((int*) &n, (double*) x, (double*) wsave);
 }
 
-void cosqi(Int n, Double * wsave) {
-  dcosqi_((int *) &n, (double *) wsave);
+void FFTPack::cosqi(Int n, Float* wsave) {
+  cosqi_((int*) &n, (float*) wsave);
 }
 
-void cosqf(Int n, Float * x, Float * wsave) {
-  cosqf_((int *) &n, (float *) x, (float *) wsave);
+void FFTPack::cosqi(Int n, Double* wsave) {
+  dcosqi_((int*) &n, (double*) wsave);
 }
 
-void cosqf(Int n, Double * x, Double * wsave) {
-  dcosqf_((int *) &n, (double *) x, (double *) wsave);
+void FFTPack::cosqf(Int n, Float* x, Float* wsave) {
+  cosqf_((int*) &n, (float*) x, (float*) wsave);
 }
 
-void cosqb(Int n, Float * x, Float * wsave) {
-  cosqb_((int *) &n, (float *) x, (float *) wsave);
+void FFTPack::cosqf(Int n, Double* x, Double* wsave) {
+  dcosqf_((int*) &n, (double*) x, (double*) wsave);
 }
 
-void cosqb(Int n, Double * x, Double * wsave) {
-  dcosqb_((int *) &n, (double *) x, (double *) wsave);
+void FFTPack::cosqb(Int n, Float* x, Float* wsave) {
+  cosqb_((int*) &n, (float*) x, (float*) wsave);
 }
+
+void FFTPack::cosqb(Int n, Double* x, Double* wsave) {
+  dcosqb_((int*) &n, (double*) x, (double*) wsave);
+}
+// Local Variables: 
+// compile-command: "gmake OPTLIB=1 FFTPack"
+// End: 
