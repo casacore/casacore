4fdadf6bc9ba0e814b706abaf6e0c9a93a27c9ba
Ger van Diepen <gervandiepen@gmail.com>
1999-03-17 14:49:37 +0000

    *** empty log message ***

---
+++ code/casa/implement/OS/DOos.cc
@@ -0,0 +1,227 @@
+//# DOos.cc: Functions used to implement the DO functionality
+//# Copyright (C) 1999
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//#
+//# $Id$
+
+
+//# Includes
+#include <trial/OS/DOos.h>
+#include <aips/OS/Path.h>
+#include <aips/OS/File.h>
+#include <aips/OS/RegularFile.h>
+#include <aips/OS/Directory.h>
+#include <aips/OS/DirectoryIterator.h>
+#include <aips/OS/SymLink.h>
+#include <aips/Arrays/Vector.h>
+#include <aips/Utilities/String.h>
+#include <aips/Exceptions/Error.h>
+
+
+Vector<String> DOos::fileNames (const String& directoryName,
+				const String& fileNamePattern,
+				const String& fileTypes,
+				Bool all, Bool follow)
+{
+  // Determine if and how to select on file type.
+  Bool takeRegular = ToBool (fileTypes.contains ('r'));
+  Bool takeDirectory = ToBool (fileTypes.contains ('d'));
+  Bool takeSymLink = ToBool (fileTypes.contains ('s'));
+  Bool takeReadable = ToBool (fileTypes.contains ('R'));
+  Bool takeWritable = ToBool (fileTypes.contains ('W'));
+  Bool takeExecutable = ToBool (fileTypes.contains ('X'));
+  Bool checkType = ToBool (takeRegular || takeDirectory || takeSymLink);
+  Bool checkAcc = ToBool (takeReadable || takeWritable || takeExecutable);
+  Bool check = ToBool (checkType || checkAcc);
+  // Set up the iterator. Default pattern is all.
+  Vector<String> result;
+  Directory dir (directoryName);
+  uInt n = 0;
+  DirectoryIterator iter (dir);
+  if (! fileNamePattern.empty()) {
+    iter = DirectoryIterator (dir, Regex::fromPattern (fileNamePattern));
+  }
+  // Iterate through the directory and add matching name to result.
+  // Skip names starting with . if all is False.
+  for (; !iter.pastEnd(); iter++) {
+    String name = iter.name();
+    if (name[0] != '.'  ||  all) {
+      if (check) {
+	File file(name);
+	if (checkType) {
+	  if (!(   (takeRegular && file.isRegular (follow))
+		|| (takeDirectory && file.isDirectory (follow))
+		|| (takeSymLink && file.isSymLink()))) {
+	    continue;
+	  }
+	}
+	if (checkAcc) {
+	  if (!(    (takeReadable && file.isReadable())
+		 || (takeWritable && file.isWritable())
+		 || (takeExecutable && file.isExecutable()))) {
+	    continue;
+	  }
+	}
+      }
+      if (n >= result.nelements()) {
+	result.resize (result.nelements() + 100, True);
+      }
+      result(n++) = name;
+    }
+  }
+  result.resize (n, True);
+  return result;
+}
+
+void DOos::makeDirectory (const String& directoryName)
+{
+  File file(directoryName);
+  if (file.exists()) {
+    throw (AipsError ("DOos::makeDirectory - a file " + directoryName +
+		      " already exists"));
+  }
+  Directory dir(file);
+  dir.create (False);
+}
+
+String DOos::fullName (const String& fileName)
+{
+  return Path(fileName).absoluteName();
+}
+
+String DOos::dirName (const String& fileName)
+{
+  return Path(fullName(fileName)).dirName();
+}
+
+String DOos::baseName (const String& fileName)
+{
+  return Path(fullName(fileName)).baseName();
+}
+
+Double DOos::totalSize (const String& fileName, Bool follow)
+{
+  File file(fileName);
+  if (!file.exists()) {
+    throw (AipsError ("DOos::totalSize - file " + fileName +
+		      " does not exist"));
+  }
+  if (! file.isDirectory (follow)) {
+    if (file.isRegular (follow)) {
+      return RegularFile(file).size();
+    }
+    return 0;
+  }
+  Double size = 0;
+  DirectoryIterator iter (file);
+  // Iterate through the directory.
+  for (; !iter.pastEnd(); iter++) {
+    size += totalSize(fileName + '/' + iter.name(), follow);
+  }
+  return size;
+}
+
+Double DOos::freeSpace (const String& fileName, Bool follow)
+{
+  File file(fileName);
+  if (file.isDirectory (follow)) {
+    return Directory(file).freeSpace();
+  }
+  if (file.isRegular (follow)  ||  file.isSymLink()) {
+    String name = Path(Path(fileName).absoluteName()).dirName();
+    return Directory(name).freeSpace();
+  }
+  throw (AipsError ("DOos::freeSpace - file " + fileName +
+		    " does not exist"));
+  return 0;
+}
+
+void DOos::copy (const String& to, const String& from, Bool overwrite,
+		 Bool follow)
+{
+  File file(from);
+  if (! file.exists()) {
+    throw (AipsError ("DOos::copy - file " + from + " does not exist"));
+  }
+  if (file.isRegular (follow)) {
+    RegularFile(file).copy (to, overwrite);
+  } else if (file.isDirectory (follow)) {
+    Directory(file).copy (to, overwrite);
+  } else if (file.isSymLink()) {
+    SymLink(file).copy (to, overwrite);
+  } else {
+    throw (AipsError ("DOos::copy - file " + from + " is not a regular file "
+		      "directory, nor symlink"));
+  }
+}
+
+void DOos::move (const String& to, const String& from, Bool overwrite,
+		 Bool follow)
+{
+  File file(from);
+  if (! file.exists()) {
+    throw (AipsError ("DOos::move - file " + from + " does not exist"));
+  }
+  if (file.isRegular (follow)) {
+    RegularFile(file).move (to, overwrite);
+  } else if (file.isDirectory (follow)) {
+    Directory(file).move (to, overwrite);
+  } else if (file.isSymLink()) {
+    SymLink(file).copy (to, overwrite);
+  } else {
+    throw (AipsError ("DOos::move - file " + from + " is not a regular file "
+		      "directory, nor symlink"));
+  }
+}
+
+void DOos::remove (const String& fileName, Bool recursive, Bool mustExist,
+		   Bool follow)
+{
+  File file(fileName);
+  if (! file.exists()) {
+    if (mustExist) {
+      throw (AipsError ("DOos::remove - file " + fileName +
+			" does not exist"));
+    }
+  } else {
+    if (file.isRegular (follow)) {
+      RegularFile rfile(file);
+      rfile.remove();
+    } else if (file.isDirectory (follow)) {
+      Directory dir(file);
+      if (recursive) {
+	dir.removeRecursive();
+      } else {
+	dir.remove();
+      }
+    } else if (file.isSymLink()) {
+      SymLink symlink(file);
+      symlink.remove();
+    } else {
+      throw (AipsError ("DOos::remove - file " + fileName +
+			" is not a regular file, directory, nor symlink"));
+    }
+  }
+}
---
+++ code/casa/implement/OS/DOos.h
@@ -0,0 +1,146 @@
+//# DOos.h: Functions used to implement the DO functionality
+//# Copyright (C) 1999
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//#
+//# $Id$
+
+#if !defined(AIPS_DOOS_H)
+#define AIPS_DOOS_H
+
+//# Includes
+#include <aips/aips.h>
+#include <trial/Tasking/ApplicationObject.h>
+
+//# Forward Declarations
+class String;
+
+
+// <summary>
+// DO for accessing os-specific functions
+// </summary>
+
+// <use visibility=export>
+
+// <reviewed reviewer="" date="yyyy/mm/dd" tests="" demos="">
+// </reviewed>
+
+// <prerequisite>
+//   <li> <linkto class=Tasking>Tasking</linkto>
+//   <li> <linkto module=OS>OS</linkto>
+// </prerequisite>
+
+// <etymology>
+// </etymology>
+
+// <synopsis>
+// This class is the connection between the Glish os server, and the
+// OS module. It is meant for access to OS-specific functions, in
+// particular file handling.
+// </synopsis>
+
+// <example>
+// </example>
+
+// <motivation>
+// </motivation>
+
+// <thrown>
+//    <li> AipsError if AIPSPATH or HOME is not defined
+// </thrown>
+
+// <todo asof="1997/09/16">
+//   <li> Check for feasable extensions
+// </todo>
+
+
+class DOos
+{
+public:
+  // Give all file names in the directory matching the given pattern
+  // and file types.
+  // <br>The pattern can be a string like the filename pattern given in
+  // a shell (e.g. '*.cc'). If the string is empty, all files are taken
+  // into account.
+  // <br>Filetypes is a string determining which file types will be selected.
+  // Each character in the string determines a file type. They are:
+  // <dl>
+  // <dt>r<dd>regular file
+  // <dt>d<dd>directory
+  // <dt>s<dd>symbolic link
+  // <dt>R<dd>readable file
+  // <dt>W<dd>writable file
+  // <dt>X<dd>executable file
+  // </dl>
+  // The all flag determines if file names starting with a . will also
+  // be selected.
+  static Vector<String> fileNames (const String& directoryName,
+				   const String& fileNamePattern,
+				   const String& fileTypes,
+				   Bool all = False,
+				   Bool follow = True);
+
+  // Make a directory. It throws an exception if a file with that
+  // name already exists.
+  static void makeDirectory (const String& directoryName);
+
+  // Return the full absolute name for the given name.
+  static String fullName (const String& fileName);
+
+  // Return the full direcotry name of the given name.
+  static String dirName (const String& fileName);
+
+  // Return the base name of the given file.
+  static String baseName (const String& fileName);
+
+  // Return the total size of the file or all files (recursively)
+  // in the directory.
+  // If follow is False, symbolic links are not followed.
+  static Double totalSize (const String& fileName, Bool follow = True);
+
+  // Return the total size on the device the given directory is on.
+  // If follow is False, symbolic links are not followed.
+  static Double freeSpace (const String& fileName, Bool follow = True);
+
+  // Copy the file (or directory recursively).
+  // If from is a symbolic link and follow is False, only the
+  // symbolic link is copied.
+  static void copy (const String& to, const String& from,
+		    Bool overwrite = True, Bool follow = True);
+
+  // Move the file or directory.
+  // If from is a symbolic link and follow is False, only the
+  // symbolic link is moved.
+  static void move (const String& to, const String& from,
+		    Bool overwrite = True, Bool follow = True);
+
+  // Remove the file (or directory recursively).
+  // If fileName is a symbolic link and follow is False, only the
+  // symbolic link is removed.
+  static void remove (const String& fileName, Bool recursive,
+		      Bool mustExist = True, Bool follow = True);
+};
+
+
+#endif
