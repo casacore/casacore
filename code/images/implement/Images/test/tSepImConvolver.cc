b45c8da88ccadaed6e2225e830cda07bec41ea38
Neil Killeen <nkilleen@unimelb.edu.au>
1999-04-01 12:14:32 +0000

    initial versiob

---
+++ code/images/implement/Images/test/tSepImConvolver.cc
@@ -0,0 +1,194 @@
+//# tSepImConvolver.cc: 
+//# Copyright (C) 1996,1997,1999
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//# $Id$
+//
+#include <trial/Images/SepImageConvolver.h>
+
+#include <aips/aips.h>
+#include <aips/Functionals/Gaussian1D.h>
+#include <aips/Arrays/Vector.h>
+#include <aips/Exceptions/Error.h>
+#include <aips/Inputs/Input.h>
+#include <aips/Logging.h>
+#include <aips/Mathematics/Constants.h>
+#include <trial/Mathematics/VectorKernel.h>
+#include <aips/Tasking/Aipsrc.h>
+#include <aips/Utilities/String.h>
+
+
+#include <trial/Images/PagedImage.h>
+#include <iostream.h>
+
+
+
+main (int argc, char **argv)
+{
+
+try {
+
+   Input inputs(1);
+
+// Get inputs
+
+   String root = Aipsrc::aipsRoot();
+   String name = root + "/code/trial/implement/Images/test/test_image";
+   inputs.Create("in", name, "Input file name");
+   inputs.Create("out", "tSepImageConv_image", "Output root image name");
+   inputs.Create("axis", "-1", "axis to convolve");
+   inputs.Create("fwhm", "3", "FWHM in pixels");
+   inputs.Create("type", "gauss", "Kernel type (box, gauss, hann");
+   inputs.ReadArguments(argc, argv);
+
+   const String in = inputs.GetString("in");
+   const String out = inputs.GetString("out");
+   const String kernelType = inputs.GetString("type");
+   Int axis = inputs.GetInt("axis");
+   Double fwhm = inputs.GetDouble("fwhm");
+//
+   if (in.empty()) {
+      cout << "You must give an input image name" << endl;
+      exit(1);
+   }
+   if (out.empty()) {
+      cout << "You must give an output image name" << endl;
+      exit(1);
+   }
+   if (fwhm <= 0.0) {
+      cout << "FWHM must be positive" << endl;
+      exit(1);
+   }
+ 
+
+   DataType imageType = imagePixelType(in);
+   if (imageType!=TpFloat) {
+      cout << "The image must be of type Float" << endl;
+      exit(1);
+   }
+
+// Construct images
+
+   PagedImage<Float> inImage(in);
+   if (axis > Int(inImage.ndim())) {
+      cout << "Axis must be <=" << inImage.ndim() << endl;
+      exit(1);
+   }
+   PagedImage<Float> outImage1(TiledShape(inImage.shape()),
+                              inImage.coordinates(), out+String("_a"));
+
+   PagedImage<Float> outImage2(TiledShape(inImage.shape()),
+                              inImage.coordinates(), out+String("_b"));
+
+// Make a Gaussian kernel parameters
+
+   IPosition shape = inImage.shape();
+   const Double sigma = fwhm / sqrt(Double(8.0) * C::ln2);
+   const Double norm = 1.0 / (sigma * sqrt(2.0 * C::pi));
+   LogOrigin or("tSeparableImageConvolver", "main()", WHERE);
+   LogIO os(or);
+   Vector<Int> types = VectorKernel::toKernelTypes(kernelType);
+   VectorKernel::KernelTypes type = VectorKernel::KernelTypes(types(0));
+cout << "type=" << type << endl;
+
+
+// Give kernel with vectors directly
+
+   {
+
+// Make convolver
+
+      SepImageConvolver<Float> sic(inImage, os, True);
+//
+// Set the state of the convolver
+
+      if (axis<=0) {
+         for (uInt j=0; j<shape.nelements(); j++) {
+            const Double refPix = shape(j)/2;
+            const Gaussian1D<Double> gauss(norm, refPix, fwhm);
+            Vector<Float> kernel(shape(j));
+            for (Int i=0; i<shape(j); i++) kernel(i) = gauss(Double(i));
+            sic.setKernel(j, kernel);
+         }
+      } else {
+         axis--;
+         const Double refPix = shape(axis)/2;
+         const Gaussian1D<Double> gauss(norm, refPix, fwhm);
+         Vector<Float> kernel(shape(axis));
+         for (Int i=0; i<shape(axis); i++) kernel(i) = gauss(Double(i));
+         sic.setKernel(axis, kernel);
+      }
+      sic.convolve(outImage1);
+   }
+//
+// Give kernel parameters
+//
+
+   {
+
+// Make convolver
+
+      SepImageConvolver<Float> sic(inImage, os, True);
+//
+// Set the state of the convolver
+
+      if (axis<=0) {
+         for (uInt j=0; j<shape.nelements(); j++) {
+            sic.setKernel(j, type, fwhm);
+         }
+      } else {
+         axis--;
+         sic.setKernel(axis, type, fwhm);
+      }
+      sic.convolve(outImage2);
+   }
+
+// Test some other things
+
+   {
+      SepImageConvolver<Float> sic(inImage, os, True);
+
+// Copy constructor
+      cout << "Copy constructor" << endl;
+      SepImageConvolver<Float> sic2(sic);
+
+// Assignment
+      cout << "Assignment" << endl;
+      sic2 = sic;
+
+// Set/Get a kernel
+      
+      sic.setKernel(0, type, fwhm);
+      Vector<Float> kernel = sic.getKernel(uInt(0));
+   }
+}
+
+  catch (AipsError x) {
+     cerr << "aipserror: error " << x.getMesg() << endl;
+     return 1;
+  } end_try;
+
+   exit(0);
+}
+
