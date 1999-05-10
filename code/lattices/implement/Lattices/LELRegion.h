fcc4c05f00e09448544202849153f07c026b201a
Ger van Diepen <gervandiepen@gmail.com>
1999-05-10 13:58:37 +0000

    Added class LELRegionAsBool to handle a region as a bollean lattice

--- code/lattices/implement/Lattices/LELRegion.h
+++ code/lattices/implement/Lattices/LELRegion.h
@@ -31,6 +31,7 @@
 
 //# Includes
 #include <trial/Lattices/LELInterface.h>
+#include <trial/Lattices/LatticeRegion.h>
 
 //# Forward Declarations
 class LattRegionHolder;
@@ -53,38 +54,28 @@ class LattRegionHolder;
 // </prerequisite>
 
 // <etymology>
-//  This derived LEL letter class handles regions.
+// This derived LEL letter class handles regions.
 // </etymology>
 
 // <synopsis>
 // This LEL letter class is derived from LELInterface.  It
-// is used to construct LEL objects that know how to convert
-// between numerical types, such as Double to Float.  They 
-// operate on numerical Lattices and return a numerical Lattice. 
-// The LELConvert object is embedded in the tree, and the conversion
-// actually happens at tree evaluation time.
+// is used to construct LEL objects from regions.
+// The internal region is an <linkto class=ImageRegion>ImageRegion</linkto>
+// object, thus the region can be of any type.
+// With operator [] a region is applied to an image (expression).
+// At that stage possible world coordinates are converted to lattice
+// coordinates.
+// <p>
+// The attributes of a LELRegion object define an empty shape,
+// because in general the shape of a region is only known after
+// it is applied to an image.
 // <p>
 // A description of the implementation details of the LEL classes can
 // be found in <a href="../../../notes/216/216.html">Note 216</a>
 // </synopsis> 
 
-// <example>
-// Examples are not very useful as the user would never use 
-// these classes directly.  Look in LatticeExprNode.cc to see 
-// how it invokes these classes.  An example of how the user
-// would indirectly use this class (through the envelope) is:
-// <srcblock>
-// IPosition shape(2,5,10);
-// ArrayLattice<Float> x(shape); x.set(1.0);
-// ArrayLattice<Double> y(shape); 
-// y.copyData(x);                 // y = x;
-// </srcblock>
-// The LELConvert class is embedded in the tree at construction time
-// so as to handle the conversion from Float to Double at evaluation time
-// </example>
-
 // <motivation>
-//  We needed to be able to handle mixed types in the LEL classes
+// We needed to be able to handle regions in a LEL expression.
 // </motivation>
 
 //# <todo asof="1998/01/21">
@@ -142,6 +133,74 @@ private:
 // Member variables.
     LattRegionHolder* region_p;
 };
+
+
+
+
+// <summary>
+// Class to convert a region to a boolean node
+// </summary>
+
+// <use visibility=local>
+
+// <reviewed reviewer="" date="yyyy/mm/dd" tests="" demos="">
+// </reviewed>
+
+// <prerequisite>
+//   <li> <linkto class="LELRegion"> Lattice</linkto>
+//   <li> <linkto class="LatticeExprNode"> LatticeExprNode</linkto>
+//   <li> <linkto class="LELInterface"> LELInterface</linkto>
+// </prerequisite>
+
+// <etymology>
+// This derived LEL letter class handles a region as a boolean lattice.
+// </etymology>
+
+// <synopsis>
+// This class makes it possible to handle a region as a true
+// boolean lattice without the need to apply the region to an image.
+// It means that it is only possible if the region has absolute
+// lattice coordinates.
+// <p>
+// A description of the implementation details of the LEL classes can
+// be found in <a href="../../../notes/216/216.html">Note 216</a>
+// </synopsis> 
+
+// <motivation>
+// It is useful to be able to handle a mask as a boolean lattice.
+// </motivation>
+
+//# <todo asof="1998/01/21">
+//# </todo>
+
+
+class LELRegionAsBool : public LELInterface<Bool>
+{
+public: 
+// Constructor.
+    LELRegionAsBool (const LELRegion& region);
+
+// Destructor.
+    ~LELRegionAsBool();
+
+// Get region data.
+    virtual void eval(LELArray<Bool>& result,
+		      const Slicer& section) const;
+
+// Getting region data as a scalar cannot be done (throws an exception).
+    virtual LELScalar<Bool> getScalar() const;
+
+// Do further preparations (e.g. optimization) on the expression.
+    virtual Bool prepareScalarExpr();
+
+// Get class name
+    virtual String className() const;
+
+private:
+// Member variables.
+    LatticeRegion region_p;
+};
+
 
 
 #endif
