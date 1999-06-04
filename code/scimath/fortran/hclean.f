32c86606da1bf362debcae50ac2cfb7a3f8d2778
Mark Holdaway <mholdawa@nrao.edu>
1999-06-04 15:54:38 +0000

    as per agreement with TJC 	o changed Bool to Int (Bool had problems when I 	  compied it with the debugger turned on) 	o added more support for iteration (returns the iteration 	  it got to)

--- code/scimath/fortran/hclean.f
+++ code/scimath/fortran/hclean.f
@@ -1,31 +1,3 @@
-*=======================================================================
-*     Copyright (C) 1999
-*     Associated Universities, Inc. Washington DC, USA.
-*
-*     This library is free software; you can redistribute it and/or
-*     modify it under the terms of the GNU Library General Public
-*     License as published by the Free Software Foundation; either
-*     version 2 of the License, or (at your option) any later version.
-*
-*     This library is distributed in the hope that it will be useful,
-*     but WITHOUT ANY WARRANTY; without even the implied warranty of
-*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*     GNU Library General Public License for more details.
-*
-*     You should have received a copy of the GNU Library General Public
-*     License along with this library; if not, write to the Free
-*     Software Foundation, Inc., 675 Massachusetts Ave, Cambridge,
-*     MA 02139, USA.
-*
-*     Correspondence concerning AIPS++ should be addressed as follows:
-*            Internet email: aips2-request@nrao.edu.
-*            Postal address: AIPS++ Project Office
-*                            National Radio Astronomy Observatory
-*                            520 Edgemont Road
-*                            Charlottesville, VA 22903-2475 USA
-*
-*     $Id$
-*-----------------------------------------------------------------------
       real function maxeig(i,q,u,v)
       real i, q, u, v
       maxeig=i+sqrt(q**2+u**2+v**2)
@@ -46,15 +18,36 @@
       l2norm=i**2+q**2+u**2+v**2
       return 
       end
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+c   limage	real(nx,ny,npol) output model image (clean components image)
+c   limagestep	real(nx,ny,npol) input dirty image; output residual image
+c   lpsf	real(nx,ny) 	 input point spread function
+c   domask	integer		 domask == 0 ==> no mask is present
+c                                (formerly a logical, but that was trouble)
+c   lmask	real(nx,ny) 	 input mask image
+c   nx,ny,npol	integer		 shape of images
+c   xbeg,xend	integer		 shape of box to clean  -  this could be larger
+c   ybeg,yend                        than the inner quarter, but the subtraction
+c                                    will be incorrect then for points outside the
+c				     inner quarter   
+c   niter	integer		 maximum allowed iterations
+c   siter	integer		 starting iteration number
+c   iter	integer		 last iteration number we get to in this algorithm
+c   gain	real		 clean loop gain
+c   thres	real		 flux cleaning threshold
+c   msgput	pointer to function which outputs the status message
+c   stopnow	pointer to function which determines if it is stopping time
+c      
       subroutine hclean(limage, limagestep, lpsf, domask, lmask, nx, ny, 
-     $     npol, xbeg, xend, ybeg, yend, niter, gain, thres, msgput,
-     $     stopnow)
+     $     npol, xbeg, xend, ybeg, yend, niter, siter, iter, gain, 
+     $     thres, msgput, stopnow)
       
       implicit none
-      integer nx, ny, npol, xbeg, xend, ybeg, yend, niter, yes
+      integer nx, ny, npol, xbeg, xend, ybeg, yend
+      integer niter, siter, iter, yes
       real limage(nx, ny, npol)
       real limagestep(nx, ny, npol)
-      logical domask
+      integer domask
       real lpsf(nx, ny), lmask(nx, ny)
       real gain, thres
       external msgput
@@ -62,9 +55,10 @@
 
       real peak, mev, maxeig
       real maxVal(4), pv(4)
-      integer px, py, pol, iter, ix, iy, cycle
+      integer px, py, pol, ix, iy, cycle
+      integer x1, x2, y1, y2
       
-      do iter=1,niter
+      do iter=siter, niter
          
 c     Now find peak in residual image 
          peak=0.0
@@ -72,8 +66,7 @@ c     Now find peak in residual image
          py=ybeg
          do iy=ybeg,yend
             do ix=xbeg,xend
-c     Assume that the short cut works!
-               if(.NOT.domask.OR.(lmask(ix,iy).GT.0.000001)) then
+               if(domask.EQ.0  .OR. (lmask(ix,iy).GT.0.000001) ) then
                   if(npol.EQ.4) then
                      mev=maxeig(limageStep(ix,iy,1),
      $                    limageStep(ix,iy,2), limageStep(ix,iy,3),
@@ -117,10 +110,12 @@ c     Assume that the short cut works!
             maxVal(3)=0.0
             maxVal(4)=0.0
          end if
+
+
          
 c     // Output ten lines of information if run to the end
-         cycle=niter/10
-         if((iter.EQ.1).OR.(mod(iter,cycle)).EQ.1) then
+         cycle=max(1,(niter-siter)/10         )
+         if((iter.EQ.siter).OR.(mod(iter,cycle)).EQ.1) then
             call msgput(iter, px, py, maxVal)
             call stopnow(yes)
          endif
@@ -130,6 +125,11 @@ c     // Output ten lines of information if run to the end
          endif
          
 c  Add the scaled peak to the current image
+c
+         x1 = max( xbeg, px - nx/2 )
+         y1 = max( ybeg, py - ny/2 )
+         x2 = min( xend, px + nx/2 -1 )
+         y2 = min( yend, py + ny/2 -1 )
          if((npol.EQ.4).OR.(npol.EQ.3)) then
             do pol=1,npol
                pv(pol)=gain*maxVal(pol)
@@ -138,8 +138,8 @@ c  Add the scaled peak to the current image
                limage(px,py,pol)=limage(px,py,pol)+pv(pol)
             end do
             do pol=1,npol
-               do iy=ybeg,yend
-                  do ix=xbeg,xend
+               do iy=y1,y2
+                  do ix=x1,x2
                      limageStep(ix,iy,pol)=limageStep(ix,iy,pol)
      $                    -pv(pol) * lpsf(nx/2+ix-px+1,ny/2+iy-py+1)
                   end do
@@ -150,8 +150,8 @@ c  Add the scaled peak to the current image
             pv(4)=gain*maxVal(4)
             limage(px,py,1)=limage(px,py,1)+pv(1)
             limage(px,py,2)=limage(px,py,2)+pv(4)
-            do iy=ybeg,yend
-               do ix=xbeg,xend
+            do iy=y1,y2
+               do ix=x1,x2
                   limageStep(ix,iy,1)=limageStep(ix,iy,1)
      $              - pv(1)*lpsf(nx/2+ix-px+1,ny/2+iy-py+1)
                   limageStep(ix,iy,2)=limageStep(ix,iy,2)
@@ -161,17 +161,17 @@ c  Add the scaled peak to the current image
          else 
             pv(1)=gain*maxVal(1)
             limage(px,py,1)=limage(px,py,1)+pv(1)
-            do iy=ybeg,yend
-               do ix=xbeg,xend
+            do iy=y1,y2
+               do ix=x1,x2
                   limageStep(ix,iy,1)=limageStep(ix,iy,1)
      $                 -pv(1)*lpsf(nx/2+ix-px+1,ny/2+iy-py+1)
                end do
             end do
          end if
-         
       end do
  100  continue
-      call msgput(-iter, px, py, maxVal)
+      call msgput(-niter, px, py, maxVal)
       return
       end
+
 
