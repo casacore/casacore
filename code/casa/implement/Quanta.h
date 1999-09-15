5b1b40c8ce7c42f2cce88dfa03a119d6333c3616
Wim Brouw <brouw@astron.nl>
1999-09-15 04:35:42 +0000

    Added explanation of new basic unit '_' Updated some embedded lists

--- code/casa/implement/Quanta.h
+++ code/casa/implement/Quanta.h
@@ -1,5 +1,5 @@
 //# Quanta.h:  a module for units and quantities
-//# Copyright (C) 1998
+//# Copyright (C) 1998,1999
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
@@ -126,7 +126,8 @@
 // All SI units and some customary units are part of the classes. User
 // defined names can be added by the UnitMap::putUser() function (see
 // <linkto class="UnitMap">UnitMap</linkto>). A special set of FITS related 
-// units can be added by the <src>UnitMap::addFITS()</src> function. 
+// units can be added by the <src>UnitMap::addFITS()</src> function. For 
+// details, see  linkto class="UnitMap">UnitMap</linkto>.
 //
 // Example:
 // <srcblock>
@@ -134,7 +135,7 @@
 // </srcblock>
 // There are 5 name lists in the UnitMap, which are searched in reverse order:
 // <ol>
-//   <li> Defining units: 	m, kg, s, A, K, cd, mol, rad, sr
+//   <li> Defining units: 	m, kg, s, A, K, cd, mol, rad, sr, _
 //   <li> SI units:		including a.o. g, Jy, AU
 //   <li> Customary units:	e.g. lb, hp, ly
 //   <li> User defined units:	defined by user (e.g. beam, KPH, KM)
@@ -143,8 +144,13 @@
 // All known names can be viewed by running the tUnit test program, or
 // using the MapUnit::list() routine.
 //
-// The definitions that were current on 960509 are given at end of this file
+// The definitions that were current on 990915 are given at end of this file
 // 
+// <note role=caution>
+// There is a difference between units without a dimension (non-dimensioned
+// I will call them), and undimensioned units. Non-dimensioned examples are
+// "", "%"; undimensioned examples: "beam", "pixel".
+// </note>
 //
 //  <h3> Working with units </h3>
 // In general units are not used explicitly, but are embedded in quantities
@@ -444,9 +450,10 @@
 //    u         (micro)                      1e-06
 //    y         (yocto)                      1e-24
 //    z         (zepto)                      1e-21
-//Defining unit table (9):
+//Defining unit table (10):
 //    A         (ampere)                     1 A
 //    K         (kelvin)                     1 K
+//    _         (undimensioned)              1 _
 //    cd        (candela)                    1 cd
 //    kg        (kilogram)                   1 kg
 //    m         (metre)                      1 m
@@ -454,34 +461,36 @@
 //    rad       (radian)                     1 rad
 //    s         (second)                     1 s
 //    sr        (steradian)                  1 sr
-//SI unit table (48):
-//    $         (currency)                   1
+//SI unit table (50):
+//    $         (currency)                   1 _
+//    %         (percent)                    0.01
+//    %%        (permille)                   0.001
 //    A         (ampere)                     1 A
 //    AE        (astronomical unit)          149597870659 m
 //    AU        (astronomical unit)          149597870659 m
 //    Bq        (becquerel)                  1 s-1
-//    C         (coulomb)                    1 s A
-//    F         (farad)                      1 m-2 kg-1 s4 A2
-//    Gy        (gray)                       1 m2 s-2
-//    H         (henry)                      1 m2 kg s-2 A-2
+//    C         (coulomb)                    1 s.A
+//    F         (farad)                      1 m-2.kg-1.s4.A2
+//    Gy        (gray)                       1 m2.s-2
+//    H         (henry)                      1 m2.kg.s-2.A-2
 //    Hz        (hertz)                      1 s-1
-//    J         (joule)                      1 m2 kg s-2
-//    Jy        (jansky)                     1e-26 kg s-2
+//    J         (joule)                      1 m2.kg.s-2
+//    Jy        (jansky)                     1e-26 kg.s-2
 //    K         (kelvin)                     1 K
 //    L         (litre)                      0.001 m3
 //    M0        (solar mass)                 1.98891944407e+30 kg
-//    N         (newton)                     1 m kg s-2
-//    Ohm       (ohm)                        1 m2 kg s-3 A-2
-//    Pa        (pascal)                     1 m-1 kg s-2
-//    S         (siemens)                    1 m-2 kg-1 s3 A2
+//    N         (newton)                     1 m.kg.s-2
+//    Ohm       (ohm)                        1 m2.kg.s-3.A-2
+//    Pa        (pascal)                     1 m-1.kg.s-2
+//    S         (siemens)                    1 m-2.kg-1.s3.A2
 //    S0        (solar mass)                 1.98891944407e+30 kg
-//    Sv        (sievert)                    1 m2 s-2
-//    T         (tesla)                      1 kg s-2 A-1
+//    Sv        (sievert)                    1 m2.s-2
+//    T         (tesla)                      1 kg.s-2.A-1
 //    UA        (astronomical unit)          149597870659 m
-//    V         (volt)                       1 m2 kg s-3 A-1
-//    W         (watt)                       1 m2 kg s-3
-//    Wb        (weber)                      1 m2 kg s-2 A-1
-//    _         (dimensionless)              1
+//    V         (volt)                       1 m2.kg.s-3.A-1
+//    W         (watt)                       1 m2.kg.s-3
+//    Wb        (weber)                      1 m2.kg.s-2.A-1
+//    _         (undimensioned)              1 _
 //    a         (year)                       31557600 s
 //    arcmin    (arcmin)                     0.000290888208666 rad
 //    arcsec    (arcsec)                     4.8481368111e-06 rad
@@ -493,8 +502,8 @@
 //    g         (gram)                       0.001 kg
 //    h         (hour)                       3600 s
 //    l         (litre)                      0.001 m3
-//    lm        (lumen)                      1 cd sr
-//    lx        (lux)                        1 m-2 cd sr
+//    lm        (lumen)                      1 cd.sr
+//    lx        (lux)                        1 m-2.cd.sr
 //    m         (metre)                      1 m
 //    min       (minute)                     60 s
 //    mol       (mole)                       1 mol
@@ -503,7 +512,7 @@
 //    s         (second)                     1 s
 //    sr        (steradian)                  1 sr
 //    t         (tonne)                      1000 kg
-//Customary unit table (71):
+//Customary unit table (74):
 //    "         (arcsec)                     4.8481368111e-06 rad
 //    "_2       (square arcsec)              2.35044305391e-11 sr
 //    '         (arcmin)                     0.000290888208666 rad
@@ -513,68 +522,71 @@
 //    :         (hour)                       3600 s
 //    ::        (minute)                     60 s
 //    :::       (second)                     1 s
-//    Ah        (ampere hour)                3600 s A
+//    Ah        (ampere hour)                3600 s.A
 //    Angstrom  (angstrom)                   1e-10 m
-//    Btu       (British thermal unit (Int)) 1055.056 m2 kg s-2
+//    Btu       (British thermal unit (Int)) 1055.056 m2.kg.s-2
 //    CM        (metric carat)               0.0002 kg
-//    Cal       (large calorie (Int))        4186.8 m2 kg s-2
-//    FU        (flux unit)                  1e-26 kg s-2
-//    G         (gauss)                      0.0001 kg s-2 A-1
-//    Gal       (gal)                        0.01 m s-2
+//    Cal       (large calorie (Int))        4186.8 m2.kg.s-2
+//    FU        (flux unit)                  1e-26 kg.s-2
+//    G         (gauss)                      0.0001 kg.s-2.A-1
+//    Gal       (gal)                        0.01 m.s-2
 //    Gb        (gilbert)                    0.795774715459 A
-//    Mx        (maxwell)                    1e-08 m2 kg s-2 A-1
-//    Oe        (oersted)                    79.5774715459 m-1 A
-//    R         (mile)                       0.000258 kg-1 s A
-//    mile      (stokes)                     0.0001 m2 s-1
-//    Torr      (torr)                       133.322368421 m-1 kg s-2
+//    Mx        (maxwell)                    1e-08 m2.kg.s-2.A-1
+//    Oe        (oersted)                    79.5774715459 m-1.A
+//    R         (mile)                       0.000258 kg-1.s.A
+//    St        (stokes)                     0.0001 m2.s-1
+//    Torr      (torr)                       133.322368421 m-1.kg.s-2
 //    USfl_oz   (fluid ounce (US))           2.95735295625e-05 m3
 //    USgal     (gallon (US))                0.003785411784 m3
-//    WU        (WSRT flux unit)             5e-29 kg s-2
+//    WU        (WSRT flux unit)             5e-29 kg.s-2
 //    abA       (abampere)                   10 A
-//    abC       (abcoulomb)                  10 s A
-//    abF       (abfarad)                    1000000000 m-2 kg-1 s4 A2
-//    abH       (abhenry)                    1e-09 m2 kg s-2 A-2
-//    abOhm     (abohm)                      1e-09 m2 kg s-3 A-2
-//    abV       (abvolt)                     1e-08 m2 kg s-3 A-1
+//    abC       (abcoulomb)                  10 s.A
+//    abF       (abfarad)                    1000000000 m-2.kg-1.s4.A2
+//    abH       (abhenry)                    1e-09 m2.kg.s-2.A-2
+//    abOhm     (abohm)                      1e-09 m2.kg.s-3.A-2
+//    abV       (abvolt)                     1e-08 m2.kg.s-3.A-1
 //    ac        (acre)                       4046.8564224 m2
 //    arcmin_2  (square arcmin)              8.46159499408e-08 sr
 //    arcsec_2  (square arcsec)              2.35044305391e-11 sr
-//    ata       (technical atmosphere)       98066.5 m-1 kg s-2
-//    atm       (standard atmosphere)        101325 m-1 kg s-2
-//    bar       (bar)                        100000 m-1 kg s-2
-//    cal       (calorie (Int))              4.1868 m2 kg s-2
+//    ata       (technical atmosphere)       98066.5 m-1.kg.s-2
+//    atm       (standard atmosphere)        101325 m-1.kg.s-2
+//    bar       (bar)                        100000 m-1.kg.s-2
+//    beam      (undefined beam area)        1 _
+//    cal       (calorie (Int))              4.1868 m2.kg.s-2
 //    cwt       (hundredweight)              50.80234544 kg
 //    deg_2     (square degree)              0.000304617419787 sr
-//    dyn       (dyne)                       1e-05 m kg s-2
-//    eV        (electron volt)              1.60217733e-19 m2 kg s-2
-//    erg       (erg)                        0.001 kg s-2
+//    dyn       (dyne)                       1e-05 m.kg.s-2
+//    eV        (electron volt)              1.60217733e-19 m2.kg.s-2
+//    erg       (erg)                        0.001 kg.s-2
 //    fl_oz     (fluid ounce (Imp))          2.84130488996e-05 m3
 //    ft        (foot)                       0.3048 m
-//    fu        (flux unit)                  1e-26 kg s-2
+//    fu        (flux unit)                  1e-26 kg.s-2
 //    fur       (furlong)                    201.168 m
 //    gal       (gallon (Imp))               0.00454608782394 m3
 //    ha        (hectare)                    10000 m2
-//    hp        (horsepower)                 745.7 m2 kg s-3
+//    hp        (horsepower)                 745.7 m2.kg.s-3
 //    in        (inch)                       0.0254 m
-//    kn        (knot (Imp))                 0.514773333333 m s-1
+//    kn        (knot (Imp))                 0.514773333333 m.s-1
 //    lb        (pound (avoirdupois))        0.45359237 kg
 //    ly        (light year)                 9.46073047e+15 m
-//    mHg       (metre of mercury)           133322.387415 m-1 kg s-2
+//    mHg       (metre of mercury)           133322.387415 m-1.kg.s-2
 //    mile      (mile)                       1609.344 m
 //    n_mile    (nautical mile (Imp))        1853.184 m
 //    oz        (ounce (avoirdupois))        0.028349523125 kg
-//    sb        (stilb)                      10000 m-2 cd
+//    pixel     (pixel)                      1 _
+//    sb        (stilb)                      10000 m-2.cd
 //    sq_arcmin (square arcmin)              8.46159499408e-08 sr
 //    sq_arcsec (square arcsec)              2.35044305391e-11 sr
 //    sq_deg    (square degree)              0.000304617419787 sr
 //    statA     (statampere)                 3.33564095198e-10 A
-//    statC     (statcoulomb)                3.33564095198e-10 s A
-//    statF     (statfarad)                  1.11188031733e-12 m-2 kg-1 s4 A2
-//    statH     (stathenry)                  899377374000 m2 kg s-2 A-2
-//    statOhm   (statohm)                    899377374000 m2 kg s-3 A-2
-//    statV     (statvolt)                   299.792458 m2 kg s-3 A-1
+//    statC     (statcoulomb)                3.33564095198e-10 s.A
+//    statF     (statfarad)                  1.11188031733e-12 m-2.kg-1.s4.A2
+//    statH     (stathenry)                  899377374000 m2.kg.s-2.A-2
+//    statOhm   (statohm)                    899377374000 m2.kg.s-3.A-2
+//    statV     (statvolt)                   299.792458 m2.kg.s-3.A-1
 //    u         (atomic mass unit)           1.661e-27 kg
 //    yd        (yard)                       0.9144 m
+//    yr        (year)                       31557600 s
 // </srcblock>
 //
 // </example>
