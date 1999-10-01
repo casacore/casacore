011e4d4dd4950932698762fb97e720f2b3034596
Neil Killeen <nkilleen@unimelb.edu.au>
1999-10-01 11:06:57 +0000

    add function fft and improve documenbtation

--- code/images/implement/Images/ImageFFT.h
+++ code/images/implement/Images/ImageFFT.h
@@ -30,7 +30,11 @@
 
 #include <aips/aips.h>
 #include <trial/Images/ImageInterface.h>
+#include <trial/Coordinates/Coordinate.h>
+
 class CoordinateSystem;
+class IPosition;
+template<class T> class Vector;
 
 // <summary>
 // FFT an image
@@ -44,6 +48,7 @@ class CoordinateSystem;
 // <prerequisite>
 //   <li> <linkto class=LatticeFFT>LatticeFFT</linkto> 
 //   <li> <linkto class=ImageInterface>ImageInterface</linkto> 
+//   <li> <linkto class=TempImage>TempImage</linkto> 
 // </prerequisite>
 
 // <etymology>
@@ -51,11 +56,27 @@ class CoordinateSystem;
 // </etymology>
 
 // <synopsis>
-// This class takes the FFT of an image.  Currently, its functionality
-// is limited to taking the FFT of the sky plane of an image.  All
-// other axes are iterated through.    
+// This class takes the FFT of an image.  It can
+// take the FFT of just the sky plane(s) of an image or
+// the specified axes.
 //
-// Masked pixels are given the value 0.0 before the FFT is taken.
+// When you specify axes, if any of them are a sky axis (DirectionCoordinate)
+// you must give both sky axes.
+//
+// Masked pixels are given the value 0.0 before the FFT is taken and the
+// mask is copied to the output.  Note that it is the callers responsibility
+// to give the output a mask if the input is masked.  Otherwise the mask
+// will not be copied to the output
+//
+// This class holds the FourierTransform internally in a 
+// <linkto class=TempImage>TempImage</linkto> object.  This is
+// in memory or on disk depending upon its size and the amount
+// of memory in your computer.    The algorithm used
+// is that in <linkto class=TempLattice>TempLattice</linkto>.
+//
+// In generating the Fourier Coordinates, it is currently
+// assumed that there is no coordinate rotation.  This 
+// needs to be dealt with.
 // </synopsis>
 //
 // <example>
@@ -64,18 +85,29 @@ class CoordinateSystem;
 //// Make a constant image
 //
 //      IPosition shape(2, 10, 20);
-//      PagedImage<Float> im1(shape, CoordinateUtil::defaultCoords2D(), im1");
+//      PagedImage<Float> im1(shape, CoordinateUtil::defaultCoords2D(), "im1");
 //      im1.set(1.0);
 //
-//// FFT
+//// Create output images with masks if needed
+//
+//      PagedImage<Float> r1(shape, CoordinateUtil::defaultCoords2D(), "real1");
+//      PagedImage<Float> i1(shape, CoordinateUtil::defaultCoords2D(), "imag1");
+//      if (im1.isMasked()) {
+//         LCPagedMask mask1 = LCPagedMask(RegionHandler::makeMask (r1, "mask0"));
+//         r1.defineRegion ("mask0", ImageRegion(mask1), RegionHandler::Masks);
+//         r1.setDefaultMask("mask0");
+//         LCPagedMask mask2 = LCPagedMask(RegionHandler::makeMask (i1, "mask0"));
+//         i1.defineRegion ("mask0", ImageRegion(mask1), RegionHandler::Masks);
+//         i1.setDefaultMask("mask0");
+//      }
+///
+/// FFT
 //
 //      ImageFFT fft;
 //      fft.fftsky(im1);
-//      
-//// Recover real and imaginary. The coordinates will be overwritten
 //
-//      PagedImage<Float> r1(shape, CoordinateUtil::defaultCoords2D(), real1");
-//      PagedImage<Float> i1(shape, CoordinateUtil::defaultCoords2D(), imag1");
+//// The coordinates and mask will be overwritten
+//
 //      fft.getReal(r1);
 //      fft.getImag(i1);
 // </srcblock>
@@ -111,11 +143,16 @@ public:
 // Do the FFT of the sky plane to the uv plane
    void fftsky (const ImageInterface<Float>& in);
 
-// Return the FFT in the desired form.    The CoordinateSystem of
+// Do the FFT of the specified pixel axes (True to FT).  
+// The rest are iterated over
+   void fft (const ImageInterface<Float>& in, 
+             const Vector<Bool>& axes);
+
+// Return the FFT (from the last call to fftsky or fft) in the 
+// desired form.    The CoordinateSystem of
 // the image is overwritten by these functions to reflect the 
 // Fourier axes.  
 // <group>
-   const ImageInterface<Complex>& getComplex () const;
    void getComplex (ImageInterface<Complex>& out) const;
    void getReal (ImageInterface<Float>& out) const;
    void getImaginary (ImageInterface<Float>& out) const;
@@ -123,19 +160,26 @@ public:
    void getPhase (ImageInterface<Float>& out) const;
 // </group>
 
+// This returns a reference to the internal complex 
+// FFT.  It is only useful for getting at the pixel
+// values as it has no mask.  Its coordinates are correct.
+   const ImageInterface<Complex>& getComplex () const;
+
 private:
 
    ImageInterface<Complex>* itsTempImagePtr;
    ImageInterface<Float>* itsInImagePtr;
    Bool itsDone;
+   Coordinate* itsCoordPtr;
 
-// FFT the sky
-   void fftsky2 (ImageInterface<Complex>& out,
-                 const ImageInterface<Float>& in);
+// Check axes for multi-dim FFT
+   void checkAxes(const ImageInterface<Float>& in, const Vector<Bool>& axes);
 
-// Overwrite the coordinates for the sky axes
-// with Fourier coordinates
-   void fiddleCoordinates (LogIO& os);
+// Copy the  mask to the output
+   void copyMask (ImageInterface<Float>& out,
+                  const ImageInterface<Float>& in) const;
+   void copyMask (ImageInterface<Complex>& out,
+                  const ImageInterface<Float>& in) const;
 
 // Copy some bits and pieces from the input to the output
 // images
@@ -144,8 +188,40 @@ private:
    void copyMiscellaneous (ImageInterface<Complex>& out,   
                            const ImageInterface<Float>& in) const;
 
+// FFT the sky
+   void fftsky2 (ImageInterface<Complex>& out,
+                 const ImageInterface<Float>& in,
+                 const Vector<Int>& pixelAxes);
+
+// FFT given axes
+   void fft2(ImageInterface<Complex>& out,
+             const ImageInterface<Float>& in,
+             const Vector<Bool>& axes);
+
 // Find the sky axes in this CoordinateSystem
-   void findSky(LogIO& os, Int& dC, Vector<Int>& pixelAxes,
-                Vector<Int>& worldAxes, const CoordinateSystem& cSys);
+   Bool findSky(LogIO& os, Int& dC, Vector<Int>& pixelAxes,
+                Vector<Int>& worldAxes, const CoordinateSystem& cSys,
+                Bool throwIt);
+
+// Overwrite the coordinates for the sky axes
+// with Fourier coordinates
+   void setCoordinates (LogIO& os,
+                        ImageInterface<Complex>& out,
+                        const ImageInterface<Float>& in,
+                        const Vector<Int>& pixelAxes,
+                        const Vector<Int>& worldAxes,
+                        const Int dC);
+
+// Overwrite the coordinates for the FT axes
+   void setCoordinates2 (LogIO& os,
+                         ImageInterface<Complex>& out,
+                         const ImageInterface<Float>& in,
+                         const Vector<Bool>& axes);
+
+
+// Set Fourier coordinate
+   void setInverseCoordinate (CoordinateSystem& cSys,
+                              uInt which, const IPosition& shape,
+                              const Vector<Bool>& axes);
 };
 #endif
