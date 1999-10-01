899f9cc7dcdeaab7b76db53a132c2f31558f1626
Neil Killeen <nkilleen@unimelb.edu.au>
1999-10-01 11:56:01 +0000

    test new function fft

--- code/images/implement/Images/test/tImageFFT.cc
+++ code/images/implement/Images/test/tImageFFT.cc
@@ -38,7 +38,9 @@
 #include <aips/Utilities/String.h>
   
 #include <trial/Images/ImageFFT.h>
+#include <trial/Images/ImageRegion.h>
 #include <trial/Images/PagedImage.h>
+#include <trial/Lattices/LCPagedMask.h>
 
 #include <iostream.h>
 
@@ -49,6 +51,18 @@ void checkNumbers (const ImageInterface<Float>& rIn,
                    const ImageInterface<Float>& pOut,
                    const ImageInterface<Complex>& cOut,
                    const ImageInterface<Complex>& c2Out);
+
+void checkNumbers (const ImageInterface<Float>& rIn,
+                   const ImageInterface<Float>& rOut,
+                   const ImageInterface<Float>& iOut,
+                   const ImageInterface<Float>& aOut,
+                   const ImageInterface<Float>& pOut,
+                   const ImageInterface<Complex>& cOut,
+                   const ImageInterface<Complex>& c2Out,
+                   const Vector<Bool>& axes);
+
+void makeMask(ImageInterface<Float>& out);
+void makeMask(ImageInterface<Complex>& out);
 
 main (int argc, char **argv)
 {
@@ -63,9 +77,11 @@ try {
    String root = Aipsrc::aipsRoot();
    String name = root + "/code/trial/implement/Images/test/test_image";
    inputs.Create("in", name, "Input file name");
+   inputs.Create("axes", "-10", "axes");
    inputs.ReadArguments(argc, argv);
 
    const String in = inputs.GetString("in");
+   const Block<Int> axes = inputs.GetIntArray("axes");
    LogOrigin or("tImageFFT", "main()", WHERE);
    LogIO os(or);
  
@@ -76,60 +92,119 @@ try {
       os << LogIO::NORMAL << "You must specify the image file name" << LogIO::POST;
       return 1;
    }
-
-// Testing does not really test the functionality, as I would
-// have to write the Fourier Transform by hand !
-
+//
    DataType imageType = imagePixelType(in);
    if (imageType==TpFloat) {
+      os << LogIO::NORMAL << "Create images" << LogIO::POST;
+
+// Make images
+
       PagedImage<Float> inImage(in, True);
+//
       IPosition outShape(inImage.shape());
       PagedImage<Float> outReal(outShape, inImage.coordinates(), "tImageFFT_real.img");
+      if (inImage.isMasked()) makeMask(outReal);
       PagedImage<Float> outImag(outShape, inImage.coordinates(), "tImageFFT_imag.img");
+      if (inImage.isMasked()) makeMask(outImag);
       PagedImage<Float> outAmp(outShape, inImage.coordinates(), "tImageFFT_amp.img");
+      if (inImage.isMasked()) makeMask(outAmp);
       PagedImage<Float> outPhase(outShape, inImage.coordinates(), "tImageFFT_phase.img");
+      if (inImage.isMasked()) makeMask(outPhase);
       PagedImage<Complex> outComplex(outShape, inImage.coordinates(), "tImageFFT_complex.img");
+      if (inImage.isMasked()) makeMask(outComplex);
+
+// FFT the sky only
+
+      {
+         os << LogIO::NORMAL << "FFT the sky" << LogIO::POST;
+         ImageFFT fft;
+         fft.fftsky(inImage);
+         Array<Float> rArray0 = inImage.get();
 //
-      ImageFFT fft;
-      fft.fftsky(inImage);
-      Array<Float> rArray0 = inImage.get();
+         os << LogIO::NORMAL << "Get FFT and check values" << LogIO::POST;
+         fft.getReal(outReal);
+         fft.getImaginary(outImag);
+         fft.getAmplitude(outAmp);
+         fft.getPhase(outPhase);
+         fft.getComplex(outComplex);
+         const ImageInterface<Complex>& outComplex2 = fft.getComplex();
 //
-      fft.getReal(outReal);
-      fft.getImaginary(outImag);
-      fft.getAmplitude(outAmp);
-      fft.getPhase(outPhase);
-      fft.getComplex(outComplex);
-      const ImageInterface<Complex>& outComplex2 = fft.getComplex();
-//
-      checkNumbers(inImage, outReal, outImag, outAmp, outPhase, 
-                   outComplex, outComplex2);
+         checkNumbers(inImage, outReal, outImag, outAmp, outPhase, 
+                      outComplex, outComplex2);
 
 // Copy constructor
 
-      ImageFFT fft2(fft);
-      fft2.getReal(outReal);
-      fft2.getImaginary(outImag);
-      fft2.getAmplitude(outAmp);
-      fft2.getPhase(outPhase);
-      fft2.getComplex(outComplex);
-      const ImageInterface<Complex>& outComplex3 = fft2.getComplex();
+         os << LogIO::NORMAL << "Copy constructor, get FFT and check values" << LogIO::POST;
+         ImageFFT fft2(fft);
+         fft2.getReal(outReal);
+         fft2.getImaginary(outImag);
+         fft2.getAmplitude(outAmp);
+         fft2.getPhase(outPhase);
+         fft2.getComplex(outComplex);
+         const ImageInterface<Complex>& outComplex3 = fft2.getComplex();
 //
-      checkNumbers(inImage, outReal, outImag, outAmp, outPhase, 
-                   outComplex, outComplex3);
+         checkNumbers(inImage, outReal, outImag, outAmp, outPhase, 
+                      outComplex, outComplex3);
 
 // Assignment operator
 
-      ImageFFT fft3;
-      fft3 = fft2;
-      fft3.getReal(outReal);
-      fft3.getImaginary(outImag);
-      fft3.getAmplitude(outAmp);
-      fft3.getPhase(outPhase);
-      fft3.getComplex(outComplex);
-      const ImageInterface<Complex>& outComplex4 = fft3.getComplex();
+         os << LogIO::NORMAL << "Assignment operator, get FFT and check values" << LogIO::POST;
+         ImageFFT fft3;
+         fft3 = fft2;
+         fft3.getReal(outReal);
+         fft3.getImaginary(outImag);
+         fft3.getAmplitude(outAmp);
+         fft3.getPhase(outPhase);
+         fft3.getComplex(outComplex);
+         const ImageInterface<Complex>& outComplex4 = fft3.getComplex();
 //
-      checkNumbers(inImage, outReal, outImag, outAmp, outPhase, 
-                   outComplex, outComplex4);
+         checkNumbers(inImage, outReal, outImag, outAmp, outPhase, 
+                      outComplex, outComplex4);
+      }
+
+
+// Multi dimensional FFT.  
+
+      {
+         os << LogIO::NORMAL << "FFT all dimensions" << LogIO::POST;
+         IPosition outShape(inImage.shape());
+         PagedImage<Float> outReal2(outShape, inImage.coordinates(), "tImageFFT_real2.img");
+         if (inImage.isMasked()) makeMask(outReal2);
+         PagedImage<Float> outImag2(outShape, inImage.coordinates(), "tImageFFT_imag2.img");
+         if (inImage.isMasked()) makeMask(outImag2);
+         PagedImage<Float> outAmp2(outShape, inImage.coordinates(), "tImageFFT_amp2.img");
+         if (inImage.isMasked()) makeMask(outAmp2);
+         PagedImage<Float> outPhase2(outShape, inImage.coordinates(), "tImageFFT_phase2.img");
+         if (inImage.isMasked()) makeMask(outPhase2);
+         PagedImage<Complex> outComplex2(outShape, inImage.coordinates(), "tImageFFT_complex2.img");
+         if (inImage.isMasked()) makeMask(outComplex2);
+//
+         Vector<Bool> which(inImage.ndim(), True);
+         if (axes.nelements()==1 && axes[0]==-10) {
+            ;
+         } else {
+            for (uInt i=0; i<inImage.ndim(); i++) {
+               Bool found = False;
+               for (uInt j=0; j<axes.nelements(); j++) {
+                 if (axes[j]==i) found = True;
+               }
+               which(i) = found;
+            }
+         }
+//
+         ImageFFT fft;
+         fft.fft(inImage, which);
+         fft.getReal(outReal2);
+         fft.getImaginary(outImag2);
+         fft.getAmplitude(outAmp2);
+         fft.getPhase(outPhase2);
+         fft.getComplex(outComplex2);
+         const ImageInterface<Complex>& outComplex3 = fft.getComplex();
+//
+         checkNumbers (inImage, outReal2, outImag2, outAmp2, outPhase2, 
+                       outComplex2, outComplex3, which);
+
+      }
    } else {
       os << LogIO::NORMAL << "images of type " << imageType << " not yet supported" << LogIO::POST;
       exit(1);
@@ -198,3 +273,70 @@ void checkNumbers (const ImageInterface<Float>& rIn,
       AlwaysAssert(allNear(cOut.get(), c2Out.get(), 1e-6), AipsError);
 }
 
+
+void checkNumbers (const ImageInterface<Float>& rIn,
+                   const ImageInterface<Float>& rOut,
+                   const ImageInterface<Float>& iOut,
+                   const ImageInterface<Float>& aOut,
+                   const ImageInterface<Float>& pOut,
+                   const ImageInterface<Complex>& cOut,
+                   const ImageInterface<Complex>& c2Out,
+                   const Vector<Bool>& axes)
+{
+
+// Can only check if all axes transformed
+
+   for (uInt i=0; i<axes.nelements(); i++) {
+      if (!axes(i)) return;
+   }
+      
+ 
+// Fill a complex array with the real input
+// image.  Replace masked values by zero
+      
+      Array<Float> r0 = rIn.get();
+      Array<Bool> m0 = rIn.getMask();
+      if (rIn.isMasked()) {
+         Bool deleteIt1, deleteIt2;
+         Float* pP = r0.getStorage(deleteIt1);
+         const Bool*  pM = m0.getStorage(deleteIt2);
+         for (Int i=0; i<r0.shape().product(); i++) {
+            if (!pM[i]) pP[i] = 0.0;
+         }
+         r0.putStorage(pP, deleteIt1);
+         m0.freeStorage(pM, deleteIt2);
+      }
+//
+      Array<Complex> c(r0.shape());
+      convertArray(c, r0);
+
+// Make FFT server and FFT
+
+      FFTServer<Float,Complex> fftServer(c.shape(),FFTEnums::COMPLEX);
+      fftServer.fft(c, True);
+
+// Check numbers
+
+      AlwaysAssert(allNear(rOut.get(), real(c), 1e-6), AipsError);
+      AlwaysAssert(allNear(iOut.get(), imag(c), 1e-6), AipsError);
+      AlwaysAssert(allNear(aOut.get(), amplitude(c), 1e-6), AipsError);
+      AlwaysAssert(allNear(pOut.get(), phase(c), 1e-6), AipsError);
+      AlwaysAssert(allNear(cOut.get(), c, 1e-6), AipsError);
+      AlwaysAssert(allNear(cOut.get(), c2Out.get(), 1e-6), AipsError);
+}
+
+
+
+void makeMask(ImageInterface<Float>& out)
+{
+   LCPagedMask mask1 = LCPagedMask(RegionHandler::makeMask (out, "mask0"));
+   out.defineRegion ("mask0", ImageRegion(mask1), RegionHandler::Masks);
+   out.setDefaultMask("mask0");
+}
+
+void makeMask(ImageInterface<Complex>& out)
+{
+   LCPagedMask mask1 = LCPagedMask(RegionHandler::makeMask (out, "mask0"));
+   out.defineRegion ("mask0", ImageRegion(mask1), RegionHandler::Masks);
+   out.setDefaultMask("mask0");
+}
