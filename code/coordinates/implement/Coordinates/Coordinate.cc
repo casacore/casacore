a253b3015a5ccb0448e64dbe35925c94f585b24e
Athol Kemball <akemball@illinois.edu>
1999-10-02 00:01:09 +0000

    Remove arrayCast() in setIncrement() and setReferenceValue() call. MVPosition ambiguity is now resolved with operator*.

--- code/coordinates/implement/Coordinates/Coordinate.cc
+++ code/coordinates/implement/Coordinates/Coordinate.cc
@@ -283,9 +283,9 @@ Bool Coordinate::setWorldAxisUnits(const Vector<String> &units,
 	Vector<Double> factor;
 	ok = find_scale_factor(error, factor, units, worldAxisUnits());
 	if (ok) {
-	    ok = setIncrement(increment().ac() * factor.ac());
+	    ok = setIncrement(increment() * factor);
 	    if (ok) {
-		ok = setReferenceValue(referenceValue().ac() * factor.ac());
+		ok = setReferenceValue(referenceValue() * factor);
 	    }
 	} else {
 	    set_error(error);
