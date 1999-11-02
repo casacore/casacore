73a525b04e7af0970e9350c6cc7641c77a4be1a3
Wim Brouw <brouw@astron.nl>
1999-11-02 21:15:17 +0000

    Copyright only

--- code/casa/implement/Quanta/QuantumHolder.h
+++ code/casa/implement/Quanta/QuantumHolder.h
@@ -1,5 +1,5 @@
 //# QuantumHolder.h: A holder for Quantities to enable record conversions
-//# Copyright (C) 1998
+//# Copyright (C) 1998,1999
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
--- code/measures/implement/Measures/MeasureHolder.h
+++ code/measures/implement/Measures/MeasureHolder.h
@@ -1,5 +1,5 @@
 //# MeasureHolder.h: A holder for Measures to enable record conversions
-//# Copyright (C) 1998
+//# Copyright (C) 1998,1999
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
