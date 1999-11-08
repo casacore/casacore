02bcb12f3b535308ec8e5e046970ff8ea12fb812
Mark Holdaway <mholdawa@nrao.edu>
1999-11-08 22:31:52 +0000

    Copyright only

--- code/msvis/implement/MSVis/VisBuffer.h
+++ code/msvis/implement/MSVis/VisBuffer.h
@@ -1,5 +1,5 @@
 //# VisBuffer.h: buffer for iterating through MS in large blocks
-//# Copyright (C) 1996,1997,1998
+//# Copyright (C) 1996,1997,1998,1999
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
