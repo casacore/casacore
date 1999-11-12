c65a882f65a6ca88cb4c65d12a5e4a7d2274671b
Ralph Marson <rmarson@nrao.edu>
1999-11-12 00:42:22 +0000

    Did the documentation and updated the signature of the visibility function

--- code/components/implement/ComponentModels/TwoSidedShape.h
+++ code/components/implement/ComponentModels/TwoSidedShape.h
@@ -32,6 +32,7 @@
 #include <aips/aips.h>
 #include <trial/ComponentModels/ComponentShape.h>
 #include <trial/ComponentModels/ComponentType.h>
+#include <aips/Mathematics/Complex.h>
 #include <aips/Quanta/Unit.h>
 
 class MDirection;
@@ -39,33 +40,111 @@ class MVAngle;
 class RecordInterface;
 class String;
 template <class Qtype> class Quantum;
-template <class T> class Flux;
 template <class T> class Vector;
 
-// <summary>A TwoSided model for the spatial distribution of emission</summary>
+// <summary>Base class for component shapes with two sides</summary>
 
 // <use visibility=export> 
 
-// <reviewed reviewer="" date="yyyy/mm/dd" tests="" demos="">
+// <reviewed reviewer="" date="yyyy/mm/dd" tests="tTwoSidedShape" demos="dGaussianShape">
 // </reviewed>
 
 // <prerequisite>
 //   <li> <linkto class=ComponentShape>ComponentShape</linkto>
+//   <li> <linkto class=Quantum>Quantum</linkto>
 // </prerequisite>
 
 // <synopsis> 
 
+// This base class adds a common interface for component shapes that have two
+// sides. These shapes can be parameterised as having a major-axis width,
+// minor-axis width and a position angle (as well as the reference direction).
+// Currently there are two shapes which use this parameterisation, the 
+// <linkto class=GaussianShape>Gaussian</linkto> shape and the 
+// <linkto class=DiskShape>disk</linkto> shape. Shapes which do not have this
+// parameterisation, such as the <linkto class=PointShape>point</linkto> shape,
+// are derived directly from the 
+// <linkto class=ComponentShape>ComponentShape</linkto> class.
+
+// Functions in this class implement a more convenient way of setting the
+// parameters associated with two-sided shapes than the very flexible, but
+// clumsy, <src>parameters</src> functions. The width parameters can be set
+// using the <src>setWidth</src> functions. These functions enforce the rule
+// that the major-axis width must not be smaller than the minor-axis
+// width. Hence the axial ratio, which is the minor-axis width divided by the
+// major-axis width, must always be greater than zero and less than or equal to
+// one.
+
+// The functions in this class also free derived classes from the having to
+// deal with units. Any angular unit can be used to specify the width of the
+// shape. The widths returned by the majoraxis, minoraxis & the positionAngle
+// functions are in the same units as they where specified in, unless the
+// convertUnit function has been used to specify another unit.
+
+// For maximum speed the <src>*InRad</src> functions are provided. These
+// functions bypass all the computation involving units.
+
+// The toRecord and fromRecord functions in this class standardise the record
+// representation of classes derived from this one. In addition to the type and
+// direction fields all records, discussed in the 
+// <linkto class=ComponentShape>ComponentShape</linkto> class, there are three
+// more mandatory fields called; majoraxis, minoraxis & positionangle. These
+// fields are record representation of Quantum<Double> objects and hence
+// contain a value and unit. eg., A typical record for any of the shapes
+// derived from this class might be:
+// <srcblock>
+// c := [type = 'gaussian',
+//       direction = [type = 'direction',
+//                    refer = 'b1950',
+//                    m0 = [value = .1, unit = 'rad']
+//                    m1 = [value = -.1, unit = 'rad']
+//                   ],
+//       majoraxis = [value=.5, unit='arcmin'],
+//       minoraxis = [value=1, unit='arcsec'],
+//       positionangle = [value=10, unit='deg']
+//      ]
+// </srcblock>
 // </synopsis>
 //
 // <example>
+
+// Because this is an abstract base class, an actual instance of this class
+// cannot be constructed. However the interface it defines can be used inside a
+// function. This allows functions which have TwoSidedShapes as arguments to
+// work for any derived class. Hence the printShape function shown below will
+// work for GaussianShapes and DiskShapes but not for PointShapes.
+
+// In this example the printShape function prints out the type of model it is
+// working with, the reference direction of that model, the major-axis width,
+// minor-axis width and the position angle. This example is also available in
+// the <src>dGaussianShape.cc</src> file.
+
+// <srcblock>
+//  void printShape(const TwoSidedShape& theShape) {
+//    cout << "This is a " << ComponentType::name(theShape.type())
+//         << " shape " << endl 
+//         << "with a reference direction of "
+//         << theShape.refDirection() << endl
+//         << "and a major axis of " << theShape.majorAxis() << endl
+//         << "minor axis of " << theShape.minorAxis() << endl
+//         << "and position angle of " << theShape.positionAngle() << endl;
+//  }
+//  
+// </srcblock>
 // </example>
 //
+
+// <motivation>
+// This base class was created to allow the reuse of a number of common
+// functions by all derived classes.
+// </motivation>
+
 // <todo asof="1997/07/16">
 //   <li> Nothing so far
 // </todo>
 
-// <linkfrom anchor="TwoSidedCompRep" classes="SkyCompRep TwoSidedComponent">
-//  <here>TwoSidedCompRep</here> - a twoSided component with copy semantics
+// <linkfrom anchor="TwoSidedShape" classes="ComponentShape">
+//  <here>TwoSidedShape</here> - a base class for shapes with two sides
 // </linkfrom>
 
 class TwoSidedShape: public ComponentShape
@@ -76,7 +155,7 @@ public:
   // derived class will be used.
   virtual ~TwoSidedShape();
 
-  // return the actual component shape.
+  // return the actual component type.
   virtual ComponentType::Shape type() const = 0;
 
   // set/get the width and orientation of the Shape. The width of the major
@@ -115,53 +194,69 @@ public:
   virtual Double sample(const MDirection& direction, 
 			const MVAngle& pixelSize) const = 0;
 
-  // Calculate the amount of flux that is in the pixels of specified, constant
-  // size centered on the specified directions. The returned values will always
-  // be between zero and one (inclusive). All the supplied directions must have
-  // the same reference frame (that is specified in the refFrame argument).
+  // Same as the previous function except that many directions can be sampled
+  // at once. The reference frame and pixel size must be the same for all the
+  // specified directions. A default implementation of this function is
+  // available that uses the single pixel sample function described above.
+  // However customised versions of this function will be more efficient as
+  // intermediate values only need to be computed once.
   virtual void sample(Vector<Double>& scale, 
 		      const Vector<MDirection::MVType>& directions, 
 		      const MDirection::Ref& refFrame,
 		      const MVAngle& pixelSize) const = 0;
 
-  // Return the Fourier transform of the component at the specified twoSided in
-  // the spatial frequency domain. The point is specified by a 3 element vector
+  // Return the Fourier transform of the component at the specified point in
+  // the spatial frequency domain. The point is specified by a 3-element vector
   // (u,v,w) that has units of meters and the frequency of the observation, in
   // Hertz. These two quantities can be used to derive the required spatial
   // frequency <src>(s = uvw*freq/c)</src>. The w component is not used in
-  // these functions.
+  // these functions.  The scale factor returned by this function can be used
+  // to scale the flux at the origin of the Fourier plane in order to determine
+  // the visibility at the specified point.
 
-  // The reference position for the transform is the direction of the
-  // component. As this component is symmetric about this point the transform
-  // is always a real value.
+  // The "origin" of the transform is the reference direction of the
+  // component. This means for symmetric components, where the reference
+  // direction is at the centre, that the Fourier transform will always be
+  // real.
+  virtual DComplex visibility(const Vector<Double>& uvw,
+			      const Double& frequency) const = 0;
 
-  // The total flux of the component must be supplied in the flux variable and
-  // the corresponding visibility is returned in the same variable.
-  virtual void visibility(Flux<Double>& flux, const Vector<Double>& uvw,
+  // Same as the previous function except that many (u,v,w) points can be
+  // sampled at once. The observation frequency is the same for all the
+  // specified points. The uvw Matrix must have first dimension of three and
+  // the second dimension must match the length of the scale vector. A default
+  // implementation of this function is available that uses the single point
+  // visibility function described above.  However customised versions of this
+  // function may be more efficient as intermediate values only need to be
+  // computed once.
+  virtual void visibility(Vector<DComplex>& scale, const Matrix<Double>& uvw,
 			  const Double& frequency) const = 0;
+
+  // determine whether the shape is symmetric or not. Always returns True.
+  virtual Bool isSymmetric() const;
 
   // Return a pointer to a copy of this object upcast to a ComponentShape
   // object. The class that uses this function is responsible for deleting the
   // pointer. This is used to implement a virtual copy constructor.
   virtual ComponentShape* clone() const = 0;
 
-  // set/get the shape parameters associated with the TwoSided. There are three
-  // these being in order: the major axis, the minor axis and the position
-  // angle. All these angular quantities are specified in radians. The Vector
-  // supplied to the setParameters function must have three elements and the
-  // Vector supplied to and returned by the parameters function will have three
-  // elements.
+  // set/get the shape parameters associated with this shape. There are always
+  // three these being in order: the major-axis, the minor-axis and the
+  // position angle. All these angular quantities are specified and returned in
+  // radians. The Vector supplied to the <src>setParameters</src> function must
+  // have three elements and the Vector supplied to and returned by the
+  // <src>parameters</src> function will have three elements.
   // <group>
   virtual uInt nParameters() const;
   virtual void setParameters(const Vector<Double>& newParms);
   virtual void parameters(Vector<Double>& compParms) const;
   // </group>
 
-  // This functions convert between a RecordInterface and a
-  // TwoSidedShape. These functions define how a TwoSidedShape is represented
-  // in glish and this is detailed in the synopsis above.  They return False if
-  // the record is malformed and append an error message to the supplied string
-  // giving the reason.
+  // This functions convert between a Record and a shape derived from this
+  // class. These functions define how the object is represented in glish and
+  // this is detailed in the synopsis above.  They return False if the record
+  // is malformed and append an error message to the supplied string giving the
+  // reason.
   // <group>
   virtual Bool fromRecord(String& errorMessage,
 			  const RecordInterface& record);
@@ -171,10 +266,12 @@ public:
 
   // Convert the parameters of the component to the specified units. The
   // supplied record must have three fields, namely 'majoraxis', 'minoraxis' &
-  // 'positionangle'. These fileds must contains strings that are angular
-  // units and this function will convert the corresponding parameters to the
+  // 'positionangle'. These fields must contains strings that are angular units
+  // and this function will convert the corresponding parameters to the
   // specified units. This will have no effect on the shape of this class but
-  // will affect the format of the record returned by the toRecord function. 
+  // will affect the format of the record returned by the toRecord function,
+  // and the units used in the the Quanta returned by the majoraxis, minoraxis
+  // & positionangle functions.
   virtual Bool convertUnit(String& errorMessage,
 			   const RecordInterface& record);
 
@@ -184,27 +281,32 @@ public:
   virtual Bool ok() const;
 
 protected:
-  // The default TwoSidedShape is at the J2000 North Pole.
+  // The constructors and assignment operator are protected as only derived
+  // classes should use them.
+  // <group>
+  //# The default TwoSidedShape is at the J2000 North Pole.
   TwoSidedShape();
 
-  // Construct a TwoSidedShape at the specified direction.
+  //# Construct a TwoSidedShape at the specified direction, and return all all
+  //# widths using the specified units.
   TwoSidedShape(const MDirection& direction, const Unit& majorAxisUnit, const
 		Unit& minorAxisUnit, const Unit& paUnit);
 
-  // The copy constructor uses copy semantics.
+  //# The copy constructor uses copy semantics.
   TwoSidedShape(const TwoSidedShape& other);
 
-  // The assignment operator uses copy semantics.
+  //# The assignment operator uses copy semantics.
   TwoSidedShape& operator=(const TwoSidedShape& other);
+  // </group>
 
-  // Set the units for the major axis, minor axis and position angle.
+  //# Set the units for the major axis, minor axis and position angle.
   // <group>
   void majorAxisUnit(const Unit& majorAxisUnit);
   void minorAxisUnit(const Unit& minorAxisUnit);
   void paUnit(const Unit& paUnit);
   // </group>
 
-  // Get the current units for the major axis, minor axis or position angle.
+  //# Get the current units for the major axis, minor axis or position angle.
   // <group>
   const Unit& majorAxisUnit();
   const Unit& minorAxisUnit();
