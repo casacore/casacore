d913517198d4e45c11dc44a3eac79bc55c85c9ea
Ralph Marson <rmarson@nrao.edu>
1999-11-12 00:43:54 +0000

    Updated the documentation and the visibility function signature

--- code/components/implement/ComponentModels/PointShape.h
+++ code/components/implement/ComponentModels/PointShape.h
@@ -32,19 +32,19 @@
 #include <aips/aips.h>
 #include <trial/ComponentModels/ComponentShape.h>
 #include <trial/ComponentModels/ComponentType.h>
+#include <aips/Mathematics/Complex.h>
 
 class MVAngle;
 class MDirection;
-//class MVDirection;
 class RecordInterface;
 class String;
 template <class T> class Vector;
 
-// <summary>A point model for the spatial distribution of emission</summary>
+// <summary>A shape where emission comes from only one direction</summary>
 
 // <use visibility=export> 
 
-// <reviewed reviewer="" date="yyyy/mm/dd" tests="" demos="">
+// <reviewed reviewer="" date="yyyy/mm/dd" tests="tPointShape" demos="dPointShape">
 // </reviewed>
 
 // <prerequisite>
@@ -53,39 +53,42 @@ template <class T> class Vector;
 
 // <synopsis> 
 
-// A PointShape object models the spatial distribution of radiation from the
-// sky as a point source.
+// This class represents the shape of components where the emission comes from
+// only one point in the sky. 
 
-// The reference direction is defined in celestial co-ordinates, using a
-// <linkto class=MDirection>MDirection</linkto> object. It indicates where the
-// point is on the sky. The direction can be specified both in the constructor
-// or with the <src>setRefDirection</src> function.
+// This class like the other component shapes becomes more useful when used
+// through the <linkto class=SkyComponent>SkyComponent</linkto> class, which
+// incorperates the flux and spectral variation of the emission, or through the
+// <linkto class=ComponentList>ComponentList</linkto> class, which handles
+// groups of SkyComponent objects.
 
-// The flux, or integrated intensity, is always one. This class does not model
-// the actual flux or its variation with frequency. It solely models the way
-// the emission varies with position on the sky.
+// The reference direction for a point shape is the direction at which all the
+// emission comes from and is defined in celestial co-ordinates, using a
+// <linkto class=MDirection>MDirection</linkto> object.  The direction can be
+// specified both in the constructor or with the <src>setRefDirection</src>
+// function.
 
-// The <src>scale</src> member function is used to sample the component at any
-// point on the sky. The scale factor calculated by this function is the
-// proportion of the flux that is within a specified pixel size centered on the
-// specified direction. It will return a value of one if the sample direction
-// is co-incident, to within half a pixel with the direction of the point
-// source. Otherwise it will return zero.
+// The <src>sample</src> member functions are used to determine the proportion
+// of flux the component at any point on the sky. For a point component this is
+// either zero or one depending on whether the specified pixel contains the
+// point source or not.
 
-// This class contains functions that return the Fourier transform of the
-// component at a specified spatial frequency. There are described more fully
-// in the description of the <src>visibility</src> functions below.
+// The <src>visibility</src> functions that return the Fourier transform of the
+// component at a specified spatial frequency. For a point shape the Fourier
+// transform is a constant value. Hence these functions return one, regardless
+// of the input parameters.
 
-// This class also contains functions which perform the conversion between
-// Records and PointShape objects. This defines how a PointShape object is
+// This class also contains functions (<src>toRecord</src> &
+// <src>fromRecord</src>) which perform the conversion between Records and
+// PointShape objects. These functions define how a PointShape object is
 // represented in glish. The format of the record that is generated and
 // accepted by these functions is:
 // <srcblock>
-// c := [type = "point",
-//       direction = [type = "direction",
-//                    refer = "j2000",
-//                    m0 = [value = 0, unit = "deg"]
-//                    m1 = [value = 0, unit = "deg"]
+// c := [type = 'point',
+//       direction = [type = 'direction',
+//                    refer = 'j2000',
+//                    m0 = [value = 0, unit = 'deg']
+//                    m1 = [value = 0, unit = 'deg']
 //                   ]
 //      ]
 // </srcblock>
@@ -93,59 +96,57 @@ template <class T> class Vector;
 // and its format is defined in the Measures module. Its refer field defines
 // the reference frame for the direction and the m0 and m1 fields define the
 // latitude and longitude in that frame.
-
 // </synopsis>
 //
+
 // <example>
 // Suppose I had an image of a region of the sky and we wanted to subtract
 // a point source from it. This could be done as follows:
-// <ul> 
-// <li> Construct a PointCompRep to represent the point source
+// <li> 
+// <li> Construct a SkyComponent with to represent the point source
 // <li> Project the component onto an image
 // <li> Convolve the image by the point spread function
 // <li> subtract the convolved model from the dirty image.
-// </ul>
-// Shown below is the code to perform the first two steps in this process. See
-// the <linkto class=Convolver>Convolver</linkto> class and the
-// <linkto module=Lattices>Lattice</linkto> module for the functions necessary
-// to perform the last two items. This example is also available in the
-// <src>dPointCompRep.cc</src> file.
+// </li>
+// Shown below is the code to perform the first step in this process, ie
+// construct the SkyComponent. This example is also available in the
+// <src>dPointShape.cc</src> file.  Note that it is more accurate to do
+// subtraction of point components in the (u,v) domain
 // <srcblock>
-// Quantity J1934_ra = Quantity(19.0/24*360, "deg") + Quantity(39, "'");
-// Quantity J1934_dec = Quantity(-63, "deg") + Quantity(43, "'");
-// MDirection J1934_dir(J1934_ra, J1934_dec, MDirection::J2000);
-// Flux<Double> J1934_flux(6.28, 0.1, 0.15, 0.01);
-// PointCompRep J1934(J1934_flux, J1934_dir);
-// // This component can now be projected onto an image
-// CoordinateSystem coords;
-// {
-//   Double pixInc = Quantity(1, "''").getValue("rad");
-//   Matrix<Double> xform(2,2);
-//   xform = 0.0; xform.diagonal() = 1.0;
-//   Double refPixel = 32.0;
-//   DirectionCoordinate dirCoord(MDirection::J2000,
-// 				 Projection(Projection::SIN),
-// 				 J1934_ra.getValue("rad"),
-// 				 J1934_dec.getValue("rad"),
-// 				 pixInc , pixInc, xform,
-// 				 refPixel, refPixel);
-//   coords.addCoordinate(dirCoord);
-// }
-// CoordinateUtil::addIQUVAxis(coords);
-// CoordinateUtil::addFreqAxis(coords);
-// PagedImage<Float> skyModel(IPosition(4,64,64,4,8), coords, 
-// 			     "model_tmp.image");
-// skyModel.set(0.0f);
-// J1934.project(skyModel);
+//  MDirection J1934_dir;
+//  { // get the right direction into J1934_dir
+//    Quantity J1934_ra; MVAngle::read(J1934_ra, "19:39:");
+//    Quantity J1934_dec; MVAngle::read(J1934_dec, "-63.43.");
+//    J1934_dir = MDirection(J1934_ra, J1934_dec, MDirection::J2000);
+//  }
+//  { // One way to construct the SkyComponent
+//    SkyComponent J1934(ComponentType::POINT, ComponentType::CONSTANT_SPECTRUM);
+//    J1934.shape().setRefDirection(J1934_dir);
+//    J1934.flux() = Flux<Double>(6.28, 0.1, 0.15, 0.01);
+//    printShape(J1934.shape());
+//  }
+//  { // An alternative way to construct the SkyComponent
+//    const Flux<Double> flux(6.28, 0.1, 0.15, 0.01);
+//    const PointShape shape(J1934_dir);
+//    const ConstantSpectrum spectrum;
+//    SkyComponent component(flux, shape, spectrum);
+//    printShape(component.shape());
+//  }
 // </srcblock>
+// Note how the member functions of the PointShape class (the setDirection
+// function) are accessable through the shape function in the SkyComponent
+// class. The printShape function is the example shown for the ComponentShape
+// class.
 // </example>
 //
-// <todo asof="1997/07/16">
-//   <li> Nothing so far
+// <todo asof="1999/11/11">
+//   <li> Use Measures & Quanta in the interface to the visibility functions.
 // </todo>
 
-// <linkfrom anchor="PointCompRep" classes="SkyCompRep PointComponent">
-//  <here>PointCompRep</here> - a point component with copy semantics
+// <linkfrom anchor="PointShape" 
+//           classes="ComponentShape GaussianShape DiskShape">
+//  <here>PointShape</here>
+// - a shape where emission comes from only one direction
 // </linkfrom>
 
 class PointShape: public ComponentShape
@@ -153,35 +154,33 @@ class PointShape: public ComponentShape
 public:
   // The default PointShape is at the J2000 North Pole.
   PointShape();
-
+  
   // Construct a point shape at the specified direction.
-  // flux argument.
   PointShape(const MDirection& direction);
-
+  
   // The copy constructor uses copy semantics.
   PointShape(const PointShape& other);
 
-  // The destructor does nothing.
+  // The destructor does nothing special.
   virtual ~PointShape();
 
   // The assignment operator uses copy semantics.
   PointShape& operator=(const PointShape& other);
 
-  // get the shape of the component. This function always returns
+  // Return the type of shape. This function always returns
   // ComponentType::POINT.
   virtual ComponentType::Shape type() const;
 
   // Calculate the proportion of the flux that is in a pixel of the specified
-  // size centerd on the specified direction. Because this is a point shape the
-  // returned value is either zero or one.  It is one if the specified
+  // size centered on the specified direction. Because this is a point shape
+  // the returned value is either zero or one.  It is one if the specified
   // direction is less than half a pixelSize away from the reference direction.
   virtual Double sample(const MDirection& direction, 
 			const MVAngle& pixelSize) const;
 
-  // Calculate the amount of flux that is in the pixels of specified, constant
-  // size centered on the specified directions. The returned values will always
-  // be between zero and one (inclusive). All the supplied directions must have
-  // the same reference frame (that is specified in the refFrame argument).
+  // Same as the previous function except that many directions can be sampled
+  // at once. The reference frame and pixel size must be the same for all the
+  // specified directions. This is a customised version.
   virtual void sample(Vector<Double>& scale, 
 		      const Vector<MDirection::MVType>& directions, 
 		      const MDirection::Ref& refFrame, 
@@ -195,34 +194,42 @@ public:
   // these functions.
 
   // The reference position for the transform is the direction of the
-  // component. Hence the transform is always a constant real value of one.
+  // component. Hence the returned value is always a constant real value of
+  // one.  The input arguments are ignored except in debug mode where the
+  // length of the uvw Vector and sign of the frequency variable are checked.
+  virtual DComplex visibility(const Vector<Double>& uvw,
+			      const Double& frequency) const;
 
-  // The total flux of the component must be supplied in the flux variable and
-  // the corresponding visibility is returned in the same variable.
-  virtual void visibility(Flux<Double>&, const Vector<Double>&,
-			  const Double&) const;
+  // Same as the previous function except that many (u,v,w) points can be
+  // sampled at once. As with the previous function the returned value is
+  // always a constant real vector of one.  The input arguments are ignored
+  // except in debug mode where the shape of the uvw Matrix and the scale
+  // Vector are checked as is the  sign of the frequency variable.
+  virtual void visibility(Vector<DComplex>& scale, const Matrix<Double>& uvw,
+			  const Double& frequency) const;
+
+  // A point shape is symmetric so this function always returns True;
+  virtual Bool isSymmetric() const;
 
   // Return a pointer to a copy of this object upcast to a ComponentShape
   // object. The class that uses this function is responsible for deleting the
   // pointer. This is used to implement a virtual copy constructor.
   virtual ComponentShape* clone() const;
 
-  // return the number of parameters in this shape and set/get them.
-  //
-  // As this is a point shape there are none. So calling
-  // <src>setShapeParameters</src> or <src>shapeParameters</src> with anything
-  // other than a zero length Vector will throw an exception.
+  // return the number of parameters in this shape and set/get them.  As this
+  // is a point shape there are none. So calling <src>setParameters</src> or
+  // <src>parameters</src> with anything other than a zero length Vector will
+  // throw an exception (when compiled in debug mode).
   // <group>
   virtual uInt nParameters() const;
   virtual void setParameters(const Vector<Double>& newParms);
   virtual void parameters(Vector<Double>& compParms) const;
   // </group>
 
-  // This functions convert between a RecordInterface and a PointShape. These
-  // functions define how a point shape is represented in glish and this is
-  // detailed in the synopsis above.  They return False if the RecordInterface
-  // is malformed and append an error message to the supplied string giving the
-  // reason.
+  // This functions convert between a Record and a PointShape. These functions
+  // define how a point shape is represented in glish and this is detailed in
+  // the synopsis above.  They return False if the supplied Record is malformed
+  // and append an error message to the supplied String giving the reason.
   // <group>
   virtual Bool fromRecord(String& errorMessage,
 			  const RecordInterface& record);
@@ -234,10 +241,8 @@ public:
   // True.
   virtual Bool convertUnit(String&, const RecordInterface&);
 
-  // Function which checks the internal data of this class for correct
-  // dimensionality and consistent values. Returns True if everything is fine
-  // otherwise returns False.
+  // Function which checks the internal data of this class for consistent
+  // values. Returns True if everything is fine otherwise returns False.
   virtual Bool ok() const;
-
 };
 #endif
