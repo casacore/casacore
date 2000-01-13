91435fdb8b26e91f1c3bdc226e6e3e5a820c5b5c
Mark Holdaway <mholdawa@nrao.edu>
2000-01-13 23:09:08 +0000

    only copyright; change had already been made

--- code/lattices/implement/Lattices/LatticeCleaner.h
+++ code/lattices/implement/Lattices/LatticeCleaner.h
@@ -1,5 +1,5 @@
 //# Cleaner.h: this defines Cleaner a class for doing convolution
-//# Copyright (C) 1996,1997,1998,1999
+//# Copyright (C) 1996,1997,1998,1999,2000
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
