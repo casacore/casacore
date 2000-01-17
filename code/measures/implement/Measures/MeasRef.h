7a6cfc470416d4981df37dea48a6322e7c58f844
Wim Brouw <brouw@astron.nl>
2000-01-17 21:44:26 +0000

    *** empty log message ***

--- code/measures/implement/Measures/MeasRef.h
+++ code/measures/implement/Measures/MeasRef.h
@@ -1,5 +1,5 @@
 //# MeasRef.h: Reference frame for physical measures
-//# Copyright (C) 1995,1996,1997,1999
+//# Copyright (C) 1995,1996,1997,1999,2000
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
@@ -38,7 +38,7 @@
 class String;
 template <class Ms> class MeasRef;
 #if defined(AIPS_STDLIB)
-#include <iosfwd.h>
+#include <iosfwd>
 #else
 class ostream;
 #endif
