d9a425fcff5e82338662abd5cf50d4670465b823
Ger van Diepen <gervandiepen@gmail.com>
2000-01-19 15:03:58 +0000

    Added quite some test for new functions

--- code/lattices/implement/Lattices/test/tSubLattice.cc
+++ code/lattices/implement/Lattices/test/tSubLattice.cc
@@ -1,5 +1,5 @@
 //# tSubLattice.cc: Test program for class SubLattice
-//# Copyright (C) 1998,1999
+//# Copyright (C) 1998,1999,2000
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This program is free software; you can redistribute it and/or modify it
@@ -27,8 +27,10 @@
 
 #include <trial/Lattices/SubLattice.h>
 #include <trial/Lattices/ArrayLattice.h>
+#include <trial/Lattices/PagedArray.h>
 #include <trial/Lattices/LatticeIterator.h>
 #include <trial/Lattices/LatticeStepper.h>
+#include <trial/Lattices/LCPagedMask.h>
 #include <aips/Arrays/Vector.h>
 #include <aips/Arrays/ArrayMath.h>
 #include <aips/Arrays/ArrayLogical.h>
@@ -64,6 +66,162 @@ void testVectorROIter (const Lattice<Int>& sublat,
 }
 
 
+void testRest()
+{
+    PagedArray<Int> pa(IPosition(1,10), "tSubLattice_tmp.pa");
+    AlwaysAssertExit (pa.isPaged());
+    AlwaysAssertExit (pa.isPersistent());
+    AlwaysAssertExit (pa.isWritable());
+    AlwaysAssertExit (pa.name(True) == "tSubLattice_tmp.pa");
+    LCPagedMask mask(IPosition(1,10), "tSubLattice_tmp.pa/mask");
+    Slicer slicer(IPosition(1,1), IPosition(1,3));
+    Slicer slfull(IPosition(1,0), IPosition(1,10));
+    {
+        // A SubLattice as a Lattice copy (RO).
+	SubLattice<Int> sl(pa);
+	AlwaysAssertExit (!sl.isMasked());
+	AlwaysAssertExit (!sl.hasPixelMask());
+	AlwaysAssertExit (sl.isPaged());
+	AlwaysAssertExit (sl.isPersistent());
+	AlwaysAssertExit (!sl.isWritable());
+	AlwaysAssertExit (!sl.isMaskWritable());
+	// A copy of the SubLattice.
+	SubLattice<Int> sl1(sl, True);
+	AlwaysAssertExit (!sl1.isMasked());
+	AlwaysAssertExit (!sl1.hasPixelMask());
+	AlwaysAssertExit (sl1.isPaged());
+	AlwaysAssertExit (sl1.isPersistent());
+	AlwaysAssertExit (!sl1.isWritable());
+	AlwaysAssertExit (!sl1.isMaskWritable());
+	AlwaysAssertExit (sl1.name(True) == "tSubLattice_tmp.pa");
+    }
+    {
+        // A SubLattice as a Lattice copy (RW).
+	SubLattice<Int> sl(pa, True);
+	AlwaysAssertExit (!sl.isMasked());
+	AlwaysAssertExit (!sl.hasPixelMask());
+	AlwaysAssertExit (sl.isPaged());
+	AlwaysAssertExit (sl.isPersistent());
+	AlwaysAssertExit (sl.isWritable());
+	AlwaysAssertExit (!sl.isMaskWritable());
+	// A RW copy of the SubLattice.
+	SubLattice<Int> sl1(sl, True);
+	AlwaysAssertExit (!sl1.isMasked());
+	AlwaysAssertExit (!sl1.hasPixelMask());
+	AlwaysAssertExit (sl1.isPaged());
+	AlwaysAssertExit (sl1.isPersistent());
+	AlwaysAssertExit (sl1.isWritable());
+	AlwaysAssertExit (!sl1.isMaskWritable());
+	// A RO copy of the SubLattice.
+	SubLattice<Int> sl2(sl, False);
+	AlwaysAssertExit (!sl2.isMasked());
+	AlwaysAssertExit (!sl2.hasPixelMask());
+	AlwaysAssertExit (sl2.isPaged());
+	AlwaysAssertExit (sl2.isPersistent());
+	AlwaysAssertExit (!sl2.isWritable());
+	AlwaysAssertExit (!sl2.isMaskWritable());
+	AlwaysAssertExit (sl2.name(True) == "tSubLattice_tmp.pa");
+    }
+    {
+        // A RO SubLattice as a masked Lattice.
+	SubLattice<Int> sl(pa, mask);
+	AlwaysAssertExit (sl.isMasked());
+	AlwaysAssertExit (!sl.hasPixelMask());
+	AlwaysAssertExit (sl.isPaged());
+	AlwaysAssertExit (!sl.isPersistent());
+	AlwaysAssertExit (!sl.isWritable());
+	AlwaysAssertExit (!sl.isMaskWritable());
+	// A copy of the SubLattice.
+	SubLattice<Int> sl1(sl, True);
+	AlwaysAssertExit (sl1.isMasked());
+	AlwaysAssertExit (!sl1.hasPixelMask());
+	AlwaysAssertExit (sl1.isPaged());
+	AlwaysAssertExit (!sl1.isPersistent());
+	AlwaysAssertExit (!sl1.isWritable());
+	AlwaysAssertExit (!sl1.isMaskWritable());
+	AlwaysAssertExit (sl1.name(True) == "tSubLattice_tmp.pa");
+    }
+    {
+        // A RW SubLattice as a masked Lattice.
+	SubLattice<Int> sl(pa, mask, True);
+	AlwaysAssertExit (sl.isMasked());
+	AlwaysAssertExit (!sl.hasPixelMask());
+	AlwaysAssertExit (sl.isPaged());
+	AlwaysAssertExit (!sl.isPersistent());
+	AlwaysAssertExit (sl.isWritable());
+	AlwaysAssertExit (sl.isMaskWritable());
+	// A RW copy of the SubLattice.
+	SubLattice<Int> sl1(sl, True);
+	AlwaysAssertExit (sl1.isMasked());
+	AlwaysAssertExit (!sl1.hasPixelMask());
+	AlwaysAssertExit (sl1.isPaged());
+	AlwaysAssertExit (!sl1.isPersistent());
+	AlwaysAssertExit (sl1.isWritable());
+	AlwaysAssertExit (!sl1.isMaskWritable());
+	// A RO copy of the SubLattice.
+	SubLattice<Int> sl2(sl, False);
+	AlwaysAssertExit (sl2.isMasked());
+	AlwaysAssertExit (!sl2.hasPixelMask());
+	AlwaysAssertExit (sl2.isPaged());
+	AlwaysAssertExit (!sl2.isPersistent());
+	AlwaysAssertExit (!sl2.isWritable());
+	AlwaysAssertExit (!sl2.isMaskWritable());
+    }
+    {
+        // A small region of a lattice.
+	SubLattice<Int> sl(pa, slicer, True);
+	AlwaysAssertExit (!sl.isMasked());
+	AlwaysAssertExit (!sl.hasPixelMask());
+	AlwaysAssertExit (sl.isPaged());
+	AlwaysAssertExit (!sl.isPersistent());
+	AlwaysAssertExit (sl.isWritable());
+	AlwaysAssertExit (!sl.isMaskWritable());
+	// A RW copy of the SubLattice.
+	SubLattice<Int> sl1(sl, True);
+	AlwaysAssertExit (!sl1.isMasked());
+	AlwaysAssertExit (!sl1.hasPixelMask());
+	AlwaysAssertExit (sl1.isPaged());
+	AlwaysAssertExit (!sl1.isPersistent());
+	AlwaysAssertExit (sl1.isWritable());
+	AlwaysAssertExit (!sl1.isMaskWritable());
+	// A RO copy of the SubLattice.
+	SubLattice<Int> sl2(sl, False);
+	AlwaysAssertExit (!sl2.isMasked());
+	AlwaysAssertExit (!sl2.hasPixelMask());
+	AlwaysAssertExit (sl2.isPaged());
+	AlwaysAssertExit (!sl2.isPersistent());
+	AlwaysAssertExit (!sl2.isWritable());
+	AlwaysAssertExit (!sl2.isMaskWritable());
+    }
+    {
+        // A full region of a lattice.
+	SubLattice<Int> sl(pa, slfull, True);
+	AlwaysAssertExit (!sl.isMasked());
+	AlwaysAssertExit (!sl.hasPixelMask());
+	AlwaysAssertExit (sl.isPaged());
+	AlwaysAssertExit (sl.isPersistent());
+	AlwaysAssertExit (sl.isWritable());
+	AlwaysAssertExit (!sl.isMaskWritable());
+	// A RW copy of the SubLattice.
+	SubLattice<Int> sl1(sl, True);
+	AlwaysAssertExit (!sl1.isMasked());
+	AlwaysAssertExit (!sl1.hasPixelMask());
+	AlwaysAssertExit (sl1.isPaged());
+	AlwaysAssertExit (sl1.isPersistent());
+	AlwaysAssertExit (sl1.isWritable());
+	AlwaysAssertExit (!sl1.isMaskWritable());
+	// A RO copy of the SubLattice.
+	SubLattice<Int> sl2(sl, False);
+	AlwaysAssertExit (!sl2.isMasked());
+	AlwaysAssertExit (!sl2.hasPixelMask());
+	AlwaysAssertExit (sl2.isPaged());
+	AlwaysAssertExit (sl2.isPersistent());
+	AlwaysAssertExit (!sl2.isWritable());
+	AlwaysAssertExit (!sl2.isMaskWritable());
+    }
+}
+
+
 main ()
 {
   try {
@@ -75,6 +233,9 @@ main ()
       Slicer slicer(IPosition(4,4,2,1,3), IPosition(4,14,10,3,23),
 		    IPosition(4,2,3,1,4), Slicer::endIsLast);
       SubLattice<Int> sublat (lattice, slicer, True);
+      AlwaysAssertExit (!sublat.isPaged());
+      AlwaysAssertExit (!sublat.isPersistent());
+      AlwaysAssertExit (!sublat.isMasked());
       AlwaysAssertExit (sublat.isWritable());
       AlwaysAssertExit (sublat.shape() == slicer.length());
       Array<Int> arr1, arr2;
@@ -86,6 +247,9 @@ main ()
 			      arr(slicer.start(), slicer.end(),
 				  slicer.stride())));
       testVectorROIter (sublat, lattice, slicer);
+
+      // Test some other SubLattice functions.
+      testRest();
     }
   } catch (AipsError x) {
     cerr << "Caught exception: " << x.getMesg() << endl;
