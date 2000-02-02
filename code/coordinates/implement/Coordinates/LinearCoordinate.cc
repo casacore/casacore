90c25a7b73460455d5ad621cd237ab107183957c
Neil Killeen <nkilleen@unimelb.edu.au>
2000-02-02 14:31:26 +0000

    doc. change

--- code/coordinates/implement/Coordinates/LinearCoordinate.cc
+++ code/coordinates/implement/Coordinates/LinearCoordinate.cc
@@ -1,5 +1,5 @@
-//# <LinearCoordinate.h>: this defines LinearCoordinate
-//# Copyright (C) 1997,1998,1999
+//# LinearCoordinate.cc: this defines LinearCoordinate
+//# Copyright (C) 1997,1998,1999,2000
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
