0277595118bf1a1ee454cc2b41fb17174bed4b9c
Neil Killeen <nkilleen@unimelb.edu.au>
2000-02-18 04:53:10 +0000

    reimplement with IQ > 20

--- code/coordinates/implement/Coordinates/Spectral2Coordinate.cc
+++ code/coordinates/implement/Coordinates/Spectral2Coordinate.cc
@@ -37,6 +37,17 @@
 Bool SpectralCoordinate::toWorld(MFrequency& world, 
 				 Double pixel) const
 {
+    static MVFrequency world_tmp;
+    Bool ok = toWorld(world_tmp, pixel);
+    if (ok) {
+       world.set(world_tmp, type_p);
+    }
+    return ok;
+}
+
+Bool SpectralCoordinate::toWorld(MVFrequency& world, 
+				 Double pixel) const
+{
     static Double world_tmp;
     static Quantum<Double> q_tmp;
 //
@@ -45,41 +56,23 @@ Bool SpectralCoordinate::toWorld(MFrequency& world,
        const Unit& units = Unit(worldAxisUnits()(0));
        q_tmp.setValue(world_tmp);
        q_tmp.setUnit(units);
-       world = MFrequency(q_tmp, type_p);
+       world = MVFrequency(q_tmp);
     }
-    return ok;
-}
-
-Bool SpectralCoordinate::toWorld(MVFrequency& world, 
-				 Double pixel) const
-{
-    static MFrequency world_tmp;
-    Bool ok = toWorld(world_tmp, pixel);
-    if (ok) world = world_tmp.getValue();    // Hz
     return ok;
 }
 
 Bool SpectralCoordinate::toPixel(Double& pixel,
                                  const MFrequency& world) const
 {
-    static Double world_tmp;
-    static Quantum<Double> q_tmp;
-
-// Convert MFrequency to current units
-
-    const Unit& units = Unit(worldAxisUnits()(0));
-    world_tmp = world.get(units).getValue();
-
-// Convert to pixel
-
-    return toPixel(pixel, world_tmp);
+    return toPixel(pixel, world.getValue());
 }
  
 Bool SpectralCoordinate::toPixel(Double& pixel,
                                  const MVFrequency& world) const
 {
-   static MFrequency world_tmp;
-   world_tmp.set(world, type_p);      // MVFrequency in Hz
+   static Double world_tmp;
+   const Unit& units = Unit(worldAxisUnits()(0));
+   world_tmp = world.get(units).getValue();
    return toPixel(pixel, world_tmp);
 }
  
