f167864889468eaf4e97f99f07a8aed81d8b461a
Neil Killeen <nkilleen@unimelb.edu.au>
2000-02-22 04:26:13 +0000

    copyright only

--- code/lattices/implement/LatticeMath/Fit2D.h
+++ code/lattices/implement/LatticeMath/Fit2D.h
@@ -1,5 +1,5 @@
 //# Fit2D.h: Class to fit 2-D objects to Lattices or Arrays
-//# Copyright (C) 1997,1998,1999
+//# Copyright (C) 1997,1998,1999,2000
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
