3b1e7465fffffd318e622de59f3b4a7c619c2481
Ralph Marson <rmarson@nrao.edu>
2000-03-07 06:38:56 +0000

    Commented out just about everything.

--- code/components/implement/ComponentModels/test/tConstantSpectrum.cc
+++ code/components/implement/ComponentModels/test/tConstantSpectrum.cc
@@ -1,5 +1,5 @@
 //# tSpectralModel.cc:  tests the SpectralModel class
-//# Copyright (C) 1998,1999
+//# Copyright (C) 1998,1999,2000
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
@@ -26,32 +26,32 @@
 //# $Id$
 
 #include <aips/aips.h>
-#include <trial/ComponentModels/Flux.h>
-#include <trial/ComponentModels/SpectralIndex.h>
 #include <trial/ComponentModels/ConstantSpectrum.h>
-#include <trial/ComponentModels/SpectralModel.h>
-#include <aips/Measures/MeasureHolder.h>
-#include <aips/Arrays/Vector.h>
-#include <aips/Containers/Record.h>
-#include <aips/Containers/RecordFieldId.h>
+// #include <trial/ComponentModels/Flux.h>
+// #include <trial/ComponentModels/SpectralModel.h>
+// #include <aips/Measures/MeasureHolder.h>
+// #include <aips/Arrays/Vector.h>
+// #include <aips/Containers/Record.h>
+// #include <aips/Containers/RecordFieldId.h>
 #include <aips/Exceptions/Error.h>
 #include <aips/Exceptions/Excp.h>
-#include <aips/Measures/MFrequency.h>
-#include <aips/Quanta/MVFrequency.h>
-#include <aips/Quanta/Quantum.h>
-#include <aips/Utilities/Assert.h>
-#include <aips/Utilities/DataType.h>
-#include <aips/Glish/GlishRecord.h>
-#include <iomanip.h>
+// #include <aips/Measures/MFrequency.h>
+// #include <aips/Quanta/MVFrequency.h>
+// #include <aips/Quanta/Quantum.h>
+// #include <aips/Utilities/Assert.h>
+// #include <aips/Utilities/DataType.h>
+#include <aips/Utilities/String.h>
+// #include <aips/Glish/GlishRecord.h>
+// #include <iomanip.h>
 #include <iostream.h>
 
-void plotSpectrum(const Flux<Double> & refFlux,
-		  const SpectralModel & modelSpectrum);
+
+// void plotSpectrum(const Flux<Double> & refFlux,
+// 		  const SpectralModel & modelSpectrum);
 int main() {
   try {
-    Vector<Double> indices(4);
-    indices(0) = 1.0; indices(1) = 0.5; indices(2) = 0.5; indices(3) = -2.0; 
-    const SpectralIndex siModel(MFrequency(Quantity(1, "GHz")), indices);
+    const ConstantSpectrum spModel;
+    /*
     const Flux<Double> flux(1.0, 0.1, 0.0, 0.01);
     plotSpectrum(flux, siModel);
     {
@@ -76,6 +76,7 @@ int main() {
       AlwaysAssert(otherSiModel.refFrequency().getValue() ==
 		   siModel.refFrequency().getValue(), AipsError);
     }
+    */
   }
   catch (AipsError x) {
     cerr << x.getMesg() << endl;
@@ -86,38 +87,38 @@ int main() {
   return 0;
 }
 
-void plotSpectrum(const Flux<Double> & refFlux,
-                  const SpectralModel & modelSpectrum) {
-  cout << "This is a "
-       << ComponentType::name(modelSpectrum.type())
-       << " spectrum with a reference frequency of: "
-       << modelSpectrum.refFrequency().get("GHz") << endl
-       << modelSpectrum.refFrequency().getRef()
-       << endl;
-  Vector<Double> parms(modelSpectrum.nParameters());
-  modelSpectrum.parameters(parms);
-  cout << "The parameters are: " << parms << endl;
-  const MVFrequency step(Quantity(100.0, "MHz"));
-  MVFrequency sampleFreq = modelSpectrum.refFrequency().getValue();
-  Flux<Double> modelFlux;
-  cout << "Frequency\t I-Flux\t Q-Flux\t U-Flux\t V-Flux\n";
-  for (uInt i = 0; i < 11; i++) {
-    modelFlux = refFlux.copy();
-    modelSpectrum.sample(modelFlux,
-			 MFrequency(sampleFreq,
-				    modelSpectrum.refFrequency().getRef()));
-    modelFlux.convertPol(ComponentType::STOKES);
-    cout << setprecision(3) << sampleFreq.get("GHz")
- 	 << "\t\t " << modelFlux.value(0u).re
- 	 << "\t " << modelFlux.value(1u).re
- 	 << "\t " << modelFlux.value(2u).re
- 	 << "\t " << modelFlux.value(3u).re
- 	 << " " << modelFlux.unit().getName() << endl;
-    sampleFreq += step;
-  }
-}
+// void plotSpectrum(const Flux<Double> & refFlux,
+//                   const SpectralModel & modelSpectrum) {
+//   cout << "This is a "
+//        << ComponentType::name(modelSpectrum.type())
+//        << " spectrum with a reference frequency of: "
+//        << modelSpectrum.refFrequency().get("GHz") << endl
+//        << modelSpectrum.refFrequency().getRef()
+//        << endl;
+//   Vector<Double> parms(modelSpectrum.nParameters());
+//   modelSpectrum.parameters(parms);
+//   cout << "The parameters are: " << parms << endl;
+//   const MVFrequency step(Quantity(100.0, "MHz"));
+//   MVFrequency sampleFreq = modelSpectrum.refFrequency().getValue();
+//   Flux<Double> modelFlux;
+//   cout << "Frequency\t I-Flux\t Q-Flux\t U-Flux\t V-Flux\n";
+//   for (uInt i = 0; i < 11; i++) {
+//     modelFlux = refFlux.copy();
+//     modelSpectrum.sample(modelFlux,
+// 			 MFrequency(sampleFreq,
+// 				    modelSpectrum.refFrequency().getRef()));
+//     modelFlux.convertPol(ComponentType::STOKES);
+//     cout << setprecision(3) << sampleFreq.get("GHz")
+//  	 << "\t\t " << modelFlux.value(0u).re
+//  	 << "\t " << modelFlux.value(1u).re
+//  	 << "\t " << modelFlux.value(2u).re
+//  	 << "\t " << modelFlux.value(3u).re
+//  	 << " " << modelFlux.unit().getName() << endl;
+//     sampleFreq += step;
+//   }
+// }
 
 // Local Variables: 
-// compile-command: "gmake OPTLIB=1 tSpectralModel"
+// compile-command: "gmake OPTLIB=1 tConstantSpectrum"
 // End: 
 
