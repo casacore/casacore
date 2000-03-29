0eb4884763902ef029a1df2cb57deb3439b9c428
Neil Killeen <nkilleen@unimelb.edu.au>
2000-03-29 09:49:31 +0000

    Adjust per code review

--- code/coordinates/implement/Coordinates.h
+++ code/coordinates/implement/Coordinates.h
@@ -1,5 +1,5 @@
 //# Coordinates.h : Classes to interconvert computation positions with physical
-//# Copyright (C) 1996,1997,1998,1999
+//# Copyright (C) 1996,1997,1998,1999,2000
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
@@ -47,29 +47,34 @@
 
 // <prerequisite>
 //   <li> Knowledge of astronomical coordinate conversions in general. Probably the
-//        most single useful document is "Representations of celestial coordinates in
-//        FITS" Eric W. Greisen and Mark Calabretta. This is the "WCS" paper, and is
-//        currently in late draft, available via anonymous ftp from
-//        fits.cv.nrao.edu.
+//        best documents are the papers by Mark Calabretta and Eric Greisen.
+//        The initial draft from 1996 can be found at 
+//        http://www.atnf.csiro.au/~mcalabre.  It is this draft that the
+//        Coordinate classes are based upon.  Since then, this paper has evolved 
+//        into three which can be found at the above address, and will be published in the
+//        Astronomy and Astrophysics Supplement Series (probably in 2000).
+//        The design has changed since the initial draft.  When these papers
+//        are finalized, and the IAU has ratified the new standards, WCSLIB
+//        (Mark Calabretta's implementation of these conventions) will be
+//        revised for the new designs.  At that time, the Coordinate classes
+//        may also be revised.
 //   <li> Generic AIPS++ classes; especially those in the 
 //        <linkto module=Arrays>Arrays</linkto> module.
-//   <li> Perhaps some of the information in the
-//        <linkto module=Measures>Measures</linkto> module.
+//   <li> The <linkto module=Measures>Measures</linkto> module.
 // </prerequisite>
 //
 
-// <reviewed reviewer="" date="yyyy/mm/dd" demos="dCoordinateSystem.cc">
+// <reviewed reviewer="Peter Barnes" date="1999/12/24">
 // </reviewed>
 
 // <synopsis>
-
 // The primary notion is that a <linkto class=Coordinate>Coordinate</linkto>
-// can interconvert between a length <src>n</src> <src>Vector<Double></src> (the
-// pixel coordinate) and a length <src>m</src> <src>Vector<Double></src> (the
-// "world" coordinate). Note that <src>m</src> and <src>n</src> do not in 
+// can interconvert between a length "n" Vector<Double> (the
+// pixel coordinate) and a length "m" Vector<Double> (the
+// "world" coordinate). Note that "m" and "n" do not in 
 // principle have to be the same (so that one can get both the RA and DEC from
-//  an image slice, for example), however in practice they currently always are.
-// Each coordinate has the full mapping from pixel to world coordinates, i.e.
+// an image slice, for example), however in practice they currently always are.
+// Each Coordinate has the full mapping from pixel to world coordinates, i.e.
 // it has a reference value, reference pixel, increments, and an arbitrary
 // transformation matrix. To go from a pixel to a world coordinate the following
 // steps are applied:
@@ -78,21 +83,38 @@
 //    <li> The result is multiplied by the transformation matrix.
 //    <li> The result is multiplied by an increment per output axis to convert
 //         it into physical coordinates.
-//    <li> For some coordinate types (e.g., Direction), a nonlinear function is
+//    <li> For some coordinate types (e.g., Direction), a non-linear function is
 //         applied to this result.
 // </ol>
 //
-// A <linkto class=CoordinateSystem>CoordinateSystem</linkto> is the
-// class that application programmers will usually interact
-// with. A <src>CoordinateSystem</src> consists of one or more independent Coordinates
-// which control the mapping of various axes. Normally one group will be for
-// RA/DEC, another for a STOKES axis, and another group for the spectral axis.
-// The axes may be transposed arbitrarily,
-// for example RA could be the first axis, and DEC the third. 
+// The classes are arranged as follows.  The base class is
+// <linkto class=Coordinate>Coordinate</linkto> which defines the
+// interface.  Classes derived from it are
+// <ol>
+//   <li> <linkto class=DirectionCoordinate>DirectionCoordinate</linkto> 
+//   <li> <linkto class=LinearCoordinate>LinearCoordinate</linkto> 
+//   <li> <linkto class=SpectralCoordinate>SpectralCoordinate</linkto> 
+//   <li> <linkto class=TabularCoordinate>TabularCoordinate</linkto> 
+//   <li> <linkto class=StokesCoordinate>StokesCoordinate</linkto> 
+//   <li> <linkto class=CoordinateSystem>CoordinateCoordinate</linkto> 
+// </ol>
 //
-// Normally the CoordinateSystem being manipulated will be embedded in a <src>PagedImage</src>
-// or other object.    Note that the axes of the <src>PagedImage</src> do not
-// necessarily map directly to the axes in the <src>CoordinateSystem</src>.  Functionality
+// Other classes are <linkto class=Projection>Projection</linkto>  
+// which is used to specify an astronomical projection for 
+// DirectionCoordinates, and  <linkto class=LinearXform>LinearXform</linkto>  
+// a helper class which the application programmer  will not interact with.
+//
+// <linkto class=CoordinateSystem>CoordinateSystem</linkto> is
+// the class that application programmers will usually interact
+// with. A CoordinateSystem consists of a collection of the other 
+// classes derived from Coordinate.  Normally one group will be for
+// RA/DEC, another for a Stokes axis, and another group for the spectral axis.
+// The axes may be transposed arbitrarily, for example RA could be 
+// the first axis, and DEC the third. 
+//
+// Normally the CoordinateSystem being manipulated will be embedded in a PagedImage
+// or other object.    Note that the axes of the PagedImage do not
+// necessarily map directly to the axes in the CoordinateSystem.  Functionality
 // is provided to determine this mapping.
 //
 // One or more axes from the CoordinateSystem may be removed. Pixel axes and/or
@@ -101,10 +123,8 @@
 // <br>
 // If a world axis is removed, the corresponding pixel axis is also removed.
 // This means that one can be sure that a pixel axis always has a
-// corresponding world axis. The opposite is not necessarily true: a
-// world axis can exist without a pixel axis.
-//
-// 
+// corresponding world axis (it makes no sense otherwise).  The opposite is 
+// not necessarily true: a world axis can exist without a pixel axis.
 //
 // The linear transformation and sky projection computations are carried out in an
 // underlying library -- WCSLIB -- written by Mark Calabretta of the ATNF.
@@ -122,10 +142,14 @@
 // <srcblock>
 //    Matrix<Double> xform(2,2);                                    // 1
 //    xform = 0.0; xform.diagonal() = 1.0;                          // 2
+//    Quantum<Double> refLon(135.0, "deg");
+//    Quantum<Double> refLat(60.0, "deg");
+//    Quantum<Double> incLon(-1.0, "deg");
+//    Quantum<Double> incLat(1.0, "deg");
 //    DirectionCoordinate radec(MDirection::J2000,                  // 3
 //                            Projection(Projection::SIN),          // 4
-//                            135*C::pi/180.0, 60*C::pi/180.0,      // 5
-//                            -1*C::pi/180.0, 1*C::pi/180,          // 6
+//                            refLon, refLat,                       // 5
+//                            incLon, incLat,                       // 6
 //                            xform,                                // 7
 //                            128, 128);                            // 8
 // </srcblock>
@@ -136,17 +160,17 @@
 //         matrix.
 //    <li> <i>3:</i>This defines the astronomical type of the world 
 //         coordinate. Most of the time it will probably be J2000
-//         or B1950, but many other possibilities are possible as listed
+//         or B1950, but there are many other possibilities as listed
 //         in the <linkto class=MDirection>MDirection</linkto> class
 //         header.
 //    <li> <i>4:</i>The <linkto class=Projection>Projection</linkto> class
 //         defines the "geometry" that is used to map <src>xy<-->world</src>. SIN
 //         is the most common projection for radio interferometers. Note that
-//         SIN can optionally take parameters as defined in Calabretta+Greisen.
+//         SIN can optionally take parameters as defined in Calabretta and Greisen.
 //         If not provided, they default to 0.0, which is the "old" SIN
 //         convention.
 //    <li> <i>5:</i>Set the reference position to RA=135, DEC=60 degrees.
-//         Note that the native units of a Direction is radians.
+//         Note that the native units of a DirectionCoordinate is radians.
 //    <li> <i>6:</i> Set the increments to -1 degree in RA, and +1 degree
 //         in DEC.
 //    <li> <i>7:</i> Set the previously defined transformation matrix.
@@ -155,8 +179,9 @@
 //         coordinate has the reference value.
 // </ul>
 //
-// In this example is is more convenient to change the units to degrees. This can
-// be accomplished as follows:
+// Although we happeend to create our DirectionCoordinate with Quanta in degrees,
+// these have been converted to radians by the constructor.  We can set the native units
+// to degrees if we wish as follows:
 // <srcblock>
 //    Vector<String> units(2); units = "deg";                       //  9
 //    radec.setWorldAxisUnits(units);                               // 10
@@ -192,6 +217,7 @@
 //
 // Suppose we have an image with a Stokes axis. It can be set up as follows:
 // <srcblock>
+//    Vector<Int> iquv(4);
 //    iquv(0) = Stokes::I; iquv(1) = Stokes::Q;                    // 21
 //    iquv(2) = Stokes::U; iquv(3) = Stokes::V;                    // 22
 //    StokesCoordinate stokes(iquv);                               // 23
@@ -199,7 +225,7 @@
 // We create an integer array the same length as the Stokes axis, and place
 // the corresponding Stokes enum into each element of the array. The values
 // must be unique, e.g. there can only be one "I" plane.
-// Besides the generic <src>Vector<Double></src> toWorld/fromWorld interface,
+// Besides the generic <src>Vector<Double></src> toWorld/toPixel interface,
 // you can also directly interconvert between Stokes enum and and (zero-relative)
 // plane number:
 // <srcblock>
@@ -217,10 +243,10 @@
 // A Spectral ("frequency") coordinate may be created as follows:
 // <srcblock>
 //    SpectralCoordinate spectral(MFrequency::TOPO,               // 27
-//				1400 * 1.0E+6,                    // 28
-//				20 * 1.0E+3,                      // 29
-//				0,                                // 30
-//				1420.40575 * 1.0E+6);             // 31
+//				  1.4E+9,                         // 28
+//				  2.0E+4,                         // 29
+//				  0,                              // 30
+//				  1420.40575E+6);                 // 31
 // </srcblock>
 // The default frequency units of a spectral coordinate are Hz, although they
 // may be changed to whatever is convenient. The first line (27) defines the
@@ -233,12 +259,13 @@
 // </example>
 //
 // <motivation>
-// The primary motivation is to provide support for turning pixel locations in an
+// The primary motivation is to provide support for converting pixel locations in an
 // image to physical ("world") positions.
 // </motivation>
 
-// <todo asof="1997/01/13">
-// <li> Allow spectral axes which aren't regularly gridded in frequency?
+// <todo asof="2000/01/01">
+// <li> Add measures interfaces that handle reference frame conversions
+// <li> offset coordinates
 // </todo>
 
 // </module>
