f3d56ab50558b383ad457a48984c393688344304
Ralph Marson <rmarson@nrao.edu>
2000-04-14 07:15:37 +0000

    first tests of the Tables version of this class

--- code/ms/implement/MeasurementSets/test/tMSMainBuffer.cc
+++ code/ms/implement/MeasurementSets/test/tMSMainBuffer.cc
@@ -33,46 +33,115 @@
 #include <iostream.h>
 
 #include <trial/MeasurementSets/MSMainBuffer.h>
-#include <aips/Arrays/Vector.h>
-#include <aips/Arrays/ArrayLogical.h>
-#include <aips/Arrays/ArrayMath.h>
-#include <aips/Arrays/Array.h>
-#include <aips/Mathematics/Constants.h>
+#include <aips/Tables/Table.h>
+// #include <aips/Arrays/Vector.h>
+// #include <aips/Arrays/ArrayLogical.h>
+// #include <aips/Arrays/ArrayMath.h>
+// #include <aips/Arrays/Array.h>
+// #include <aips/Mathematics/Constants.h>
 
 int main() {
   try {
-    MSMainBuffer mainBuffer;
-    { // test the addRows, rows, nCorrelations & nChannels functions.
-      AlwaysAssert(mainBuffer.rows() == 0, AipsError);
-      mainBuffer.addRows(5);
-      AlwaysAssert(mainBuffer.rows() == 5, AipsError);
-      Matrix<Complex> data(4, 32);
-      data = Complex(0.5,1.1);
-      mainBuffer.data(0, data); 
-      AlwaysAssert(mainBuffer.nCorrelations(0) == 4, AipsError);
-      AlwaysAssert(mainBuffer.nChannels(0) == 32, AipsError);
-    }
-    { // test the antenna1 functions
-      for (uInt i = 0; i < mainBuffer.rows(); i++) {
- 	mainBuffer.antenna1(i, i+1);
+    // Check the default constructor
+    MSMainBuffer newBuffer;
+    // test the ok function.
+    AlwaysAssert(newBuffer.ok(), AipsError);
+    // test the addRow & nrow functions.
+    AlwaysAssert(newBuffer.nrow() == 0, AipsError);
+    newBuffer.addRow(20);
+    AlwaysAssert(newBuffer.ok(), AipsError);
+    AlwaysAssert(newBuffer.nrow() == 20, AipsError);
+    {
+      MSMainBuffer buffer(1);
+      { // test the addRow & nrow functions.
+    	AlwaysAssert(buffer.nrow() == 1, AipsError);
+    	buffer.addRow(4);
+    	AlwaysAssert(buffer.nrow() == 5, AipsError);
       }
-      Vector<Int> expectedResult(5);
-      for (uInt i = 0; i < mainBuffer.rows(); i++) {
- 	const Int ip1 =  static_cast<Int>(i) + 1;
-  	AlwaysAssert(mainBuffer.antenna1(i) == ip1, AipsError);
-  	expectedResult(i) = ip1;
+      { // test the antenna1 functions.
+//     	AlwaysAssert(buffer.antenna1()(0) == -1, AipsError);
+//    	AlwaysAssert(buffer.antenna1()(4) == -1, AipsError);
+//     	buffer.antenna1().put(0, 0);
+//     	buffer.antenna1().put(4, 1);
       }
-      AlwaysAssert(allEQ(mainBuffer.antenna1(), expectedResult), AipsError);
+//       { // test the polarizationId functions.
+//  	AlwaysAssert(buffer.polarizationId()(0) == -1, AipsError);
+//  	AlwaysAssert(buffer.polarizationId()(4) == -1, AipsError);
+//  	buffer.polarizationId().put(0, 1);
+//  	buffer.polarizationId().put(4, 3);
+//       }
+//       { // test the flagRow functions.
+//   	AlwaysAssert(buffer.flagRow()(0) == False, AipsError);
+//   	AlwaysAssert(buffer.flagRow()(4) == False, AipsError);
+//   	buffer.flagRow().put(4, True);
+//       }
+//       { // Check the assignment operator & copy constructor
+//    	MSDataDescBuffer otherBuffer(buffer);
+//    	AlwaysAssert(otherBuffer.ok(), AipsError);
+//    	AlwaysAssert(otherBuffer.nrow() == 5, AipsError);
+// 	newBuffer = otherBuffer;
+//    	AlwaysAssert(newBuffer.ok(), AipsError);
+// 	// Check the reference semantics by adding data here and seeing if it
+// 	// is mirrored into the newBuffer object.
+// 	buffer.spectralWindowId().put(1, 100);
+// 	buffer.polarizationId().put(1, 101);
+// 	buffer.flagRow().put(1, True);
+//       }
     }
-    { // test the complex data access functions
-      AlwaysAssert(mainBuffer.data(0).shape().isEqual(IPosition(2,4,32)),
- 		   AipsError);
-      Matrix<Complex> data(4, 32);
-      data = Complex(0.5, 1.1);
-      AlwaysAssert(allNear(mainBuffer.data(0), Complex(0.5,1.1), 
-			   C::flt_epsilon),
-		   AipsError);
-    }
+//     { // check the data has not been lost.
+//       AlwaysAssert(newBuffer.nrow() == 5, AipsError);
+//       AlwaysAssert(newBuffer.spectralWindowId()(0) ==  0, AipsError);
+//       AlwaysAssert(newBuffer.spectralWindowId()(4) ==  1, AipsError);
+//       AlwaysAssert(newBuffer.polarizationId()(0) ==  1, AipsError);
+//       AlwaysAssert(newBuffer.polarizationId()(4) ==  3, AipsError);
+//       AlwaysAssert(newBuffer.flagRow()(0) == False, AipsError);
+//       AlwaysAssert(newBuffer.flagRow()(4) == True, AipsError);
+//       // check the reference semantics
+//       AlwaysAssert(newBuffer.spectralWindowId()(1) ==  100, AipsError);
+//       AlwaysAssert(newBuffer.polarizationId()(1) ==  101, AipsError);
+//       AlwaysAssert(newBuffer.flagRow()(1) == True, AipsError);
+//     }
+//     { // Check the isValid functions
+//       AlwaysAssert(newBuffer.isValid(True) == False, AipsError);
+//       AlwaysAssert(newBuffer.isValid(3u) == False, AipsError);
+//       AlwaysAssert(newBuffer.isValid(4u) == True, AipsError);
+//       AlwaysAssert(newBuffer.isValid() == False, AipsError);
+//     }
+//     { // Check the match functions
+//     }
+
+//     MSMainBuffer mainBuffer;
+//     { // test the addRows, rows, nCorrelations & nChannels functions.
+//       AlwaysAssert(mainBuffer.rows() == 0, AipsError);
+//       mainBuffer.addRows(5);
+//       AlwaysAssert(mainBuffer.rows() == 5, AipsError);
+//       Matrix<Complex> data(4, 32);
+//       data = Complex(0.5,1.1);
+//       mainBuffer.data(0, data); 
+//       AlwaysAssert(mainBuffer.nCorrelations(0) == 4, AipsError);
+//       AlwaysAssert(mainBuffer.nChannels(0) == 32, AipsError);
+//     }
+//     { // test the antenna1 functions
+//       for (uInt i = 0; i < mainBuffer.rows(); i++) {
+//  	mainBuffer.antenna1(i, i+1);
+//       }
+//       Vector<Int> expectedResult(5);
+//       for (uInt i = 0; i < mainBuffer.rows(); i++) {
+//  	const Int ip1 =  static_cast<Int>(i) + 1;
+//   	AlwaysAssert(mainBuffer.antenna1(i) == ip1, AipsError);
+//   	expectedResult(i) = ip1;
+//       }
+//       AlwaysAssert(allEQ(mainBuffer.antenna1(), expectedResult), AipsError);
+//     }
+//     { // test the complex data access functions
+//       AlwaysAssert(mainBuffer.data(0).shape().isEqual(IPosition(2,4,32)),
+//  		   AipsError);
+//       Matrix<Complex> data(4, 32);
+//       data = Complex(0.5, 1.1);
+//       AlwaysAssert(allNear(mainBuffer.data(0), Complex(0.5,1.1), 
+// 			   C::flt_epsilon),
+// 		   AipsError);
+//     }
   }
   catch (AipsError x) {
     cerr << x.getMesg() << endl;
@@ -83,5 +152,5 @@ int main() {
 }
 
 // Local Variables: 
-// compile-command: "gmake OPTLIB=1 tMSMainBuffer"
+// compile-command: "gmake OPTLIB=1 XLIBLIST=0 tMSMainBuffer"
 // End: 
