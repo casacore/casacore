9cdc864603f085ac656b1f8f5c2c0f3b8c45d3ec
Ralph Marson <rmarson@nrao.edu>
2000-04-27 05:20:20 +0000

    Updated to test the Tables version of the class

--- code/ms/implement/MeasurementSets/test/tMSFieldBuffer.cc
+++ code/ms/implement/MeasurementSets/test/tMSFieldBuffer.cc
@@ -42,6 +42,7 @@
 #include <aips/Measures/MEpoch.h>
 
 int main() {
+  const String filename = "tMSFieldBuffer_tmp.table";
   try {
     // Check the default constructor
     MSFieldBuffer newBuffer;
@@ -53,132 +54,90 @@ int main() {
     AlwaysAssert(newBuffer.ok(), AipsError);
     AlwaysAssert(newBuffer.nrow() == 20, AipsError);
     {
-      MSFieldBuffer fieldBuffer(1);
+      MSFieldBuffer fieldBuffer;
       { // test the addRow & nrow functions.
-	AlwaysAssert(fieldBuffer.nrow() == 1, AipsError);
-	fieldBuffer.addRow(4);
-	AlwaysAssert(fieldBuffer.nrow() == 5, AipsError);
+ 	AlwaysAssert(fieldBuffer.nrow() == 0, AipsError);
+ 	fieldBuffer.addRow(1);
+ 	AlwaysAssert(fieldBuffer.nrow() == 1, AipsError);
+ 	fieldBuffer.addRow(4, 1);
+ 	AlwaysAssert(fieldBuffer.nrow() == 5, AipsError);
       }
       { // test the name functions.
-	AlwaysAssert(fieldBuffer.name(0) == String(""), AipsError);
-	AlwaysAssert(fieldBuffer.name(4) == String(""), AipsError);
-	fieldBuffer.name(0, "row 0");
-	fieldBuffer.name(4, "row 4");
+	AlwaysAssert(fieldBuffer.name()(0) == String(""), AipsError);
+	AlwaysAssert(fieldBuffer.name()(4) == String(""), AipsError);
+	fieldBuffer.name().put(0, "row 0");
+	fieldBuffer.name().put(4, "row 4");
       }
       { // test the code functions.
-	AlwaysAssert(fieldBuffer.code(0) == String(""), AipsError);
-	AlwaysAssert(fieldBuffer.code(4) == String(""), AipsError);
-	fieldBuffer.code(0, "code 0");
-	AlwaysAssert(fieldBuffer.code(0) == String("code 0"), AipsError);
-	fieldBuffer.code(4, "code 4");
-	AlwaysAssert(fieldBuffer.code(0) == String("code 0"), AipsError);
+	AlwaysAssert(fieldBuffer.code()(0) == String(""), AipsError);
+	AlwaysAssert(fieldBuffer.code()(4) == String(""), AipsError);
+	fieldBuffer.code().put(0, "code 0");
+	fieldBuffer.code().put(4, "code 4");
       }
       { // test the numPoly functions.
-	AlwaysAssert(fieldBuffer.numPoly(0) == 0, AipsError);
-	AlwaysAssert(fieldBuffer.numPoly(4) == 0, AipsError);
-	fieldBuffer.numPoly(0, 1);
-	fieldBuffer.numPoly(4, 2);
+	AlwaysAssert(fieldBuffer.numPoly()(0) == 0, AipsError);
+	AlwaysAssert(fieldBuffer.numPoly()(4) == 1, AipsError);
       }
-      { // test the time and timeFrame functions.
-	AlwaysAssert(fieldBuffer.time(0) == 0, AipsError);
-	AlwaysAssert(fieldBuffer.time(4) == 0, AipsError);
-	AlwaysAssert(fieldBuffer.timeFrame() == MEpoch::UTC, AipsError);
-	fieldBuffer.time(0, 1.0);
-	fieldBuffer.time(4, 2.0);
-	fieldBuffer.timeFrame(MEpoch::TAI);
+      { // test the time functions.
+ 	AlwaysAssert(near(fieldBuffer.time()(0), 0.0), AipsError);
+ 	AlwaysAssert(near(fieldBuffer.time()(4), 0.0), AipsError);
+	AlwaysAssert(fieldBuffer.timeMeas().getMeasRef().getType() == 
+		     MEpoch::UTC, AipsError);
+  	fieldBuffer.time().put(0, 1.0);
+  	fieldBuffer.time().put(4, 2.0);
       }
       { // test the delayDir & delayFrame functions.
-	AlwaysAssert(fieldBuffer.delayDir(0).shape() == IPosition(2, 2, 2),
-		     AipsError);
-	AlwaysAssert(allNear(fieldBuffer.delayDir(0), 0.0, C::dbl_epsilon),
-		     AipsError);
-	AlwaysAssert(fieldBuffer.delayDir(1).shape() == IPosition(2, 2, 1),
-		     AipsError);
-	AlwaysAssert(allNear(fieldBuffer.delayDir(1), 0.0, C::dbl_epsilon),
-		     AipsError);
-	AlwaysAssert(fieldBuffer.delayDir(4).shape() == IPosition(2, 2, 3),
-		     AipsError);
-	AlwaysAssert(allNear(fieldBuffer.delayDir(4), 0.0, C::dbl_epsilon),
-		     AipsError);
-	AlwaysAssert(fieldBuffer.delayFrame() == MDirection::J2000, AipsError);
-	fieldBuffer.delayDir(0, Matrix<Double>(2, 2, 2.0));
-	fieldBuffer.delayDir(4, Matrix<Double>(2, 3, 3.0));
-	fieldBuffer.delayFrame(MDirection::B1950);
-#if defined (AIPS_DEBUG)
-	try { 
-	  fieldBuffer.delayDir(4, Matrix<Double>(2, 2, 3.0));
-	  throw(AipsError("Exception not thrown"));
-	}
-	catch (AipsError x) {
-	  AlwaysAssert(x.getMesg().contains("direction.ncolumn()"), AipsError);
-	}
-#endif
+ 	AlwaysAssert(fieldBuffer.delayDir()(0).shape() == IPosition(2, 2, 1),
+ 		     AipsError);
+ 	AlwaysAssert(allNear(fieldBuffer.delayDir()(0), 0.0, C::dbl_epsilon),
+ 		     AipsError);
+ 	AlwaysAssert(fieldBuffer.delayDir()(4).shape() == IPosition(2, 2, 2),
+ 		     AipsError);
+ 	AlwaysAssert(allNear(fieldBuffer.delayDir()(4), 0.0, C::dbl_epsilon),
+ 		     AipsError);
+ 	AlwaysAssert(fieldBuffer.delayDirMeas(4, 0.0).getRef().getType()
+		     == MDirection::J2000, AipsError);
+ 	fieldBuffer.delayDir().put(0, Matrix<Double>(2, 1, 1.0));
+ 	fieldBuffer.delayDir().put(4, Matrix<Double>(2, 2, 2.0));
       }
       { // test the phaseDir & phaseFrame functions.
-	AlwaysAssert(fieldBuffer.phaseDir(0).shape() == IPosition(2, 2, 2),
+	AlwaysAssert(fieldBuffer.phaseDir()(0).shape() == IPosition(2, 2, 1),
 		     AipsError);
-	AlwaysAssert(allNear(fieldBuffer.phaseDir(0), 0.0, C::dbl_epsilon),
+	AlwaysAssert(allNear(fieldBuffer.phaseDir()(0), 0.0, C::dbl_epsilon),
 		     AipsError);
-	AlwaysAssert(fieldBuffer.phaseDir(1).shape() == IPosition(2, 2, 1),
+	AlwaysAssert(fieldBuffer.phaseDir()(4).shape() == IPosition(2, 2, 2),
 		     AipsError);
-	AlwaysAssert(allNear(fieldBuffer.phaseDir(1), 0.0, C::dbl_epsilon),
+	AlwaysAssert(allNear(fieldBuffer.phaseDir()(4), 0.0, C::dbl_epsilon),
 		     AipsError);
-	AlwaysAssert(fieldBuffer.phaseDir(4).shape() == IPosition(2, 2, 3),
-		     AipsError);
-	AlwaysAssert(allNear(fieldBuffer.phaseDir(4), 0.0, C::dbl_epsilon),
-		     AipsError);
-	AlwaysAssert(fieldBuffer.phaseFrame() == MDirection::J2000, AipsError);
-	fieldBuffer.phaseDir(0, Matrix<Double>(2, 2, 20.0));
-	fieldBuffer.phaseDir(4, Matrix<Double>(2, 3, 30.0));
-	fieldBuffer.phaseFrame(MDirection::AZEL);
-#if defined (AIPS_DEBUG)
-	try { 
-	  fieldBuffer.phaseDir(4, Matrix<Double>(2, 2, 3.0));
-	  throw(AipsError("Exception not thrown"));
-	}
-	catch (AipsError x) {
-	  AlwaysAssert(x.getMesg().contains("direction.ncolumn()"), AipsError);
-	}
-#endif
+ 	AlwaysAssert(fieldBuffer.phaseDirMeas(4, 0.0).getRef().getType()
+		     == MDirection::J2000, AipsError);
+	fieldBuffer.phaseDir().put(0, Matrix<Double>(2, 1, 20.0));
+	fieldBuffer.phaseDir().put(4, Matrix<Double>(2, 2, 30.0));
       }
       { // test the referenceDir & referenceFrame functions.
-	AlwaysAssert(fieldBuffer.referenceDir(0).shape() == IPosition(2, 2, 2),
-		     AipsError);
-	AlwaysAssert(allNear(fieldBuffer.referenceDir(0), 0.0, C::dbl_epsilon),
-		     AipsError);
-	AlwaysAssert(fieldBuffer.referenceDir(1).shape() == IPosition(2, 2, 1),
-		     AipsError);
-	AlwaysAssert(allNear(fieldBuffer.referenceDir(1), 0.0, C::dbl_epsilon),
-		     AipsError);
-	AlwaysAssert(fieldBuffer.referenceDir(4).shape() == IPosition(2, 2, 3),
-		     AipsError);
-	AlwaysAssert(allNear(fieldBuffer.referenceDir(4), 0.0, C::dbl_epsilon),
-		     AipsError);
-	AlwaysAssert(fieldBuffer.referenceFrame() == MDirection::J2000,
-		     AipsError);
-	fieldBuffer.referenceDir(0, Matrix<Double>(2, 2, 10.0));
-	fieldBuffer.referenceDir(4, Matrix<Double>(2, 3, 15.0));
-	fieldBuffer.referenceFrame(MDirection::GALACTIC);
-#if defined (AIPS_DEBUG)
-	try { 
-	  fieldBuffer.referenceDir(4, Matrix<Double>(2, 2, 3.0));
-	  throw(AipsError("Exception not thrown"));
-	}
-	catch (AipsError x) {
-	  AlwaysAssert(x.getMesg().contains("direction.ncolumn()"), AipsError);
-	}
-#endif
+	AlwaysAssert(fieldBuffer.referenceDir()(0).shape() == 
+		     IPosition(2, 2, 1), AipsError);
+	AlwaysAssert(allNear(fieldBuffer.referenceDir()(0), 0.0,
+			     C::dbl_epsilon), AipsError);
+	AlwaysAssert(fieldBuffer.referenceDir()(4).shape() ==
+		     IPosition(2, 2, 2), AipsError);
+	AlwaysAssert(allNear(fieldBuffer.referenceDir()(4), 0.0,
+			     C::dbl_epsilon), AipsError);
+ 	AlwaysAssert(fieldBuffer.referenceDirMeas(4, 0.0).getRef().getType()
+		     == MDirection::J2000, AipsError);
+	fieldBuffer.referenceDir().put(0, Matrix<Double>(2, 1, 10.0));
+	fieldBuffer.referenceDir().put(4, Matrix<Double>(2, 2, 15.0));
       }
       { // test the sourceID functions.
-	AlwaysAssert(fieldBuffer.sourceId(0) == -1, AipsError);
-	AlwaysAssert(fieldBuffer.sourceId(4) == -1, AipsError);
-	fieldBuffer.sourceId(0, 10);
-	fieldBuffer.sourceId(4, 20);
+	AlwaysAssert(fieldBuffer.sourceId()(0) == -1, AipsError);
+	AlwaysAssert(fieldBuffer.sourceId()(4) == -1, AipsError);
+	fieldBuffer.sourceId().put(0, 10);
+	fieldBuffer.sourceId().put(4, 20);
       }
       { // test the flagRow functions.
-	AlwaysAssert(fieldBuffer.flagRow(0) == False, AipsError);
-	AlwaysAssert(fieldBuffer.flagRow(4) == False, AipsError);
-	fieldBuffer.flagRow(3, True);
+	AlwaysAssert(fieldBuffer.flagRow()(0) == False, AipsError);
+	AlwaysAssert(fieldBuffer.flagRow()(4) == False, AipsError);
+	fieldBuffer.flagRow().put(3, True);
       }
       { // Check the assignment operator & copy constructor
 	MSFieldBuffer otherBuffer(fieldBuffer);
@@ -186,69 +145,59 @@ int main() {
 	AlwaysAssert(otherBuffer.nrow() == 5, AipsError);
 	newBuffer = otherBuffer;
 	AlwaysAssert(newBuffer.ok(), AipsError);
+	// Check the reference semantics by adding data here and seeing if it
+	// is mirrored into the newBuffer object.
+	fieldBuffer.delayDir().put(1, Matrix<Double>(2, 2, 1.2));
+	fieldBuffer.name().put(1, "name 1");
+	fieldBuffer.flagRow().put(1, True);
+	// Save the buffer to disk
+	fieldBuffer.save(filename, True);
       }
     }
     { // check the data has not been lost.
       AlwaysAssert(newBuffer.nrow() == 5, AipsError);
-      AlwaysAssert(newBuffer.name(0) == String("row 0"), AipsError);
-      AlwaysAssert(newBuffer.name(1) == String(""), AipsError);
-      AlwaysAssert(newBuffer.name(4) == String("row 4"), AipsError);
-      AlwaysAssert(newBuffer.code(0) == String("code 0"), AipsError);
-      AlwaysAssert(newBuffer.code(1) == String(""), AipsError);
-      AlwaysAssert(newBuffer.code(4) == String("code 4"), AipsError);
-      AlwaysAssert(newBuffer.numPoly(0) == 1, AipsError);
-      AlwaysAssert(newBuffer.numPoly(1) == 0, AipsError);
-      AlwaysAssert(newBuffer.numPoly(4) == 2, AipsError);
-      AlwaysAssert(near(newBuffer.time(0), 1.0), AipsError);
-      AlwaysAssert(near(newBuffer.time(1), 0.0), AipsError);
-      AlwaysAssert(near(newBuffer.time(4), 2.0), AipsError);
-      AlwaysAssert(newBuffer.timeFrame() == MEpoch::TAI, AipsError);
-      AlwaysAssert(newBuffer.delayDir(0).shape() == IPosition(2, 2, 2),
-		   AipsError);
-      AlwaysAssert(allNear(newBuffer.delayDir(0), 2.0, C::dbl_epsilon),
-		   AipsError);
-      AlwaysAssert(newBuffer.delayDir(1).shape() == IPosition(2, 2, 1),
-		   AipsError);
-      AlwaysAssert(allNear(newBuffer.delayDir(1), 0.0, C::dbl_epsilon),
-		   AipsError);
-      AlwaysAssert(newBuffer.delayDir(4).shape() == IPosition(2, 2, 3),
-		   AipsError);
-      AlwaysAssert(allNear(newBuffer.delayDir(4), 3.0, C::dbl_epsilon),
-		   AipsError);
-      AlwaysAssert(newBuffer.delayFrame() == MDirection::B1950, AipsError);
-      AlwaysAssert(newBuffer.phaseDir(0).shape() == IPosition(2, 2, 2),
-		   AipsError);
-      AlwaysAssert(allNear(newBuffer.phaseDir(0), 20.0, C::dbl_epsilon),
-		   AipsError);
-      AlwaysAssert(newBuffer.phaseDir(1).shape() == IPosition(2, 2, 1),
-		   AipsError);
-      AlwaysAssert(allNear(newBuffer.phaseDir(1), 0.0, C::dbl_epsilon),
-		   AipsError);
-      AlwaysAssert(newBuffer.phaseDir(4).shape() == IPosition(2, 2, 3),
-		   AipsError);
-      AlwaysAssert(allNear(newBuffer.phaseDir(4), 30.0, C::dbl_epsilon),
-		   AipsError);
-      AlwaysAssert(newBuffer.phaseFrame() == MDirection::AZEL, AipsError);
-      AlwaysAssert(newBuffer.referenceDir(0).shape() == IPosition(2, 2, 2),
-		   AipsError);
-      AlwaysAssert(allNear(newBuffer.referenceDir(0), 10.0, C::dbl_epsilon),
-		   AipsError);
-      AlwaysAssert(newBuffer.referenceDir(1).shape() == IPosition(2, 2, 1),
-		   AipsError);
-      AlwaysAssert(allNear(newBuffer.referenceDir(1), 0.0, C::dbl_epsilon),
-		   AipsError);
-      AlwaysAssert(newBuffer.referenceDir(4).shape() == IPosition(2, 2, 3),
-		   AipsError);
-      AlwaysAssert(allNear(newBuffer.referenceDir(4), 15.0, C::dbl_epsilon),
-		   AipsError);
-      AlwaysAssert(newBuffer.referenceFrame() == MDirection::GALACTIC,
-		   AipsError);
-      AlwaysAssert(newBuffer.sourceId(0) == 10, AipsError);
-      AlwaysAssert(newBuffer.sourceId(1) == -1, AipsError);
-      AlwaysAssert(newBuffer.sourceId(4) == 20, AipsError);
-      AlwaysAssert(newBuffer.flagRow(0) == False, AipsError);
-      AlwaysAssert(newBuffer.flagRow(3) == True, AipsError);
-      AlwaysAssert(newBuffer.flagRow(4) == False, AipsError);
+      AlwaysAssert(newBuffer.name()(0) == String("row 0"), AipsError);
+      AlwaysAssert(newBuffer.name()(4) == String("row 4"), AipsError);
+      AlwaysAssert(newBuffer.code()(0) == String("code 0"), AipsError);
+      AlwaysAssert(newBuffer.code()(4) == String("code 4"), AipsError);
+      AlwaysAssert(newBuffer.numPoly()(0) == 0, AipsError);
+      AlwaysAssert(newBuffer.numPoly()(4) == 1, AipsError);
+      AlwaysAssert(near(newBuffer.time()(0), 1.0), AipsError);
+      AlwaysAssert(near(newBuffer.time()(4), 2.0), AipsError);
+      AlwaysAssert(newBuffer.delayDir()(0).shape() == IPosition(2, 2, 1),
+ 		   AipsError);
+      AlwaysAssert(allNear(newBuffer.delayDir()(0), 1.0, C::dbl_epsilon),
+ 		   AipsError);
+      AlwaysAssert(newBuffer.delayDir()(4).shape() == IPosition(2, 2, 2),
+ 		   AipsError);
+      AlwaysAssert(allNear(newBuffer.delayDir()(4), 2.0, C::dbl_epsilon),
+ 		   AipsError);
+      AlwaysAssert(newBuffer.phaseDir()(0).shape() == IPosition(2, 2, 1),
+ 		   AipsError);
+      AlwaysAssert(allNear(newBuffer.phaseDir()(0), 20.0, C::dbl_epsilon),
+ 		   AipsError);
+      AlwaysAssert(newBuffer.phaseDir()(4).shape() == IPosition(2, 2, 2),
+ 		   AipsError);
+      AlwaysAssert(allNear(newBuffer.phaseDir()(4), 30.0, C::dbl_epsilon),
+ 		   AipsError);
+      AlwaysAssert(newBuffer.referenceDir()(0).shape() == IPosition(2, 2, 1),
+ 		   AipsError);
+      AlwaysAssert(allNear(newBuffer.referenceDir()(0), 10.0, C::dbl_epsilon),
+ 		   AipsError);
+      AlwaysAssert(newBuffer.referenceDir()(4).shape() == IPosition(2, 2, 2),
+ 		   AipsError);
+      AlwaysAssert(allNear(newBuffer.referenceDir()(4), 15.0, C::dbl_epsilon),
+ 		   AipsError);
+      AlwaysAssert(newBuffer.sourceId()(0) == 10, AipsError);
+      AlwaysAssert(newBuffer.sourceId()(4) == 20, AipsError);
+      AlwaysAssert(newBuffer.flagRow()(0) == False, AipsError);
+      AlwaysAssert(newBuffer.flagRow()(3) == True, AipsError);
+      AlwaysAssert(newBuffer.flagRow()(4) == False, AipsError);
+      // check the reference semantics
+      AlwaysAssert(allNear(newBuffer.delayDir()(1), 1.2, C::dbl_epsilon),
+ 		   AipsError);
+      AlwaysAssert(newBuffer.name()(1) == "name 1", AipsError);
+      AlwaysAssert(newBuffer.flagRow()(1) == True, AipsError);
     }
     { // Check the isValid functions
       AlwaysAssert(newBuffer.isValid(True) == True, AipsError);
@@ -256,15 +205,25 @@ int main() {
       AlwaysAssert(newBuffer.isValid() == True, AipsError);
     }
     { // Check the match functions
-      AlwaysAssert(newBuffer.matchRefDir(Matrix<Double>(2, 2, 0.0)) == -1,
-		   AipsError);
-      AlwaysAssert(newBuffer.matchRefDir(Matrix<Double>(2, 2, 10.0), False)==0,
-		   AipsError);
-      AlwaysAssert(newBuffer.matchRefDir(Matrix<Double>(2, 1, 0.0)) == 2,
-		   AipsError);
-      AlwaysAssert(newBuffer.matchRefDir(Matrix<Double>(2, 1, 0.0), False)==3,
-		   AipsError);
+      AlwaysAssert(newBuffer.matchRefDir(Matrix<Double>(2, 1, 0.0)) == -1,
+ 		   AipsError);
+      AlwaysAssert(newBuffer.matchRefDir(Matrix<Double>(2, 1, 10.0), False)==0,
+ 		   AipsError);
+      AlwaysAssert(newBuffer.matchRefDir(Matrix<Double>(2, 2, 0.0)) == 2,
+ 		   AipsError);
+      AlwaysAssert(newBuffer.matchRefDir(Matrix<Double>(2, 2, 0.0), False)==3,
+ 		   AipsError);
     }
+  }
+  catch (AipsError x) {
+    cerr << x.getMesg() << endl;
+    cout << "FAIL" << endl;
+    return 1;
+  }
+  try {
+    // Check that the Table ended up on disk (after the save function).
+    NewMSField ms(filename);
+    ms.markForDelete();
   }
   catch (AipsError x) {
     cerr << x.getMesg() << endl;
