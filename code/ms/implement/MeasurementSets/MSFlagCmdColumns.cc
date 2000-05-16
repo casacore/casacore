1571ef4c8231442df96a8ec31e032a590e4ac808
Ralph Marson <rmarson@nrao.edu>
2000-05-16 04:55:35 +0000

    Derived the rw class frmo the ro class and added a default ctr

--- code/ms/implement/MeasurementSets/MSFlagCmdColumns.cc
+++ code/ms/implement/MeasurementSets/MSFlagCmdColumns.cc
@@ -25,40 +25,107 @@
 //#
 //# $Id$
 
-//# Includes
 #include <aips/MeasurementSets/NewMSFlagCmdColumns.h>
-
-NewMSFlagCmdColumns::NewMSFlagCmdColumns(NewMSFlagCmd& msFlagCmd):
-applied_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::APPLIED)),
-command_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::COMMAND)),
-interval_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::INTERVAL)),
-level_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::LEVEL)),
-reason_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::REASON)),
-severity_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::SEVERITY)),
-time_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::TIME)),
-type_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::TYPE)),
-timeMeas_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::TIME)),
-intervalQuant_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::INTERVAL)),
-timeQuant_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::TIME))
-{}
-
-NewMSFlagCmdColumns::~NewMSFlagCmdColumns() {}
+#include <aips/MeasurementSets/NewMSFlagCmd.h>
 
 RONewMSFlagCmdColumns::RONewMSFlagCmdColumns(const NewMSFlagCmd& msFlagCmd):
-applied_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::APPLIED)),
-command_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::COMMAND)),
-interval_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::INTERVAL)),
-level_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::LEVEL)),
-reason_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::REASON)),
-severity_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::SEVERITY)),
-time_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::TIME)),
-type_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::TYPE)),
-timeMeas_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::TIME)),
-intervalQuant_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::INTERVAL)),
-timeQuant_p(msFlagCmd,NewMSFlagCmd::columnName(NewMSFlagCmd::TIME))
+  applied_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::APPLIED)),
+  command_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::COMMAND)),
+  interval_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::INTERVAL)),
+  level_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::LEVEL)),
+  reason_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::REASON)),
+  severity_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::SEVERITY)),
+  time_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::TIME)),
+  type_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::TYPE)),
+  timeMeas_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::TIME)),
+  intervalQuant_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::INTERVAL)),
+  timeQuant_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::TIME))
 {}
 
 RONewMSFlagCmdColumns::~RONewMSFlagCmdColumns() {}
 
+RONewMSFlagCmdColumns::RONewMSFlagCmdColumns():
+  applied_p(),
+  command_p(),
+  interval_p(),
+  level_p(),
+  reason_p(),
+  severity_p(),
+  time_p(),
+  type_p(),
+  timeMeas_p(),
+  intervalQuant_p(),
+  timeQuant_p()
+{}
 
+void RONewMSFlagCmdColumns::attach(const NewMSFlagCmd& msFlagCmd)
+{
+  applied_p.attach(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::APPLIED));
+  command_p.attach(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::COMMAND));
+  interval_p.attach(msFlagCmd, NewMSFlagCmd::
+		    columnName(NewMSFlagCmd::INTERVAL));
+  level_p.attach(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::LEVEL));
+  reason_p.attach(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::REASON));
+  severity_p.attach(msFlagCmd, NewMSFlagCmd::
+		    columnName(NewMSFlagCmd::SEVERITY));
+  time_p.attach(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::TIME));
+  type_p.attach(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::TYPE));
+  timeMeas_p.attach(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::TIME));
+  intervalQuant_p.attach(msFlagCmd, NewMSFlagCmd::
+			 columnName(NewMSFlagCmd::INTERVAL));
+  timeQuant_p.attach(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::TIME));
+}
 
+NewMSFlagCmdColumns::NewMSFlagCmdColumns(NewMSFlagCmd& msFlagCmd):
+  RONewMSFlagCmdColumns(msFlagCmd),
+  applied_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::APPLIED)),
+  command_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::COMMAND)),
+  interval_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::INTERVAL)),
+  level_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::LEVEL)),
+  reason_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::REASON)),
+  severity_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::SEVERITY)),
+  time_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::TIME)),
+  type_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::TYPE)),
+  timeMeas_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::TIME)),
+  intervalQuant_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::INTERVAL)),
+  timeQuant_p(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::TIME))
+{}
+
+NewMSFlagCmdColumns::~NewMSFlagCmdColumns() {}
+
+NewMSFlagCmdColumns::NewMSFlagCmdColumns():
+  RONewMSFlagCmdColumns(),
+  applied_p(),
+  command_p(),
+  interval_p(),
+  level_p(),
+  reason_p(),
+  severity_p(),
+  time_p(),
+  type_p(),
+  timeMeas_p(),
+  intervalQuant_p(),
+  timeQuant_p()
+{}
+
+void NewMSFlagCmdColumns::attach(NewMSFlagCmd& msFlagCmd)
+{
+  RONewMSFlagCmdColumns::attach(msFlagCmd);
+  applied_p.attach(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::APPLIED));
+  command_p.attach(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::COMMAND));
+  interval_p.attach(msFlagCmd, NewMSFlagCmd::
+		    columnName(NewMSFlagCmd::INTERVAL));
+  level_p.attach(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::LEVEL));
+  reason_p.attach(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::REASON));
+  severity_p.attach(msFlagCmd, NewMSFlagCmd::
+		    columnName(NewMSFlagCmd::SEVERITY));
+  time_p.attach(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::TIME));
+  type_p.attach(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::TYPE));
+  timeMeas_p.attach(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::TIME));
+  intervalQuant_p.attach(msFlagCmd, NewMSFlagCmd::
+			 columnName(NewMSFlagCmd::INTERVAL));
+  timeQuant_p.attach(msFlagCmd, NewMSFlagCmd::columnName(NewMSFlagCmd::TIME));
+}
+// Local Variables: 
+// compile-command: "gmake NewMSFlagCmdColumns"
+// End: 
--- code/ms/implement/MeasurementSets/MSFlagCmdColumns.h
+++ code/ms/implement/MeasurementSets/MSFlagCmdColumns.h
@@ -25,21 +25,20 @@
 //#
 //# $Id$
 
-#if !defined(AIPS_NewMSFLAGCMDCOLUMNS_H)
-#define AIPS_NewMSFLAGCMDCOLUMNS_H
+#if !defined(AIPS_NEWMSFLAGCMDCOLUMNS_H)
+#define AIPS_NEWMSFLAGCMDCOLUMNS_H
 
-#include <aips/MeasurementSets/NewMSFlagCmd.h>
-#include <aips/Tables/ScalarColumn.h>
-#include <aips/Tables/ArrayColumn.h>
-#include <aips/Quanta/Unit.h>
-#include <aips/Quanta/Quantum.h>
+#include <aips/aips.h>
+#include <aips/Measures/MEpoch.h>
 #include <aips/TableMeasures/ScalarMeasColumn.h>
 #include <aips/TableMeasures/ScalarQuantColumn.h>
+#include <aips/Tables/ScalarColumn.h>
+#include <aips/Utilities/String.h>
 
-class MEpoch;
+class NewMSFlagCmd;
 
 // <summary>
-// A convenience class to provide easy access to NewMSFlagCmd columns
+// A class to provide easy read-only access to NewMSFlagCmd columns
 // </summary>
 
 // <use visibility=export>
@@ -49,85 +48,6 @@ class MEpoch;
 
 // <prerequisite>
 //   <li> NewMSFlagCmd
-//   <li> ArrayColumn
-//   <li> ScalarColumn
-// </prerequisite>
-//
-// <etymology>
-// NewMSFlagCmdColumns stands for NewMeasurementSet FlagCmd Table columns.
-// </etymology>
-//
-// <synopsis>
-// This class provides access to the columns in the NewMSFlagCmd Table,
-// it does the declaration of all the Scalar and ArrayColumns with the
-// correct types, so the application programmer doesn't have to
-// worry about getting those right. There is an access function
-// for every predefined column. Access to non-predefined columns will still
-// have to be done with explicit declarations.
-// See <linkto class=NewMSColumns> NewMSColumns</linkto> for an example.
-// </synopsis>
-//
-// <motivation>
-// See <linkto class=NewMSColumns> NewMSColumns</linkto> for the motivation.
-// </motivation>
-
-class NewMSFlagCmdColumns
-{
-public:
-
-NewMSFlagCmdColumns(NewMSFlagCmd& msFlagCmd);
-
-~NewMSFlagCmdColumns();
-
-  // Access to columns
-  ScalarColumn<Bool>& applied() {return applied_p;}
-  ScalarColumn<String>& command() {return command_p;}
-  ScalarColumn<Double>& interval() {return interval_p;}
-  ScalarColumn<Int>& level() {return level_p;}
-  ScalarColumn<String>& reason() {return reason_p;}
-  ScalarColumn<Int>& severity() {return severity_p;}
-  ScalarColumn<Double>& time() {return time_p;}
-  ScalarColumn<String>& type() {return type_p;}
-  
-  // Access to Measure columns
-  ScalarMeasColumn<MEpoch>& timeMeas() { return timeMeas_p;}
-
-   // Access to Quantum columns
-  ScalarQuantColumn<Double>& intervalQuant() { return intervalQuant_p;}
-  ScalarQuantColumn<Double>& timeQuant() { return timeQuant_p;}
-
-private:
-  ScalarColumn<Bool> applied_p;
-  ScalarColumn<String> command_p;
-  ScalarColumn<Double> interval_p;
-  ScalarColumn<Int> level_p;
-  ScalarColumn<String> reason_p;
-  ScalarColumn<Int> severity_p;
-  ScalarColumn<Double> time_p;
-  ScalarColumn<String> type_p;
-
-  // Access to Measure columns
-  ScalarMeasColumn<MEpoch> timeMeas_p;
-
-   // Access to Quantum columns
-  ScalarQuantColumn<Double> intervalQuant_p;
-  ScalarQuantColumn<Double> timeQuant_p;
-
-
-};
-
-// <summary>
-// A convenience class to provide easy access to NewMSFlagCmd columns
-// </summary>
-
-// <use visibility=export>
-
-// <reviewed reviewer="Bob Garwood" date="1997/02/01" tests="" demos="">
-// </reviewed>
-
-// <prerequisite>
-//   <li> NewMSFlagCmd
-//   <li> ArrayColumn
 //   <li> ScalarColumn
 // </prerequisite>
 //
@@ -136,13 +56,13 @@ private:
 // </etymology>
 //
 // <synopsis>
-// This class provides read-only access to the columns in the NewMSFlagCmd Table.
-// It does the declaration of all the Scalar and ArrayColumns with the
-// correct types, so the application programmer doesn't have to
-// worry about getting those right. There is an access function
-// for every predefined column. Access to non-predefined columns will still
-// have to be done with explicit declarations.
-// See <linkto class=RONewMSColumns> RONewMSColumns</linkto> for an example.
+// This class provides read-only access to the columns in the NewMSFlagCmd
+// Table.  It does the declaration of all the ScalarColumns with the correct
+// types, so the application programmer doesn't have to worry about getting
+// those right. There is an access function for every predefined column. Access
+// to non-predefined columns will still have to be done with explicit
+// declarations.  See <linkto class=RONewMSColumns> RONewMSColumns</linkto> for
+// an example.
 // </synopsis>
 //
 // <motivation>
@@ -152,30 +72,43 @@ private:
 class RONewMSFlagCmdColumns
 {
 public:
+  // Create a columns object that accesses the data in the specified Table
+  RONewMSFlagCmdColumns(const NewMSFlagCmd& msFlagCmd);
 
-RONewMSFlagCmdColumns(const NewMSFlagCmd& msFlagCmd);
+  // The destructor does nothing special
+  ~RONewMSFlagCmdColumns();
 
-~RONewMSFlagCmdColumns();
-
-// Access to columns
+  // Access to required columns
+  // <group>
   const ROScalarColumn<Bool>& applied() const {return applied_p;}
   const ROScalarColumn<String>& command() const {return command_p;}
+  const ROScalarQuantColumn<Double>& intervalQuant() const {
+    return intervalQuant_p;}
   const ROScalarColumn<Double>& interval() const {return interval_p;}
   const ROScalarColumn<Int>& level() const {return level_p;}
   const ROScalarColumn<String>& reason() const {return reason_p;}
   const ROScalarColumn<Int>& severity() const {return severity_p;}
   const ROScalarColumn<Double>& time() const {return time_p;}
-  const ROScalarColumn<String>& type() const {return type_p;}
-  
-  // Access to Measure columns
-  const ROScalarMeasColumn<MEpoch>& timeMeas() const { return timeMeas_p;}
-
-   // Access to Quantum columns
-  const ROScalarQuantColumn<Double>& intervalQuant() const { return intervalQuant_p;}
   const ROScalarQuantColumn<Double>& timeQuant() const { return timeQuant_p;}
+  const ROScalarMeasColumn<MEpoch>& timeMeas() const { return timeMeas_p;}
+  const ROScalarColumn<String>& type() const {return type_p;}
+  // </group>
 
+protected:
+  //# default constructor creates a object that is not usable. Use the attach
+  //# function correct this.
+  RONewMSFlagCmdColumns();
+
+  //# attach this object to the supplied table.
+  void attach(const NewMSFlagCmd& msFlagCmd);
 
 private:
+  //# Make the assignment operator and the copy constructor private to prevent
+  //# any compiler generated one from being used.
+  RONewMSFlagCmdColumns(const RONewMSFlagCmdColumns&);
+  RONewMSFlagCmdColumns& operator=(const RONewMSFlagCmdColumns&);
+
+  //# required columns
   ROScalarColumn<Bool> applied_p;
   ROScalarColumn<String> command_p;
   ROScalarColumn<Double> interval_p;
@@ -185,13 +118,125 @@ private:
   ROScalarColumn<Double> time_p;
   ROScalarColumn<String> type_p;
 
-  // Access to Measure columns
+  //# Access to Measure columns
   ROScalarMeasColumn<MEpoch> timeMeas_p;
 
-   // Access to Quantum columns
+  //# Access to Quantum columns
   ROScalarQuantColumn<Double> intervalQuant_p;
   ROScalarQuantColumn<Double> timeQuant_p;
-
 };
 
+// <summary>
+// A class to provide easy read-write access to NewMSFlagCmd columns
+// </summary>
+
+// <use visibility=export>
+
+// <reviewed reviewer="Bob Garwood" date="1997/02/01" tests="" demos="">
+// </reviewed>
+
+// <prerequisite>
+//   <li> NewMSFlagCmd
+//   <li> ScalarColumn
+// </prerequisite>
+//
+// <etymology>
+// NewMSFlagCmdColumns stands for NewMeasurementSet FlagCmd Table columns.
+// </etymology>
+//
+// <synopsis>
+// This class provides access to the columns in the NewMSFlagCmd Table, it does
+// the declaration of all the ScalarColumns with the correct types, so the
+// application programmer doesn't have to worry about getting those
+// right. There is an access function for every predefined column. Access to
+// non-predefined columns will still have to be done with explicit
+// declarations.  See <linkto class=NewMSColumns> NewMSColumns</linkto> for an
+// example.
+// </synopsis>
+//
+// <motivation>
+// See <linkto class=NewMSColumns> NewMSColumns</linkto> for the motivation.
+// </motivation>
+
+class NewMSFlagCmdColumns: public RONewMSFlagCmdColumns
+{
+public:
+  // Create a columns object that accesses the data in the specified Table
+  NewMSFlagCmdColumns(NewMSFlagCmd& msFlagCmd);
+  
+  // The destructor does nothing special
+  ~NewMSFlagCmdColumns();
+
+  // Read-write access to required columns
+  // <group>
+  ScalarColumn<Bool>& applied() {return applied_p;}
+  ScalarColumn<String>& command() {return command_p;}
+  ScalarColumn<Double>& interval() {return interval_p;}
+  ScalarQuantColumn<Double>& intervalQuant() {return intervalQuant_p;}
+  ScalarColumn<Int>& level() {return level_p;}
+  ScalarColumn<String>& reason() {return reason_p;}
+  ScalarColumn<Int>& severity() {return severity_p;}
+  ScalarColumn<Double>& time() {return time_p;}
+  ScalarQuantColumn<Double>& timeQuant() {return timeQuant_p;}
+  ScalarMeasColumn<MEpoch>& timeMeas() {return timeMeas_p;}
+  ScalarColumn<String>& type() {return type_p;}
+  // </group>
+
+  // Read-only access to required columns
+  // <group>
+  const ROScalarColumn<Bool>& applied() const {
+    return RONewMSFlagCmdColumns::applied();}
+  const ROScalarColumn<String>& command() const {
+    return RONewMSFlagCmdColumns::command();}
+  const ROScalarQuantColumn<Double>& intervalQuant() const {
+    return RONewMSFlagCmdColumns::intervalQuant();}
+  const ROScalarColumn<Double>& interval() const {
+    return RONewMSFlagCmdColumns::interval();}
+  const ROScalarColumn<Int>& level() const {
+    return RONewMSFlagCmdColumns::level();}
+  const ROScalarColumn<String>& reason() const {
+    return RONewMSFlagCmdColumns::reason();}
+  const ROScalarColumn<Int>& severity() const {
+    return RONewMSFlagCmdColumns::severity();}
+  const ROScalarColumn<Double>& time() const {
+    return RONewMSFlagCmdColumns::time();}
+  const ROScalarQuantColumn<Double>& timeQuant() const {
+    return RONewMSFlagCmdColumns::timeQuant();}
+  const ROScalarMeasColumn<MEpoch>& timeMeas() const {
+    return RONewMSFlagCmdColumns::timeMeas();}
+  const ROScalarColumn<String>& type() const {
+    return RONewMSFlagCmdColumns::type();}
+  // </group>
+
+protected:
+  //# default constructor creates a object that is not usable. Use the attach
+  //# function correct this.
+  NewMSFlagCmdColumns();
+
+  //# attach this object to the supplied table.
+  void attach(NewMSFlagCmd& msFlagCmd);
+
+private:
+  //# Make the assignment operator and the copy constructor private to prevent
+  //# any compiler generated one from being used.
+  NewMSFlagCmdColumns(const NewMSFlagCmdColumns&);
+  NewMSFlagCmdColumns& operator=(const NewMSFlagCmdColumns&);
+
+  //# required columns
+  ScalarColumn<Bool> applied_p;
+  ScalarColumn<String> command_p;
+  ScalarColumn<Double> interval_p;
+  ScalarColumn<Int> level_p;
+  ScalarColumn<String> reason_p;
+  ScalarColumn<Int> severity_p;
+  ScalarColumn<Double> time_p;
+  ScalarColumn<String> type_p;
+
+  //# Access to Measure columns
+  ScalarMeasColumn<MEpoch> timeMeas_p;
+
+  //# Access to Quantum columns
+  ScalarQuantColumn<Double> intervalQuant_p;
+  ScalarQuantColumn<Double> timeQuant_p;
+};
 #endif
