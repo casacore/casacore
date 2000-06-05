0e4aa6f8f6c3ac552c71504f3ac4f2a56290c3a8
Ger van Diepen <gervandiepen@gmail.com>
2000-06-05 09:25:08 +0000

    Increased bucketsize and added tests for other type of columns

--- code/tables/implement/Tables/test/tStMan.cc
+++ code/tables/implement/Tables/test/tStMan.cc
@@ -70,13 +70,32 @@ void newtab (uInt nrrow, const DataManager& stman)
   dstra4.setMaxLength (20);
   td.addColumn (dstra4);
 
+  td.addColumn (ScalarColumnDesc<Bool>("b1"));
+  td.addColumn (ArrayColumnDesc<Bool>("ba1",IPosition(2,2,3),
+				      ColumnDesc::Direct));
+  td.addColumn (ArrayColumnDesc<Bool>("ba2", -1, ColumnDesc::FixedShape));
+  td.addColumn (ArrayColumnDesc<Bool>("ba3"));
+
+  td.addColumn (ScalarColumnDesc<Float>("f1"));
+  td.addColumn (ArrayColumnDesc<Float>("fa1",IPosition(2,2,3),
+				       ColumnDesc::Direct));
+  td.addColumn (ArrayColumnDesc<Float>("fa2", -1, ColumnDesc::FixedShape));
+  td.addColumn (ArrayColumnDesc<Float>("fa3"));
+
+  td.addColumn (ScalarColumnDesc<DComplex>("dc1"));
+  td.addColumn (ArrayColumnDesc<DComplex>("dca1",IPosition(2,2,3),
+					  ColumnDesc::Direct));
+  td.addColumn (ArrayColumnDesc<DComplex>("dca2", -1, ColumnDesc::FixedShape));
+  td.addColumn (ArrayColumnDesc<DComplex>("dca3"));
+
   // Now create a new table from the description.
-  // Use copy constructor to test if it works fine.
-  // (newtab and newtabcp have the same underlying object).
   SetupNewTable newtab("tStMan_tmp.data", td, Table::New);
   // Create a storage manager for it.
   newtab.bindAll (stman);
   newtab.setShapeColumn("stra3",IPosition(2,2,3));
+  newtab.setShapeColumn("ba2",IPosition(2,2,3));
+  newtab.setShapeColumn("fa2",IPosition(2,2,3));
+  newtab.setShapeColumn("dca2",IPosition(2,2,3));
   Table tab(newtab, nrrow);
 
   Array<String> emptyArray(IPosition(2,2,3));
@@ -87,6 +106,18 @@ void newtab (uInt nrrow, const DataManager& stman)
   ArrayColumn<String> stra2 (tab, "stra2");
   ArrayColumn<String> stra3 (tab, "stra3");
   ArrayColumn<String> stra4 (tab, "stra4");
+  ScalarColumn<Bool> b1 (tab, "b1");
+  ArrayColumn<Bool> ba1 (tab, "ba1");
+  ArrayColumn<Bool> ba2 (tab, "ba2");
+  ArrayColumn<Bool> ba3 (tab, "ba3");
+  ScalarColumn<Float> f1 (tab, "f1");
+  ArrayColumn<Float> fa1 (tab, "fa1");
+  ArrayColumn<Float> fa2 (tab, "fa2");
+  ArrayColumn<Float> fa3 (tab, "fa3");
+  ScalarColumn<DComplex> dc1 (tab, "dc1");
+  ArrayColumn<DComplex> dca1 (tab, "dca1");
+  ArrayColumn<DComplex> dca2 (tab, "dca2");
+  ArrayColumn<DComplex> dca3 (tab, "dca3");
   for (uInt i=0; i<nrrow; i++) {
     AlwaysAssertExit (str1(i) == "");
     AlwaysAssertExit (str2(i) == "");
@@ -94,6 +125,18 @@ void newtab (uInt nrrow, const DataManager& stman)
     AlwaysAssertExit (allEQ (stra2(i), emptyArray));
     AlwaysAssertExit (allEQ (stra3(i), emptyArray));
     AlwaysAssertExit (! stra4.isDefined(i));
+    AlwaysAssertExit (  b1.isDefined(i));
+    AlwaysAssertExit (  ba1.isDefined(i));
+    AlwaysAssertExit (  ba2.isDefined(i));
+    AlwaysAssertExit (! ba3.isDefined(i));
+    AlwaysAssertExit (  f1.isDefined(i));
+    AlwaysAssertExit (  fa1.isDefined(i));
+    AlwaysAssertExit (  fa2.isDefined(i));
+    AlwaysAssertExit (! fa3.isDefined(i));
+    AlwaysAssertExit (  dc1.isDefined(i));
+    AlwaysAssertExit (  dca1.isDefined(i));
+    AlwaysAssertExit (  dca2.isDefined(i));
+    AlwaysAssertExit (! dca3.isDefined(i));
   }
 }
 
@@ -109,6 +152,18 @@ void checktab1()
   ROArrayColumn<String> stra2 (tab, "stra2");
   ROArrayColumn<String> stra3 (tab, "stra3");
   ROArrayColumn<String> stra4 (tab, "stra4");
+  ROScalarColumn<Bool> b1 (tab, "b1");
+  ROArrayColumn<Bool> ba1 (tab, "ba1");
+  ROArrayColumn<Bool> ba2 (tab, "ba2");
+  ROArrayColumn<Bool> ba3 (tab, "ba3");
+  ROScalarColumn<Float> f1 (tab, "f1");
+  ROArrayColumn<Float> fa1 (tab, "fa1");
+  ROArrayColumn<Float> fa2 (tab, "fa2");
+  ROArrayColumn<Float> fa3 (tab, "fa3");
+  ROScalarColumn<DComplex> dc1 (tab, "dc1");
+  ROArrayColumn<DComplex> dca1 (tab, "dca1");
+  ROArrayColumn<DComplex> dca2 (tab, "dca2");
+  ROArrayColumn<DComplex> dca3 (tab, "dca3");
   uInt nrrow = tab.nrow();
   for (uInt i=0; i<nrrow; i++) {
     AlwaysAssertExit (str1(i) == "");
@@ -117,6 +172,18 @@ void checktab1()
     AlwaysAssertExit (allEQ (stra2(i), emptyArray));
     AlwaysAssertExit (allEQ (stra3(i), emptyArray));
     AlwaysAssertExit (! stra4.isDefined(i));
+    AlwaysAssertExit (  b1.isDefined(i));
+    AlwaysAssertExit (  ba1.isDefined(i));
+    AlwaysAssertExit (  ba2.isDefined(i));
+    AlwaysAssertExit (! ba3.isDefined(i));
+    AlwaysAssertExit (  f1.isDefined(i));
+    AlwaysAssertExit (  fa1.isDefined(i));
+    AlwaysAssertExit (  fa2.isDefined(i));
+    AlwaysAssertExit (! fa3.isDefined(i));
+    AlwaysAssertExit (  dc1.isDefined(i));
+    AlwaysAssertExit (  dca1.isDefined(i));
+    AlwaysAssertExit (  dca2.isDefined(i));
+    AlwaysAssertExit (! dca3.isDefined(i));
   }
 }
 
@@ -133,12 +200,31 @@ void checktab (const String& prefix)
   filledArray(IPosition(2,1,1)) = "str_11_";
   filledArray(IPosition(2,1,2)) = "str_12_";
 
+  Array<Float> arrf(IPosition(2,2,3));
+  indgen (arrf);
+  Array<Double> arrd(IPosition(2,4,3));
+  indgen (arrd);
+  Array<DComplex> arrdc = RealToComplex (arrd);
+  Array<Bool> arrb = (fmod(arrf,float(4)) == float(0));
+
   ROScalarColumn<String> str1 (tab, "str1");
   ROScalarColumn<String> str2 (tab, "str2");
   ROArrayColumn<String> stra1 (tab, "stra1");
   ROArrayColumn<String> stra2 (tab, "stra2");
   ROArrayColumn<String> stra3 (tab, "stra3");
   ROArrayColumn<String> stra4 (tab, "stra4");
+  ROScalarColumn<Bool> b1 (tab, "b1");
+  ROArrayColumn<Bool> ba1 (tab, "ba1");
+  ROArrayColumn<Bool> ba2 (tab, "ba2");
+  ROArrayColumn<Bool> ba3 (tab, "ba3");
+  ROScalarColumn<Float> f1 (tab, "f1");
+  ROArrayColumn<Float> fa1 (tab, "fa1");
+  ROArrayColumn<Float> fa2 (tab, "fa2");
+  ROArrayColumn<Float> fa3 (tab, "fa3");
+  ROScalarColumn<DComplex> dc1 (tab, "dc1");
+  ROArrayColumn<DComplex> dca1 (tab, "dca1");
+  ROArrayColumn<DComplex> dca2 (tab, "dca2");
+  ROArrayColumn<DComplex> dca3 (tab, "dca3");
 
   uInt nrrow = tab.nrow();
   char buf[8];
@@ -186,17 +272,82 @@ void checktab (const String& prefix)
 				 emptyArray
 				 (IPosition(2,1,0), IPosition(2,1,1))));
       }
+
+      AlwaysAssertExit (b1(i) == (i%2==0));
+      AlwaysAssertExit (allEQ (ba1(i), arrb));
+      AlwaysAssertExit (allEQ (ba1.getSlice (i, Slicer(IPosition(2,1,0),
+						       IPosition(2,1,2))),
+			       arrb(IPosition(2,1,0), IPosition(2,1,1))));
+      AlwaysAssertExit (allEQ (ba2(i), !arrb));
+      AlwaysAssertExit (allEQ (ba2.getSlice (i, Slicer(IPosition(2,1,0),
+						       IPosition(2,1,2))),
+			       (!arrb)(IPosition(2,1,0), IPosition(2,1,1))));
+      AlwaysAssertExit (allEQ (ba3(i), arrb));
+      AlwaysAssertExit (allEQ (ba3.getSlice (i, Slicer(IPosition(2,1,0),
+						       IPosition(2,1,2))),
+			       arrb(IPosition(2,1,0), IPosition(2,1,1))));
+
+      AlwaysAssertExit (f1(i) == i+1);
+      AlwaysAssertExit (allEQ (fa1(i), arrf));
+      AlwaysAssertExit (allEQ (fa1.getSlice (i, Slicer(IPosition(2,1,0),
+						       IPosition(2,1,2))),
+			       arrf(IPosition(2,1,0), IPosition(2,1,1))));
+      AlwaysAssertExit (allEQ (fa2(i), arrf+float(1)));
+      AlwaysAssertExit (allEQ (fa2.getSlice (i, Slicer(IPosition(2,1,0),
+						       IPosition(2,1,2))),
+			       (arrf+float(1))
+			       (IPosition(2,1,0), IPosition(2,1,1))));
+      AlwaysAssertExit (allEQ (fa3(i), arrf+float(2)));
+      AlwaysAssertExit (allEQ (fa3.getSlice (i, Slicer(IPosition(2,1,0),
+						       IPosition(2,1,2))),
+			       (arrf+float(2))
+			       (IPosition(2,1,0), IPosition(2,1,1))));
+
+      AlwaysAssertExit (dc1(i) == DComplex(i+1,i+2));
+      AlwaysAssertExit (allEQ (dca1(i), arrdc));
+      AlwaysAssertExit (allEQ (dca1.getSlice (i, Slicer(IPosition(2,1,0),
+							IPosition(2,1,2))),
+			       arrdc(IPosition(2,1,0), IPosition(2,1,1))));
+      AlwaysAssertExit (allEQ (dca2(i), arrdc+DComplex(1,2)));
+      AlwaysAssertExit (allEQ (dca2.getSlice (i, Slicer(IPosition(2,1,0),
+							IPosition(2,1,2))),
+			       (arrdc+DComplex(1,2))
+			       (IPosition(2,1,0), IPosition(2,1,1))));
+      AlwaysAssertExit (allEQ (dca3(i), arrdc+DComplex(3,4)));
+      AlwaysAssertExit (allEQ (dca3.getSlice (i, Slicer(IPosition(2,1,0),
+							IPosition(2,1,2))),
+			       (arrdc+DComplex(3,4))
+			       (IPosition(2,1,0), IPosition(2,1,1))));
+
+      arrb = !arrb;
+      arrf += float(10);
+      arrdc += DComplex(10,20);
     }
   }
   {
     String s1(prefix + "str1_");
     String s2(prefix + "str2_");
+    indgen (arrf);
+    arrdc = RealToComplex (arrd);
+    arrb = (fmod(arrf,float(4)) == float(0));
     Vector<String> vec1 = str1.getColumn();
     Vector<String> vec2 = str2.getColumn();
     Array<String> arr1 = stra1.getColumn().reform(IPosition(2,2,3*nrrow));
     Array<String> arr2 = stra2.getColumn().reform(IPosition(2,2,3*nrrow));
     Array<String> arr3 = stra3.getColumn().reform(IPosition(2,2,3*nrrow));
     Array<String> arr4 = stra4.getColumn().reform(IPosition(2,2,3*nrrow));
+    Vector<Bool> bvec1 = b1.getColumn();
+    Array<Bool> barr1 = ba1.getColumn().reform(IPosition(2,2,3*nrrow));
+    Array<Bool> barr2 = ba2.getColumn().reform(IPosition(2,2,3*nrrow));
+    Array<Bool> barr3 = ba3.getColumn().reform(IPosition(2,2,3*nrrow));
+    Vector<Float> fvec1 = f1.getColumn();
+    Array<Float> farr1 = fa1.getColumn().reform(IPosition(2,2,3*nrrow));
+    Array<Float> farr2 = fa2.getColumn().reform(IPosition(2,2,3*nrrow));
+    Array<Float> farr3 = fa3.getColumn().reform(IPosition(2,2,3*nrrow));
+    Vector<DComplex> dcvec1 = dc1.getColumn();
+    Array<DComplex> dcarr1 = dca1.getColumn().reform(IPosition(2,2,3*nrrow));
+    Array<DComplex> dcarr2 = dca2.getColumn().reform(IPosition(2,2,3*nrrow));
+    Array<DComplex> dcarr3 = dca3.getColumn().reform(IPosition(2,2,3*nrrow));
     for (uInt i=0; i<nrrow; i++) {
       sprintf (buf, "%d", i);
       s1 += buf;
@@ -219,11 +370,42 @@ void checktab (const String& prefix)
 	AlwaysAssertExit (allEQ (arr4(IPosition(2,0,3*i),IPosition(2,1,3*i+2)),
 				 emptyArray));
       }
+
+      AlwaysAssertExit (bvec1(i) == (i%2==0));
+      AlwaysAssertExit (allEQ (barr1(IPosition(2,0,3*i), IPosition(2,1,3*i+2)),
+			       arrb));
+      AlwaysAssertExit (allEQ (barr2(IPosition(2,0,3*i), IPosition(2,1,3*i+2)),
+			       !arrb));
+      AlwaysAssertExit (allEQ (barr3(IPosition(2,0,3*i), IPosition(2,1,3*i+2)),
+			       arrb));
+
+      AlwaysAssertExit (fvec1(i) == i+1);
+      AlwaysAssertExit (allEQ (farr1(IPosition(2,0,3*i), IPosition(2,1,3*i+2)),
+			       arrf));
+      AlwaysAssertExit (allEQ (farr2(IPosition(2,0,3*i), IPosition(2,1,3*i+2)),
+			       arrf+float(1)));
+      AlwaysAssertExit (allEQ (farr3(IPosition(2,0,3*i), IPosition(2,1,3*i+2)),
+			       arrf+float(2)));
+
+      AlwaysAssertExit (dcvec1(i) == DComplex(i+1,i+2));
+      AlwaysAssertExit (allEQ(dcarr1(IPosition(2,0,3*i), IPosition(2,1,3*i+2)),
+			      arrdc));
+      AlwaysAssertExit (allEQ(dcarr2(IPosition(2,0,3*i), IPosition(2,1,3*i+2)),
+			      arrdc+DComplex(1,2)));
+      AlwaysAssertExit (allEQ(dcarr3(IPosition(2,0,3*i), IPosition(2,1,3*i+2)),
+			      arrdc+DComplex(3,4)));
+
+      arrb = !arrb;
+      arrf += float(10);
+      arrdc += DComplex(10,20);
     }
   }
   {
     String s1(prefix + "str1_");
     String s2(prefix + "str2_");
+    indgen (arrf);
+    arrdc = RealToComplex (arrd);
+    arrb = (fmod(arrf,float(4)) == float(0));
     Array<String> arr1 = stra1.getColumn(Slicer(IPosition(2,1,0),
 						IPosition(2,1,2))).
                          reform(IPosition(2,1,2*nrrow));
@@ -236,6 +418,33 @@ void checktab (const String& prefix)
     Array<String> arr4 = stra4.getColumn(Slicer(IPosition(2,1,0),
 						IPosition(2,1,2))).
                          reform(IPosition(2,1,2*nrrow));
+    Array<Bool> barr1 = ba1.getColumn(Slicer(IPosition(2,1,0),
+					     IPosition(2,1,2))).
+                        reform(IPosition(2,1,2*nrrow));
+    Array<Bool> barr2 = ba2.getColumn(Slicer(IPosition(2,1,0),
+					     IPosition(2,1,2))).
+                        reform(IPosition(2,1,2*nrrow));
+    Array<Bool> barr3 = ba3.getColumn(Slicer(IPosition(2,1,0),
+					     IPosition(2,1,2))).
+                        reform(IPosition(2,1,2*nrrow));
+    Array<Float> farr1 = fa1.getColumn(Slicer(IPosition(2,1,0),
+					      IPosition(2,1,2))).
+                         reform(IPosition(2,1,2*nrrow));
+    Array<Float> farr2 = fa2.getColumn(Slicer(IPosition(2,1,0),
+					      IPosition(2,1,2))).
+                         reform(IPosition(2,1,2*nrrow));
+    Array<Float> farr3 = fa3.getColumn(Slicer(IPosition(2,1,0),
+					      IPosition(2,1,2))).
+                         reform(IPosition(2,1,2*nrrow));
+    Array<DComplex> dcarr1 = dca1.getColumn(Slicer(IPosition(2,1,0),
+						   IPosition(2,1,2))).
+                        reform(IPosition(2,1,2*nrrow));
+    Array<DComplex> dcarr2 = dca2.getColumn(Slicer(IPosition(2,1,0),
+						   IPosition(2,1,2))).
+                             reform(IPosition(2,1,2*nrrow));
+    Array<DComplex> dcarr3 = dca3.getColumn(Slicer(IPosition(2,1,0),
+						   IPosition(2,1,2))).
+                             reform(IPosition(2,1,2*nrrow));
     for (uInt i=0; i<nrrow; i++) {
       sprintf (buf, "%d", i);
       s1 += buf;
@@ -261,6 +470,35 @@ void checktab (const String& prefix)
 				 emptyArray
 	                         (IPosition(2,1,0), IPosition(2,1,1))));
       }
+
+      AlwaysAssertExit (allEQ (barr1(IPosition(2,0,2*i), IPosition(2,0,2*i+1)),
+			       arrb(IPosition(2,1,0), IPosition(2,1,1))));
+      AlwaysAssertExit (allEQ (barr2(IPosition(2,0,2*i), IPosition(2,0,2*i+1)),
+			       (!arrb)(IPosition(2,1,0), IPosition(2,1,1))));
+      AlwaysAssertExit (allEQ (barr3(IPosition(2,0,2*i), IPosition(2,0,2*i+1)),
+			       arrb(IPosition(2,1,0), IPosition(2,1,1))));
+
+      AlwaysAssertExit (allEQ (farr1(IPosition(2,0,2*i), IPosition(2,0,2*i+1)),
+			       arrf(IPosition(2,1,0), IPosition(2,1,1))));
+      AlwaysAssertExit (allEQ (farr2(IPosition(2,0,2*i), IPosition(2,0,2*i+1)),
+			       (arrf+float(1))
+			       (IPosition(2,1,0), IPosition(2,1,1))));
+      AlwaysAssertExit (allEQ (farr3(IPosition(2,0,2*i), IPosition(2,0,2*i+1)),
+			       (arrf+float(2))
+			       (IPosition(2,1,0), IPosition(2,1,1))));
+
+      AlwaysAssertExit (allEQ(dcarr1(IPosition(2,0,2*i), IPosition(2,0,2*i+1)),
+			      arrdc(IPosition(2,1,0), IPosition(2,1,1))));
+      AlwaysAssertExit (allEQ(dcarr2(IPosition(2,0,2*i), IPosition(2,0,2*i+1)),
+			      (arrdc+DComplex(1,2))
+			      (IPosition(2,1,0), IPosition(2,1,1))));
+      AlwaysAssertExit (allEQ(dcarr3(IPosition(2,0,2*i), IPosition(2,0,2*i+1)),
+			      (arrdc+DComplex(3,4))
+			      (IPosition(2,1,0), IPosition(2,1,1))));
+
+      arrb = !arrb;
+      arrf += float(10);
+      arrdc += DComplex(10,20);
     }
   }
 }
@@ -277,12 +515,31 @@ void extab (const String& prefix)
   filledArray(IPosition(2,1,1)) = "str_11_";
   filledArray(IPosition(2,1,2)) = "str_12_";
 
+  Array<Float> arrf(IPosition(2,2,3));
+  indgen (arrf);
+  Array<Double> arrd(IPosition(2,4,3));
+  indgen (arrd);
+  Array<DComplex> arrdc = RealToComplex (arrd);
+  Array<Bool> arrb = (fmod(arrf,float(4)) == float(0));
+
   ScalarColumn<String> str1 (tab, "str1");
   ScalarColumn<String> str2 (tab, "str2");
   ArrayColumn<String> stra1 (tab, "stra1");
   ArrayColumn<String> stra2 (tab, "stra2");
   ArrayColumn<String> stra3 (tab, "stra3");
   ArrayColumn<String> stra4 (tab, "stra4");
+  ScalarColumn<Bool> b1 (tab, "b1");
+  ArrayColumn<Bool> ba1 (tab, "ba1");
+  ArrayColumn<Bool> ba2 (tab, "ba2");
+  ArrayColumn<Bool> ba3 (tab, "ba3");
+  ScalarColumn<Float> f1 (tab, "f1");
+  ArrayColumn<Float> fa1 (tab, "fa1");
+  ArrayColumn<Float> fa2 (tab, "fa2");
+  ArrayColumn<Float> fa3 (tab, "fa3");
+  ScalarColumn<DComplex> dc1 (tab, "dc1");
+  ArrayColumn<DComplex> dca1 (tab, "dca1");
+  ArrayColumn<DComplex> dca2 (tab, "dca2");
+  ArrayColumn<DComplex> dca3 (tab, "dca3");
 
   char buf[8];
   String s1(prefix + "str1_");
@@ -305,6 +562,25 @@ void extab (const String& prefix)
     } else {
       stra4.setShape (i, filledArray.shape());
     }
+
+    b1.put(i, i%2==0);
+    ba1.put (i, arrb);
+    ba2.put (i, !arrb);
+    ba3.put (i, arrb);
+
+    f1.put(i, i+1);
+    fa1.put (i, arrf);
+    fa2.put (i, arrf+float(1));
+    fa3.put (i, arrf+float(2));
+
+    dc1.put(i, DComplex(i+1,i+2));
+    dca1.put (i, arrdc);
+    dca2.put (i, arrdc+DComplex(1,2));
+    dca3.put (i, arrdc+DComplex(3,4));
+
+    arrb = !arrb;
+    arrf += float(10);
+    arrdc += DComplex(10,20);
   }
 
   checktab(prefix);
@@ -325,7 +601,7 @@ void doTest (uInt nrrow, const DataManager& stman)
 main (int argc, const char* argv[])
 {
   uInt nrrow = 10;
-  uInt bucketSize = 100;
+  uInt bucketSize = 500;
   if (argc > 1) {
     istrstream istr(argv[1]);
     istr >> nrrow;
@@ -337,7 +613,7 @@ main (int argc, const char* argv[])
 
   try {
     doTest (nrrow, StManAipsIO());
-    doTest (nrrow, StandardStMan (max(bucketSize,100u)));
+    doTest (nrrow, StandardStMan (max(bucketSize,500u)));
     doTest (nrrow, IncrementalStMan (max(bucketSize,1000u), False));
   } catch (AipsError x) {
     cout << "Caught an exception: " << x.getMesg() << endl;
