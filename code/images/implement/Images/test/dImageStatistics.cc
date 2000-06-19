d520f86c457c1543dc93acf51cddc0765c937f51
Neil Killeen <nkilleen@unimelb.edu.au>
2000-06-19 11:27:44 +0000

    remove end_try

--- code/images/implement/Images/test/dImageStatistics.cc
+++ code/images/implement/Images/test/dImageStatistics.cc
@@ -1,5 +1,5 @@
 //# dImageStatistics.cc: image statistics program
-//# Copyright (C) 1996,1997,1998,1999
+//# Copyright (C) 1996,1997,1998,1999,2000
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
@@ -416,7 +416,7 @@ try {
   catch (AipsError x) {
      cerr << "aipserror: error " << x.getMesg() << endl;
      return 1;
-  } end_try;
+  }
 
   return 0;
 }
