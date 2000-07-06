78b00ec69086ce0e44fbdbfe0362ea4da9f36ddf
Ralph Marson <rmarson@nrao.edu>
2000-07-06 00:18:32 +0000

    Updated the setMeasureRef functions Updated the setMeasureRef functions

--- code/ms/implement/MeasurementSets/MSHistoryColumns.h
+++ code/ms/implement/MeasurementSets/MSHistoryColumns.h
@@ -212,6 +212,10 @@ public:
     return RONewMSHistoryColumns::timeMeas();}
   // </group>
 
+  // set the epoch type for the TIME column. This can only be done when the
+  // table has no rows. Trying to do so at other times will throw an exception.
+  void setEpochRef(MEpoch::Types ref);
+
 protected:
   //# default constructor creates a object that is not usable. Use the attach
   //# function correct this.
