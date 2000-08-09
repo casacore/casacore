b22cc967b130afb858b457833f12235717cb7c5c
Mark Holdaway <mholdawa@nrao.edu>
2000-08-09 18:54:13 +0000

    test methods to return EVEN composite numbers.

--- code/casa/implement/Utilities/test/tCompositeNumber.cc
+++ code/casa/implement/Utilities/test/tCompositeNumber.cc
@@ -38,24 +38,49 @@ int main()
     uInt n;
     n= cn.nextLarger(41);
     cout << "Next larger composite number of 41 is " << n << endl;
+    n= cn.nextLargerEven(41);
+    cout << "Next larger even composite number of 41 is " << n << endl;
     n = cn.nextSmaller(41);
     cout << "Next smaller composite number of 41 is " << n << endl;
+    n = cn.nextSmallerEven(41);
+    cout << "Next smaller even composite number of 41 is " << n << endl;
     n = cn.nearest(41);
     cout << "The nearest composite number to 41 is " << n << endl;
+    n = cn.nearestEven(41);
+    cout << "The nearest even composite number to 41 is " << n << endl;
   
     n = cn.nextLarger(397);
     cout << "Next larger composite number of 397 is " << n << endl;
+    n = cn.nextLargerEven(397);
+    cout << "Next larger even composite number of 397 is " << n << endl;
+    n = cn.nextSmallerEven(397);
+    cout << "Next smaller even composite number of 397 is " << n << endl;
     n = cn.nextSmaller(397);
     cout << "Next smaller composite number of 397 is " << n << endl;
     n = cn.nearest(397);
     cout << "The nearest composite number to 397 is " << n << endl; 
+    n = cn.nearestEven(397);
+    cout << "The nearest even composite number to 397 is " << n << endl; 
   
+    n = cn.nextLargerEven(9362);
+    cout << "Next larger even composite number of 9362 is " << n << endl;
     n = cn.nextLarger(9362);
     cout << "Next larger composite number of 9362 is " << n << endl;
     n = cn.nextSmaller(9362);
     cout << "Next smaller composite number of 9362 is " << n << endl;
     n = cn.nearest(9362);
     cout << "The nearest composite number to 9362 is " << n << endl; 
+    n = cn.nearestEven(9362);
+    cout << "The nearest even composite number to 9362 is " << n << endl; 
+
+    cout << "Is 40 composite? " << cn.isComposite(40) << endl;
+    cout << "Is 41 composite? " << cn.isComposite(41) << endl;
+    cout << "Is 128 composite? " << cn.isComposite(128) << endl;
+    cout << "Is 129 composite? " << cn.isComposite(129) << endl;
+    cout << "Is 1024 composite? " << cn.isComposite(1024) << endl;
+    cout << "Is 1025 composite? " << cn.isComposite(1025) << endl;
+    cout << "Is 1026 composite? " << cn.isComposite(1026) << endl;
+    cout << "Is 1027 composite? " << cn.isComposite(1027) << endl;
   }
 
   {
@@ -74,5 +99,14 @@ int main()
     cout << "Next smaller composite number of 397 is " << n << endl;
     n = cn.nearest(397);
     cout << "The nearest composite number to 397 is " << n << endl; 
+
+    cout << "Is 40 composite? " << cn.isComposite(40) << endl;
+    cout << "Is 41 composite? " << cn.isComposite(41) << endl;
+    cout << "Is 128 composite? " << cn.isComposite(128) << endl;
+    cout << "Is 129 composite? " << cn.isComposite(129) << endl;
+    cout << "Is 1024 composite? " << cn.isComposite(1024) << endl;
+    cout << "Is 1025 composite? " << cn.isComposite(1025) << endl;
+    cout << "Is 1026 composite? " << cn.isComposite(1026) << endl;
+    cout << "Is 1027 composite? " << cn.isComposite(1027) << endl;
   }
 };
