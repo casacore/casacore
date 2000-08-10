c6af3e0e79413cc70cd41a82c2d898e1d24a38c0
Neil Killeen <nkilleen@unimelb.edu.au>
2000-08-10 07:47:00 +0000

    imporve function toKernelTypes

--- code/scimath/implement/Mathematics/VectorKernel.h
+++ code/scimath/implement/Mathematics/VectorKernel.h
@@ -1,5 +1,5 @@
 //# VectorKernel.h: generate Vector kernels
-//# Copyright (C) 1996,1997,1998,1999
+//# Copyright (C) 1996,1997,1998,1999,2000
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
