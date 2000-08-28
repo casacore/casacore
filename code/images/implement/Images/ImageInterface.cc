3b15b4b032d4772d3d0fd11af791eb4710f814e6
Ger van Diepen <gervandiepen@gmail.com>
2000-08-28 09:40:32 +0000

    Implemented Regionhandler functions Handle temporary close of files

--- code/images/implement/Images/ImageInterface.cc
+++ code/images/implement/Images/ImageInterface.cc
@@ -35,6 +35,7 @@
 #include <trial/Images/ImageInterface.h>
 #include <trial/Images/LELImageCoord.h>
 #include <trial/Images/ImageRegion.h>
+#include <trial/Lattices/LCRegion.h>
 #include <aips/Lattices/LatticeIterator.h>
 
 #include <aips/Arrays/ArrayMath.h>
@@ -48,23 +49,43 @@
 
 template <class T> 
 ImageInterface<T>::ImageInterface()
+: logClosed_p  (False),
+  regHandPtr_p (0)
 {
   logSink() << LogOrigin("ImageInterface<T>",
 	    "ImageInterface()",
 			 WHERE) << LogIO::DEBUGGING <<
     "Creating ImageInterface" << LogIO::POST;
+  regHandPtr_p = new RegionHandler();
+}
+
+template <class T> 
+ImageInterface<T>::ImageInterface (const RegionHandler& regHand)
+: logClosed_p  (False),
+  regHandPtr_p (0)
+{
+  logSink() << LogOrigin("ImageInterface<T>",
+	    "ImageInterface()",
+			 WHERE) << LogIO::DEBUGGING <<
+    "Creating ImageInterface" << LogIO::POST;
+  regHandPtr_p = regHand.clone();
+  regHandPtr_p->setObjectPtr (this);
 }
 
 template <class T> 
 ImageInterface<T>::ImageInterface (const ImageInterface& other)
 : MaskedLattice<T> (other),
-  coords_p (other.coords_p),
-  log_p    (other.log_p),
-  imageInfo_p(other.imageInfo_p)
+  coords_p     (other.coords_p),
+  log_p        (other.log_p),
+  logClosed_p  (other.logClosed_p),
+  imageInfo_p  (other.imageInfo_p),
+  regHandPtr_p (0)
 {
   logSink() << LogOrigin("ImageInterface<T>",
 	    "ImageInterface(const ImageInterface&)",
 			 WHERE) << LogIO::DEBUGGING << LogIO::POST;
+  regHandPtr_p = other.regHandPtr_p->clone();
+  regHandPtr_p->setObjectPtr (this);
 }
 
 template <class T> 
@@ -72,16 +93,23 @@ ImageInterface<T>& ImageInterface<T>::operator= (const ImageInterface& other)
 {
   if (this != &other) {
     MaskedLattice<T>::operator= (other);
-    coords_p = other.coords_p;
-    log_p    = other.log_p;
+    coords_p    = other.coords_p;
+    log_p       = other.log_p;
+    logClosed_p = other.logClosed_p;
     imageInfo_p = other.imageInfo_p;
+    delete regHandPtr_p;
+    regHandPtr_p = 0;
+    regHandPtr_p = other.regHandPtr_p->clone();
+    regHandPtr_p->setObjectPtr (this);
   }
   return *this;
 }
 
 template <class T> 
 ImageInterface<T>::~ImageInterface()
-{}
+{
+  delete regHandPtr_p;
+}
 
 template<class T>
 MaskedLattice<T>* ImageInterface<T>::cloneML() const
@@ -155,80 +183,105 @@ LELCoordinates ImageInterface<T>::lelCoordinates() const
 
 
 template <class T>
-Bool ImageInterface<T>::canDefineRegion() const
+ImageRegion ImageInterface<T>::makeMask (const String& name,
+					 Bool defineAsRegion,
+					 Bool setAsDefaultMask,
+					 Bool initialize,
+					 Bool value)
 {
-  return False;
-}
-template <class T>
-void ImageInterface<T>::defineRegion (const String&, const ImageRegion&,
-				      RegionHandler::GroupType, Bool)
-{}
-template <class T>
-Bool ImageInterface<T>::hasRegion (const String&,
-				   RegionHandler::GroupType) const
-{ return False; }
-template <class T>
-ImageRegion* ImageInterface<T>::getImageRegionPtr (const String&,
-						   RegionHandler::GroupType,
-						   Bool) const
-{ return 0; }
-template <class T>
-void ImageInterface<T>::renameRegion (const String&,
-				      const String&,
-				      RegionHandler::GroupType,
-				      Bool)
-{}
-template <class T>
-void ImageInterface<T>::removeRegion (const String&,
-				      RegionHandler::GroupType,
-				      Bool)
-{}
-template<class T> 
-Vector<String> ImageInterface<T>::regionNames (RegionHandler::GroupType) const
-{
-  return Vector<String>();
+  ImageRegion region = regHandPtr_p->makeMask (*this, name);
+  if (initialize) {
+    region.asMask().set (value);
+  }
+  if (defineAsRegion) {
+    defineRegion (name, region, RegionHandler::Masks);
+    if (setAsDefaultMask) {
+      setDefaultMask (name);
+    }
+  }
+  return region;
 }
 
 template <class T>
-void ImageInterface<T>::setDefaultMask (const String&)
-{}
+void ImageInterface<T>::defineRegion (const String& name,
+				      const ImageRegion& region,
+				      RegionHandler::GroupType type,
+				      Bool overwrite)
+{
+  regHandPtr_p->defineRegion (name, region, type, overwrite);
+}
+template <class T>
+Bool ImageInterface<T>::hasRegion (const String& name,
+				   RegionHandler::GroupType type) const
+{
+  return regHandPtr_p->hasRegion (name, type);
+}
+template <class T>
+ImageRegion* ImageInterface<T>::getImageRegionPtr
+                                     (const String& name,
+				      RegionHandler::GroupType type,
+				      Bool throwIfUnknown) const
+{
+  return regHandPtr_p->getRegion (name, type, throwIfUnknown);
+}
+template <class T>
+void ImageInterface<T>::renameRegion (const String& newName,
+				      const String& oldName,
+				      RegionHandler::GroupType type,
+				      Bool throwIfUnknown)
+{
+  regHandPtr_p->renameRegion (newName, oldName, type, throwIfUnknown);
+}
+template <class T>
+void ImageInterface<T>::removeRegion (const String& name,
+				      RegionHandler::GroupType type,
+				      Bool throwIfUnknown)
+{
+  regHandPtr_p->removeRegion (name, type, throwIfUnknown);
+}
+template<class T> 
+Vector<String> ImageInterface<T>::regionNames
+                                     (RegionHandler::GroupType type) const
+{
+  return regHandPtr_p->regionNames (type);
+}
+
+template <class T>
+void ImageInterface<T>::setDefaultMask (const String& name)
+{
+  regHandPtr_p->setDefaultMask (name);
+}
 template <class T>
 String ImageInterface<T>::getDefaultMask() const
-{ return ""; }
+{
+  return regHandPtr_p->getDefaultMask();
+}
+
+template <class T>
+void ImageInterface<T>::useMask (MaskSpecifier)
+{
+  throw AipsError ("ImageInterface::useMask - not implemented");
+}
 
 template <class T>
 ImageRegion ImageInterface<T>::getRegion (const String& regionName,
 					  RegionHandler::GroupType type) const
 {
-    ImageRegion* regptr = getImageRegionPtr (regionName, type, True);
-    ImageRegion reg(*regptr);
-    delete regptr;
-    return reg;
+  ImageRegion* regptr = getImageRegionPtr (regionName, type, True);
+  ImageRegion reg(*regptr);
+  delete regptr;
+  return reg;
 }
 
 template<class T>
 String ImageInterface<T>::makeUniqueRegionName (const String& rootName,
 						uInt startNumber) const
 {
-    while (True) {
-        ostrstream oss;
-	oss << startNumber;
-	String name = rootName + String(oss);
-	if (! hasRegion (name, RegionHandler::Any)) {
-	    return name;
-	}
-	startNumber++;
-   }
-   return String();
+  return regHandPtr_p->makeUniqueRegionName (rootName, startNumber);
 }
 
 
-template<class T>
-ImageInfo ImageInterface<T>::imageInfo() const
-{    
-   return imageInfo_p;
-}    
-        
+
 template<class T>
 Bool ImageInterface<T>::setImageInfo(const ImageInfo& info)
 //
@@ -258,4 +311,26 @@ Bool ImageInterface<T>::restoreImageInfo(const RecordInterface& rec)
 
 template<class T>
 void ImageInterface<T>::mergeTableLogSink (const LogIO&)
+{}
+
+
+template<class T>
+void ImageInterface<T>::closeLogSink (Bool temporarily)
+{
+  // Set log object to the global logsink only.
+  log_p = LogIO();
+  logClosed_p = temporarily;
+}
+
+template<class T>
+void ImageInterface<T>::reopenLog()
+{
+  if (logClosed_p) {
+    doReopenLogSink();
+    logClosed_p = False;
+  }
+}
+
+template<class T>
+void ImageInterface<T>::doReopenLogSink()
 {}
