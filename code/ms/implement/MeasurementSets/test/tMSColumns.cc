c1ee785038780f1fd8325503e1c106ba1c924a81
Athol Kemball <akemball@illinois.edu>
2000-09-17 23:01:09 +0000

    Migration NewMS->MS (now v2).

--- code/ms/implement/MeasurementSets/test/tMSColumns.cc
+++ code/ms/implement/MeasurementSets/test/tMSColumns.cc
@@ -1,4 +1,4 @@
-//# tNewMSColumns.cc : this program tests the NewMSColumns classes
+//# tMSColumns.cc : this program tests the MSColumns classes
 //# Copyright (C) 1999,2000
 //# Associated Universities, Inc. Washington DC, USA.
 //#
@@ -27,159 +27,159 @@
 
 //# Includes
 
-#include <aips/MeasurementSets/NewMSColumns.h>
-#include <aips/MeasurementSets/NewMeasurementSet.h>
+#include <aips/MeasurementSets/MSColumns.h>
+#include <aips/MeasurementSets/MeasurementSet.h>
 #include <aips/Exceptions/Error.h>
 #include <aips/Tables.h>
 
 int main() {
   try {
     {    
-      // create a NewMeasurementSet with all predefined columns
+      // create a MeasurementSet with all predefined columns
       TableDesc td;
-      for (uInt i = 1; i < NewMS::NUMBER_PREDEFINED_COLUMNS; i++) {
-	NewMS::addColumnToDesc(td, NewMS::PredefinedColumns(i));
+      for (uInt i = 1; i < MS::NUMBER_PREDEFINED_COLUMNS; i++) {
+	MS::addColumnToDesc(td, MS::PredefinedColumns(i));
       }
       
-      SetupNewTable newtab("tNewMSColumns_table.ms",td,Table::New);  
+      SetupNewTable newtab("tMSColumns_table.ms",td,Table::New);  
       
-      NewMeasurementSet ms(newtab,1);
+      MeasurementSet ms(newtab,1);
       
       // now add all subtables, each with all predefined columns
       
       TableDesc tdAntenna;
-      for (uInt i = 1 ; i<=NewMSAntenna::NUMBER_PREDEFINED_COLUMNS; i++) {
-	NewMSAntenna::addColumnToDesc(tdAntenna, NewMSAntenna::PredefinedColumns(i));
+      for (uInt i = 1 ; i<=MSAntenna::NUMBER_PREDEFINED_COLUMNS; i++) {
+	MSAntenna::addColumnToDesc(tdAntenna, MSAntenna::PredefinedColumns(i));
       }
       SetupNewTable antennaSetup(ms.antennaTableName(),tdAntenna,Table::New);
-      ms.rwKeywordSet().defineTable(NewMS::keywordName(NewMS::ANTENNA),Table(antennaSetup));
+      ms.rwKeywordSet().defineTable(MS::keywordName(MS::ANTENNA),Table(antennaSetup));
       
       TableDesc tddataDescription;
-      for (uInt i = 1 ; i<=NewMSDataDescription::NUMBER_PREDEFINED_COLUMNS; i++) {
-	NewMSDataDescription::addColumnToDesc(tddataDescription, NewMSDataDescription::PredefinedColumns(i));
+      for (uInt i = 1 ; i<=MSDataDescription::NUMBER_PREDEFINED_COLUMNS; i++) {
+	MSDataDescription::addColumnToDesc(tddataDescription, MSDataDescription::PredefinedColumns(i));
       }
       SetupNewTable dataDescriptionSetup(ms.dataDescriptionTableName(),tddataDescription,Table::New);
-      ms.rwKeywordSet().defineTable(NewMS::keywordName(NewMS::DATA_DESCRIPTION),
+      ms.rwKeywordSet().defineTable(MS::keywordName(MS::DATA_DESCRIPTION),
 				    Table(dataDescriptionSetup));
       
       TableDesc tdDoppler;
-      for (uInt i = 1 ; i<=NewMSDoppler::NUMBER_PREDEFINED_COLUMNS; i++) {
-	NewMSDoppler::addColumnToDesc(tdDoppler, NewMSDoppler::PredefinedColumns(i));
+      for (uInt i = 1 ; i<=MSDoppler::NUMBER_PREDEFINED_COLUMNS; i++) {
+	MSDoppler::addColumnToDesc(tdDoppler, MSDoppler::PredefinedColumns(i));
       }
       SetupNewTable dopplerSetup(ms.dopplerTableName(),tdDoppler,Table::New);
-      ms.rwKeywordSet().defineTable(NewMS::keywordName(NewMS::DOPPLER),Table(dopplerSetup));
+      ms.rwKeywordSet().defineTable(MS::keywordName(MS::DOPPLER),Table(dopplerSetup));
       
       TableDesc tdFeed;
-      for (uInt i = 1 ; i<=NewMSFeed::NUMBER_PREDEFINED_COLUMNS; i++) {
-	NewMSFeed::addColumnToDesc(tdFeed, NewMSFeed::PredefinedColumns(i));
+      for (uInt i = 1 ; i<=MSFeed::NUMBER_PREDEFINED_COLUMNS; i++) {
+	MSFeed::addColumnToDesc(tdFeed, MSFeed::PredefinedColumns(i));
       }
       SetupNewTable feedSetup(ms.feedTableName(),tdFeed,Table::New);
-      ms.rwKeywordSet().defineTable(NewMS::keywordName(NewMS::FEED),Table(feedSetup));
+      ms.rwKeywordSet().defineTable(MS::keywordName(MS::FEED),Table(feedSetup));
       
       TableDesc tdField;
-      for (uInt i = 1 ; i<=NewMSField::NUMBER_PREDEFINED_COLUMNS; i++) {
-	NewMSField::addColumnToDesc(tdField, NewMSField::PredefinedColumns(i));
+      for (uInt i = 1 ; i<=MSField::NUMBER_PREDEFINED_COLUMNS; i++) {
+	MSField::addColumnToDesc(tdField, MSField::PredefinedColumns(i));
       }
       SetupNewTable fieldSetup(ms.fieldTableName(),tdField,Table::New);
-      ms.rwKeywordSet().defineTable(NewMS::keywordName(NewMS::FIELD),Table(fieldSetup));
+      ms.rwKeywordSet().defineTable(MS::keywordName(MS::FIELD),Table(fieldSetup));
       
       TableDesc tdFlagCmd;
-      for (uInt i = 1 ; i<=NewMSFlagCmd::NUMBER_PREDEFINED_COLUMNS; i++) {
-	NewMSFlagCmd::addColumnToDesc(tdFlagCmd, NewMSFlagCmd::PredefinedColumns(i));
+      for (uInt i = 1 ; i<=MSFlagCmd::NUMBER_PREDEFINED_COLUMNS; i++) {
+	MSFlagCmd::addColumnToDesc(tdFlagCmd, MSFlagCmd::PredefinedColumns(i));
       }
       SetupNewTable flagCmdSetup(ms.flagCmdTableName(),tdFlagCmd,Table::New);
-      ms.rwKeywordSet().defineTable(NewMS::keywordName(NewMS::FLAG_CMD),Table(flagCmdSetup));
+      ms.rwKeywordSet().defineTable(MS::keywordName(MS::FLAG_CMD),Table(flagCmdSetup));
       
       TableDesc tdFreqOffset;
-      for (uInt i = 1 ; i<=NewMSFreqOffset::NUMBER_PREDEFINED_COLUMNS; i++) {
-	NewMSFreqOffset::addColumnToDesc(tdFreqOffset, NewMSFreqOffset::PredefinedColumns(i));
+      for (uInt i = 1 ; i<=MSFreqOffset::NUMBER_PREDEFINED_COLUMNS; i++) {
+	MSFreqOffset::addColumnToDesc(tdFreqOffset, MSFreqOffset::PredefinedColumns(i));
       }
       SetupNewTable freqOffsetSetup(ms.freqOffsetTableName(),tdFreqOffset,Table::New);
-      ms.rwKeywordSet().defineTable(NewMS::keywordName(NewMS::FREQ_OFFSET),Table(freqOffsetSetup));
+      ms.rwKeywordSet().defineTable(MS::keywordName(MS::FREQ_OFFSET),Table(freqOffsetSetup));
       
       TableDesc tdHistory;
-      for (uInt i = 1 ; i<=NewMSHistory::NUMBER_PREDEFINED_COLUMNS; i++) {
-	NewMSHistory::addColumnToDesc(tdHistory, NewMSHistory::PredefinedColumns(i));
+      for (uInt i = 1 ; i<=MSHistory::NUMBER_PREDEFINED_COLUMNS; i++) {
+	MSHistory::addColumnToDesc(tdHistory, MSHistory::PredefinedColumns(i));
       }
       SetupNewTable historySetup(ms.historyTableName(),tdHistory,Table::New);
-      ms.rwKeywordSet().defineTable(NewMS::keywordName(NewMS::HISTORY),Table(historySetup));
+      ms.rwKeywordSet().defineTable(MS::keywordName(MS::HISTORY),Table(historySetup));
       
       TableDesc tdObservation;
-      for (uInt i = 1 ; i<=NewMSObservation::NUMBER_PREDEFINED_COLUMNS; i++) {
-	NewMSObservation::addColumnToDesc(tdObservation, NewMSObservation::PredefinedColumns(i));
+      for (uInt i = 1 ; i<=MSObservation::NUMBER_PREDEFINED_COLUMNS; i++) {
+	MSObservation::addColumnToDesc(tdObservation, MSObservation::PredefinedColumns(i));
       }
       SetupNewTable observationSetup(ms.observationTableName(),tdObservation,Table::New);
-      ms.rwKeywordSet().defineTable(NewMS::keywordName(NewMS::OBSERVATION),Table(observationSetup));
+      ms.rwKeywordSet().defineTable(MS::keywordName(MS::OBSERVATION),Table(observationSetup));
       
       TableDesc tdPointing;
-      for (uInt i = 1 ; i<=NewMSPointing::NUMBER_PREDEFINED_COLUMNS; i++) {
-	NewMSPointing::addColumnToDesc(tdPointing, NewMSPointing::PredefinedColumns(i));
+      for (uInt i = 1 ; i<=MSPointing::NUMBER_PREDEFINED_COLUMNS; i++) {
+	MSPointing::addColumnToDesc(tdPointing, MSPointing::PredefinedColumns(i));
       }
       SetupNewTable pointingSetup(ms.pointingTableName(),tdPointing,Table::New);
-      ms.rwKeywordSet().defineTable(NewMS::keywordName(NewMS::POINTING),Table(pointingSetup));
+      ms.rwKeywordSet().defineTable(MS::keywordName(MS::POINTING),Table(pointingSetup));
       
       TableDesc tdPolarization;
-      for (uInt i = 1 ; i<=NewMSPolarization::NUMBER_PREDEFINED_COLUMNS; i++) {
-	NewMSPolarization::addColumnToDesc(tdPolarization, NewMSPolarization::PredefinedColumns(i));
+      for (uInt i = 1 ; i<=MSPolarization::NUMBER_PREDEFINED_COLUMNS; i++) {
+	MSPolarization::addColumnToDesc(tdPolarization, MSPolarization::PredefinedColumns(i));
       }
       SetupNewTable polarizationSetup(ms.polarizationTableName(),tdPolarization,Table::New);
-      ms.rwKeywordSet().defineTable(NewMS::keywordName(NewMS::POLARIZATION),Table(polarizationSetup));
+      ms.rwKeywordSet().defineTable(MS::keywordName(MS::POLARIZATION),Table(polarizationSetup));
       
       TableDesc tdProcessor;
-      for (uInt i = 1 ; i<=NewMSProcessor::NUMBER_PREDEFINED_COLUMNS; i++) {
-	NewMSProcessor::addColumnToDesc(tdProcessor, NewMSProcessor::PredefinedColumns(i));
+      for (uInt i = 1 ; i<=MSProcessor::NUMBER_PREDEFINED_COLUMNS; i++) {
+	MSProcessor::addColumnToDesc(tdProcessor, MSProcessor::PredefinedColumns(i));
       }
       SetupNewTable processorSetup(ms.processorTableName(),tdProcessor,Table::New);
-      ms.rwKeywordSet().defineTable(NewMS::keywordName(NewMS::PROCESSOR),Table(processorSetup));
+      ms.rwKeywordSet().defineTable(MS::keywordName(MS::PROCESSOR),Table(processorSetup));
       
       TableDesc tdSource;
-      for (uInt i = 1 ; i<=NewMSSource::NUMBER_PREDEFINED_COLUMNS; i++) {
-	NewMSSource::addColumnToDesc(tdSource, NewMSSource::PredefinedColumns(i));
+      for (uInt i = 1 ; i<=MSSource::NUMBER_PREDEFINED_COLUMNS; i++) {
+	MSSource::addColumnToDesc(tdSource, MSSource::PredefinedColumns(i));
       }
       SetupNewTable sourceSetup(ms.sourceTableName(),tdSource,Table::New);
-      ms.rwKeywordSet().defineTable(NewMS::keywordName(NewMS::SOURCE),Table(sourceSetup));
+      ms.rwKeywordSet().defineTable(MS::keywordName(MS::SOURCE),Table(sourceSetup));
       
       TableDesc tdSpectralWindow;
-      for (uInt i = 1 ; i<=NewMSSpectralWindow::NUMBER_PREDEFINED_COLUMNS; i++) {
-	NewMSSpectralWindow::addColumnToDesc(tdSpectralWindow, NewMSSpectralWindow::PredefinedColumns(i));
+      for (uInt i = 1 ; i<=MSSpectralWindow::NUMBER_PREDEFINED_COLUMNS; i++) {
+	MSSpectralWindow::addColumnToDesc(tdSpectralWindow, MSSpectralWindow::PredefinedColumns(i));
       }
       SetupNewTable spectralWindowSetup(ms.spectralWindowTableName(),tdSpectralWindow,Table::New);
-      ms.rwKeywordSet().defineTable(NewMS::keywordName(NewMS::SPECTRAL_WINDOW),Table(spectralWindowSetup));
+      ms.rwKeywordSet().defineTable(MS::keywordName(MS::SPECTRAL_WINDOW),Table(spectralWindowSetup));
       
       TableDesc tdState;
-      for (uInt i = 1 ; i<=NewMSState::NUMBER_PREDEFINED_COLUMNS; i++) {
-	NewMSState::addColumnToDesc(tdState, NewMSState::PredefinedColumns(i));
+      for (uInt i = 1 ; i<=MSState::NUMBER_PREDEFINED_COLUMNS; i++) {
+	MSState::addColumnToDesc(tdState, MSState::PredefinedColumns(i));
       }
       SetupNewTable stateSetup(ms.stateTableName(),tdState,Table::New);
-      ms.rwKeywordSet().defineTable(NewMS::keywordName(NewMS::STATE),Table(stateSetup));
+      ms.rwKeywordSet().defineTable(MS::keywordName(MS::STATE),Table(stateSetup));
       
       TableDesc tdSysCal;
-      for (uInt i = 1 ; i<=NewMSSysCal::NUMBER_PREDEFINED_COLUMNS; i++) {
-	NewMSSysCal::addColumnToDesc(tdSysCal, NewMSSysCal::PredefinedColumns(i));
+      for (uInt i = 1 ; i<=MSSysCal::NUMBER_PREDEFINED_COLUMNS; i++) {
+	MSSysCal::addColumnToDesc(tdSysCal, MSSysCal::PredefinedColumns(i));
       }
       SetupNewTable sysCalSetup(ms.sysCalTableName(),tdSysCal,Table::New);
-      ms.rwKeywordSet().defineTable(NewMS::keywordName(NewMS::SYSCAL),Table(sysCalSetup));
+      ms.rwKeywordSet().defineTable(MS::keywordName(MS::SYSCAL),Table(sysCalSetup));
       
       TableDesc tdWeather;
-      for (uInt i = 1 ; i<=NewMSWeather::NUMBER_PREDEFINED_COLUMNS; i++) {
-	NewMSWeather::addColumnToDesc(tdWeather, NewMSWeather::PredefinedColumns(i));
+      for (uInt i = 1 ; i<=MSWeather::NUMBER_PREDEFINED_COLUMNS; i++) {
+	MSWeather::addColumnToDesc(tdWeather, MSWeather::PredefinedColumns(i));
       }
       SetupNewTable weatherSetup(ms.weatherTableName(),tdWeather,Table::New);
-      ms.rwKeywordSet().defineTable(NewMS::keywordName(NewMS::WEATHER),Table(weatherSetup));
+      ms.rwKeywordSet().defineTable(MS::keywordName(MS::WEATHER),Table(weatherSetup));
       
       // intialize the references to the subtables just added
       ms.initRefs();
-    } // write the NewMS to disk
+    } // write the MS to disk
 
-    //  now create the NewMSColumns and RONewMSColumns objects, testing all
+    //  now create the MSColumns and ROMSColumns objects, testing all
     // declarations and column definitions for type consistency
     {
-      NewMeasurementSet ms("tNewMSColumns_table.ms",Table::Old);
-      RONewMSColumns romsc(ms);
+      MeasurementSet ms("tMSColumns_table.ms",Table::Old);
+      ROMSColumns romsc(ms);
     }
     {
-      NewMeasurementSet ms("tNewMSColumns_table.ms",Table::Update);
-      NewMSColumns msc(ms);
+      MeasurementSet ms("tMSColumns_table.ms",Table::Update);
+      MSColumns msc(ms);
       // remove the table
       ms.markForDelete();
     }
