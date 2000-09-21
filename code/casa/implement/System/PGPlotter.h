7ab7ed05cd887b9f24a6e72dac4ec56153041f7b
Neil Killeen <nkilleen@unimelb.edu.au>
2000-09-21 05:39:33 +0000

    copyright

--- code/casa/implement/System/PGPlotter.h
+++ code/casa/implement/System/PGPlotter.h
@@ -1,5 +1,5 @@
 //# PGPlotter.h: Standard plotting object for application programmers.
-//# Copyright (C) 1997
+//# Copyright (C) 1997,2000
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
