db7969f54b4e99e07ff4d7477fab6ececd231d66
Bob Garwood <bgarwood@nrao.edu>
2000-10-30 23:28:59 +0000

    The subtable was ns_SDFITS instead of NS_SDFITS. The mapping of columns was done wrong for the case wher the origial column name was NS_SDFITS.  This is now stripped and mapped correctly to the output table. The units of the TIME column were wrong.

--- code/msfits/implement/MSFits/SDFITSHandler.cc
+++ code/msfits/implement/MSFits/SDFITSHandler.cc
@@ -114,7 +114,8 @@ void SDFITSHandler::initAll(MeasurementSet &ms, Vector<Bool> &handledCols, const
 {
     // don't bother unless there are some unhandled columns
     if (anyEQ(handledCols, False)) {
-	TableDesc td = requiredTableDesc(handledCols, row);
+	Vector<String> colNames;
+	TableDesc td = requiredTableDesc(handledCols, colNames, row);
 	// is there already an SDFITS table, or is one needed
 	if (ms.keywordSet().fieldNumber("NS_SDFITS") >= 0 &&
 	    ms.keywordSet().dataType("NS_SDFITS") == TpTable) {
@@ -135,26 +136,24 @@ void SDFITSHandler::initAll(MeasurementSet &ms, Vector<Bool> &handledCols, const
 	    newtab.bindAll(stman);
 	    tab_p = new Table(newtab);
 	    AlwaysAssert(tab_p, AipsError);
-	    ms.rwKeywordSet().defineTable("ns_SDFITS", *tab_p);
+	    ms.rwKeywordSet().defineTable("NS_SDFITS", *tab_p);
 	}
-    }
-
-    // attach the TIME and INTERVAL measure columns
-    timeMeas_p.attach(*tab_p, "TIME");
-    intervalQuant_p.attach(*tab_p, "INTERVAL");
+	// attach the TIME and INTERVAL measure columns
+	timeMeas_p.attach(*tab_p, "TIME");
+	intervalQuant_p.attach(*tab_p, "INTERVAL");
     
-    initRow(handledCols, row);
+	initRow(handledCols, colNames, row);
+    }
 }
 
-void SDFITSHandler::initRow(Vector<Bool> &handledCols, const Record &row)
+void SDFITSHandler::initRow(Vector<Bool> &handledCols, const Vector<String> &colNames, const Record &row)
 {
     // need to get the mapping between row fields and tab columns
     // there are always the same number of elements in handledCols 
     // as there are fields in row
     Vector<Int> fieldMap(handledCols.nelements(),-1);
-    Vector<String> tabCols(tab_p->tableDesc().columnNames());
-    for (uInt i=0;i<tabCols.nelements();i++) {
-	Int field = row.fieldNumber(tabCols(i));
+    for (uInt i=0;i<colNames.nelements();i++) {
+	Int field = row.fieldNumber(colNames(i));
 	if (field >= 0) {
 	    fieldMap(field) = i;
 	    handledCols(field) = True;
@@ -164,93 +163,109 @@ void SDFITSHandler::initRow(Vector<Bool> &handledCols, const Record &row)
     AlwaysAssert(copier_p, AipsError);
 }
 
-TableDesc SDFITSHandler::requiredTableDesc(Vector<Bool> &handledCols, const Record &row)
+TableDesc SDFITSHandler::requiredTableDesc(Vector<Bool> &handledCols, Vector<String> &colNames, 
+					   const Record &row)
 {
     // build a TableDesc using row and any un-handled columns
     TableDesc td;
+    Regex sdfPrefix("^NS_SDFITS_");
+    Regex sdfPrefixMatch("^NS_SDFITS_.*");
+    colNames.resize(handledCols.nelements());
+    colNames = "";
     uInt colCount = 0;
     for (uInt i=0;i<handledCols.nelements();i++) {
 	if (!handledCols(i)) {
-	    colCount++;
 	    // construct the output name
 	    // its the input name unless it starts with NS_SDFITS_ in which case its
 	    // everything after the NS_SDFITS_
 	    String colName = row.name(i);
-	    if (colName.matches("^NS_SDFITS_.*")) {
-		colName = colName.after("^NS_SDFITS_");
-	    }
-	    switch (row.type(i)) {
-	    case TpBool:
-		td.addColumn(ScalarColumnDesc<Bool>(colName));
-		break;
-	    case TpUChar:
-		td.addColumn(ScalarColumnDesc<uChar>(colName));
-		break;
-	    case TpShort:
-		td.addColumn(ScalarColumnDesc<Short>(colName));
-		break;
-	    case TpInt:
-		td.addColumn(ScalarColumnDesc<Int>(colName));
-		break;
-	    case TpFloat:
-		td.addColumn(ScalarColumnDesc<Float>(colName));
-		break;
-	    case TpDouble:
-		td.addColumn(ScalarColumnDesc<Double>(colName));
-		break;
-	    case TpComplex:
-		td.addColumn(ScalarColumnDesc<Complex>(colName));
-		break;
-	    case TpDComplex:
-		td.addColumn(ScalarColumnDesc<DComplex>(colName));
-		break;
-	    case TpString:
-		td.addColumn(ScalarColumnDesc<String>(colName));
-		break;
-	    case TpArrayBool:
-		td.addColumn(ArrayColumnDesc<Bool>(colName));
-		break;
-	    case TpArrayUChar:
-		td.addColumn(ArrayColumnDesc<uChar>(colName));
-		break;
-	    case TpArrayShort:
-		td.addColumn(ArrayColumnDesc<Short>(colName));
-		break;
-	    case TpArrayInt:
-		td.addColumn(ArrayColumnDesc<Int>(colName));
-		break;
-	    case TpArrayFloat:
-		td.addColumn(ArrayColumnDesc<Float>(colName));
-		break;
-	    case TpArrayDouble:
-		td.addColumn(ArrayColumnDesc<Double>(colName));
-		break;
-	    case TpArrayComplex:
-		td.addColumn(ArrayColumnDesc<Complex>(colName));
-		break;
-	    case TpArrayDComplex:
-		td.addColumn(ArrayColumnDesc<DComplex>(colName));
-		break;
-	    case TpArrayString:
-		td.addColumn(ArrayColumnDesc<String>(colName));
-		break;
-	    default:
-		LogIO os;
-		os << LogIO::SEVERE << WHERE 
-		   << "Field " << colName 
-		   << " has an invalid type, " 
-		   << Int(row.type(i))
-		   << " -- this should never happen."
-		   << LogIO::EXCEPTION;
-		break;
+	    if (colName.matches(sdfPrefixMatch)) {
+		colName = colName.after(sdfPrefix);
+	    } 
+	    // ignore any TIME and INTERVAL here
+	    if (colName == "TIME" || colName == "INTERVAL") {
+		handledCols(i) = True;
+	    } else {
+		colNames(colCount++) = row.name(i);
+		switch (row.type(i)) {
+		case TpBool:
+		    td.addColumn(ScalarColumnDesc<Bool>(colName));
+		    break;
+		case TpUChar:
+		    td.addColumn(ScalarColumnDesc<uChar>(colName));
+		    break;
+		case TpShort:
+		    td.addColumn(ScalarColumnDesc<Short>(colName));
+		    break;
+		case TpInt:
+		    td.addColumn(ScalarColumnDesc<Int>(colName));
+		    break;
+		case TpFloat:
+		    td.addColumn(ScalarColumnDesc<Float>(colName));
+		    break;
+		case TpDouble:
+		    td.addColumn(ScalarColumnDesc<Double>(colName));
+		    break;
+		case TpComplex:
+		    td.addColumn(ScalarColumnDesc<Complex>(colName));
+		    break;
+		case TpDComplex:
+		    td.addColumn(ScalarColumnDesc<DComplex>(colName));
+		    break;
+		case TpString:
+		    td.addColumn(ScalarColumnDesc<String>(colName));
+		    break;
+		case TpArrayBool:
+		    td.addColumn(ArrayColumnDesc<Bool>(colName));
+		    break;
+		case TpArrayUChar:
+		    td.addColumn(ArrayColumnDesc<uChar>(colName));
+		    break;
+		case TpArrayShort:
+		    td.addColumn(ArrayColumnDesc<Short>(colName));
+		    break;
+		case TpArrayInt:
+		    td.addColumn(ArrayColumnDesc<Int>(colName));
+		    break;
+		case TpArrayFloat:
+		    td.addColumn(ArrayColumnDesc<Float>(colName));
+		    break;
+		case TpArrayDouble:
+		    td.addColumn(ArrayColumnDesc<Double>(colName));
+		    break;
+		case TpArrayComplex:
+		    td.addColumn(ArrayColumnDesc<Complex>(colName));
+		    break;
+		case TpArrayDComplex:
+		    td.addColumn(ArrayColumnDesc<DComplex>(colName));
+		    break;
+		case TpArrayString:
+		    td.addColumn(ArrayColumnDesc<String>(colName));
+		    break;
+		default:
+		    LogIO os;
+		    os << LogIO::SEVERE << WHERE 
+		       << "Field " << colName 
+		       << " has an invalid type, " 
+		       << Int(row.type(i))
+		       << " -- this should never happen."
+		       << LogIO::EXCEPTION;
+		    break;
+		}
 	    }
 	}
     }
+    colNames.resize(colCount, True);
     // add the TIME and INTERVAL columns
+    // TIME is an MEpoch column
     td.addColumn(ScalarColumnDesc<Double>("TIME"));
     TableMeasDesc<MEpoch> measCol(TableMeasValueDesc(td,"TIME"),
 				  TableMeasRefDesc(MEpoch::DEFAULT));
     measCol.write(td);
+    // and change the units to "s" from the default of "d"
+    TableQuantumDesc timeqd(td,"TIME",Unit("s"));
+    timeqd.write(td);
+    // INTERVAL is a Quantity column
     td.addColumn(ScalarColumnDesc<Double>("INTERVAL"));
     TableQuantumDesc quantCol(td, "INTERVAL", Unit("s"));
     quantCol.write(td);
