5fbde42d92b66e969322b4ca6737987d8fd6d2cb
Ger van Diepen <gervandiepen@gmail.com>
2001-01-10 11:54:01 +0000

    *** empty log message ***

---
+++ code/tables/implement/Tables/NullTable.h
@@ -0,0 +1,139 @@
+//# NullTable.h: Class indicating a null Table object
+//# Copyright (C) 2001
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//# $Id$
+
+#if !defined(AIPS_NULLTABLE_H)
+#define AIPS_NULLTABLE_H
+
+
+//# Includes
+#include <aips/aips.h>
+#include <aips/Tables/BaseTable.h>
+
+
+// <summary>
+// Class indicating a null Table object
+// </summary>
+
+// <use visibility=local>
+
+// <reviewed reviewer="" date="" tests="">
+// </reviewed>
+
+// <prerequisite>
+//# Classes you should understand before using this one.
+//   <li> <linkto class=BaseTable>BaseTable</linkto>
+// </prerequisite>
+
+// <etymology>
+// NullTable represents a null table object, i.e. a Table object without
+// an underlying table..
+// </etymology>
+
+// <synopsis> 
+// Nullable is used to represent a null table.
+// The default Table constructor used to a create a null pointer
+// which results in core dumps when the Table object is actually used.
+// The NullTable object makes it possible to catch such cases
+// and throw an appropriate exception.
+// </synopsis> 
+
+
+class NullTable : public BaseTable
+{
+public:
+  // Default constructor.
+  NullTable();
+
+  virtual ~NullTable();
+
+  // The table is a null table.
+  virtual Bool isNull() const;
+
+  // All functions throw a "null table" exception.
+  // <group>
+  virtual void reopenRW();
+  virtual Bool isMultiUsed (Bool checkSubTable) const;
+  virtual const TableLock& lockOptions() const;
+  virtual void mergeLock (const TableLock& lockOptions);
+  virtual Bool hasLock (FileLocker::LockType) const;
+  virtual Bool lock (FileLocker::LockType, uInt nattempts);
+  virtual void unlock();
+  virtual void flush (Bool sync);
+  virtual void resync();
+  virtual uInt getModifyCounter() const;
+  virtual Bool isWritable() const;
+  virtual TableRecord& keywordSet();
+  virtual TableRecord& rwKeywordSet();
+  virtual BaseColumn* getColumn (uInt columnIndex) const;
+  virtual BaseColumn* getColumn (const String& columnName) const;
+  virtual Bool canAddRow() const;
+  virtual void addRow (uInt nrrow, Bool initialize);
+  virtual Bool canRemoveRow() const;
+  virtual void removeRow (uInt rownr);
+  virtual DataManager* findDataManager (const String& dataManagerName) const;
+  virtual void addColumn (const ColumnDesc& columnDesc);
+  virtual void addColumn (const ColumnDesc& columnDesc,
+			  const String& dataManager, Bool byName);
+  virtual void addColumn (const ColumnDesc& columnDesc,
+			  const DataManager& dataManager);
+  virtual void addColumn (const TableDesc& tableDesc,
+			  const DataManager& dataManager);
+  virtual Bool canRemoveColumn (const String& columnName) const;
+  virtual void removeColumn (const String& columnName);
+  virtual Bool canRenameColumn (const String& columnName) const;
+  virtual void renameColumn (const String& newName, const String& oldName);
+  virtual Vector<uInt> rowNumbers() const;
+  virtual BaseTable* root();
+  virtual Bool rowOrder() const;
+  virtual Vector<uInt>* rowStorage();
+  virtual Bool adjustRownrs (uInt nrrow, Vector<uInt>& rownrs,
+			     Bool determineOrder) const;
+  virtual BaseTable* doSort (PtrBlock<BaseColumn*>&,
+			     const PtrBlock<ObjCompareFunc*>&,
+			     const Block<Int>& sortOrder,
+			     int sortOption);
+  virtual void renameSubTables (const String& newName,
+				const String& oldName);
+
+private:
+  // Copy constructor is forbidden, because copying a table requires
+  // some more knowledge (like table name of result).
+  // Declaring it private, makes it unusable.
+  NullTable (const NullTable&);
+
+  // Assignment is forbidden, because copying a table requires
+  // some more knowledge (like table name of result).
+  // Declaring it private, makes it unusable.
+  NullTable& operator= (const NullTable&);
+
+  // Throw an exception with the name of the function.
+  void throwError (const String& name) const;
+};
+
+
+
+#endif
