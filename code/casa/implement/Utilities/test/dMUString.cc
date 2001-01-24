fef20f6902826495828e651505697220939d3dbc
Wim Brouw <brouw@astron.nl>
2001-01-24 03:57:28 +0000

    Needed Regex.h

--- code/casa/implement/Utilities/test/dMUString.cc
+++ code/casa/implement/Utilities/test/dMUString.cc
@@ -1,5 +1,5 @@
 //# dMUString.cc: test of MUString class
-//# Copyright (C) 1996,1997,1998,1999,2000
+//# Copyright (C) 1996,1997,1998,1999,2000,2001
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This program is free software; you can redistribute it and/or modify it
@@ -30,6 +30,7 @@
 #include <aips/Exceptions/Error.h>
 #include <aips/Quanta/Quantum.h>
 #include <aips/Quanta/MVAngle.h>
+#include <aips/Utilities/Regex.h>
 
 main()
 {
