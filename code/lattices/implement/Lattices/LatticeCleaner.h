5a469c97ec01b542d800871a91bf50eefac03958
Tim Cornwell <t.cornwell@skatelescope.org>
2001-02-26 21:51:04 +0000

    *** empty log message ***

--- code/lattices/implement/Lattices/LatticeCleaner.h
+++ code/lattices/implement/Lattices/LatticeCleaner.h
@@ -1,5 +1,5 @@
 //# Cleaner.h: this defines Cleaner a class for doing convolution
-//# Copyright (C) 1996,1997,1998,1999,2000
+//# Copyright (C) 1996,1997,1998,1999,2000,2001
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
