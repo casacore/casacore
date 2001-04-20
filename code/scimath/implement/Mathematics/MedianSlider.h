2e6c2921fb3c6d06035901d369225b5f177d924c
Oleg Smirnov <osmirnov@gmail.com>
2001-04-20 17:08:43 +0000

    fixed documentation tags

--- code/scimath/implement/Mathematics/MedianSlider.h
+++ code/scimath/implement/Mathematics/MedianSlider.h
@@ -34,7 +34,7 @@
 //# Forward Declarations
 
 // <summary>
-// Optimized sliding median computator
+// Class to compute sliding median
 // </summary>
 
 // <use visibility=export>
@@ -43,56 +43,18 @@
 // </reviewed>
 
 // <synopsis>
-//#! What does the class do?  How?  For whom?  This should include code
-//#! fragments as appropriate to support text.  Code fragments shall be
-//#! delimited by <srcblock> </srcblock> tags.  The synopsis section will
-//#! usually be dozens of lines long.
+// MedianSlider is a class for efficient computing of sliding medians.
 // </synopsis>
 //
 // <example>
-//#! One or two concise (~10-20 lines) examples, with a modest amount of
-//#! text to support code fragments.  Use <srcblock> and </srcblock> to
-//#! delimit example code.
 // </example>
 //
 // <motivation>
-//#! Insight into a class is often provided by a description of
-//#! the circumstances that led to its conception and design.  Describe
-//#! them here.
+// Flagging Agents make extended use of sliding medians.
 // </motivation>
 //
-// <templating arg=T>
-//#! A list of member functions that must be provided by classes that
-//#! appear as actual template arguments.  For example:  imagine that you
-//#! are writing a templated sort class, which does a quicksort on a
-//#! list of arbitrary objects.  Anybody who uses your templated class
-//#! must make sure that the actual argument class (say, Int or
-//#! String or Matrix) has comparison operators defined.
-//#! This tag must be repeated for each template formal argument in the
-//#! template class definition -- that's why this tag has the "arg" attribute.
-//#! (Most templated classes, however, are templated on only a single
-//#! argument.)
-//    <li>
-//    <li>
-// </templating>
-//
-// <thrown>
-//#! A list of exceptions thrown if errors are discovered in the function.
-//#! This tag will appear in the body of the header file, preceding the
-//#! declaration of each function which throws an exception.
-//    <li>
-//    <li>
-// </thrown>
-//
 // <todo asof="yyyy/mm/dd">
-//#! A List of bugs, limitations, extensions or planned refinements.
-//#! The programmer should fill in a date in the "asof" field, which
-//#! will usually be the date at which the class is submitted for review.
-//#! If, during the review, new "todo" items come up, then the "asof"
-//#! date should be changed to the end of the review period.
-//   <li> add this feature
-//   <li> fix this bug
-//   <li> start discussion of this possible extension
+//   <li> think about a 2D sliding median
 // </todo>
 
 class MedianSlider
@@ -107,7 +69,7 @@ public:
 // Adds a datum to the slider. Once the window is full, newer values will 
 // push out older values. Returns the new median value.
 // If flag is set to true, adds a "flagged" datum, one which takes
-// up space in the window but is not concidered for median computations.
+// up space in the window but is skipped during median computations.
   Float add      ( Float d,Bool flag=False );
 // Adds a flagged datum
   Float add      ()                          { return add(0,True); }
@@ -121,7 +83,8 @@ public:
 // Returns the number of values currently in the window. This is less
 // than the window width initially.
   Int  size    ();    
-// Returns the number of non-flagged values
+
+// Returns the number of non-flagged values in window
   Int  nval    ();
   
 // Returns the current median value  
@@ -129,15 +92,19 @@ public:
 
 // Returns a previous value (from n steps ago) from the sliding window
   Float prevVal  ( uInt n,Bool &flag );
-// Returns value from midpoint (center) of window
+
+// Returns value from midpoint (center) of window, possibly with flag
   Float midpoint ( Bool &flag );
-  Float midpoint ()               { Bool dum; return midpoint(dum); }
+  Float midpoint ()               
+          { Bool dum; return midpoint(dum); }
+
 // Returns the difference between the current median and the value
 // at window center. Optionally, also returns flag of median center
   Float diff ( Bool &flag )       { return midpoint(flag) - median(); };
-  Float diff ()                   { Bool dum; return diff(dum); }
+  Float diff ()                   
+           { Bool dum; return diff(dum); }
 
-// returns memory usage for a given halfwin size 
+// returns total memory usage (in bytes) for a given halfwin size 
   static size_t objsize ( int halfwin )
   { return sizeof(MedianSlider)+(sizeof(Float)+sizeof(uInt)+sizeof(Bool))*(halfwin*2+1); }
   
