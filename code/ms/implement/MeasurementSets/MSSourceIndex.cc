cd786c69eddab81cd4717e64a8b989f6f3f86172
Athol Kemball <akemball@illinois.edu>
2001-04-21 18:16:18 +0000

    Defects AOCso2381: add matchSourceName() methods

--- code/ms/implement/MeasurementSets/MSSourceIndex.cc
+++ code/ms/implement/MeasurementSets/MSSourceIndex.cc
@@ -1,5 +1,5 @@
 //# MSSourceIndex.cc:  this defined MSSourceIndex
-//# Copyright (C) 2000
+//# Copyright (C) 2000,2001
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
@@ -27,23 +27,31 @@
 
 #include <trial/MeasurementSets/MSSourceIndex.h>
 
+#include <aips/Arrays/MaskedArray.h>
+#include <aips/Arrays/ArrayMath.h>
+#include <aips/Arrays/ArrayLogical.h>
 #include <aips/Arrays/ArrayUtil.h>
 #include <aips/MeasurementSets/MSSource.h>
 
 MSSourceIndex::MSSourceIndex() 
-    : MSTableIndex()
+  : MSTableIndex(), msSourceCols_p(0)
 {;}
 
 MSSourceIndex::MSSourceIndex(const MSSource &source)
-    : MSTableIndex(source, stringToVector("SOURCE_ID,SPECTRAL_WINDOW_ID"))
-{ attachIds();}
+  : MSTableIndex(source, stringToVector("SOURCE_ID,SPECTRAL_WINDOW_ID"))
+{ 
+  attachIds();
+  msSourceCols_p = new ROMSSourceColumns(source);
+}
 
 MSSourceIndex::MSSourceIndex(const MSSourceIndex &other)
     : MSTableIndex(other)
 { attachIds();}
 
 MSSourceIndex::~MSSourceIndex()
-{;}
+{
+  if (msSourceCols_p) delete msSourceCols_p;
+}
 
 MSSourceIndex &MSSourceIndex::operator=(const MSSourceIndex &other)
 {
@@ -65,4 +73,50 @@ void MSSourceIndex::attachIds()
     sourceId_p.attachToRecord(accessKey(), "SOURCE_ID");
     spwId_p.attachToRecord(accessKey(), "SPECTRAL_WINDOW_ID");
 }
+
+Vector<Int> MSSourceIndex::matchSourceName(const String& name)
+{
+// Match a source name to a set of source id's
+// Input:
+//    name             const String&            Source name to match
+// Output:
+//    matchSourceName  Vector<Int>              Matching source id's
+//
+  Vector<Int> retval;
+  if (!msSourceCols_p->isNull() && msSourceCols_p->nrow() > 0) {
+    LogicalArray maskArray = (msSourceCols_p->name().getColumn()==name);
+    MaskedArray<Int> maskSourceId(msSourceCols_p->sourceId().getColumn(), 
+				  maskArray);
+    retval = maskSourceId.getCompressedArray();
+  };
+  return retval;
+}; 
+
+Vector<Int> MSSourceIndex::matchSourceName(const Vector<String>& names)
+{
+// Match a set of source names to a set of source id's
+// Input:
+//    names            const Vector<String>&    Source names to match
+// Output:
+//    matchSourceNames Vector<Int>              Matching source id's
+//
+  Vector<Int> matchedSourceIds;
+  // Match each source name individually
+  for (Int fld=0; fld<names.nelements(); fld++) {
+    // Add to list of source id's
+    Vector<Int> currentMatch = matchSourceName(names(fld));
+    if (currentMatch.nelements() > 0) {
+      Vector<Int> temp(matchedSourceIds);
+      matchedSourceIds.resize(matchedSourceIds.nelements() +
+			     currentMatch.nelements(), True);
+      matchedSourceIds = concatenateArray(temp, currentMatch);
+    };
+  };
+  return matchedSourceIds;
+};
+
+
+
+
+
 
