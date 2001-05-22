e575eb3532899b220fc004b95fdc6a583d70e4a2
Ger van Diepen <gervandiepen@gmail.com>
2001-05-22 06:20:32 +0000

    *** empty log message ***

---
+++ code/images/implement/Images/ExtendImage.cc
@@ -0,0 +1,286 @@
+//# ExtendImage.cc: An extension of an ImageInterface object
+//# Copyright (C) 2001
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//# $Id$
+
+#include <trial/Images/ExtendImage.h>
+#include <trial/Images/ImageUtilities.h>
+#include <trial/Lattices/ExtendLattice.h>
+#include <trial/Lattices/LatticeRegion.h>
+#include <aips/Arrays/IPosition.h>
+#include <aips/Arrays/Vector.h>
+#include <aips/Quanta/UnitMap.h>
+#include <aips/Utilities/Assert.h>
+#include <aips/Exceptions/Error.h>
+
+
+template<class T>
+ExtendImage<T>::ExtendImage()
+: itsImagePtr  (0),
+  itsExtLatPtr (0)
+{}
+
+template<class T>
+ExtendImage<T>::ExtendImage (const ImageInterface<T>& image,
+			     const IPosition& newShape,
+			     const CoordinateSystem& newCsys)
+: itsImagePtr (image.cloneII())
+{
+  IPosition newAxes, stretchAxes;
+  if (! ImageUtilities::findExtendAxes (newAxes, stretchAxes,
+					newShape, image.shape(),
+					newCsys, image.coordinates())) {
+    throw AipsError ("ExtendImage - "
+		     "new csys or shape incompatible with old ones");
+  }
+  itsExtLatPtr = new ExtendLattice<T> (image, newShape, newAxes, stretchAxes);
+  setCoordsMember (newCsys);
+  setLogMember (image.logSink());
+  setImageInfoMember (image.imageInfo());
+}
+
+template<class T>
+ExtendImage<T>::ExtendImage (const ExtendImage<T>& other)
+: ImageInterface<T> (other),
+  itsImagePtr (other.itsImagePtr->cloneII())
+{
+  itsExtLatPtr = new ExtendLattice<T> (*other.itsExtLatPtr);
+}
+
+template<class T>
+ExtendImage<T>::~ExtendImage()
+{
+  delete itsImagePtr;
+  delete itsExtLatPtr;
+}
+
+template<class T>
+ExtendImage<T>& ExtendImage<T>::operator= (const ExtendImage<T>& other)
+{
+  if (this != &other) {
+    ImageInterface<T>::operator= (other);
+    delete itsImagePtr;
+    itsImagePtr = other.itsImagePtr->cloneII();
+    delete itsExtLatPtr;
+    itsExtLatPtr = new ExtendLattice<T> (*other.itsExtLatPtr);
+  }
+  return *this;
+}
+
+template<class T>
+ImageInterface<T>* ExtendImage<T>::cloneII() const
+{
+  return new ExtendImage<T> (*this);
+}
+
+template<class T>
+String ExtendImage<T>::imageType() const
+{
+  return "ExtendImage";
+}
+
+
+template <class T>
+Bool ExtendImage<T>::ok() const
+{
+  return itsExtLatPtr->ok();
+}
+
+template<class T>
+Bool ExtendImage<T>::isMasked() const
+{
+  return itsExtLatPtr->isMasked();
+}
+
+template<class T>
+Bool ExtendImage<T>::isPersistent() const
+{
+  return itsExtLatPtr->isPersistent();
+}
+
+template<class T>
+Bool ExtendImage<T>::isPaged() const
+{
+  return itsExtLatPtr->isPaged();
+}
+
+template<class T>
+Bool ExtendImage<T>::isWritable() const
+{
+  return itsExtLatPtr->isWritable();
+}
+
+template<class T>
+Bool ExtendImage<T>::hasPixelMask() const
+{
+  return itsExtLatPtr->hasPixelMask();
+}
+
+template<class T>
+const Lattice<Bool>& ExtendImage<T>::pixelMask() const
+{
+  return itsExtLatPtr->pixelMask();
+}
+template<class T>
+Lattice<Bool>& ExtendImage<T>::pixelMask()
+{
+  return itsExtLatPtr->pixelMask();
+}
+
+template<class T>
+const LatticeRegion* ExtendImage<T>::getRegionPtr() const
+{
+  return itsExtLatPtr->getRegionPtr();
+}
+
+template<class T>
+IPosition ExtendImage<T>::shape() const
+{
+  return itsExtLatPtr->shape();
+}
+
+template<class T>
+void ExtendImage<T>::resize (const TiledShape&)
+{
+  throw (AipsError ("ExtendImage::resize is not possible"));
+}
+
+template<class T>
+Bool ExtendImage<T>::setUnits(const Unit&)
+{
+  return False;
+}
+
+template<class T>
+Unit ExtendImage<T>::units() const
+{
+  return itsImagePtr->units();
+}
+
+template<class T>
+String ExtendImage<T>::name (Bool stripPath) const
+{
+  return itsImagePtr->name (stripPath);
+}
+  
+template<class T>
+const RecordInterface& ExtendImage<T>::miscInfo() const
+{
+  return itsImagePtr->miscInfo();
+}
+
+template<class T>
+Bool ExtendImage<T>::setMiscInfo (const RecordInterface&)
+{
+  return False;
+}
+
+template<class T>
+Bool ExtendImage<T>::doGetSlice (Array<T>& buffer,
+			      const Slicer& section)
+{
+  return itsExtLatPtr->doGetSlice (buffer, section);
+}
+
+template<class T>
+void ExtendImage<T>::doPutSlice (const Array<T>& sourceBuffer,
+				 const IPosition& where, 
+				 const IPosition& stride)
+{
+  itsExtLatPtr->doPutSlice (sourceBuffer, where, stride);
+}
+
+template<class T>
+Bool ExtendImage<T>::doGetMaskSlice (Array<Bool>& buffer,
+				     const Slicer& section)
+{
+  return itsExtLatPtr->doGetMaskSlice (buffer, section);
+}
+
+template<class T>
+uInt ExtendImage<T>::advisedMaxPixels() const
+{
+  return itsExtLatPtr->advisedMaxPixels();
+}
+
+template<class T>
+IPosition ExtendImage<T>::doNiceCursorShape (uInt maxPixels) const
+{
+  return itsExtLatPtr->niceCursorShape (maxPixels);
+}
+
+template<class T>
+LatticeIterInterface<T>* ExtendImage<T>::makeIter
+                               (const LatticeNavigator& navigator) const
+{
+  return itsExtLatPtr->makeIter (navigator);
+}
+
+template<class T>
+Bool ExtendImage<T>::lock (FileLocker::LockType type, uInt nattempts)
+{
+  return itsExtLatPtr->lock (type, nattempts);
+}
+template<class T>
+void ExtendImage<T>::unlock()
+{
+  itsExtLatPtr->unlock();
+  itsImagePtr->unlock();
+}
+template<class T>
+Bool ExtendImage<T>::hasLock (FileLocker::LockType type) const
+{
+  return itsExtLatPtr->hasLock (type);
+}
+template<class T>
+void ExtendImage<T>::resync()
+{
+  itsExtLatPtr->resync();
+  itsImagePtr->resync();
+}
+template<class T>
+void ExtendImage<T>::flush()
+{
+  itsImagePtr->flush();
+}
+template<class T>
+void ExtendImage<T>::tempClose()
+{
+  itsExtLatPtr->tempClose();
+  itsImagePtr->tempClose();
+  closeLogSink (True);
+}
+template<class T>
+void ExtendImage<T>::reopen()
+{
+  itsImagePtr->reopen();
+  reopenLog();
+}
+
+template<class T>
+void ExtendImage<T>::doReopenLogSink()
+{
+  setLogMember (itsImagePtr->logSink());
+}
---
+++ code/images/implement/Images/test/tImageUtilities.cc
@@ -0,0 +1,122 @@
+//# tImageUtilities.cc: Test program for the static ImageUtilities functions
+//# Copyright (C) 2001
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//# $Id$
+
+#include <trial/Images/ImageUtilities.h>
+#include <trial/Coordinates/CoordinateSystem.h>
+#include <trial/Coordinates/CoordinateUtil.h>
+#include <aips/Arrays/IPosition.h>
+#include <aips/Utilities/Assert.h>
+#include <aips/iostream.h>
+
+
+void doIt()
+{
+  CoordinateSystem csys2 = CoordinateUtil::defaultCoords2D();
+  CoordinateSystem csys3 = CoordinateUtil::defaultCoords3D();
+  CoordinateSystem csys4 = CoordinateUtil::defaultCoords4D();
+  AlwaysAssertExit (ImageUtilities::compareCoordinates (csys2, csys2) == 0);
+  AlwaysAssertExit (ImageUtilities::compareCoordinates (csys3, csys3) == 0);
+  AlwaysAssertExit (ImageUtilities::compareCoordinates (csys4, csys4) == 0);
+  AlwaysAssertExit (ImageUtilities::compareCoordinates (csys2, csys3) == -1);
+  AlwaysAssertExit (ImageUtilities::compareCoordinates (csys2, csys4) == -1);
+  AlwaysAssertExit (ImageUtilities::compareCoordinates (csys3, csys4) == -1);
+  AlwaysAssertExit (ImageUtilities::compareCoordinates (csys3, csys2) == 1);
+  AlwaysAssertExit (ImageUtilities::compareCoordinates (csys4, csys2) == 1);
+  AlwaysAssertExit (ImageUtilities::compareCoordinates (csys4, csys3) == 1);
+
+  IPosition newAxes, stretchAxes;
+  AlwaysAssertExit (ImageUtilities::findExtendAxes (newAxes, stretchAxes,
+						    IPosition(4,5,6,1,5),
+						    IPosition(4,5,1,1,1),
+						    csys4, csys4));
+  AlwaysAssertExit (newAxes.isEqual (IPosition()));
+  AlwaysAssertExit (stretchAxes.isEqual (IPosition(2,1,3)));
+  AlwaysAssertExit (ImageUtilities::findExtendAxes (newAxes, stretchAxes,
+						    IPosition(3,5,6,3),
+						    IPosition(2,5,6),
+						    csys3, csys2));
+  AlwaysAssertExit (newAxes.isEqual (IPosition(1,2)));
+  AlwaysAssertExit (stretchAxes.isEqual (IPosition()));
+  AlwaysAssertExit (ImageUtilities::findExtendAxes (newAxes, stretchAxes,
+						    IPosition(4,5,6,3,4),
+						    IPosition(2,5,6),
+						    csys4, csys2));
+  AlwaysAssertExit (newAxes.isEqual (IPosition(2,2,3)));
+  AlwaysAssertExit (stretchAxes.isEqual (IPosition()));
+  AlwaysAssertExit (ImageUtilities::findExtendAxes (newAxes, stretchAxes,
+						    IPosition(4,5,6,3,4),
+						    IPosition(3,5,6,1),
+						    csys4, csys3));
+  AlwaysAssertExit (newAxes.isEqual (IPosition(1,2)));
+  AlwaysAssertExit (stretchAxes.isEqual (IPosition(1,3)));
+}
+
+void doIt2()
+{
+  IPosition newAxes, stretchAxes;
+  {
+    CoordinateSystem csys1;
+    CoordinateSystem csys2;
+    CoordinateUtil::addDirAxes (csys1);
+    CoordinateUtil::addFreqAxis (csys2);
+    AlwaysAssertExit (ImageUtilities::compareCoordinates (csys1, csys2) == 9);
+    AlwaysAssertExit (ImageUtilities::compareCoordinates (csys2, csys1) == 9);
+    AlwaysAssertExit (! ImageUtilities::findExtendAxes (newAxes, stretchAxes,
+							IPosition(2,5,6),
+							IPosition(1,8),
+							csys1, csys2));
+
+    CoordinateUtil::addDirAxes (csys2);
+    AlwaysAssertExit (ImageUtilities::findExtendAxes (newAxes, stretchAxes,
+						      IPosition(3,8,5,6),
+						      IPosition(2,1,1),
+						      csys2, csys1));
+    AlwaysAssertExit (newAxes.isEqual (IPosition(1,0)));
+    AlwaysAssertExit (stretchAxes.isEqual (IPosition(2,1,2)));
+
+    CoordinateUtil::addFreqAxis (csys1);
+    AlwaysAssertExit (ImageUtilities::compareCoordinates (csys1, csys2) == 9);
+    AlwaysAssertExit (ImageUtilities::compareCoordinates (csys2, csys1) == 9);
+    AlwaysAssertExit (! ImageUtilities::findExtendAxes (newAxes, stretchAxes,
+							IPosition(3,5,6,3),
+							IPosition(3,3,4,6),
+							csys1, csys2));
+  }
+}
+  
+int main()
+{
+  try {
+    doIt();
+    doIt2();
+  } catch (AipsError& x) {
+    cout << "Unexpected exception: " << x.getMesg() << endl;
+    return 1;
+  }
+  cout << "OK" << endl;
+  return 0;
+}
