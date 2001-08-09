1fab3121878d5e231f235193621e26b26c3b992a
Neil Killeen <nkilleen@unimelb.edu.au>
2001-08-09 13:03:37 +0000

    copyright

--- code/casa/implement/System/PGPlotterInterface.cc
+++ code/casa/implement/System/PGPlotterInterface.cc
@@ -1,5 +1,5 @@
 //# PGPlotterInterface.cc: Abstract base class for PGPLOT style plotting.
-//# Copyright (C) 1997
+//# Copyright (C) 1997,2001
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
--- code/casa/implement/System/PGPlotterInterface.h
+++ code/casa/implement/System/PGPlotterInterface.h
@@ -1,5 +1,5 @@
 //# PGPlotterInterface.h: Abstract base class for PGPLOT style plotting.
-//# Copyright (C) 1997,2000
+//# Copyright (C) 1997,2000,2001
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
