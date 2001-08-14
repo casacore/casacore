a63d9bf2b129310ca575e34e65800a1856568bd5
Wim Brouw <brouw@astron.nl>
2001-08-14 05:55:49 +0000

    Formatted

--- code/scimath/implement/Mathematics/AutoDiffRep.cc
+++ code/scimath/implement/Mathematics/AutoDiffRep.cc
@@ -27,10 +27,6 @@
 
 //# Includes
 #include <trial/Mathematics/AutoDiffRep.h>
-#include <aips/Arrays/Vector.h>
-
-//# Constants
-template <class T> T AutoDiffRep<T>::aZero = T(0);
 
 //# Constructors
 template <class T>
@@ -44,48 +40,47 @@ AutoDiffRep<T>::AutoDiffRep(const T &v) :
 template <class T>
 AutoDiffRep<T>::AutoDiffRep(const T &v, const uInt ndiffs, const uInt n) :
   val_p(v), nd_p(ndiffs), nocopy_p(False),
-  grad_p(ndiffs>0 ? new T[ndiffs] : 0) {
-  for (uInt i=0; i<nd_p; i++) grad_p[i] = (i==n ? T(1) : T(0));
+  grad_p(ndiffs) {
+  grad_p = T(0);
+  grad_p[n] = T(1);
 }
 
 template <class T>
 AutoDiffRep<T>::AutoDiffRep(const T &v, const uInt ndiffs) :
   val_p(v), nd_p(ndiffs), nocopy_p(False),
-  grad_p(ndiffs>0 ? new T[ndiffs] : 0) {
-  for (uInt i=0; i<nd_p; i++) grad_p[i] = T(0);
+  grad_p(ndiffs) {
+  grad_p = T(0);
 }
 
 template <class T>
 AutoDiffRep<T>::AutoDiffRep(const uInt ndiffs) :
   val_p(0), nd_p(ndiffs), nocopy_p(False),
-  grad_p(ndiffs>0 ? new T[ndiffs] : 0) {
-  for (uInt i=0; i<nd_p; i++) grad_p[i] = T(0);
+  grad_p(ndiffs) {
+  grad_p = T(0);
 }
 
 template <class T>
 AutoDiffRep<T>::AutoDiffRep(const AutoDiffRep<T> &other) :
   val_p(other.val_p), nd_p(other.nd_p), nocopy_p(False),
-  grad_p(other.nd_p>0 ? new T[other.nd_p] : 0) {
-  for (uInt i=0; i<nd_p; i++) grad_p[i] = other.grad_p[i];
+  grad_p(other.nd_p) {
+  grad_p = other.grad_p;
 }
 
 template <class T>
 AutoDiffRep<T>::AutoDiffRep(const T &v, const Vector<T> &derivs) :
   val_p(v), nd_p(derivs.nelements()), nocopy_p(False),
-  grad_p(nd_p>0 ? new T[nd_p] : 0) {
-  for (uInt i=0; i<nd_p; i++) grad_p[i] = derivs(i);
+  grad_p(nd_p) {
+  grad_p = derivs;
 }
 
 template<class T>
-AutoDiffRep<T>::~AutoDiffRep() {
-  delete [] grad_p; grad_p = 0;
-}
+AutoDiffRep<T>::~AutoDiffRep() {}
 
 template <class T>
 AutoDiffRep<T> &AutoDiffRep<T>::operator=(const T &v) {
   val_p = v;
   nd_p = 0;
-  grad_p = 0;
+  grad_p.resize(nd_p);
   return *this;
 }
 
@@ -94,14 +89,14 @@ AutoDiffRep<T> &AutoDiffRep<T>::operator=(const AutoDiffRep<T> &other) {
   if (this != &other) {
     val_p = other.val_p;
     nd_p = other.nd_p;
-    grad_p = (nd_p>0 ? new T[nd_p] : 0);
-    for (uInt i=0; i<nd_p; i++) grad_p[i] = other.grad_p[i];
+    grad_p.resize(nd_p);
+    grad_p = other.grad_p;
   };
   return *this;
 }
 
 template <class T> void AutoDiffRep<T>::derivatives(Vector<T> &res) const { 
   res.resize(nd_p);
-  for (uInt i=0; i<nd_p; i++) res(i) = grad_p[i];
+  res = grad_p;
 }
 
