ae1ce32377bfe547055f8e9aad16c10da5514c62
Wim Brouw <brouw@astron.nl>
2001-08-29 23:04:27 +0000

    Removed completely superfluous includes of Assert, Error and Vector

--- code/casa/implement/BasicMath/Functional.h
+++ code/casa/implement/BasicMath/Functional.h
@@ -1,5 +1,5 @@
-//# Functional.h: Objects which map a domain object into a range object via operator().
-//# Copyright (C) 1995,1996,1999,2000
+//# Functional.h: Map a domain object into a range object via operator().
+//# Copyright (C) 1995,1996,1999-2001
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
@@ -28,16 +28,13 @@
 #if !defined(AIPS_FUNCTIONAL_H)
 #define AIPS_FUNCTIONAL_H
 
+//# Includes
 #include <aips/aips.h>
-#include <aips/Exceptions/Error.h>
-#include <aips/Utilities/Assert.h>
-#include <aips/Arrays/Vector.h>
 
 //# Forward declaration
 template<class T> class Lattice;
 
-// <summary>
-// Objects which map a domain object into a range object via operator().
+// <summary> Map a domain object into a range object via operator().
 // </summary>
 
 // <use visibility=export>
@@ -50,27 +47,32 @@ template<class T> class Lattice;
 // </etymology>
 //
 // <synopsis>
-// A Functional<Domain,Range> is an abstract base class which encapsulates the
-// mapping of an object of type ``Domain'' into one an object of type
-// ``Range''. This operation is invoked via operator() to make it look more like
+// A <src>Functional<Domain,Range></src> is an abstract base class which
+// encapsulates the mapping of an object of type <src>Domain</src> into an
+// object of type <src>Range</src>.
+// This operation is invoked via operator() to make it look like
 // a function call.
 //
-// While these functions are ``function-like'', there is no guarantee that
-// evaluations of the same parameter will yield the same result (the implementor
-// of a particular class could, for example, merely choose to emit a random
-// number). However implementors of Functional classes are strongly encouraged 
-// to implement (visible) side-effect free semantics in their classes.
+// While these functions are <src>function-like</src>, there is no guarantee
+// that evaluations of the same parameter will yield the same result
+// (the implementor of a particular class could, for example, merely choose
+// to emit a random number).
+// However implementors of <src>Functional</src> classes are strongly
+// encouraged to implement (visible) side-effect free semantics in their
+// classes.
 // 
-// A Functional object is used in circumstances similar to those in which an
-// ordinary C-function pointer would be used. An advantage of the Functional
-// objects is that it is possible to have more than one of them at the same
-// Time. With a C-pointer one normally has to manipulate global variables to
-// simulate multiple objects. Another potential advantage (not yet
+// A <src>Functional</src> object is used in circumstances similar to those
+// in which a function pointer could be used. An advantage of the
+// <src>Functional</src> objects is that it is possible to have more than
+// one of them at the same time.
+// Another potential advantage (not yet
 // implemented) is that it will be possible to perform functional
 // composition at run time, e.g. a=b+c where a,b, and c are
-// Functionals. Another advantage is that since the Functional
-// implementations are likely going to be templated, it is likely that the
-// same source code would yield instantiations for all the numeric types.
+// <src>Functionals</src>.
+// Another advantage is that since the Functional implementations
+// will in general be templated, the same source code would yield
+// instantiations for all the numeric types and for specializations like
+// automatic derivatives.
 //
 // To be of greatest utility, a library of functions that do mathematics, 
 // plotting, etc. on Functional objects needs to be developed.
@@ -79,24 +81,23 @@ template<class T> class Lattice;
 // <example>
 // The following simple example shows how you can write a function that uses a
 // Functional object. 
-// <srcBlock>
+// <srcblock>
 // Double integrate1D(const Functional<Float,Float> &f,
 //                    Double x1, Double x2, Double dx) {
 //     uInt n = (xend - xstart) / dx;
 //     Double sum = 0.0;
-//     for (uInt i=0; i < n; i++) {
-//       sum += f(x1 + i*dx) * dx;
-//     }
+//     for (uInt i=0; i < n; i++) sum += f(x1 + i*dx) * dx;
 //     return sum; 
 // }
-// </srcBlock>
+// </srcblock>
 // Obviously this isn't a very serious algorithm!
 // </example>
 // 
 // <motivation>
-// The specific application that caused the implementation of these Functional
+// The specific application that caused the implementation of these 
+// <src>Functional</src>
 // classes was the creation of the <linkto module="Fitting">Fitting
-// module</linkto>, which needed classes to represent the fitting functions.
+// </linkto> module, which needed classes to represent the fitting functions.
 // </motivation>
 //
 // <templating arg=Domain>
@@ -107,27 +108,24 @@ template<class T> class Lattice;
 // <templating arg=Range>
 //    <li> A copy constructor is absolutely required for Range objects because
 //         operator() returns Range objects by value.
-//    <li> Accessible default constructors, assignment operators, and destructors
-//         will almost always also be required.
+//    <li> Accessible default constructors, assignment operators,
+//	   and destructors will almost always also be required.
 // </templating>
 //
-// <todo asof="1996/02/22">
-//   <li> My guess is that Functional classes will most often be accessed
-//   polymorphically through more derived classes, such as Function1D and
-//   FunctionND. If this is not the case, then a clone() member function will be
-//   needed here (until covariant returns are available in standard compilers).
-//   <li> A Functional class which merely wraps up a pointer-to-function will
-//   doubtless be convenient eventually.
+// <todo asof="2001/08/29">
+//   <li> For polymorphic access it could be that a <src>clone()</src> function
+//		is needed at this level.
 // </todo>
 
-template<class Domain, class Range> class Functional
-{
-public:
-    virtual ~Functional();
+template<class Domain, class Range> class Functional {
+ public:
+  //# Constructors
+  // Destructor
+  virtual ~Functional();
 
-    // Map a Domain ``x'' into a Range ``y'' value. Must be overridden in every
-    // derived class.
-    virtual Range operator()(const Domain &x) const = 0;
+  //# Operators
+  // Map a Domain <src>x</src> into a Range <src>y</src> value.
+  virtual Range operator()(const Domain &x) const = 0;
 };
 
 #endif
