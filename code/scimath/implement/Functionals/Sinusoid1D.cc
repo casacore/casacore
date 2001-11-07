f87a84dab738f34e120315b588e8dd7483c252a2
Wim Brouw <brouw@astron.nl>
2001-11-07 02:42:00 +0000

    Added cloneAD and cloneBase; made work for zero derivatives wanted

--- code/scimath/implement/Functionals/Sinusoid1D.cc
+++ code/scimath/implement/Functionals/Sinusoid1D.cc
@@ -37,3 +37,26 @@ T NQSinusoid1D<T>::eval(typename NQFunction1D<T>::FunctionArg x) const {
   return param_p[AMPLITUDE]*
     cos(T(C::_2pi)*(x[0] - param_p[X0])/param_p[PERIOD]);
 }
+
+//# Member functions
+template<class T>
+Function<typename FunctionTraits<T>::DiffType>
+*NQSinusoid1D<T>::cloneAD() const {
+  Function<typename FunctionTraits<T>::DiffType> *t =
+    new NQSinusoid1D<typename FunctionTraits<T>::DiffType>();
+  for (uInt i=0; i<nparameters(); ++i) {
+    (*t)[i] = typename FunctionTraits<T>::DiffType(param_p[i]);
+  };
+  return t;
+}
+
+template<class T>
+Function<typename FunctionTraits<T>::BaseType>
+*NQSinusoid1D<T>::cloneBase() const {
+  Function<typename FunctionTraits<T>::BaseType> *t =
+    new NQSinusoid1D<typename FunctionTraits<T>::BaseType>();
+  for (uInt i=0; i<nparameters(); ++i) {
+    (*t)[i] = typename FunctionTraits<T>::DiffType(param_p[i]).value();
+  };
+  return t;
+}
