d2a7d4da0faebcf88f09e5f907f9fa3bd2c6d878
Neil Killeen <nkilleen@unimelb.edu.au>
2001-12-05 04:04:01 +0000

    rewrite to handle multiple models provide outfile keyword to save as image '

--- code/lattices/implement/LatticeMath/test/tFit2D.cc
+++ code/lattices/implement/LatticeMath/test/tFit2D.cc
@@ -26,6 +26,9 @@
 //# $Id$
 
 #include <aips/Fitting.h>
+#include <trial/Coordinates/CoordinateSystem.h>
+#include <trial/Coordinates/CoordinateUtil.h>
+#include <trial/Images/PagedImage.h>
 #include <trial/Fitting/Fit2D.h>
 #include <trial/Tasking/PGPlotter.h>
 #include <aips/Functionals/NQGaussian2D.h>
@@ -38,6 +41,11 @@
 #include <aips/Mathematics/Constants.h>
 #include <aips/Utilities/Assert.h>
 
+NQGaussian2D<Double> addModel (Array<Float>& pixels, Double height, Double x, Double y, 
+                               Double major,  Double minor, Double pa);
+
+void addNoise (Array<Float>& pixels, Array<Float>& sigma, Double noise);
+
 int main(int argc, char **argv)
 {
 
@@ -48,170 +56,160 @@ int main(int argc, char **argv)
 // 
    Input inputs(1);
    inputs.version ("$Revision$");
+   inputs.create("nmodels", "1", "nmodels"); 
    inputs.create("noise", "0.0001", "Noise");
-   inputs.create("height", "3.0", "Height");
-   inputs.create("xcen", "0.0", "xcen");
-   inputs.create("ycen", "0.0", "ycen");
    inputs.create("major", "10.0", "major");
    inputs.create("minor", "5.0", "minor");
    inputs.create("pa", "45", "pa");                  // +x -> +y
-//
-   inputs.create("nx", "40", "nx");
-   inputs.create("ny", "40", "ny");   
-   inputs.create("min", "0.0", "min");  
-   inputs.create("max", "0.0", "max");  
-   inputs.create("nbins", "20", "nbins"); 
-   inputs.create("plotter", "", "plotter"); 
-//
+   inputs.create("nx", "64", "nx");
+   inputs.create("ny", "64", "ny");   
    inputs.create("norm", "False", "Normalize");
    inputs.create("mask", "1,1,1,1,1,1", "Mask");
    inputs.create("include", "0.0", "include");
    inputs.create("exclude", "0.0", "exclude");
+   inputs.create("outfile", "", "outfile");
 //
    inputs.readArguments(argc, argv);
+   const Int nModels  = inputs.getInt("nmodels");   
    const Double noise = inputs.getDouble("noise");
-   const Double height = inputs.getDouble("height");
-   const Double xcen = inputs.getDouble("xcen");
-   const Double ycen = inputs.getDouble("ycen");
-   const Double major = inputs.getDouble("major");
-   const Double minor= inputs.getDouble("minor");
-   const Double pa = inputs.getDouble("pa") * C::pi / 180.0;          // +x -> +y
-//
+   Double major = inputs.getDouble("major");
+   Double minor= inputs.getDouble("minor");
+   Double pa = inputs.getDouble("pa") * C::pi / 180.0;          // +x -> +y
    const Int nx = inputs.getInt("nx");   
    const Int ny = inputs.getInt("ny");   
-   const Int nbins = inputs.getInt("nbins");   
-   const Double minD = inputs.getDouble("min");
-   const Double maxD = inputs.getDouble("max"); 
-   const String device = inputs.getString("plotter"); 
-//
    const Bool norm = inputs.getBool("norm");
    const Block<Int> mask = inputs.getIntArray("mask");
    const Block<Double> includeRange = inputs.getDoubleArray("include");
    const Block<Double> excludeRange = inputs.getDoubleArray("exclude");
-//
-// Make data
+   const String outfile = inputs.getString("outfile");
 //
    LogOrigin or("tFit2D", "main()", WHERE);
    LogIO logger(or);
-   MLCG generator; 
-   Normal noiseGen(&generator, 0.0, noise);  
 //
-   NQGaussian2D<Double> gauss2d;
-   gauss2d.setHeight(height);
-   gauss2d.setMajorAxis(major);
-   gauss2d.setMinorAxis(minor);
-   if (xcen==0.0 && ycen==0.0) {
-      gauss2d.setXcenter(Double(nx/2));
-      gauss2d.setYcenter(Double(ny/2));
-   } else {
-      gauss2d.setXcenter(xcen);
-      gauss2d.setYcenter(ycen);
-   }
-   gauss2d.setPA(Fit2D::paToGauss2D(pa));          // +y -> -x
+   Fit2D fitter(logger);
 //
    IPosition shape(2,nx,ny);
    Array<Float> pixels(shape);
    Array<Float> sigma(shape);
-   Array<Float> resid;
-   Array<Bool> pixelMask;
 //
-   Vector<Float> data(nx*ny);
-   Float dMin = 1e30;
-   Float dMax = -1e30;
+   Double xsep = nx / nModels;
+   Double ysep = ny / nModels;
+   Double xPos, yPos;
+   if (nModels==1) {
+      xPos = nx / 2.0;
+      yPos = ny / 2.0;     
+   } else {
+      xPos = xsep / 2.0;
+      yPos = ysep / 2.0;
+   }
+   Double height = 1.0;
+
 //
-   IPosition loc(2);
-   uInt k = 0;
-   for (Int j=0; j<shape(1); j++) {
-      for (Int i=0; i<shape(0); i++) {
-         loc(0) = i;
-         loc(1) = j;
+   Vector<Double> trueHeight(nModels);
+   Vector<Double> trueX(nModels);
+   Vector<Double> trueY(nModels);
+   Vector<Double> trueMajor(nModels);
+   Vector<Double> trueMinor(nModels);
+   Vector<Double> truePA(nModels);
 //
-         pixels(loc) = gauss2d(Double(i), Double(j)) + noiseGen();
-         if (noise==0.0) {
-             sigma(loc) = 1.0;
-         } else {
-             sigma(loc) = noise;
+   for (Int i=0; i<nModels; i++) {
+
+// Add model to data array
+
+      NQGaussian2D<Double> gauss2d = addModel(pixels, height, xPos, yPos, major, minor, pa);
+      trueHeight(i) = height;
+      trueX(i) = xPos;
+      trueY(i) = yPos;
+      trueMajor(i) = major;
+      trueMinor(i) = minor;
+      truePA(i) = pa;
+
+// Set Parameters mask
+
+      Vector<Double> parameters(gauss2d.nparameters());
+      Vector<Bool> parameterMask(parameters.nelements(), True);
+      for (uInt i=0; i<parameters.nelements(); i++) {
+         parameters(i) = gauss2d[i];
+         if (mask[i]==0) {
+            parameterMask(i) = False;
          }
-//
-         data(k) = pixels(loc);
-         dMin = min(dMin,data(k));
-         dMax = max(dMax,data(k));
-         k++;
       }
-   }
-//
-// Plot data
-//
-   if (!device.empty()) {
-      PGPlotter plotter(device);
-      if (minD==0.0 && maxD==0.0) {
-         plotter.hist(data, dMin, dMax, nbins, 0);
-      } else {
-         plotter.hist(data, Float(minD), Float(maxD), nbins, 0);
-      }
-   }
+      parameters(5) = Fit2D::paFromGauss2D(parameters(5));
 
-//
-//cout << "pixels=" << pixels << endl;
-//cout << "sigma=" << sigma << endl;
-//cout << "norm=" << norm << endl;
-//cout << endl << endl;
-
-//
-// Set mask
-//
-   Vector<Double> parameters(gauss2d.nparameters());
-   Vector<Bool> parameterMask(parameters.nelements(), True);
-   Vector<Int> iMask(parameters.nelements(), 1);
-   for (uInt i=0; i<parameters.nelements(); i++) {
-      parameters(i) = gauss2d[i];
-      if (mask[i]==0) {
-         parameterMask(i) = False;
-         iMask(i) = 0;
-      }
-   }
-   parameters(5) = Fit2D::paFromGauss2D(parameters(5));
-//
 // convert axial ratio to minor axis (availableParameter
 // interface uses axial ratio)
-//
-   parameters(4) = parameters(4)*parameters(3);  
-   cout << "      mask      = " << iMask << endl;
-   cout << "True values     = " << parameters << endl;
-   cout << "True pa (+x -> +y) = " << parameters(5) * 180.0 / C::pi << endl;
-//
+
+      parameters(4) = parameters(4)*parameters(3);  
+/*
+      cout << "      mask      = " << parameterMask << endl;
+      cout << "True values     = " << parameters << endl;
+      cout << "True pa (+x -> +y) = " << parameters(5) * 180.0 / C::pi << endl;
+*/
+
 // Set starting guess
+
+      Vector<Double> startParameters(parameters.copy());
+      for (uInt i=0; i<parameters.nelements(); i++) {
+         startParameters(i) = parameters(i) * 0.9;
+      }
+/*
+      cout << "Start values    = " << startParameters << endl;
+      cout << "Start pa (+x -> +y) = " << startParameters(5) * 180.0 / C::pi << endl;
+*/
+
+// Add model to fitter
+
+      fitter.addModel (Fit2D::GAUSSIAN, startParameters, parameterMask);
+
+// Update model
+
+      height *= 0.75;
+      xPos += xsep;
+      yPos += ysep;
 //
-   Vector<Double> startParameters(parameters.copy());
-   for (uInt i=0; i<parameters.nelements(); i++) {
-      startParameters(i) = parameters(i) * 0.9;
+      major *= 0.9;
+      minor *= 0.9;
+      pa += C::pi / 180 * 20.0;
+      if (pa > C::pi) pa -= C::pi;
+      cerr << endl;
    }
-   cout << "Start values    = " << startParameters << endl;
-   cout << "Start pa (+x -> +y) = " << startParameters(5) * 180.0 / C::pi << endl;
-//
-// Make fitter and set state
-//
-   Fit2D fitter(logger);
-   fitter.addModel (Fit2D::GAUSSIAN, startParameters, parameterMask);
+
+// Add noise
+ 
+   addNoise (pixels, sigma, noise);
+
+// Save image
+
+   if (outfile!=String("")) {
+       CoordinateSystem cSys = CoordinateUtil::defaultCoords2D();
+       PagedImage<Float> im(shape, cSys, outfile);
+       im.put(pixels);  
+   }
+
+// Set other state of fitter
+
    if (includeRange.nelements()==2) {
       fitter.setIncludeRange(includeRange[0], includeRange[1]);
    }
    if (excludeRange.nelements()==2) {
       fitter.setExcludeRange(excludeRange[0], excludeRange[1]);
    }
-//
+
+// Generate estimate
+/*
    Vector<Double> p = fitter.estimate(Fit2D::GAUSSIAN, pixels);
    cout << "Estimate values = " << p << endl;
    cout << "Estimate pa (+x -> +y) = " << p(5) * 180.0 / C::pi << endl;
-   Fit2D::ErrorTypes status = fitter.fit(pixels, pixelMask, sigma, norm);
+*/
+
+// Make fit
+
+   Fit2D::ErrorTypes status = fitter.fit(pixels, sigma, norm);
    if (status==Fit2D::OK) {
       Vector<Double> solution = fitter.availableSolution();
-      cout << "Solution     = " << solution << endl;
-      cout << "Solution pa (+x -> +y) = " << solution(5) * 180.0 / C::pi << endl;
       Vector<Double> cv = fitter.covariance().diagonal();
       cout << "Covariance     = " << cv << endl;
-//      cout << "SNR        = " << fitter.availableSolution() / 
-//                             sqrt(cv) << endl;
+      cout << "SNR        = " << fitter.availableSolution() / sqrt(cv) << endl;
       cout << "Chi squared = " << fitter.chiSquared() << endl << endl;
       cout << "Number of iterations = " << fitter.numberIterations() << endl;
       cout << "Number of points     = " << fitter.numberPoints() << endl;
@@ -221,12 +219,17 @@ int main(int argc, char **argv)
 //         throw (AipsError("Solution not accurate to 1e-6"));
 //      }
 //
-      cout << "Number of models = " << fitter.nModels() << endl;
+      cout << endl << "Number of models = " << fitter.nModels() << endl;
       for (uInt i=0; i<fitter.nModels(); i++) {
-        cout << "Model " << i << " of type " << Fit2D::type(fitter.type(i)) <<
-                " has solution "  << fitter.availableSolution(i) << endl;
+        Vector<Double> xx(5);
+        xx(0) = trueHeight(i); xx(1) = trueX(i); xx(2) = trueY(i); xx(3) = trueMajor(i); xx(4) = truePA(i);
+//
+        cout << "Model " << i << " of type " << Fit2D::type(fitter.type(i)) << endl;
+        cout << "   Actual values = " << xx << endl;
+        cout << "   Solution      = " << fitter.availableSolution(i) << endl;
       }
 //
+      Array<Float> resid;
       fitter.residual(resid, pixels);
       cout << "Residual min and max = " << min(resid) << " " << max(resid) << endl;
    } else {
@@ -234,10 +237,11 @@ int main(int argc, char **argv)
    }
 
 // Test copy constructor
+
    {
       cout << endl << endl << "Test copy constructor" << endl;
       Fit2D fitter2(fitter);
-      Fit2D::ErrorTypes status = fitter2.fit(pixels, pixelMask, sigma, norm);      
+      fitter2.fit(pixels, sigma, norm);      
       if (!allEQ(fitter.availableSolution(),fitter2.availableSolution()) ||
          fitter.numberIterations() != fitter2.numberIterations() ||
          fitter.chiSquared() != fitter2.chiSquared() ||
@@ -254,7 +258,7 @@ int main(int argc, char **argv)
       cout << endl << endl << "Test assignment operator" << endl;
       Fit2D fitter2(logger);
       fitter2 = fitter;
-      Fit2D::ErrorTypes status = fitter2.fit(pixels, pixelMask, sigma, norm);
+      fitter2.fit(pixels, sigma, norm);
       if (!allEQ(fitter.availableSolution(),fitter2.availableSolution()) ||
          fitter.numberIterations() != fitter2.numberIterations() ||
          fitter.chiSquared() != fitter2.chiSquared() ||
@@ -269,4 +273,43 @@ int main(int argc, char **argv)
  }   
 }
 
+NQGaussian2D<Double> addModel (Array<Float>& pixels, Double height, Double xcen, Double ycen,
+                               Double major,  Double minor, Double pa)
+{
+   NQGaussian2D<Double> gauss2d;
+   gauss2d.setHeight(height);
+   gauss2d.setMajorAxis(major);
+   gauss2d.setMinorAxis(minor);
+   gauss2d.setXcenter(xcen);
+   gauss2d.setYcenter(ycen);
+   gauss2d.setPA(Fit2D::paToGauss2D(pa));          // +y -> -x
+//
+   IPosition shape = pixels.shape();
+   IPosition loc(2);
+   for (Int j=0; j<shape(1); j++) {
+      for (Int i=0; i<shape(0); i++) {
+         loc(0) = i;
+         loc(1) = j;
+         pixels(loc) += gauss2d(Double(i), Double(j));
+      }
+   }
+   return gauss2d;
+}
+
+
+void addNoise (Array<Float>& pixels, Array<Float>& sigma, Double noise)
+{
+   sigma = 1.0;
+   if (noise>0.0) sigma = noise;
+//
+   MLCG generator; 
+   Normal noiseGen(&generator, 0.0, noise);  
+//
+   Bool deleteIt;
+   Float* pData = pixels.getStorage(deleteIt);
+   for (uInt k=0; k<pixels.shape().product(); k++){
+      pData[k] += noiseGen();
+   }
+   pixels.putStorage(pData, deleteIt);
+}
 
