49a8b826b5a6da671e3dd6ca2ccc7cd1bd2369d9
Wim Brouw <brouw@astron.nl>
2002-01-09 06:21:58 +0000

    Added addFunction overload for the AutoDiff specialization

--- code/scimath/implement/Functionals/CompoundFunction.h
+++ code/scimath/implement/Functionals/CompoundFunction.h
@@ -1,5 +1,5 @@
 //# NQCompoundFunction.h: Sum of a collection of Functions
-//# Copyright (C) 2001
+//# Copyright (C) 2001,2002
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
@@ -193,6 +193,10 @@ public NQCompoundParam<AutoDiff<T> > {
     eval(typename Function<AutoDiff<T> >::FunctionArg x) const;
   
   //# Member functions
+// Add a function to the sum. All functions must have the same 
+  // <src>ndim()</src> as the first one. Returns the (zero relative) number 
+  // of the function just added.
+  uInt addFunction(const Function<AutoDiff<T> > &newFunction);
   // Consolidate the parameter settings. This could be necessary if
   // parameters have been set, and a copy constructor called. This is
   // necessary before and after the setting of <em>local</em> parameters; i.e.
