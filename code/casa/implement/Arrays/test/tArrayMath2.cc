27627cd1fe9acc46d66e7c0a1dcf4df205e1997d
Ger van Diepen <gervandiepen@gmail.com>
2002-01-31 09:13:16 +0000

    Needed aips/iostream.h Added copyright

--- code/casa/implement/Arrays/test/tArrayMath2.cc
+++ code/casa/implement/Arrays/test/tArrayMath2.cc
@@ -1,3 +1,31 @@
+//# tArrayMath2.cc: This program tests the ArrayMath class
+//# Copyright (C) 1993,1994,1995,1996,1997,1999,2000,2002
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This program is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU General Public License as published by the Free
+//# Software Foundation; either version 2 of the License, or (at your option)
+//# any later version.
+//#
+//# This program is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+//# more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with this program; if not, write to the Free Software Foundation, Inc.,
+//# 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//# $Id$
+
+//# Includes
 #include <aips/Arrays/Array.h>
 #include <aips/Arrays/Vector.h>
 #include <aips/Arrays/ArrayMath.h>
@@ -5,6 +33,7 @@
 #include <aips/Arrays/ArrayIO.h>
 #include <aips/Utilities/Assert.h>
 #include <aips/OS/Timer.h>
+#include <aips/iostream.h>
 
 
 typedef Array<Float> PartFunc (const Array<Float>&, const IPosition& axes);
