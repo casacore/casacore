deb433e3a9cf255e0a87dfd69229e9a6738afcf7
Neil Killeen <nkilleen@unimelb.edu.au>
2002-02-05 13:25:43 +0000

    copyright

--- code/images/implement/Images/ImageSummary.h
+++ code/images/implement/Images/ImageSummary.h
@@ -1,5 +1,5 @@
 //# ImageSummary.h: List descriptive information from an image 
-//# Copyright (C) 1996,1997,1998,1999,2000,2001
+//# Copyright (C) 1996,1997,1998,1999,2000,2001,2002
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
