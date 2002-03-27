90f0dd326f5031feac9321cef84e0b20a6eb580a
Neil Killeen <nkilleen@unimelb.edu.au>
2002-03-27 05:50:59 +0000

    copyright

--- code/components/implement/SpectralComponents/SpectralFit.cc
+++ code/components/implement/SpectralComponents/SpectralFit.cc
@@ -1,5 +1,5 @@
 //# SpectralFit.cc: Least Squares fitting of spectral elements to spectrum
-//# Copyright (C) 2001
+//# Copyright (C) 2001,2002
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
--- code/components/implement/SpectralComponents/test/tSpectralFit.cc
+++ code/components/implement/SpectralComponents/test/tSpectralFit.cc
@@ -1,5 +1,5 @@
 //# tSpectralFit.cc -- test SpectralElement; SpectralEstimate and SpectralFit
-//# Copyright (C) 2001
+//# Copyright (C) 2001,2002
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This program is free software; you can redistribute it and/or modify it
