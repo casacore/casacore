9291fc2cb59dd4c7d9ec9da79c07ed0040036daa
Wim Brouw <brouw@astron.nl>
2002-04-26 07:05:53 +0000

    Output for test program for Functional compilation of strings

---
+++ code/scimath/implement/Functionals/test/tFuncExpression.cc
@@ -0,0 +1,90 @@
+//# tFuncExpression.cc: This program test the functional run-time expressions
+//# Copyright (C) 2000,2002
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This program is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU General Public License as published by the Free
+//# Software Foundation; either version 2 of the License, or (at your option)
+//# any later version.
+//#
+//# This program is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+//# more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with this program; if not, write to the Free Software Foundation, Inc.,
+//# 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//# $Id$
+
+//# Includes
+#include <trial/Functionals/FuncExpression.h>
+#include <trial/Functionals/FuncExprData.h>
+#include <aips/Exceptions/Error.h>
+#include <aips/Utilities/String.h>
+
+#include <aips/iostream.h>
+
+int main() {
+
+  try {
+    cout << "------------ test functional expressions ------------" << endl;
+
+    cout << "--- Check base -----" << endl;
+    // Make an operator base
+    FuncExprData base;
+    cout << base << endl;
+
+    cout << "--- Check expression syntax ----" << endl;
+    const uInt n=16;
+    String exprlist[n] = {
+      String("+-(25*30+2)--(75+2)"),
+      String("1+2-3"),
+      String("1+2/3"),
+      String("2^2^3"),
+      String("1+(2+3"),
+      String("1"),
+      String(""),
+      String("pi"),
+      String("sin(1)"),
+      String("cop(1)"),
+      String("pi(2)+1.5"),
+      String("sin(1,2)"),
+      String("2*p"),
+      String("2*p1"),
+      String("2*p[1]"),
+      String("2*x0")
+    };
+    for (uInt i=0; i<n; ++i) {
+      FuncExpression expr;
+      String myexpr = exprlist[i];
+      cout << "Expression: '" << myexpr << "'" << endl; 
+      if (!expr.create(myexpr)) {
+	cout << expr.errorMessage() << endl;
+      };
+      cout << expr;
+      Double res;
+      cout << "Value: ";
+      if (!expr.exec(res)) cout <<expr.errorMessage() << endl;
+      else cout << res << endl;
+      cout << "----------------------------------------------------" << endl;
+    };
+  }  catch (AipsError x) {
+    cerr << x.getMesg() << endl;
+    cout << "FAIL" << endl;
+    return 1;
+  }
+  catch (...) {
+    cerr << "Exception not derived from AipsError" << endl;
+    cout << "FAIL" << endl;
+    return 2;
+  };
+}
