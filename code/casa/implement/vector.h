f19aacff53628f3d271665f0154ffc003c6f22e8
Wim Brouw <brouw@astron.nl>
2002-05-01 05:09:57 +0000

    Added AIPS_VECTOR_AUX_TEMPLARES(T) macro to aid in providing templates

--- code/casa/implement/vector.h
+++ code/casa/implement/vector.h
@@ -31,10 +31,38 @@
 // Define the standard C++ include file. 
 // This is an interim solution to cater for the SGI non-existence of
 // them (e.g. <cstring>)
+// Make sure any special macros are set
 #include <aips/aips.h>
 
 #include <vector>
 
 using std::vector;
+
+// A special macro to create the auxilliary template definitions for
+// various compilers
+// Use if defined a vector<T> as AIPS_VECTOR_AUX_TEMPLATES(T)
+
+#if defined(AIPS_VECTOR_AUX_TEMPLATES)
+#undef AIPS_VECTOR_AUX_TEMPLATES
+#endif
+
+#if defined(AIPS_GCC)
+#define AIPS_VECTOR_AUX_TEMPLATES(T) \
+template \
+T *__uninitialized_copy_aux<T *, T *>(T *, T *, T *, __false_type); \
+template \
+T *__uninitialized_copy_aux<T const *, T *>(T const *, T const *, \
+					    T *, __false_type); \
+template \
+T *fill_n<T *, uInt, T >(T *, uInt, T const &); \
+template \
+void fill<T *, T >(T *, T *, T const &); \
+template \
+T *__uninitialized_fill_n_aux<T *, uInt, T >(T *, uInt, \
+			      T const &, __false_type);
+
+#else
+#define AIPS_MVECTORAUX_TEMPLATES(T)
+#endif
 
 #endif
