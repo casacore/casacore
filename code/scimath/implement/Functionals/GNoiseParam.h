64949127202931bbf8b68d91dc3c930a0d1a1b40
Wim Brouw <brouw@astron.nl>
2002-05-28 01:18:55 +0000

    Initial version

---
+++ code/scimath/implement/Functionals/GNoiseParam.h
@@ -0,0 +1,112 @@
+//# GNoiseParam.h: A one dimensional normal distribution 
+//# Copyright (C) 2002
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//# $Id$
+
+#if !defined(AIPS_GNOISEPARAM_H)
+#define AIPS_GNOISEPARAM_H
+
+//# Includes
+#include <aips/aips.h>
+#include <aips/Functionals/Function.h>
+#include <aips/Mathematics/Random.h>
+
+//# Forward declarations
+
+// <summary> A one dimensional normal distribution 
+// </summary>
+
+// <use visibility=local>
+
+// <reviewed reviewer="" date="" tests="tFunctionHolder" 
+// demos="">
+// </reviewed>
+
+// <prerequisite>
+//   <li> <linkto class="FunctionParam">FunctionParam</linkto> class
+//   <li> <linkto class="Function">Function</linkto> class
+// </prerequisite>
+
+// <etymology> 
+// Gaussian Noise generator.
+// </etymology>
+
+// <synopsis> 
+// A <src>GNoise</src> is described by a mean and a variance (Note these are
+// not parameters in the <src>Function</src> sense, but more like the
+// order of a polynomial. The defaults are 0 and 1.
+// </synopsis> 
+//
+// <example>
+// <srcblock>
+//    GNoiseFunction<Double> sf;
+//    sf();            // = 0.12
+// </srcblock>
+// </example>
+
+// <templating arg=T>
+//  <li> T should have standard numerical operators
+// </templating>
+
+// <thrown>
+//    <li> AipsError if incorrect parameter number specified.
+// </thrown>
+
+template<class T> class GNoiseParam : public Function<T> {
+ public:
+  //# Enumerations
+  
+  //# Constructors
+  // Constructs the GNoise, Defaults:
+  // mean=0, var=1.0
+  // <group>
+  GNoiseParam();
+  GNoiseParam(const Double &mean, const Double &var);
+  // </group>
+
+  // Copy constructor (deep copy)
+  GNoiseParam(const GNoiseParam<T> &other);
+
+  // Copy assignment (deep copy)
+  GNoiseParam<T> &operator=(const GNoiseParam<T> &other);
+    
+  // Destructor
+  virtual ~GNoiseParam();
+
+  //# Operators    
+  virtual uInt ndim() const { return 0; };
+
+  //# Member functions
+
+ protected:
+  //# Data
+  // Random generator
+  ACG genit_p;
+  // Normal noise
+  mutable Normal noise_p;
+
+};
+
+#endif
---
+++ code/scimath/implement/Functionals/UnaryParam.h
@@ -0,0 +1,116 @@
+//# UnaryParam.h: Parameter handling for one dimensional unary function
+//# Copyright (C) 2002
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//# $Id$
+
+#if !defined(AIPS_UNARYPARAM_H)
+#define AIPS_UNARYPARAM_H
+
+//# Includes
+#include <aips/aips.h>
+#include <aips/Functionals/Function.h>
+
+//# Forward declarations
+
+// <summary> Parameter handling for one dimensional unary function
+// </summary>
+
+// <use visibility=local>
+
+// <reviewed reviewer="" date="" tests="tFunctionHolder" 
+// demos="">
+// </reviewed>
+
+// <prerequisite>
+//   <li> <linkto class="FunctionParam">FunctionParam</linkto> class
+//   <li> <linkto class="Function">Function</linkto> class
+// </prerequisite>
+
+// <etymology> 
+// A 1-dimensional unary hat.
+// </etymology>
+
+// <synopsis> 
+// A <src>Unary</src> is described by a height, a center and a width
+// (halfwidth). The value is:
+// <srcblock>
+//      height          (|x-center| <  width)
+//   0.5height          (|x-center| == width)
+//      0               (|x-center| >  width)
+// </srcblock>
+// The parameters are enumerated by HEIGHT, CENTER and WIDTH. They have
+// default values of (1, 0, 1).
+// </synopsis> 
+//
+// <example>
+// <srcblock>
+//    UnaryFunction<Double> sf(5.0, 25.0, 7);
+//    sf(25);            // = 5.0
+// </srcblock>
+// </example>
+
+// <templating arg=T>
+//  <li> T should have standard numerical operators
+// </templating>
+
+// <thrown>
+//    <li> AipsError if incorrect parameter number specified.
+// </thrown>
+
+template<class T> class UnaryParam : public Function<T> {
+ public:
+  //# Enumerations
+  // Parameter numbers
+  enum { HEIGHT=0, CENTER, WIDTH };
+  
+  //# Constructors
+  // Constructs the Unary, Defaults:
+  // height=1, center=0, width=1.
+  // <note role=warning> Could not use default arguments
+  // that worked both with gcc and IRIX </note>
+  // <group>
+  UnaryParam();
+  explicit UnaryParam(const T &height);
+  UnaryParam(const T &height, const T &center);
+  UnaryParam(const T &height, const T &center, const T &width);
+  // </group>
+
+  // Copy constructor (deep copy)
+  UnaryParam(const UnaryParam<T> &other);
+
+  // Copy assignment (deep copy)
+  UnaryParam<T> &operator=(const UnaryParam<T> &other);
+    
+  // Destructor
+  virtual ~UnaryParam();
+
+  //# Operators    
+  virtual uInt ndim() const { return 1; };
+
+  //# Member functions
+
+};
+
+#endif
