2c24f91a9549fba668d7c084e7bd82f27b914094
Bob Garwood <bgarwood@nrao.edu>
2002-07-24 18:46:46 +0000

    initial version

---
+++ code/scimath/implement/Mathematics/VanVleck.cc
@@ -0,0 +1,317 @@
+//# VanVleck.cc:  this implements VanVleck.
+//# Copyright (C) 2002
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//# $Id$
+
+//# Includes
+
+#include <trial/Mathematics/VanVleck.h>
+
+#include <aips/Functionals/ScalarSampledFunctional.h>
+#include <aips/Mathematics/Math.h>
+#include <aips/Arrays/ArrayMath.h>
+
+
+// initial values for the static data members
+
+Interpolate1D<Double, Double> *VanVleck::itsInterp = NULL;
+Vector<Double> VanVleck::itsQx0;
+Vector<Double> VanVleck::itsQx1;
+Vector<Double> VanVleck::itsQy0;
+Vector<Double> VanVleck::itsQy1;
+Int VanVleck::itsSize = 65;
+
+void VanVleck::size(Int npts)
+{
+  if (itsSize != npts) {
+    itsSize = npts;
+    initInterpolator();
+  }
+}
+
+void VanVleck::setQuantization(const Matrix<Double> &qx, 
+			       const Matrix<Double> &qy)
+{
+
+  // should double check that first dimension is 2
+  if (itsQx1.nelements() != qx.ncolumn()) {
+    itsQx0.resize(qx.ncolumn());
+    itsQx1.resize(qx.ncolumn());
+  }
+  if (itsQy0.nelements() != qy.ncolumn()) {
+    itsQy0.resize(qy.ncolumn());
+    itsQy1.resize(qy.ncolumn());
+  }
+  itsQx0 = qx.row(0);
+  itsQx1 = qx.row(1);
+  itsQy0 = qy.row(0);
+  itsQy1 = qy.row(1);
+  initInterpolator();
+}
+
+void VanVleck::initInterpolator()
+{
+  delete itsInterp;
+  itsInterp = 0;
+
+  if (itsQx0.nelements() == 0) return;
+
+  Vector<Double> rs(itsSize);
+  Vector<Double> rhos(itsSize);
+  Double twoN = 2.0*itsSize;
+  Double denom = cos(C::pi/twoN);
+  Int midi = (itsSize-1)/2;
+  rhos[midi] = 0.0;
+  rs[midi] = 0.0;
+
+  for (Int i=1;i<=midi;i++) {
+    // for the rhos, choose the modified Chebyshev points
+    // upper side
+    Double hi = midi+i;
+    rhos[hi] = -cos(Double(2*hi+1)*C::pi/twoN)/denom;
+    rs[hi] = rs[hi-1] + rinc(rhos[hi-1],rhos[hi]);
+    // lower side
+    Double lo = midi-i;
+    rhos[lo] = -cos(Double(2*lo+1)*C::pi/twoN)/denom;
+    rs[lo] = rs[lo+1] + rinc(rhos[lo+1],rhos[lo]);
+  }
+  ScalarSampledFunctional<Double> fx(rs);
+  ScalarSampledFunctional<Double> fy(rhos);
+  itsInterp = new Interpolate1D<Double,Double>(fx, fy, True);
+  AlwaysAssert(itsInterp, AipsError);
+  itsInterp->setMethod(Interpolate1D<Double,Double>::spline);
+}
+
+void VanVleck::getTable(Vector<Double> &rs,
+			Vector<Double> &rhos)
+{
+  rs.resize(itsInterp->getX().nelements());
+  rs = itsInterp->getX();
+  rhos.resize(itsInterp->getY().nelements());
+  rhos = itsInterp->getY();
+}
+
+double VanVleck::drbydrho(double *rho)
+{
+  Double s = 0.0;
+  Double a;
+  for (uInt i=0;i<(itsQx0.nelements()-1);i++) {
+    a = itsQx1[i+1]-itsQx1[i];
+    for (uInt j=0;j<(itsQy0.nelements()-1);j++) {
+      s+=a*(itsQy1[j+1]-itsQy1[j])
+	*g(itsQx0[i],itsQy0[j],*rho);
+    }
+  }
+  return s;
+}
+
+#define NEED_UNDERSCORES
+#if defined(NEED_UNDERSCORES)
+#define dqags dqags_
+#endif
+
+extern "C" { 
+   void dqags(Double (*)(Double *), Double*, Double *, Double *, Double *, Double *,
+	      Double *, Int *, Int*, Int *, Int *, Int *, Int *, Double *);
+}
+
+
+
+Double VanVleck::rinc(Double &rhoi, Double &rhof)
+{
+  Double work[4096];
+  Int iwork[1024];
+  Double result, abserr;
+  Int neval, ier, last;
+
+  Double epsabs=1.0e-6;
+  Double epsrel=1.0e-6;
+  Int limit=1024;
+  Int lenw = 4*limit;
+  dqags(drbydrho, &rhoi, &rhof, &epsabs, &epsrel, &result, &abserr,
+	&neval, &ier, &limit, &lenw, &last, iwork, work);
+  if (ier != 0) {
+    cout << "Error in dqags : " << ier << endl;
+  }
+  return result;
+}
+
+Double VanVleck::threshNgt3(Int n, Double zerolag)
+{
+  Double x = 0.0;
+  Bool odd = True;
+  if (n%2 == 0) {
+    x = 1.0;
+    odd = False;
+  }
+  Double tol = 1.0e-8;
+  Double sqrt2 = sqrt(2.0);
+  Double sqrt2dpi = sqrt(2.0/C::pi);
+  Double fp, f;
+  for (Int i=0;i<30;i++) {
+    fp = 0.0;
+    f = zerolag;
+    if (odd) {
+      for (Int k=1;k<=(n-1)/2;k++) {
+	f -= (2*k-1)*erfc((2*k-1)*x/sqrt2);
+	Double twoKm1 = 2*k-1;
+	fp += sqrt2dpi*twoKm1*twoKm1*exp(-0.5*(twoKm1*x)*(twoKm1*x));
+      }
+    } else {
+      f -= 1.0;
+      for (Int k=1;k<=(n-2)/2;k++) {
+	f -= 8*k*erfc(k*x/sqrt2);
+	fp += 8*k*k*sqrt2dpi*exp(-0.5*(k*x)*(k*x));
+      }
+    }
+    Double deltax = -f/fp;
+    Double signdx = (deltax>=0) ? 1.0 : -1.0;
+    deltax = signdx * min(0.5,abs(deltax));
+    x += deltax;
+    if (odd) x = max(0.0, x);
+    if (abs(deltax/x) < tol) break;
+  }
+  return x;
+}
+
+Double VanVleck::invErf(Double x)
+{
+  // these are translations of Mathematic code supplied by Fred Schwab
+  // based upon approximations published by Blair, Edwards, and Johnson.
+ 
+  Double absx = abs(x);
+  Double result;
+  if (absx<=0.75) {
+    // from table 10 of Blair et. al.
+    // maximum relative error of 4.47e-8
+    Double t = x*x-0.75*0.75;
+    Double p1, p2, p3, q1, q2, q3, q4;
+    p1 = -13.0959967422;
+    p2 =  26.785225760;
+    p3 =  -9.289057635;
+    q1 = -12.0749426297;
+    q2 =  30.960614529;
+    q3 = -17.149977991;
+    q4 =   1.0;
+    result = x*(p1+t*(p2+t*p3))/(q1+t*(q2+t*(q3+t*q4)));
+  } else if (absx<=0.9375) {
+    // from table 29 of Blair et. al.
+    // maximum relative error of 4.17e-8
+    Double t = x*x-.9375*.9375;
+    Double p1,p2,p3,p4,q1,q2,q3,q4;
+    p1 = -0.12402565221;
+    p2 =  1.0688059574;
+    p3 = -1.9594556078;
+    p4 =  0.4230581357;
+    q1 = -0.8827697997;
+    q2 =  0.8900743359;
+    q3 = -2.1757031196;
+    q4 =  1.0;
+    result = x*(p1+t*(p2+t*(p3+t*p4)))/(q1+t*(q2+t*(q3+t*q4)));
+  } else if (absx<(1-1e-100)) {
+    // from table 50 of Blair et. al.
+    // maximum relative error of 2.45e-8
+    Double t = 1.0/sqrt(-log(1.0-absx));
+    Double p1,p2,p3,p4,p5,p6,q1,q2,q3;
+    p1 =  0.1550470003116;
+    p2 =  1.382719649631;
+    p3 =  0.690969348887;
+    p4 = -1.128081391617;
+    p5 =  0.680544246825;
+    p6 = -0.16444156791;
+    q1 = 0.155024849822;
+    q2 = 1.385228141995;
+    q3 = 1.0;
+    Double signx = (x>=0) ? 1.0 : -1.0;
+    result = signx*(p1/t+p2+t*(p3+t*(p4+t*(p5+t*p6))))/(q1+t*(q2+t*q3));
+  } else {
+    result = C::dbl_max;
+    if (x<0) {
+      result = -result;
+    }
+  }
+  return result;
+}
+
+Double VanVleck::invErfc(Double x)
+{
+  Double result;
+  if (x>=2.0) {
+    result = -C::dbl_max;
+  } if (x>=0.0625 && x<2.0) {
+    // just use invErf(1-x)
+    result = invErf(1.0-x);
+  } else if (x>=1e-100) {
+    // From table 50 of Blair et al as well as table 70
+    Double t = 1.0/sqrt(-log(x));
+    Double p1,p2,p3,p4,p5,p6;
+    Double q1,q2,q3;
+    p1 =  0.1550470003116;
+    p2 =  1.382719649631;
+    p3 =  0.690969348887;
+    p4 = -1.128081391617;
+    p5 =  0.680544246825;
+    p6 = -0.16444156791;
+    q1 = 0.155024849822;
+    q2 = 1.385228141995;
+    q3 = 1.0;
+    result = (p1/t+p2+t*(p3+t*(p4+t*(p5+t*p6)))) / (q1+t*(q2+t*q3));
+  } else if (x>=1e-1000) {
+    // from table 70 of Blair et al
+    // maximum relative error of 2.45e-8
+    Double t = 1.0/sqrt(-log(x));
+    Double p1,p2,p3,p4;
+    Double q1,q2,q3;
+    p1 = 0.00980456202915;
+    p2 = 0.363667889171;
+    p3 = 0.97302949837;
+    p4 = -0.5374947401;
+    q1 = 0.00980451277802;
+    q2 = 0.363699971544;
+    q3 = 1.0;
+    result = (p1/t+p2+t*(p3+t*p4))/(q1+t*(q2+t*q3));
+  } else {
+    result = C::dbl_max;
+  }
+  return result;
+}
+
+Double VanVleck::predictNgt3(Int n, Double threshhold)
+{
+  Double result = 0.0;
+  if (n%2 == 0) {
+    // even n
+    for (Int k=1;k<=(n-2)/2;k++) {
+      result += erfc(k*threshhold/sqrt(2.0));
+    }
+    result = 1.0 + 8.0*result;
+  } else {
+    // odd n
+    for (Int k=1;k<=(n-1)/2;k++) {
+      result += (2*k-1)*erfc((2*k-1)*threshhold/sqrt(2.0));
+    }
+  }
+  return result;
+}
---
+++ code/scimath/implement/Mathematics/VanVleck.h
@@ -0,0 +1,194 @@
+//# VanVleck.h: Class of static functions to aid with vanVleck corrections.
+//# Copyright (C) 2002
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+
+#if !defined(AIPS_VANVLECK_H)
+#define AIPS_VANVLECK_H
+
+//#! Includes go here
+#include <aips/aips.h>
+#include <aips/Arrays/Matrix.h>
+#include <aips/Functionals/Interpolate1D.h>
+#include <aips/Mathematics/Constants.h>
+
+
+//# Forward Declarations
+
+// <summary>
+// A class of static functions to aid with vanVleck corrections of lag data.
+// </summary>
+
+// <use visibility=export>
+
+// <reviewed reviewer="" date="yyyy/mm/dd" tests="" demos="">
+// </reviewed>
+
+// <prerequisite>
+//   <li> Familiarity with the issues involved in turning digitally 
+//        sampled lag data from a correlator into spectral data.
+// </prerequisite>
+//
+// <etymology>
+// This provides the functions necessary to determine the van Vleck correction
+// for a general n-level by m-level correlator.
+// </etymology>
+//
+// <synopsis>
+// This provides the functions necessary to determine the van Vleck correction
+// for a general n-level by m-level correlator.
+// </synopsis>
+//
+// <example>
+// </example>
+//
+// <motivation>
+// The GBT spectrometer provides the measured auto-correlation and
+// cross-correlation lags.  The gbt MeasurementSet filler (gbtmsfiller)
+// needs to convert those lags to the spectral domain.  These functions
+// allow the filler to calculate the van Vleck correction appropriate
+// for each measured set of lags.  They are of general and hence are
+// not specific to the GBT spectrometer.
+//
+// The functions here are static because of the nature of the underlying
+// numerical quadrature fortran code used to integrate the 
+// drbyrho function.
+// </motivation>
+//
+// <thrown>
+//    <li>
+//    <li>
+// </thrown>
+//
+// <todo asof="2002/07/19">
+//   <li> The inverse error functions may be more generally useful.
+//        It exists here only as a private member function to be
+//        used internally.
+// </todo>
+
+class VanVleck
+{
+public:
+  // Set the interpolation table size.
+  // Must be an odd number.  The default size is 65.
+  static void size(Int npts);
+  
+  // Set the x and y quantization functions.
+  // Each matrix should have dimensions (2,n)
+  // where n is the number of levels.  The first
+  // row (0,...) is the (n-1) threshold levels and 
+  // the second row is the n quantizations based 
+  // on those thresholds.  The thresholds may 
+  // include a DC offset. The (0,(n-1)) element is
+  // never used and need not be set.
+  static void setQuantization(const Matrix<Double> &qx,
+			      const Matrix<Double> &qy);
+  
+  // Get the data used in setting up the interpolation
+  static void getTable(Vector<Double> &rs, Vector<Double> &rhos);
+  
+  // Given a rho return the corresponding corrected r
+  // Returns 0.0 if no quantization has been set yet.
+  static Double r(const Double rho);
+
+  // Given a measured zero-lag autocorrelation and number of
+  // levels (n>=3) return the first positive quantizer input
+  // threshold level.  This can be used to set the up the
+  // matrix arguments used in setQuantization.
+  static Double thresh(Int n, Double zerolag)
+  { return ( (n>3) ? threshNgt3(n,zerolag) : threshN3(zerolag) ); }
+
+  // Predict a given zero-lag given n and a threshold.  This
+  // is included here to be used as a check against the output
+  // of thresh.
+  static Double predict(Int n, Double threshhold)
+  { return ( (n>3) ? predictNgt3(n,threshhold) : predictN3(threshhold));}
+
+private:
+  // the number of points to use in setting up the interpolator
+  static Int itsSize;
+  
+  // The interpolator
+  static Interpolate1D<Double, Double> *itsInterp;
+  
+  // the quantization functions
+  static Vector<Double> itsQx0, itsQx1, itsQy0, itsQy1;
+
+  // The fortran numerical integration function will call this.
+  // For a given rho and quantization functions, this computes,
+  // via Price's theorem, the value dr/drho of the derivative,
+  // with respect to rho, of the expected value of the correlator
+  // output.
+  static Double drbydrho(Double *rho);
+
+  // For a given rhoi, rhof, this produces a high-accuracy numerical
+  // approximation to the integral of drbydrho over the range
+  // rhoi to rhof.  It calls the standard QUADPACK adaptive Gaussian quadrature
+  // procedure, dqags, to do the numerical integration.
+  static Double rinc(Double &rhoi, Double &rhof);
+
+  // Function used within drbydrho.
+  static Double g(const Double &x, const Double &y, const Double &rho);
+
+  // initialize the interpolator
+  static void initInterpolator();
+
+  // compute first threshhold for a given zerolag for n>3
+  static Double threshNgt3(Int n, Double zerolag);
+
+  // compute first threshhold for a given zerolag for n==3
+  static Double threshN3(Double zerolag)
+  { return sqrt(2.0)*invErfc(zerolag);}
+  
+  // inverse err fn - used by invErfc
+  static Double invErf(Double x);
+  
+  // inverse complementary err fn - used by threshN3
+  static Double invErfc(Double x);
+
+  // Predict a zero-lag value given the indicated first threshold level
+  // for n>3.
+  static Double predictNgt3(Int n, Double threshhold);
+
+  // Predict a zero-lag value given the indicated first threshold level
+  // for n=3.
+  static Double predictN3(Double threshhold)
+  { return erfc(threshhold/sqrt(2.0));}
+};
+
+
+inline 
+Double VanVleck::g(const Double &x, const Double &y,
+		   const Double &rho)
+{
+  return exp(-.5*(x*x-2.0*rho*x*y+y*y)/(1.0-rho*rho))/
+    (C::_2pi*sqrt(1.0-rho*rho));
+}
+
+inline 
+Double VanVleck::r(const Double rho)
+{ return (*itsInterp)(rho);}
+
+
+#endif
