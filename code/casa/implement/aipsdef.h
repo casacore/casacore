ccd828d151e81f41bce4b1b28e85e3cc2f44cb52
Wes Young <wyoung@nrao.edu>
2002-08-01 15:55:21 +0000

    Added AIPS_GCC3 to the list that doesn't handle vector forward declaration very well.

--- code/casa/implement/aipsdef.h
+++ code/casa/implement/aipsdef.h
@@ -84,7 +84,7 @@ namespace std {};
 #define WHATEVER_SUN_EXCEPTSPEC(X)
 #endif
 
-#if defined(AIPS_USE_NEW_SGI)
+#if defined(AIPS_USE_NEW_SGI) || defined(AIPS_GCC3)
 #if defined(WHATEVER_VECTOR_FORWARD_DEC)
 #undef WHATEVER_VECTOR_FORWARD_DEC
 #endif
