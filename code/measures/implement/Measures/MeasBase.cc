ce3c8eeb3aca1792b42e94097d21f4ffe32f48a3
Wim Brouw <brouw@astron.nl>
2002-11-20 21:17:14 +0000

    UChange the virtual assure to an assured

--- code/measures/implement/Measures/MeasBase.cc
+++ code/measures/implement/Measures/MeasBase.cc
@@ -99,7 +99,7 @@ Bool MeasBase<Mv,Mr>::areYou(const String &tp) const {
 }
 
 template <class Mv, class Mr>
-void MeasBase<Mv,Mr>::assure(const String &tp) const {
+void MeasBase<Mv,Mr>::assured(const String &tp) const {
   if (capitalize(tp) != tellMe()) {
     throw(AipsError("Illegal Measure type in context: " +
 		    tellMe()));
