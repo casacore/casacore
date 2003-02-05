775b07199b779de8c12e2ac9c8a4d9617182365b
Neil Killeen <nkilleen@unimelb.edu.au>
2003-02-05 03:53:29 +0000

    copyright

--- code/coordinates/implement/Coordinates/CoordinateSystem.h
+++ code/coordinates/implement/Coordinates/CoordinateSystem.h
@@ -1,5 +1,5 @@
 //# CoordinateSystem.h: Interconvert pixel and image coordinates.
-//# Copyright (C) 1997,1998,1999,2000,2001,2002
+//# Copyright (C) 1997,1998,1999,2000,2001,2002,2003
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
