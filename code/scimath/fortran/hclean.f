f8b479509eaf9cc8a6cc7a4b9a0d34e3516702df
Tim Cornwell <t.cornwell@skatelescope.org>
2003-07-16 19:35:18 +0000

    Revert to separate IQUV deconvolutions since the joint deconvolution was show to be stallable.

--- code/scimath/fortran/hclean.f
+++ code/scimath/fortran/hclean.f
@@ -1,5 +1,5 @@
 *=======================================================================
-*     Copyright (C) 1999,2000
+*     Copyright (C) 1999,2000,2003
 *     Associated Universities, Inc. Washington DC, USA.
 *
 *     This library is free software; you can redistribute it and/or
@@ -26,25 +26,48 @@
 *
 *     $Id$
 *-----------------------------------------------------------------------
-      real function maxeig(i,q,u,v)
-      real i, q, u, v
-      maxeig=i+sqrt(q**2+u**2+v**2)
-      return 
-      end
-      real function mineig(i,q,u,v)
-      real i, q, u, v
-      mineig=i-sqrt(q**2+u**2+v**2)
-      return 
-      end
-      real function eigprod(i,q,u,v)
-      real i, q, u, v
-      eigprod=i**2-(q**2+u**2+v**2)
-      return 
-      end
-      real function l2norm(i,q,u,v)
-      real i, q, u, v
-      l2norm=i**2+q**2+u**2+v**2
-      return 
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+c
+c   limagestep	real(nx,ny,npol) input dirty image; output residual image
+c   domask	integer		 domask == 0 ==> no mask is present
+c                                (formerly a logical, but that was trouble)
+c   lmask	real(nx,ny) 	 input mask image
+c   nx,ny,npol	integer		 shape of images
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+      subroutine maximg(limagestep, domask, lmask, nx, ny, npol, fmin,
+     $     fmax)
+      
+      implicit none
+      integer nx, ny, npol, domask
+      integer xbeg, xend, ybeg, yend
+      real limagestep(nx, ny, npol)
+      real lmask(nx, ny)
+      real fmin, fmax
+
+      real wpeak
+      integer pol, ix, iy
+      integer x1, x2, y1, y2
+      
+c     Now find peak in residual image 
+      fmin=1e20
+      fmax=-1e20
+      do pol=1,npol
+         do iy=1,ny
+            do ix=1,nx
+               if((domask.eq.0).or.(lmask(ix,iy).GT.0.5)) then
+                  wpeak = limageStep(ix,iy,pol)
+                  if(wpeak.GT.fmax) then
+                     fmax=wpeak
+                  end if
+                  if(wpeak.LT.fmin) then
+                     fmin=wpeak
+                  end if
+               end if
+            end do
+         end do
+      end do
+      
+      return
       end
 cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 c
@@ -85,129 +108,75 @@ cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
       external stopnow
 
       real cthres
-      real peak, mev, maxeig
-      real maxVal(4), pv(4)
-      integer px, py, pol, ix, iy, cycle
+      real maxval, pv, val
+      integer px, py, pol, ix, iy, cycle, i, maxiter
       integer x1, x2, y1, y2
       
-      do iter=siter, niter
-         
-c     Now find peak in residual image 
-         peak=0.0
-         px=xbeg
-         py=ybeg
-         do iy=ybeg,yend
-            do ix=xbeg,xend
-               if(domask.EQ.0  .OR. (lmask(ix,iy).GT.0.000001) ) then
-                  if(npol.EQ.4) then
-                     mev=maxeig(limageStep(ix,iy,1),
-     $                    limageStep(ix,iy,2), limageStep(ix,iy,3),
-     $                    limageStep(ix,iy,4))
-                  else if(npol.EQ.3) then
-                     mev=maxeig(limageStep(ix,iy,1),
-     $                    limageStep(ix,iy,2), limageStep(ix,iy,3),
-     $                    0.0)
-                  else if(npol.EQ.2) then
-                     mev=limageStep(ix,iy,1)+ abs(limageStep(ix,iy,2))
-                  else 
-                     mev=limageStep(ix,iy,1)
+      maxiter = siter
+      yes=0
+      do pol=1,npol
+         do iter=siter, niter
+            maxval=0.0
+            px=1
+            py=1
+            do iy=1,ny
+               do ix=1,nx
+                  if((domask.eq.0).or.(lmask(ix,iy).GT.0.5)) then
+                     val = abs(limagestep(ix,iy,pol))
+                     if(val.GT.maxval) then
+                        px=ix
+                        py=iy
+                        maxval=val
+                     end if
                   end if
-                  if(abs(mev).GT.peak) then
-                     px=ix
-                     py=iy
-                     peak=abs(mev)
-                  end if
-               end if
+               end do
             end do
-         end do
-      
-         if(npol.EQ.4) then
-            maxVal(1)=limageStep(px,py,1)
-            maxVal(2)=limageStep(px,py,2)
-            maxVal(3)=limageStep(px,py,3)
-            maxVal(4)=limageStep(px,py,4)
-         else if(npol.EQ.3) then
-            maxVal(1)=limageStep(px,py,1)
-            maxVal(2)=limageStep(px,py,2)
-            maxVal(3)=limageStep(px,py,3)
-            maxVal(4)=0.0
-         else if(npol.EQ.2) then
-            maxVal(1)=limageStep(px,py,1)
-            maxVal(2)=0.0
-            maxVal(3)=0.0
-            maxVal(4)=limageStep(px,py,2)
-         else 
-            maxVal(1)=limageStep(px,py,1)
-            maxVal(2)=0.0
-            maxVal(3)=0.0
-            maxVal(4)=0.0
-         end if
+c     
+            maxval=limageStep(px,py,pol)
 
-
-         
+            if (cspeedup .gt. 0.0) then
+               cthres = thres * 2.0**(real(iter-siter)/cspeedup )
+            else
+               cthres = thres
+            endif
+            if((yes.EQ.1).OR.(abs(maxval).LT.cthres)) then
+               goto 200
+            endif
+            
 c     // Output ten lines of information if run to the end
-         cycle=max(1,(niter-siter)/10         )
-         if((iter.EQ.siter).OR.(mod(iter,cycle)).EQ.1) then
-            call msgput(iter, px, py, maxVal)
-            call stopnow(yes)
-         endif
-         if (cspeedup .gt. 0.0) then
-            cthres = thres * 2.0**(real(iter-siter)/cspeedup )
-         else
-            cthres = thres
-         endif
-         if((yes.EQ.1).OR.(peak.LT.cthres)) then
-            call msgput(-iter, px, py, maxVal)
-            return
-         endif
-         
-c  Add the scaled peak to the current image
-c
-         x1 = max( xbeg, px - nx/2 )
-         y1 = max( ybeg, py - ny/2 )
-         x2 = min( xend, px + nx/2 -1 )
-         y2 = min( yend, py + ny/2 -1 )
-         if((npol.EQ.4).OR.(npol.EQ.3)) then
-            do pol=1,npol
-               pv(pol)=gain*maxVal(pol)
-            end do
-            do pol=1,npol
-               limage(px,py,pol)=limage(px,py,pol)+pv(pol)
-            end do
-            do pol=1,npol
-               do iy=y1,y2
-                  do ix=x1,x2
-                     limageStep(ix,iy,pol)=limageStep(ix,iy,pol)
-     $                    -pv(pol) * lpsf(nx/2+ix-px+1,ny/2+iy-py+1)
-                  end do
-               end do
-            end do
-         else if(npol.EQ.2) then
-            pv(1)=gain*maxVal(1)
-            pv(4)=gain*maxVal(4)
-            limage(px,py,1)=limage(px,py,1)+pv(1)
-            limage(px,py,2)=limage(px,py,2)+pv(4)
+            cycle=max(1,(niter-siter)/10)
+
+            if((iter.EQ.siter).OR.(mod(iter,cycle)).EQ.1) then
+               call msgput(npol, pol, iter, px, py, maxval)
+               call stopnow(yes)
+            endif
+            if(yes.EQ.1) then
+               goto 200
+            endif
+
+            x1 = max( 1, px - nx/2 )
+            y1 = max( 1, py - ny/2 )
+            x2 = min( nx, px + nx/2 -1 )
+            y2 = min( ny, py + ny/2 -1 )
+            pv=gain*maxval
+            limage(px,py,pol)=limage(px,py,pol)+pv
             do iy=y1,y2
                do ix=x1,x2
-                  limageStep(ix,iy,1)=limageStep(ix,iy,1)
-     $              - pv(1)*lpsf(nx/2+ix-px+1,ny/2+iy-py+1)
-                  limageStep(ix,iy,2)=limageStep(ix,iy,2)
-     $              - pv(4)*lpsf(nx/2+ix-px+1,ny/2+iy-py+1)
+                  limageStep(ix,iy,pol)=limageStep(ix,iy,pol)
+     $                 -pv * lpsf(nx/2+ix-px+1,ny/2+iy-py+1)
                end do
             end do
-         else 
-            pv(1)=gain*maxVal(1)
-            limage(px,py,1)=limage(px,py,1)+pv(1)
-            do iy=y1,y2
-               do ix=x1,x2
-                  limageStep(ix,iy,1)=limageStep(ix,iy,1)
-     $                 -pv(1)*lpsf(nx/2+ix-px+1,ny/2+iy-py+1)
-               end do
-            end do
+
+         end do
+ 200     continue
+         if(iter.gt.siter) then
+            call msgput(npol, pol, -iter, px, py, maxval)
          end if
+         maxiter=max(iter, maxiter)
       end do
+
  100  continue
-      call msgput(-niter, px, py, maxVal)
+      iter=maxiter
       if (iter .gt. niter) then
          iter = niter
       endif
