270ee1d209e9e34d7ee848e7626888a44b1a60cb
Neil Killeen <nkilleen@unimelb.edu.au>
2003-09-10 03:23:12 +0000

    copright

--- code/coordinates/implement/Coordinates/Direction2Coordinate.cc
+++ code/coordinates/implement/Coordinates/Direction2Coordinate.cc
@@ -1,5 +1,5 @@
 //# Direction2Coordinate.cc: this defines measures related DirectionCoordinate code
-//# Copyright (C) 1997,1999,2000,2001,2002
+//# Copyright (C) 1997,1999,2000,2001,2002,2003
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
