20254d5a419aaf306d72708aa196eff56614bdfc
Neil Killeen <nkilleen@unimelb.edu.au>
2003-09-10 03:24:48 +0000

    make sure some vectors are copies not references

--- code/coordinates/implement/Coordinates/LinearXform2.cc
+++ code/coordinates/implement/Coordinates/LinearXform2.cc
@@ -1,5 +1,5 @@
 //# <ClassFileName.h>: this defines <ClassName>, which ...
-//# Copyright (C) 1997,1998,1999,2000,2001
+//# Copyright (C) 1997,1998,1999,2000,2001,2003
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
@@ -54,9 +54,9 @@ LinearXform LinearXform::fourierInvert (const Vector<Bool>& axes,
 // Short cut which enables us to separate out axes
 
       pc0 = pc();
-      Vector<Double> d = pc0.diagonal();
+      Vector<Double> d = pc0.diagonal().copy();
       for (uInt i=0; i<nWorldAxes(); i++) {
-         if (axes(i)) d(i) = 1.0 / d(i);
+         if (axes[i]) d[i] = 1.0 / d[i];
       }
       pc0.diagonal() = d;
    } else {
@@ -67,12 +67,12 @@ LinearXform LinearXform::fourierInvert (const Vector<Bool>& axes,
       pc0 = invert(pc());
    }
 //
-   Vector<Double> cdelt0 = cdelt();   
-   Vector<Double> crpix0 = LinearXform::crpix();
+   Vector<Double> cdelt0 = cdelt().copy();
+   Vector<Double> crpix0 = LinearXform::crpix().copy();
    for (uInt i=0; i<nWorldAxes(); i++) {
-      if (axes(i)) {
-         cdelt0(i) = scale(i) / cdelt0(i);
-         crpix0(i) = crpix(i);
+      if (axes[i]) {
+         cdelt0[i] = scale[i] / cdelt0[i];
+         crpix0[i] = crpix[i];
       }
    }
    return LinearXform(crpix0, cdelt0, pc0);
