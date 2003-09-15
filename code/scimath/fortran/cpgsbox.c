90138102067b20cb52883e52b431e0ad22182df3
Mark Calabretta <mcalabre@atnf.csiro.au>
2003-09-15 03:58:46 +0000

    PGSBOX 3.2: added a C wrapper for PGLBOX.

---
+++ code/scimath/fortran/cpgsbox.c
@@ -0,0 +1,140 @@
+/*============================================================================
+*
+*   PGSBOX 3.2 - a non-linear coordinate axis plotter for PGPLOT.
+*   Copyright (C) 1997-2003, Mark Calabretta
+*
+*   This library is free software; you can redistribute it and/or modify it
+*   under the terms of the GNU Library General Public License as published
+*   by the Free Software Foundation; either version 2 of the License, or (at
+*   your option) any later version.
+*
+*   This library is distributed in the hope that it will be useful, but
+*   WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library
+*   General Public License for more details.
+*
+*   You should have received a copy of the GNU Library General Public License
+*   along with this library; if not, write to the Free Software Foundation,
+*   Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*
+*   Correspondence concerning WCSLIB may be directed to:
+*      Internet email: mcalabre@atnf.csiro.au
+*      Postal address: Dr. Mark Calabretta
+*                      Australia Telescope National Facility, CSIRO
+*                      PO Box 76
+*                      Epping NSW 1710
+*                      AUSTRALIA
+*
+*   Author: Mark Calabretta, Australia Telescope National Facility
+*   $Id$
+*===========================================================================*/
+
+#include <string.h>
+#include "cpgsbox.h"
+
+void pgsbok_(const float blc[2], const float trc[2], char idents[3][80],
+             const char opt[2], const int *labctl, const int *labden,
+             const int ci[7], const int gcode[2], const double *tiklen,
+             const int *ng1, const double *grid1, const int *ng2,
+             const double *grid2, const int *doeq, nlfunc_t nlfunc,
+             const int *nlc, const int *nli, const int *nld, char *nlcprm,
+             int *nliprm, double *nldprm, const int *nc, int *ic,
+             double cache[][4], int *ierr);
+
+void pglbok_(char idents[3][80], const char opt[2], const int *labctl,
+             const int *labden, const int ci[7], const int gcode[2],
+             const double *tiklen, const int *ng1, const double *grid1,
+             const int *ng2, const double *grid2, const int *doeq,
+             const int *nc, int *ic, double cache[][4], int *ierr);
+
+void cpgsbox(
+  const float blc[2],
+  const float trc[2],
+  char (*idents)[80],
+  const char opt[2],
+  int labctl,
+  int labden,
+  const int ci[7],
+  const int gcode[2],
+  double tiklen,
+  int ng1,
+  const double *grid1,
+  int ng2,
+  const double *grid2,
+  int doeq,
+  nlfunc_t nlfunc,
+  int nlc,
+  int nli,
+  int nld,
+  char nlcprm[],
+  int nliprm[],
+  double nldprm[],
+  int nc,
+  int *ic,
+  double cache[][4],
+  int *ierr)
+
+{
+  char ids[3][80];
+  int j, k;
+
+  /* Convert variable length strings to fixed-length char arrays. */
+  k = 0;
+  for (j = 0; j < 3; j++) {
+    if (strlen(idents[j]) > 80) {
+       strncpy(ids[j], idents[j], 80);
+    } else {
+       strcpy(ids[j], idents[j]);
+       for (k = strlen(idents[j]); k < 80; k++) {
+          ids[j][k] = ' ';
+       }
+    }
+  }
+
+  pgsbok_(blc, trc, ids, opt, &labctl, &labden, ci, gcode, &tiklen, &ng1,
+          grid1, &ng2, grid2, &doeq, nlfunc, &nlc, &nli, &nld, nlcprm, nliprm,
+          nldprm, &nc, ic, cache, ierr);
+  return;
+}
+
+/*==========================================================================*/
+
+void cpglbox(
+  char (*idents)[80],
+  const char opt[2],
+  int labctl,
+  int labden,
+  const int ci[7],
+  const int gcode[2],
+  double tiklen,
+  int ng1,
+  const double *grid1,
+  int ng2,
+  const double *grid2,
+  int doeq,
+  int nc,
+  int *ic,
+  double cache[][4],
+  int *ierr)
+
+{
+  char ids[3][80];
+  int j, k;
+
+  /* Convert variable length strings to fixed-length char arrays. */
+  k = 0;
+  for (j = 0; j < 3; j++) {
+    if (strlen(idents[j]) > 80) {
+       strncpy(ids[j], idents[j], 80);
+    } else {
+       strcpy(ids[j], idents[j]);
+       for (k = strlen(idents[j]); k < 80; k++) {
+          ids[j][k] = ' ';
+       }
+    }
+  }
+
+  pglbok_(ids, opt, &labctl, &labden, ci, gcode, &tiklen, &ng1, grid1, &ng2,
+          grid2, &doeq, &nc, ic, cache, ierr);
+  return;
+}
---
+++ code/scimath/fortran/cpgsbox.h
@@ -0,0 +1,115 @@
+/*============================================================================
+*
+*   PGSBOX 3.2 - a non-linear coordinate axis plotter for PGPLOT.
+*   Copyright (C) 1997-2003, Mark Calabretta
+*
+*   This library is free software; you can redistribute it and/or modify it
+*   under the terms of the GNU Library General Public License as published
+*   by the Free Software Foundation; either version 2 of the License, or (at
+*   your option) any later version.
+*
+*   This library is distributed in the hope that it will be useful, but
+*   WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library
+*   General Public License for more details.
+*
+*   You should have received a copy of the GNU Library General Public License
+*   along with this library; if not, write to the Free Software Foundation,
+*   Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*
+*   Correspondence concerning WCSLIB may be directed to:
+*      Internet email: mcalabre@atnf.csiro.au
+*      Postal address: Dr. Mark Calabretta
+*                      Australia Telescope National Facility, CSIRO
+*                      PO Box 76
+*                      Epping NSW 1710
+*                      AUSTRALIA
+*
+*=============================================================================
+*
+*   cpgsbox() and cpglbox() are C wrappers for PGSBOX and PGLBOX.  Refer to
+*   the prologue of pgsbox.f for an explanation of the argument list and usage
+*   notes.
+*
+*   The argument lists for cpgsbox()/cpglbox() differ from PGSBOX/PGLBOX in
+*   the following respects:
+*
+*      idents   char[3][80]
+*                        Fixed length character array.
+*      opt      char[2]  Fixed length character array.
+*      nlfunc   nlfunc_t typedef for external function defined in cpgsbox.h.
+*      cache    double[][4]
+*                        Array indices reversed.
+*
+*   Note also that the array arguments to cpgsbox()/cpglbox() are all
+*   0-relative, while several of those of PGSBOX/PGLBOX are 0-relative (GRID1,
+*   GRID2, and CACHE) with the remainder 1-relative.  In particular, the two-
+*   dimensional CACHE array has a mixture of 0-, and 1-relative indices, and
+*   the indices are reversed in C because of the differing C and FORTRAN array
+*   indexing policy.  Moreover, as in PGSBOX/PGLBOX, nc is the upper array
+*   index, not the array length, so the array should be dimensioned as
+*   cache[nc+1][4].
+*
+*   Author: Mark Calabretta, Australia Telescope National Facility
+*   $Id$
+*===========================================================================*/
+#ifndef cpgsbox_h
+#define cpgsbox_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void nlfunc_t(int *, int *, int *, int *, char *, int *, double *,
+                      double *, double *, int *, double *, int *);
+
+void cpgsbox(
+  const float blc[2],
+  const float trc[2],
+  char (*idents)[80],
+  const char opt[2],
+  int labctl,
+  int labden,
+  const int ci[7],
+  const int gcode[2],
+  double tiklen,
+  int ng1,
+  const double *grid1,
+  int ng2,
+  const double *grid2,
+  int doeq,
+  nlfunc_t nlfunc,
+  int nlc,
+  int nli,
+  int nld,
+  char nlcprm[],
+  int nliprm[],
+  double nldprm[],
+  int nc,
+  int *ic,
+  double cache[][4],
+  int *ierr);
+
+void cpglbox(
+  char (*idents)[80],
+  const char opt[2],
+  int labctl,
+  int labden,
+  const int ci[7],
+  const int gcode[2],
+  double tiklen,
+  int ng1,
+  const double *grid1,
+  int ng2,
+  const double *grid2,
+  int doeq,
+  int nc,
+  int *ic,
+  double cache[][4],
+  int *ierr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
