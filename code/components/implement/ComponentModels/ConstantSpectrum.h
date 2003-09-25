fee5cd7bb88cfadf5cc0fc697ead187a0dd78747
Neil Killeen <nkilleen@unimelb.edu.au>
2003-09-25 05:22:34 +0000

    copyright

--- code/components/implement/ComponentModels/ConstantSpectrum.h
+++ code/components/implement/ComponentModels/ConstantSpectrum.h
@@ -1,5 +1,5 @@
 //# ConstantSpectrum.h: Model the spectral variation with a constant
-//# Copyright (C) 1998,1999,2000
+//# Copyright (C) 1998,1999,2000,2003
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
--- code/components/implement/ComponentModels/SpectralModel.h
+++ code/components/implement/ComponentModels/SpectralModel.h
@@ -1,5 +1,5 @@
 //# SpectralModel.h: Base class for Spectral Models
-//# Copyright (C) 1998,1999,2000
+//# Copyright (C) 1998,1999,2000,2003
 //# Associated Universities, Inc. Washington DC, USA.
 //#
 //# This library is free software; you can redistribute it and/or modify it
