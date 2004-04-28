26fd2e2d3ea11cadc619fcf50367f3338999381d
Neil Killeen <nkilleen@unimelb.edu.au>
2004-04-28 09:58:39 +0000

    test functions collapse and copyDataAndMask

--- code/lattices/implement/Lattices/test/tLatticeUtilities.cc
+++ code/lattices/implement/Lattices/test/tLatticeUtilities.cc
@@ -28,44 +28,39 @@
 #include <aips/aips.h>
 #include <aips/Exceptions/Error.h>
 #include <aips/Arrays/IPosition.h>
+#include <aips/Arrays/ArrayLogical.h>
+#include <aips/Mathematics/Math.h>
+#include <aips/Logging/LogIO.h>
+#include <aips/Logging/LogOrigin.h>
 #include <aips/Utilities/Assert.h>
 #include <aips/Lattices/TempLattice.h>
 #include <trial/Lattices/LatticeUtilities.h>
+#include <trial/Lattices/SubLattice.h>
+#include <trial/Lattices/LatticeExprNode.h>
+#include <trial/Lattices/LatticeExpr.h>
+#include <aips/Lattices/ArrayLattice.h>
 
 #include <aips/iostream.h>
 
+void doMinMax();
+void doCollapse();
+void doCopy();
 
 int main()
 {
   try {
+    
+// minMax
 
-    TempLattice<Float> lat(IPosition(3,1024,1024,10), 1.0);
-    lat.set(0.0);
-    lat.putAt( 1.0, IPosition(3, 10, 10, 0) );
-    lat.putAt( -1.0, IPosition(3, 20, 20, 2) );
-    lat.putAt( 1.0, IPosition(3, 1000, 900, 3) );
-    lat.putAt( -1.0, IPosition(3, 1002, 990, 4) );
-    lat.putAt( 2.0, IPosition(3, 400, 500, 5) );
-    lat.putAt( -2.0, IPosition(3, 10, 1000, 6) );
-    lat.putAt( 3.0, IPosition(3, 900, 100, 7) );
-    lat.putAt( -3.0, IPosition(3, 500, 100, 8) );
-    lat.putAt( 4.0, IPosition(3, 1020, 1020, 8) );
-    lat.putAt( -4.0, IPosition(3, 800, 800, 9) );
+     doMinMax();
 
-    Float lmin, lmax;
-    IPosition lminPos(3, 0);
-    IPosition lmaxPos(3, 0);
+// Collapse
 
-    minMax(lmin, lmax, lminPos, lmaxPos, lat);
+     doCollapse();
 
-    IPosition trueMaxPos = IPosition(3, 1020, 1020, 8);
-    IPosition trueMinPos = IPosition(3, 800, 800, 9);
-    if (trueMaxPos == lmaxPos && lmax == 4.0) {
-      cout << "Max find in LatticeMinMax succeeded" << endl;
-    }
-    if (trueMinPos == lminPos && lmin == -4.0) {
-      cout << "Min find in LatticeMinMax succeeded" << endl;
-    }
+// Copy
+
+     doCopy();
 
   } catch (AipsError x) {
     cout<< "FAIL"<< endl;
@@ -76,6 +71,147 @@ int main()
   return 0;
 };
 
-// Local Variables: 
-// compile-command: "gmake OPTLIB=1 tLatticeUtilities"
-// End: 
+
+
+
+void doMinMax()
+{
+    cerr << "minMax " << endl;
+    TempLattice<Float> lat(IPosition(3,512,512,10), 1.0);
+    lat.set(0.0);
+    lat.putAt( 1.0, IPosition(3, 10, 10, 0) );
+    lat.putAt( -1.0, IPosition(3, 20, 20, 2) );
+    lat.putAt( 1.0, IPosition(3, 500, 400, 3) );
+    lat.putAt( -1.0, IPosition(3, 502, 490, 4) );
+    lat.putAt( 2.0, IPosition(3, 400, 500, 5) );
+    lat.putAt( -2.0, IPosition(3, 10, 400, 6) );
+    lat.putAt( 3.0, IPosition(3, 400, 100, 7) );
+    lat.putAt( -3.0, IPosition(3, 500, 100, 8) );
+
+    Float lmin, lmax;
+    IPosition lminPos(3, 0);
+    IPosition lmaxPos(3, 0);
+
+    minMax(lmin, lmax, lminPos, lmaxPos, lat);
+
+    IPosition trueMaxPos = IPosition(3, 400, 100, 7);
+    IPosition trueMinPos = IPosition(3, 500, 100, 8);
+    AlwaysAssert(trueMaxPos == lmaxPos && lmax == 3.0, AipsError);
+    AlwaysAssert(trueMinPos == lminPos && lmin == -3.0, AipsError);
+}
+
+
+void doCollapse ()
+{
+    cerr << "Collapse" << endl;
+    IPosition shape(3, 10, 20, 30);
+    ArrayLattice<Float> latIn(shape);
+    latIn.set(1.0);
+    Array<Float> data;
+    Array<Bool> mask;
+    IPosition axes(2); axes(0) = 1; axes(1) = 2;
+
+// Unmasked input
+
+    {       
+       cerr << "  Unmasked" << endl;
+       SubLattice<Float> mLatIn(latIn);
+//
+       LatticeUtilities::collapse (data, axes, mLatIn, True);
+       AlwaysAssert(data.ndim()==1, AipsError);       
+       AlwaysAssert(data.shape()(0)==shape(0), AipsError);
+       AlwaysAssert(allNear(data, Float(1.0), 1.0e-6), AipsError);
+//
+       LatticeUtilities::collapse (data, mask, axes, mLatIn, True, True, True);
+       AlwaysAssert(data.ndim()==1, AipsError);       
+       AlwaysAssert(mask.ndim()==1, AipsError);
+       AlwaysAssert(data.shape()(0)==shape(0), AipsError);
+       AlwaysAssert(mask.shape()(0)==shape(0), AipsError);
+       AlwaysAssert(allNear(data, Float(1.0), 1.0e-6), AipsError);
+       AlwaysAssert(allEQ(mask, True), AipsError);
+    }
+
+// Masked Input
+
+    {       
+       cerr << "  Masked" << endl;
+       SubLattice<Float> mLatIn(latIn);
+//
+       ArrayLattice<Bool> maskLat(shape);
+       maskLat.set(True);       
+       mLatIn.setPixelMask(maskLat, True);
+//
+       LatticeUtilities::collapse (data, axes, mLatIn, True);
+       AlwaysAssert(data.ndim()==1, AipsError);       
+       AlwaysAssert(data.shape()(0)==shape(0), AipsError);
+       AlwaysAssert(allNear(data, Float(1.0), 1.0e-6), AipsError);
+//
+       LatticeUtilities::collapse (data, mask, axes, mLatIn, True, True, True);
+       AlwaysAssert(data.ndim()==1, AipsError);       
+       AlwaysAssert(mask.ndim()==1, AipsError);
+       AlwaysAssert(data.shape()(0)==shape(0), AipsError);
+       AlwaysAssert(mask.shape()(0)==shape(0), AipsError);
+       AlwaysAssert(allNear(data, Float(1.0), 1.0e-6), AipsError);
+       AlwaysAssert(allEQ(mask, True), AipsError);
+    }
+}
+
+
+
+void doCopy ()
+{
+    cerr << "copyDataAndMask" << endl;
+    LogIO os(LogOrigin("tLatticeUtilities", "doCopy", WHERE));
+//
+    IPosition shape(2, 5, 10);
+    IPosition pos(2,0);
+    ArrayLattice<Float> latIn(shape);
+    latIn.set(1.0);
+    SubLattice<Float> mLatIn(latIn, True);
+    ArrayLattice<Bool> maskIn(shape);
+    maskIn.set(True);
+    mLatIn.setPixelMask(maskIn, True);
+
+// Unmasked output
+
+    {       
+       cerr << "  Unmasked output" << endl;
+//
+       ArrayLattice<Float> latOut(shape);
+       SubLattice<Float> mLatOut(latOut, True);
+//
+       LatticeUtilities::copyDataAndMask (os, mLatOut, mLatIn, False);
+       AlwaysAssert(allNear(mLatOut.get(), Float(1.0), 1.0e-6), AipsError);
+       AlwaysAssert(allEQ(mLatOut.getMask(), True), AipsError);
+    }
+//
+    {       
+       cerr << "  Masked output" << endl;
+//
+       ArrayLattice<Float> latOut(shape);
+       SubLattice<Float> mLatOut(latOut, True);
+       ArrayLattice<Bool> latMaskOut(shape);       
+       latMaskOut.set(False);
+       mLatOut.setPixelMask(latMaskOut, True);
+//
+       LatticeUtilities::copyDataAndMask (os, mLatOut, mLatIn, False);
+       AlwaysAssert(allNear(mLatOut.get(), Float(1.0), 1.0e-6), AipsError);
+       AlwaysAssert(allEQ(mLatOut.getMask(), True), AipsError);
+
+// Now set one mask value to False so the output pixel should be zero
+
+
+       Lattice<Bool>& pixelMaskIn = mLatIn.pixelMask();
+       pixelMaskIn.set(True);
+       pixelMaskIn.putAt(False,pos);
+       LatticeUtilities::copyDataAndMask (os, mLatOut, mLatIn, True);
+//
+       {
+          Array<Float> dataOut = mLatOut.get();
+          Array<Bool> maskOut = mLatOut.getMask();
+          AlwaysAssert(near(dataOut(pos), Float(0.0), 1.0e-6), AipsError);
+          AlwaysAssert(maskOut(pos)==False, AipsError);
+       }
+   }
+}
+
