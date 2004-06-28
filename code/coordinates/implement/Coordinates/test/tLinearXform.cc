8f26a1fe5d6ab819eb3a27646a551f5e53cc8ffc
Ger van Diepen <gervandiepen@gmail.com>
2004-06-28 10:27:53 +0000

    Include MatrixMathLA.h instead of MatrixMath.h

--- code/coordinates/implement/Coordinates/test/tLinearXform.cc
+++ code/coordinates/implement/Coordinates/test/tLinearXform.cc
@@ -30,7 +30,7 @@
 #include <aips/Arrays/Matrix.h>
 #include <aips/Arrays/ArrayLogical.h>
 #include <aips/Arrays/ArrayMath.h>
-#include <aips/Arrays/MatrixMath.h>
+#include <aips/Arrays/MatrixMathLA.h>
 #include <aips/Mathematics/Math.h>
 #include <trial/Coordinates/LinearXform.h>
 #include <aips/Exceptions/Error.h>
