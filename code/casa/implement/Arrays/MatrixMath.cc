e3fc874f022e700a34d40c86f0f76e092374f090
Ger van Diepen <gervandiepen@gmail.com>
2004-06-28 07:32:33 +0000

    Moved the LAPACK dependent functions to MatrixMathLA

--- code/casa/implement/Arrays/MatrixMath.cc
+++ code/casa/implement/Arrays/MatrixMath.cc
@@ -25,19 +25,13 @@
 //#
 //# $Id$
 
-#include <aips/Mathematics/Complex.h>
+#include <aips/Arrays/MatrixMath.h>
 #include <aips/Arrays/Vector.h>
 #include <aips/Arrays/Matrix.h>
-#include <aips/Arrays/MatrixMath.h>
-#include <aips/Arrays/ArrayLogical.h>
-#include <aips/Arrays/ArrayMath.h>
 #include <aips/Arrays/ArrayError.h>
-#include <aips/Utilities/String.h>
-#include <aips/Utilities/Assert.h>
-#include <aips/stdio.h>
+#include <aips/iostream.h>
 #include <aips/math.h>
 
-#include <aips/Arrays/ArrayIO.h>
                                       // the vector dot/scalar/inner product
 template<class T> T innerProduct (const Vector<T> &A, const Vector<T> &B) {
                                    // check for correct dimensions
@@ -113,14 +107,6 @@ Vector<T> directProduct(const Vector<T>& x, const Vector<T>& y)
 }
 
 
-template <class T>
-Vector<T> outerProduct (const Matrix<T> &A, const Vector<T> &x) {
-  static int count = 0;
-  if(count==0) 
-    cerr << "outerProduct(Matrix, Vector) is deprecated; use product(...)\n";
-  return product(A,x);
-}
-
 template <class T> 
 Matrix<T> product (const Vector<T> &x, const Matrix<T> &yT) {
   if (yT.nrow()!= 1) 
@@ -130,14 +116,6 @@ Matrix<T> product (const Vector<T> &x, const Matrix<T> &yT) {
   A.column(0) = x;
 
   return product(A,yT);
-}
-
-template <class T>
-Matrix<T> outerProduct (const Vector<T> &x, const Matrix<T> &yT) {
-  static int count = 0;
-  if(count==0) 
-    cerr << "outerProduct(Vector, Matrix) is deprecated; use product(...)\n";
-  return product(x, yT);
 }
 
                                  // matrix multiplication or cayley product
@@ -153,14 +131,6 @@ Matrix<T> product (const Matrix<T> &A, const Matrix<T> &B) {
 	  for (uInt k = 0; k < A.ncolumn(); k++) result(i,j) += A(i,k) * B(k,j);
       }
   return result;
-}
-
-template <class T>
-Matrix<T> cayleyProduct (const Matrix<T> &A, const Matrix<T> &B) {
-  static int count = 0;
-  if(count==0) 
-    cerr << "cayleyProduct(Matrix, Matrix) is deprecated; use product(...)\n";
-  return product(A,B);
 }
 
 template <class T> Matrix<T> transpose (const Matrix<T> &A) {
@@ -180,194 +150,4 @@ Matrix<T> directProduct(const  Matrix<T> &A, const Matrix<T> &B) {
 	}
     }
     return dpAB;
-}
-
-template<class T> 
-Matrix<T> invert(const Matrix<T> &in){
-  Matrix<T> out;
-  T det;
-  invert(out, det, in);
-  return out;
-}
-
-template<class T> 
-T determinate(const Matrix<T> &in){
-  Matrix<T> out;
-  T det;
-  invert(out, det, in);
-  return det;
-}
-
-template<class T> 
-void invert(Matrix<T> &out, T& det, const Matrix<T> &in)
-{
-  AlwaysAssert(in.nrow() == in.ncolumn(), AipsError);
-  
-  Int m = in.nrow();
-  Int lda = m; Int n = m;               // m, n, lda
-  
-  out.resize(in.shape());
-  out = in;
-  Bool deleteIt;
-  T *a = out.getStorage(deleteIt);      // a
-  
-  Block<Int> ipiv(m);                   // ipiv
-  Int info;                             // info
-
-  getrf(&m, &n, a, &lda, ipiv.storage(), &info);
-
-  if (info == 0) { // LU decomposition worked! 
-    // Calculate the determinate
-    // It is just the product of the diagonal elements
-    det = out(0,0);
-    for (Int i = 1; i < n; i++)
-      det *= out(i,i);
-
-    // Calculate the inverse using back substitution
-    Int lwork = 32 * n; // Lazy - we should really get this from ilaenv
-    Block<T> work(lwork);
-    getri(&m, a, &lda, ipiv.storage(), work.storage(), &lwork, &info);
-  }
-  out.putStorage(a, deleteIt); 
-  AlwaysAssert(info >= 0, AipsError); // illegal argument to *getri or *getrf
-  if (info > 0) {
-    out.resize(0,0);
-  }
-}
-
-
-template<class T> Matrix<T> invertSymPosDef(const Matrix<T> &in)
-{
-  Int i, j, k, n;
-  n = in.nrow();
-
-  Vector<T> diag(n);
-  Vector<T> b(n);
-  Matrix<T> tmp(n,n);
-  Matrix<T> out(n,n);
-
-  for(i = 0; i < n; i++) {
-    for(j = 0; j < n; j++) {
-      tmp(i,j) = in(i,j);
-    }
-  }
-
-  // Cholesky decomposition: A = L*trans(L)
-  CholeskyDecomp(tmp, diag);
-
-  // Solve inverse of A by forward and backward substitution.  The right
-  // hand side is a unit matrix, the solution is thus the inverse of A.
-  for(j = 0; j < n; j++) {
-    // one column at a time
-    for(k = 0; k < n; k++) {
-      b(k) = T(0.0);
-    }
-    b(j) = T(1.0);
-    CholeskySolve(tmp, diag, b, b);
-    for(k = 0; k < n; k++) {
-      out(k,j) = b(k);
-    }
-  }
-  return out;
-}
-
-
-template<class T> void invertSymPosDef(Matrix<T> & out, T& determinate, 
-				       const Matrix<T> &in)
-{
-  Int i, j, k, n;
-  n = in.nrow();
-
-  Vector<T> diag(n);
-  Vector<T> b(n);
-  Matrix<T> tmp(n,n);
-
-  for(i = 0; i < n; i++) {
-    for(j = 0; j < n; j++) {
-      tmp(i,j) = in(i,j);
-    }
-  }
-
-  // Cholesky decomposition: A = L*trans(L)
-  CholeskyDecomp(tmp, diag);
-  
-  determinate = diag(0)*diag(0);
-  for(k = 1; k < n; k++) determinate = determinate*diag(k)*diag(k);
-
-  // Solve inverse of A by forward and backward substitution.  The right
-  // hand side is a unit matrix, the solution is thus the inverse of A.
-  for(j = 0; j < n; j++) {
-    // one column at a time
-    for(k = 0; k < n; k++) {
-      b(k) = T(0.0);
-    }
-    b(j) = T(1.0);
-    CholeskySolve(tmp, diag, b, b);
-    for(k = 0; k < n; k++) {
-      out(k,j) = b(k);
-    }
-  }
-}
-
-template<class T> void CholeskyDecomp(Matrix<T> &A, Vector<T> &diag)
-{
-  // This function performs Cholesky decomposition.
-  // A is a positive-definite symmetric matrix. Only the upper triangle of
-  // A is needed on input. On output, the lower triangle of A contains the
-  // Cholesky factor L.  The diagonal elements of L are returned in vector
-  // diag.
-
-  Int i, j, k, n;
-  T sum;
-  n = A.nrow();
-  // Cholesky decompose A = L*trans(L)
-  for(i = 0; i < n; i++) {
-    for(j = i; j < n; j++) {
-      sum = A(i,j);
-      for(k = i-1; k >=0; k--) {
-	sum = sum - A(i,k)*A(j,k);
-      }
-      if(i == j) {
-	if(sum <= T(0.0)) {
-	  throw(AipsError("invertSymPosDef(const Matrix<T> &in): Matrix is"
-			  "not positive definite"));
-	}
-	diag(i) = sqrt(sum);
-      } else {
-	A(j,i) = sum/diag(i);
-      }
-    }
-  }
-}
-
-template<class T> void CholeskySolve(Matrix<T> &A, Vector<T> &diag, 
-				     Vector<T> &b, Vector<T> &x)
-{
-  // Solve linear equation A*x = b, where A positive-definite symmetric.
-  // On input, A contains Cholesky factor L in its low triangle except the
-  // diagonal elements which are in vector diag.  On return x contains the
-  // solution.  b and x can be the same vector to save memory space.
-
-  Int i, k, n;
-  T sum;
-
-  n = A.nrow();
-        
-  // solve by forward and backward substitution.  
-  // L*y = b
-  for(i = 0; i < n; i++) {
-    sum = b(i);
-    for(k = i-1; k >=0; k--) {
-      sum = sum - A(i,k)*x(k);
-    }
-    x(i) = sum/diag(i);
-  }
-  // trans(L)*x = y
-  for(i = n-1; i >= 0; i--) {
-    sum = x(i);
-    for(k = i+1; k < n; k++) {
-	sum = sum - A(k,i)*x(k);
-      }
-    x(i) = sum/diag(i);
-  }
 }
--- code/casa/implement/Arrays/MatrixMath.h
+++ code/casa/implement/Arrays/MatrixMath.h
@@ -29,7 +29,6 @@
 #define AIPS_MATRIXMATH_H
 
 
-
 #include <aips/aips.h>
 #include <aips/Arrays/Vector.h>
 #include <aips/Arrays/Matrix.h>
@@ -83,27 +82,10 @@ template <class T>
    Matrix<T> product (const Vector<T> &x, const Matrix<T> &yT);
 
 //
-// The matrix/outer product of a vector and a transposed vector. 
-// <note> The function's second argument is actually a transposed vector
-// stored as the only row in a 1xN matrix. </note> 
-// <note role=warning> This is deprecated - use "product(Vector, Matrix)" 
-// </note>
-//
-template <class T>
-   Matrix<T> outerProduct (const Vector<T> &x, const Matrix<T> &yT);
-
-//
 // The vector/outer product of an MxN matrix and an N-length vector.
 //
 template <class T>
    Vector<T> product (const Matrix<T> &A, const Vector<T> &x);
-
-//
-// The vector/outer product of an MxN matrix and an N-length vector.
-// <note role=warning> This is deprecated - use "product(Matrix, Vector)" 
-// </note>
-template <class T>
-   Vector<T> outerProduct (const Matrix<T> &A, const Vector<T> &x);
 
 // 
 // The direct product of two vectors.
@@ -120,14 +102,6 @@ template <class T>
 //
 template <class T> 
    Matrix<T> product (const Matrix<T> &A, const Matrix<T> &B);
-
-//
-// The matrix multiplication or cayley product of an MxN matrix and
-// an NxP matrix.
-// <note role=warning> This is deprecated - use "product(Matrix, Matrix)"
-// </note>
-template <class T> 
-   Matrix<T> cayleyProduct (const Matrix<T> &A, const Matrix<T> &B);
 
 //
 // The infinity norm (or maximum value of the sum of the absolute values 
@@ -209,116 +183,6 @@ Matrix<Double> adjoint (const Matrix<Double> &A);
 // The real part of a product of a Complex Matrix and a Complex Matrix
 //
   Matrix<Float> rproduct (const Matrix<Complex>&, const Matrix<Complex>&);
-
-// Routines which calculate the inverse of a matrix.  The inverse is very
-// often the worst way to do a calculation.  Nevertheless it is often
-// convenient. The present implementation uses LU decomposition implemented
-// by LAPACK. The determinate can be calculated "for free" as it is the
-// product of the diagonal terms after decomposition.  If the input matrix is
-// singular, a matrix with no rows or columns is returned.  <src>in</src>
-// must be a square matrix.  
-// <note role="warning">This function will only work for complex types if
-// Complex and DComplex map onto their FORTRAN counterparts.</note>
-//# We could special case small matrices for efficiency. 
-//<group>
-template<class T> void invert(Matrix<T> & out, T& determinate, 
-			      const Matrix<T> &in);
-template<class T> Matrix<T> invert(const Matrix<T> &in);
-template<class T> T determinate(const Matrix<T> &in);
-//</group>
-
-// This function inverts a symmetric positive definite matrix.  It is
-// written in C++, so it should work with any data type for which
-// operators +, -, *, /, =, and sqrt are defined.  The function uses
-// the Cholesky decomposition method to invert the matrix.  Cholesky
-// decomposition is about a factor of 2 better than LU decomposition
-// where symmetry is ignored. 
-template<class T> void invertSymPosDef(Matrix<T> & out, T& determinate, 
-				       const Matrix<T> &in);
-template<class T> Matrix<T> invertSymPosDef(const Matrix<T> &in);
-
-//</group>
-
-//# These are actually used by invertSymPosDef. They will not
-//# normally be called by the end user.
-
-//# This function performs Cholesky decomposition.
-//# A is a positive-definite symmetric matrix. Only the upper triangle of
-//# A is needed on input. On output, the lower triangle of A contains the
-//# Cholesky factor L.  The diagonal elements of L are returned in vector
-//# diag.
-template<class T> void CholeskyDecomp(Matrix<T> &A, Vector<T> &diag);
-
-//# Solve linear equation A*x = b, where A positive-definite symmetric.
-//# On input, A contains Cholesky factor L in its low triangle except the
-//# diagonal elements which are in vector diag.  On return x contains the
-//# solution.  b and x can be the same vector to save memory space.
-template<class T> void CholeskySolve(Matrix<T> &A, Vector<T> &diag, 
-				     Vector<T> &b, Vector<T> &x);
-
-
-//# These are the LAPACK routines actually used by invert. They will not
-//# normally be called by the end user.
-
-#if !defined(NEED_FORTRAN_UNDERSCORES)
-#define NEED_FORTRAN_UNDERSCORES 1
-#endif
-
-#if NEED_FORTRAN_UNDERSCORES
-#define sgetrf sgetrf_
-#define dgetrf dgetrf_
-#define cgetrf cgetrf_
-#define zgetrf zgetrf_
-#define sgetri sgetri_
-#define dgetri dgetri_
-#define cgetri cgetri_
-#define zgetri zgetri_
-#endif
-
-extern "C" {
-      void sgetrf(const int *m, const int *n, float *a, const int *lda,
-		  int *ipiv, int *info);
-      void dgetrf(const int *m, const int *n, double *a, const int *lda,
-		  int *ipiv, int *info);
-      void cgetrf(const int *m, const int *n, Complex *a, const int *lda,
-		  int *ipiv, int *info);
-      void zgetrf(const int *m, const int *n, DComplex *a, const int *lda,
-		  int *ipiv, int *info);
-      void sgetri(const int *m, float *a, const int *lda, const int *ipiv,
-		  float *work, const int *lwork, int *info);
-      void dgetri(const int *m, double *a, const int *lda, const int *ipiv,
-		  double *work, const int *lwork, int *info);
-      void cgetri(const int *m, Complex *a, const int *lda, const int *ipiv,
-		  Complex *work, const int *lwork, int *info);
-      void zgetri(const int *m, DComplex *a, const int *lda, const int *ipiv,
-		  DComplex *work, const int *lwork, int *info);
-};
-
-//# Overloaded versions of the above to make templating work more easily
-inline void getrf(const int *m, const int *n, float *a, const int *lda,
-		  int *ipiv, int *info)
-   { sgetrf(m, n, a, lda, ipiv, info); }
-inline void getrf(const int *m, const int *n, double *a, const int *lda,
-		  int *ipiv, int *info)
-   { dgetrf(m, n, a, lda, ipiv, info); }
-inline void getrf(const int *m, const int *n, Complex *a, const int *lda,
-		  int *ipiv, int *info)
-   { cgetrf(m, n, a, lda, ipiv, info); }
-inline void getrf(const int *m, const int *n, DComplex *a, const int *lda,
-		  int *ipiv, int *info)
-   { zgetrf(m, n, a, lda, ipiv, info); }
-inline void getri(const int *m, float *a, const int *lda, const int *ipiv,
-		  float *work, const int *lwork, int *info)
-   { sgetri(m, a, lda, ipiv, work, lwork, info); }
-inline void getri(const int *m, double *a, const int *lda, const int *ipiv,
-		  double *work, const int *lwork, int *info)
-   { dgetri(m, a, lda, ipiv, work, lwork, info); }
-inline void getri(const int *m, Complex *a, const int *lda, const int *ipiv,
-		  Complex *work, const int *lwork, int *info)
-   { cgetri(m, a, lda, ipiv, work, lwork, info); }
-inline void getri(const int *m, DComplex *a, const int *lda, const int *ipiv,
-		  DComplex *work, const int *lwork, int *info)
-   { zgetri(m, a, lda, ipiv, work, lwork, info); }
 
 
 #endif
