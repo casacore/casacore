08148764d3b2cfb3e135bd630489ae9497001dfc
Kumar Golap <kgolap@nrao.edu>
2004-09-01 00:41:02 +0000

    Fixed a problem with memory usage...make sure memory is available for in memory handling of arrays

--- code/msvis/implement/MSVis/SubMS.cc
+++ code/msvis/implement/MSVis/SubMS.cc
@@ -35,6 +35,7 @@
 #include <casa/Arrays/ArrayUtil.h>
 #include <casa/Logging/LogIO.h>
 #include <casa/OS/File.h>
+#include <casa/OS/HostInfo.h>
 #include <casa/Containers/Record.h>
 #include <casa/BasicSL/String.h>
 #include <casa/Utilities/Assert.h>
@@ -618,6 +619,19 @@ Bool SubMS::fillMainTable(const String& whichCol){
       for (uInt k=1; k < nchan_p.nelements(); ++k){
 	sameShape= sameShape && (nchan_p[k] == nchan_p[k-1]);      
       }
+    }
+
+    if(sameShape){
+      //Checking to make sure we have in memory capability else 
+      // use visbuffer
+      
+      Double datavol= mssel_p.nrow()*nchan_p[0]*npol_p[0]*sizeof(Complex);
+      Double memAvail= Double (HostInfo::memoryTotal())*(1024);
+      //Factoring in 30% for flags and other stuff
+      if ((datavol*1.3) >  memAvail)
+	sameShape = False;
+
+
     }
 
     if(sameShape){
