2b997cbd54c0cd68d4c732cbfe752ce648895c02
Wes Young <wyoung@nrao.edu>
2004-11-09 21:09:31 +0000

    Added Flagging module include

---
+++ code/flagging/implement/Flagging.h
@@ -0,0 +1,69 @@
+//# <Flagging.h>:  a module for Flagging
+//# Copyright (C) 2003
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//#! ========================================================================
+//# $Id$
+
+
+#if !defined (AIPS_MODULE_FLAGGING_H)
+#define AIPS_MODULE_FLAGGING_H
+
+// <module>
+//
+// <summary>
+// </summary>
+
+// <prerequisite>
+//   <li> SomeModule
+//   <li> SomeClass
+//   <li> some concept (perhaps with reference)
+// </prerequisite>
+//
+
+// <reviewed reviewer="" date="yyyy/mm/dd" demos="">
+// </reviewed>
+
+// <etymology>
+// </etymology>
+//
+// <synopsis>
+// </synopsis>
+//
+// <example>
+// </example>
+//
+// <motivation>
+// </motivation>
+
+// <todo asof="yyyy/mm/dd">
+//   <li> add this feature
+//   <li> fix this bug
+//   <li> discuss possible extension
+// </todo>
+
+// </module>
+
+#endif
+
