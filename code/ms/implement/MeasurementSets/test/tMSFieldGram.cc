85943ed038f25ebee18dd647fc1e71fdd74a13cb
David Debonis <ddebonis@naro.edu>
2005-01-13 23:56:00 +0000

    fix for unit test

--- code/ms/implement/MeasurementSets/test/tMSFieldGram.cc
+++ code/ms/implement/MeasurementSets/test/tMSFieldGram.cc
@@ -35,44 +35,29 @@
 
 #include <casa/namespace.h>
 
-//namespace casa { //# NAMESPACE CASA - BEGIN
-using namespace casa;
-
 int main(int argc, char **argv)
 {
   try {
     cout << "before ms constructor called " << endl;
-    const String msName = "5921.ms";
+    const String msName = "3C273XC1_tmp.ms";
     MeasurementSet ms(msName);
     MeasurementSet * mssel;
-    //Table sorted=ms.keywordSet().asTable("SORTED_TABLE");
     cout << "Original table has rows " << ms.nrow() << endl;
-    msFieldGramParseCommand(ms, "field='>0'");
-    cout << "check  rows from TableExprNode= " << MSSelection::msFieldTableExprNode->nrow() << endl;
+    msFieldGramParseCommand(ms, "field='<1'");
+    const TableExprNode *node = &msFieldGramParseNode();
+    cout << "TableExprNode has rows = " << node->nrow() << endl;
     Table tablesel(ms.tableName(), Table::Update);
-    mssel = new MeasurementSet(tablesel(*MSSelection::msFieldTableExprNode, MSSelection::msFieldTableExprNode->nrow() ));
+    mssel = new MeasurementSet(tablesel(*node, node->nrow() ));
     cout << "After mssel constructor called " << endl;
     mssel->rename(ms.tableName()+"/SELECTED_TABLE", Table::Scratch);
+    mssel->flush();
     if(mssel->nrow()==0){
-      cout << " Error, No data selected" << endl;
+      cout << "Check your input, No data selected" << endl;
     }
     else {
       cout << "selected table has rows " << mssel->nrow() << endl;
     }
     delete mssel;
-    // Second selection
-    msFieldGramParseCommand(ms, "field='<1'");
-    cout << "check  rows from TableExprNode= " << MSSelection::msFieldTableExprNode->nrow() << endl;
-    //    Table tablesel(ms.tableName(), Table::Update);
-    mssel = new MeasurementSet(tablesel(*MSSelection::msFieldTableExprNode, MSSelection::msFieldTableExprNode->nrow() ));
-    cout << "After mssel constructor called " << endl;
-    mssel->rename(ms.tableName()+"/SELECTED_TABLE", Table::Scratch);
-    if(mssel->nrow()==0){
-      cout << " Error, No data selected" << endl;
-    }
-    else {
-      cout << "selected table has rows " << mssel->nrow() << endl; 
-    }
   } catch (AipsError x) {
     cout << "ERROR: " << x.getMesg() << endl;
     return 1;
