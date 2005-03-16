98f8ab23c7779322700da178c9c217fa1f41bda9
Dongshan Guo <dguo@nrao.edu>
2005-03-16 20:37:44 +0000

    Field name wildcard selection complete

--- code/ms/implement/MeasurementSets/test/tMSFieldGram.cc
+++ code/ms/implement/MeasurementSets/test/tMSFieldGram.cc
@@ -37,17 +37,15 @@
 int main(int argc, char **argv)
 {
   try {
-    cout << "before ms constructor called " << endl;
-    const String msName = "3C273XC1_tmp.ms";
+    const String msName = "n1333.ms";
     MeasurementSet ms(msName);
     MeasurementSet * mssel;
     cout << "Original table has rows " << ms.nrow() << endl;
-    if(msFieldGramParseCommand(&ms, "0,1")==0) {
+    if(msFieldGramParseCommand(&ms, "'0328*'")==0) {
       const TableExprNode *node = msFieldGramParseNode();
       cout << "TableExprNode has rows = " << node->nrow() << endl;
       Table tablesel(ms.tableName(), Table::Update);
       mssel = new MeasurementSet(tablesel(*node, node->nrow() ));
-      cout << "After mssel constructor called " << endl;
       mssel->rename(ms.tableName()+"/SELECTED_TABLE", Table::Scratch);
       mssel->flush();
       if(mssel->nrow()==0){
