fce12fd80ed743195677210ee7aadeada6f6ba70
David Debonis <ddebonis@naro.edu>
2005-06-22 17:01:15 +0000

    script error when processing this header file fixed

--- code/fits/implement/FITS/hdu.h
+++ code/fits/implement/FITS/hdu.h
@@ -1135,7 +1135,7 @@ class AsciiTableExtension : public BinaryTableExtension {
 
 } //# NAMESPACE CASA - END
 
-# endif
 #ifndef AIPS_NO_TEMPLATE_SRC
 #include <fits/FITS/hdu.cc>
 #endif //# AIPS_NO_TEMPLATE_SRC
+# endif
