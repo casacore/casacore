b7384b0ffaac02c3ee04b036bb9a8725711bfc5b
Ger van Diepen <gervandiepen@gmail.com>
2005-10-10 12:06:42 +0000

    cxx2html could not handle :: before WCS class names

--- code/coordinates/implement/Coordinates/DirectionCoordinate.h
+++ code/coordinates/implement/Coordinates/DirectionCoordinate.h
@@ -39,9 +39,9 @@
 #include <casa/Quanta/RotMatrix.h>
 #include <wcslib/wcs.h>
 
-class ::celprm;
-class ::prjprm;
-class ::wcsprm;
+class celprm;
+class prjprm;
+class wcsprm;
 
 namespace casa { //# NAMESPACE CASA - BEGIN
 
