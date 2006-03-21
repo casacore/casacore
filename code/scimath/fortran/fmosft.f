55e18d71cdf2358f13991be40361ddd19e471472
Kumar Golap <kgolap@nrao.edu>
2006-03-21 02:23:16 +0000

    fix the gridder and weighting scheme...

--- code/scimath/fortran/fmosft.f
+++ code/scimath/fortran/fmosft.f
@@ -29,20 +29,21 @@
 C
 C Grid a number of visibility records
 C
-      subroutine gmosft (uvw, dphase, xypos, values, nvispol, nvischan,
+      subroutine gmosft (uvw, dphase, values, nvispol, nvischan,
      $     dopsf, flag, rflag, weight, nrow, rownum,
      $     scale, offset, grid, nx, ny, npol, nchan, freq, c,
      $     support, convsize, sampling, convfunc, 
      $     chanmap, polmap,
-     $     sumwt)
+     $     sumwt, weightgrid, convweight, doweightgrid)
 
       implicit none
       integer nx, ny, npol, nchan, nvispol, nvischan, nrow
       complex values(nvispol, nvischan, nrow)
       complex grid(nx, ny, npol, nchan)
+     
       double precision uvw(3, nrow), freq(nvischan), c, scale(3),
      $     offset(3)
-      double precision dphase(nrow), uvdist, xypos(2,nrow)
+      double precision dphase(nrow), uvdist
       double precision xlast, ylast
       complex phasor
       integer flag(nvispol, nvischan, nrow)
@@ -53,15 +54,18 @@ C
       integer support
       integer chanmap(nchan), polmap(npol)
       integer dopsf
+      complex weightgrid(nx, ny, npol, nchan)
+      integer doweightgrid
 
       complex nvalue
-
+      complex nweight
       integer convsize, sampling
       complex convfunc(convsize, convsize), cwt, crot
+      complex convweight(convsize, convsize)
 
       complex shiftx(-support:support), shifty(-support:support)
       complex sconv(-support:support, -support:support)
-      real sconv2(-support:support, -support:support)
+      complex sconv2(-support:support, -support:support)
       real sumsconv
       real sumsconv2
       real ratioofbeam
@@ -92,9 +96,8 @@ C
          iloc(2)=iy+convsize/2+1
          do ix=-support,support
             iloc(1)=ix+convsize/2+1
-            sconv(ix,iy)=conjg(convfunc(iloc(1), iloc(2)))
-            sconv2(ix,iy)=abs(sconv(ix,iy))
-            sumsconv2=sumsconv2+sconv2(ix,iy)
+            sconv(ix,iy)=(convfunc(iloc(1), iloc(2)))
+            sconv2(ix,iy)=convweight(iloc(1), iloc(2))
          end do
       end do
       doshift=.FALSE.
@@ -107,54 +110,13 @@ C
          rend=nrow
       end if
 
+
+
+
       xlast=0.0
       ylast=0.0
       do irow=rbeg, rend
-         if((xypos(1,irow).ne.xlast).or.(xypos(2,irow).ne.ylast)) then
-            xlast=xypos(1,irow)
-            ylast=xypos(2,irow)
-            doshift=.TRUE.
-            phase=-2.0D0*pi*xypos(1,irow)/nx
-            crot=cmplx(cos(phase), sin(phase))
-C            shiftx(0)=cmplx(1.0, 0.0)*phase/sin(phase)
-            shiftx(0)=cmplx(1.0, 0.0)
-            do ix=1,support
-               shiftx(ix)=shiftx(ix-1)*crot
-               shiftx(-ix)=conjg(shiftx(ix))
-               
-            end do
-            phase=-2.0D0*pi*xypos(2,irow)/ny
-            crot=cmplx(cos(phase), sin(phase))
-C            shifty(0)=cmplx(1.0, 0.0)*phase/sin(phase)
-            shifty(0)=cmplx(1.0, 0.0)
-            do iy=1,support
-               shifty(iy)=shifty(iy-1)*crot
-               shifty(-iy)=conjg(shifty(iy))
-            end do
-            sumsconv=0.0
-            if(sampling.eq.1) then
-               do iy=-support,support
-                  iloc(2)=iy+convsize/2+1
-                  do ix=-support,support
-                     iloc(1)=ix+convsize/2+1
-                     if(doshift) then
-                        sconv(ix,iy)=conjg(convfunc(iloc(1),
-     $                       iloc(2)))*shiftx(ix)*shifty(iy)
-                        sumsconv=sumsconv+real(sconv(ix,iy))
-                     else
-                        sconv(ix,iy)=conjg(convfunc(iloc(1),
-     $                       iloc(2)))
-                        sumsconv=sumsconv+real(sconv(ix,iy))
-                     end if
-                  end do
-               end do
-            end if
-         end if
-         if((sumsconv .lt. 0.0) .or. (sumsconv .gt. 0.0)) then
-            ratioofbeam=sumsconv2/sumsconv
-         else
-            ratioofbeam=1.0
-         end if
+
          if(rflag(irow).eq.0) then 
             do ichan=1, nvischan
                achan=chanmap(ichan)+1
@@ -175,6 +137,10 @@ C     rotate but we do want to reproject uvw
                               nvalue=weight(ichan,irow)*
      $                             (values(ipol,ichan,irow)*phasor)
                            end if
+                           if(doweightgrid .gt. 0) then
+                              nweight=cmplx(weight(ichan,irow))
+                           end if
+                           
 C     norm will be the value we would get for the peak
 C     at the phase center. We will want to normalize 
 C     the final image by this term.
@@ -187,7 +153,16 @@ C     the final image by this term.
      $                                   grid(loc(1)+ix,
      $                                   loc(2)+iy,apol,achan)+
      $                                   nvalue*sconv(ix,iy)
-                                    norm=norm+sconv2(ix,iy)
+
+                                    if(doweightgrid .gt. 0) then
+                                       iloc(1)=nx/2+1+ix
+                                       iloc(2)=ny/2+1+iy
+                                       weightgrid(iloc(1),iloc(2),
+     $                                  apol,achan)= weightgrid(
+     $                                  iloc(1),iloc(2),apol,achan)
+     $                                      + nweight*sconv2(ix,iy)
+
+                                    end if
                                  end do
                               end do
                            else 
@@ -210,11 +185,9 @@ C     the final image by this term.
      $                                nvalue*cwt
                                  norm=norm+real(cwt)
                               end do
-                           end if
-C                           sumwt(apol,achan)=sumwt(apol,achan)+
-C     $                          weight(ichan,irow)*norm*ratioofbeam
-                           sumwt(apol,achan)=sumwt(apol,achan)+
-     $                          weight(ichan,irow)*norm  
+                           end if  
+                           sumwt(apol, achan)= sumwt(apol,achan)+
+     $                             weight(ichan,irow)
                         end if
                      end do
                   end if
@@ -227,7 +200,7 @@ C     $                          weight(ichan,irow)*norm*ratioofbeam
 C
 C Degrid a number of visibility records
 C
-      subroutine dmosft (uvw, dphase, xypos, values, nvispol, nvischan,
+      subroutine dmosft (uvw, dphase, values, nvispol, nvischan,
      $     flag, rflag,
      $     nrow, rownum, scale, offset, grid, nx, ny, npol, nchan, freq,
      $     c, support, convsize, sampling, convfunc,
@@ -239,7 +212,7 @@ C
       complex grid(nx, ny, npol, nchan)
       double precision uvw(3, nrow), freq(nvischan), c, scale(2),
      $     offset(2)
-      double precision dphase(nrow), uvdist, xypos(2,nrow)
+      double precision dphase(nrow), uvdist
       double precision xlast, ylast
       complex phasor
       integer flag(nvispol, nvischan, nrow)
@@ -255,6 +228,8 @@ C
 
       complex shiftx(-support:support), shifty(-support:support)
       complex sconv(-support:support, -support:support)
+      real sconv2(-support:support, -support:support) 
+      real sumsconv2
 
       real norm, phase
 
@@ -275,11 +250,14 @@ C
          shiftx(ix)=1.0
          shifty(ix)=1.0
       end do
+      sumsconv2=0.0
       do iy=-support,support
          iloc(2)=iy+convsize/2+1
          do ix=-support,support
             iloc(1)=ix+convsize/2+1
             sconv(ix,iy)=conjg(convfunc(iloc(1), iloc(2)))
+C            sconv2(ix,iy)=abs(sconv(ix,iy))
+C            sumsconv2=sumsconv2+sconv2(ix,iy)
          end do
       end do
       doshift=.FALSE.
@@ -295,40 +273,7 @@ C
       xlast=0.0
       ylast=0.0
       do irow=rbeg, rend
-         if((xypos(1,irow).ne.xlast).or.(xypos(2,irow).ne.ylast)) then
-            xlast=xypos(1,irow)
-            ylast=xypos(2,irow)
-            doshift=.TRUE.
-            phase=-2.0D0*pi*xypos(1,irow)/nx
-            crot=cmplx(cos(phase), sin(phase))
-            shiftx(0)=cmplx(1.0, 0.0)
-            do ix=1,support
-               shiftx(ix)=shiftx(ix-1)*crot
-               shiftx(-ix)=conjg(shiftx(ix))
-            end do
-            phase=-2.0D0*pi*xypos(2,irow)/ny
-            crot=cmplx(cos(phase), sin(phase))
-            shifty(0)=cmplx(1.0, 0.0)
-            do iy=1,support
-               shifty(iy)=shifty(iy-1)*crot
-               shifty(-iy)=conjg(shifty(iy))
-            end do
-            if(sampling.eq.1) then
-               do iy=-support,support
-                  iloc(2)=iy+convsize/2+1
-                  do ix=-support,support
-                     iloc(1)=ix+convsize/2+1
-                     if(doshift) then
-                        sconv(ix,iy)=conjg(convfunc(iloc(1),
-     $                       iloc(2)))*shiftx(ix)*shifty(iy)
-                     else
-                        sconv(ix,iy)=conjg(convfunc(iloc(1),
-     $                       iloc(2)))
-                     end if
-                  end do
-               end do
-            end if
-         end if
+
          if(rflag(irow).eq.0) then
             do ichan=1, nvischan
                achan=chanmap(ichan)+1
@@ -341,10 +286,11 @@ C
                         if((flag(ipol,ichan,irow).ne.1).and.
      $                       (apol.ge.1).and.(apol.le.npol)) then
                            nvalue=0.0
+                           norm=0.0
                            if(sampling.eq.1) then
                               do iy=-support,support
                                  do ix=-support,support
-                                    nvalue=nvalue+conjg(sconv(ix,iy))*
+                                    nvalue=nvalue+(sconv(ix,iy))*
      $                                   grid(loc(1)+ix,loc(2)+iy,
      $                                   apol,achan)
                                  end do
@@ -368,9 +314,10 @@ C
      $                                   apol,achan)
                                  end do
                               end do
-                           end if
-                           values(ipol,ichan,irow)=nvalue*conjg(phasor)
-                        end if
+                           end if 
+                           values(ipol,ichan,irow)=nvalue*conjg(
+     $                         phasor)
+                       end if
                      end do
                   end if
                end if
