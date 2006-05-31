d48dc33c3ac11abdd993884c6a5851a168364864
Ger van Diepen <gervandiepen@gmail.com>
2006-05-31 13:16:07 +0000

    Work around cfitsio-3 problem which can define a wcs struct.

--- code/fits/implement/FITS/blockio.h
+++ code/fits/implement/FITS/blockio.h
@@ -30,9 +30,17 @@
 //# Include this file first, because it may set LFS variables used by cfitsio.
 #include <casa/aips.h>
 
+// Make sure that cfitsio does not declare the wcs headers.
 extern "C"{
-#include <fitsio.h>  // header file from cfitsio
-#include <fitsio2.h> // using core functions of cfitsio
+#ifdef WCSLIB_GETWCSTAB
+# include <fitsio.h>  // header file from cfitsio
+# include <fitsio2.h> // using core functions of cfitsio
+#else
+# define WCSLIB_GETWCSTAB
+# include <fitsio.h>  // header file from cfitsio
+# include <fitsio2.h> // using core functions of cfitsio
+# undef WCSLIB_GETWCSTAB
+#endif
 }
 
 #include <stdlib.h>
