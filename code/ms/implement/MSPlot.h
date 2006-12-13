8c5faeb609e19cc9e44b5b8b8cc8d0f077ce3a65
Ger van Diepen <gervandiepen@gmail.com>
2006-12-13 02:15:22 +0000

    Added missing module header

---
+++ code/ms/implement/MSPlot.h
@@ -0,0 +1,50 @@
+//# MSPlot.h: Plotting of MeasurementSet data
+//# Copyright (C) 2006
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//# $Id$
+
+#ifndef MS_MSPLOT_MODULE_H
+#define MS_MSPLOT_MODULE_H
+
+#include <ms/MSPlot/MsPlot.h>
+
+namespace casa { //# NAMESPACE CASA - BEGIN
+
+// <module>
+//
+// <summary>
+// Plotting of MeasurementSet data
+// </summary>
+
+// </module>
+
+
+} //# NAMESPACE CASA - END
+
+#endif
+
+
+
+
