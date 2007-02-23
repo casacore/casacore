172992ed14abeaa12cf155ea5bb061d99d5ce243
Malte Marquarding <Malte.Marquarding@csiro.au>
2007-02-23 00:20:48 +0000

    tidying up, use Prepend instead of Append

--- code/setup.py
+++ code/setup.py
@@ -1,19 +1,30 @@
 import sys
 import os
 
-def run_scons(targets):
+deps = {'casa' : None,
+	'tables': ['casa'],
+	'mirlib': ['casa'],
+	'scimath': ['casa'],
+	'measures': ['casa', 'tables', 'scimath']
+	}
+
+def run_scons(targets, extraarg=""):
     cwd = os.getcwd()
     for target in targets:
-        os.chdir(os.path.dirname(target))
+        os.chdir(target)
         command = "scons " #+ os.path.basename(target)
         # copy the command line args into the new command
         for arg in sys.argv[1:]:
-            command += " " + arg
+            command += " " + arg + extraarg
         print "Building package: " + target
         sys.stdout.flush()
 	print command
         os.system(command)
         sys.stdout.flush()
         os.chdir(cwd)
-
-run_scons(['casa/','tables/', 'mirlib/'])
+# always build casa with install target
+xarg = ""
+if "-h" not in sys.argv[1:] and "install" not in sys.argv[1:]:
+    xarg = "install"
+run_scons(['casa'], xarg)
+run_scons(['tables/', 'mirlib/'])
