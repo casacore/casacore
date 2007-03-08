46a2a7f838e0ad138d98b5dfd6f1e79d91bbfce0
Malte Marquarding <Malte.Marquarding@csiro.au>
2007-03-08 04:51:48 +0000

    added custom compiler settings to SConstruct files

--- casa/tools/utils.py
+++ casa/tools/utils.py
@@ -12,7 +12,7 @@ def generate(env):
         path = env.GetBuildPath('SConscript').replace('SConscript', '')	
 	if recursive:
 	    # remove '*' from pattern is accidentally specified
-	    pattern=pattern.replace("*","")
+	    pattern=pattern.replace("*", "")
 	    out = []
 	    for d, ld, fls in os.walk(path):
 		# remove directorys to be excluded 
@@ -28,9 +28,9 @@ def generate(env):
 	    return [ i.replace(path, '') for i in  glob.glob(path + pattern) ]
     env.SGlob = SGlob
 
-    def AddCustomPath(path=""):
-        if not len(path) or not os.path.exists(path):
-            return
+    def AddCustomPath(path=None):
+        if path is None or not os.path.exists(path):
+            Exit(1)
         env.PrependUnique(CPPPATH = [os.path.join(path, "include")])
         env.PrependUnique(LIBPATH = [os.path.join(path, "lib")])
     env.AddCustomPath = AddCustomPath
@@ -42,10 +42,27 @@ def generate(env):
 	return p + "_" + platform.machine()
     env.PlatformIdent = PlatformIdent
 
-
-#    env.SetupTests(rootdir):
-#	fp = Dir(rootdir).fullpath
-	
+    def CheckFortran(conf):
+	    
+	if not conf.env.has_key("FORTRAN"):
+	    # auto-detect fortran
+	    detect_fortran = conf.env.Detect(['gfortran', 'g77', 'f77'])
+	    conf.env["FORTRAN"] = detect_fortran
+	    fdict = {'gfortran': 'gfortran', 'g77': 'g2c', 'f77': 'f2c'}
+	    f2clib = conf.env.get("f2clib", fdict[detect_fortran])
+	    if not conf.CheckLib(f2clib):
+		Exit(1)
+	else:
+	    if not conf.env.has_key("f2clib"):
+		print "A custom fortran compiler also needs f2clib defined"
+		Exit(1)
+	    else:
+		if not conf.CheckLib(env["f2clib"]):
+		    Exit(1)
+	if conf.env["FORTRAN"].startswith("g77"):
+	    conf.env.Append(SHFORTRANFLAGS="-Wno-globals")
+	    conf.env.Append(FORTRANFLAGS="-Wno-globals")
+    env.CheckFortran = CheckFortran
 
     def null_action(target, source, env): return 0
 
