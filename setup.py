5504d60bedd077e85b22bd3c875d15575a9dbcf2
Malte Marquarding <Malte.Marquarding@csiro.au>
2007-03-16 03:24:10 +0000

    added run_tests target if tests=1 is set. This call casacore_assay. It only work from this top-level directory

--- setup.py
+++ setup.py
@@ -1,5 +1,28 @@
 import sys
 import os
+import re
+
+def run_tests(pkgname, prefix):
+    platfld = "LD_LIBRARY_PATH"
+    if  sys.platform == "darwin":
+	platfld = "DYLD_LIBRARY_PATH"
+    ldpth = os.environ.get(platfld)
+    os.environ[platfld] = os.path.join(prefix, "lib")
+    if ldpth is not None:
+	os.environ[platfld] += os.path.pathsep + ldpth
+    pth = os.environ.get("PATH")
+    os.environ["PATH"] = os.path.join(prefix, "bin")
+    if pth is not None:
+	os.environ["PATH"] += os.path.pathsep + pth
+    bdir = os.getcwd()
+    os.chdir(os.path.join(pkgname, "tests"))
+    os.environ["AIPSPATH"] = os.path.abspath(os.curdir)
+    # gather test program executables
+    fls = [ i for i in os.listdir(".") if i.startswith("t") ]
+    fls = filter(lambda i: not re.match(re.compile(".*\.\w+$"),i ), fls)
+    for f in fls:
+	status = os.system("casacore_assay ./%s" % f)
+    os.chdir(bdir)
 
 deps = {'casa' : None,
 	'tables': ['casa'],
@@ -40,16 +63,29 @@ def run_scons(targets, args=[]):
         os.chdir(target)
         command = "scons " #+ os.path.basename(target)
         # copy the command line args into the new command
+	pfx = None
+	tests = False
         for arg in args:
             command += " " + arg
+	    if arg.startswith("prefix"):
+		pfx = arg.split("=")[-1]
+	    if arg.startswith("tests") and arg.split("=")[-1]:
+		tests = True
         print "Building package: " + target
         sys.stdout.flush()
 	print command
-        failed = os.system(command)
+	try:
+	    failed = os.system(command)
+	except KeyboardInterrupt:
+	    sys.exit()
 	if failed:
 	    sys.exit(failed)
         sys.stdout.flush()
         os.chdir(cwd)
+	if pfx is not None and tests:
+	    print "Testing  package: " + target
+	    run_tests(target, pfx)
+
 
 args = sys.argv[1:]
 if "-h" not in args:
