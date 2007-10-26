6a3d41005542ac3bdfa9c7e30ae8844ccb3e3c70
Malte Marquarding <Malte.Marquarding@csiro.au>
2007-10-26 03:43:09 +0000

    removed _dbg suffix at Ger's request

--- casa/tools/buildenv.py
+++ casa/tools/buildenv.py
@@ -4,7 +4,6 @@ def generate(env):
 	lenv["build"] = buildtype
 	if buildtype == "dbg":
 	    lenv.AppendUnique(CPPFLAGS=["-g"])
-	    lenv["PACKAGE"] += "_dbg"
 	elif buildtype == "opt":
 	    lenv.AppendUnique(CPPFLAGS=["-O2"])
 	    lenv.AppendUnique(FORTRANFLAGS=["-O2"])	    
