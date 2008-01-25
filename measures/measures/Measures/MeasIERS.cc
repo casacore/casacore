c8db135676745e49122a8e2cf8f56ac50aca20b6
Ger van Diepen <gervandiepen@gmail.com>
2008-01-25 14:46:07 +0000

    Inserted space in error message

--- measures/measures/Measures/MeasIERS.cc
+++ measures/measures/Measures/MeasIERS.cc
@@ -93,7 +93,7 @@ Bool MeasIERS::get(Double &returnValue,
 			   String("fillMeas(MeasIERS::Files, Double)"),
 			   WHERE));
 	os << LogIO::WARN <<
-	  String("Requested JD") << date << String(" is outside "
+	  String("Requested JD ") << date << String(" is outside "
 		 "the IERS table data range"
 		 "\nCalculations will proceed with less precision") << 
 	  LogIO::POST;
@@ -294,7 +294,7 @@ Bool MeasIERS::getTable(Table &table, TableRecord &kws, ROTableRow &row,
 			 String("fillMeas(MeasIERS::Files, Double)"),
 			 WHERE));
       os << LogIO::WARN <<
-	String("Requested data table") << name <<
+	String("Requested data table ") << name <<
 	String(" cannot be found in the searched directories:\n");
       for (uInt i=0; i<searched.nelements(); ++i) os << searched[i] << "\n";
       os << LogIO::POST;
