385d471ef20a4f71c0be5e0853966e01ec89b124
Ger van Diepen <gervandiepen@gmail.com>
2008-05-19 13:36:41 +0000

    Use CASACORE_NO_AUTO_TEMPLATES instead of AIPS_NO_TEMPLATE_SRC

--- lattices/lattices/Lattices/HDF5Lattice.h
+++ lattices/lattices/Lattices/HDF5Lattice.h
@@ -253,8 +253,8 @@ namespace casa { //# NAMESPACE CASA - BEGIN
 
 } //# NAMESPACE CASA - END
 
-#ifndef AIPS_NO_TEMPLATE_SRC
+#ifndef CASACORE_NO_AUTO_TEMPLATES
 #include <lattices/Lattices/HDF5Lattice.tcc>
-#endif //# AIPS_NO_TEMPLATE_SRC
+#endif //# CASACORE_NO_AUTO_TEMPLATES
 #endif
 #endif
