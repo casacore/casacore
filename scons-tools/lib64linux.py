54aad4aae0ee996a8b948a837446c42f80f2a070
Malte Marquarding <Malte.Marquarding@csiro.au>
2014-06-16 12:16:11 +0000

    Remove scons build files

--- scons-tools/lib64linux.py
+++
@@ -1,8 +0,0 @@
-import platform
-
-def get_libdir():
-    if not platform.architecture()[0].startswith("64"):
-        return "lib"
-    dist = platform.dist()[0].lower()
-    distdict = dict(suse='lib64', redhat='lib64')   
-    return distdict.get(dist, 'lib')
