25cf3ccd057edce18f61a6f8f14335c0b2c6db56
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2015-06-06 14:33:18 +0200

    Add valgrind to travis build matrix

--- travis-deps.sh
+++ travis-deps.sh
@@ -12,7 +12,7 @@ if [ "$TRAVIS_OS_NAME" = linux ]; then
     sudo apt-get install -qy cmake flex bison libblas-dev liblapack-dev \
             libcfitsio3-dev wcslib-dev libfftw3-dev gfortran libncurses5-dev \
             libreadline6-dev libhdf5-serial-dev python-all-dev libboost-dev \
-            libboost-python-dev python-numpy
+            libboost-python-dev python-numpy valgrind
 
 elif [ "$TRAVIS_OS_NAME" = osx ]; then
     brew tap homebrew/science
