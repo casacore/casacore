8933efe56ab2594c3ba51b03d13069cd7db6e45d
vsuorant <vsuorant@nrao.edu>
2015-08-28 11:31:51 -0400

    SPW parser error handling improvements
    Handling of errors in SPW parser is via an error handler now. This
    brings uniform error reporting and allows changing the error reporting
    behaviour programmatically by the clients.
    
    Instead of throwing an exception for no-match for some components of
    the expression, the error is now reported as a warning.  If no
    components were matched, an exception is thrown.
    
    Added new class MSSSpwErrorHandler.

--- ms/MSSel/MSSpwParse.h
+++ ms/MSSel/MSSpwParse.h
@@ -32,6 +32,7 @@
 #include <casacore/casa/aips.h>
 #include <casacore/ms/MSSel/MSParse.h>
 #include <casacore/casa/Arrays/Matrix.h>
+#include <casacore/ms/MSSel/MSSelectionErrorHandler.h>
 
 namespace casacore { //# NAMESPACE CASACORE - BEGIN
 
@@ -115,11 +116,12 @@ public:
   // Get table expression node object.
   static const TableExprNode* node();
   static MSSpwParse* thisMSSParser;
+  static MSSelectionErrorHandler* thisMSSpwErrorHandler;
   static Vector<Int> selectedDDIDs() {return ddidList;}
   static Vector<Int> selectedIDs() {return idList;}
   static Matrix<Int> selectedChanIDs() {return chanList;}
   static void reset() {idList.resize(0);chanList.resize(0,0);ddidList.resize(0);};
-  static void cleanup() {if (node_p) delete node_p;node_p=0x0;};
+  static void cleanup() {if (node_p) delete node_p;node_p=0x0; if (thisMSSpwErrorHandler) delete thisMSSpwErrorHandler; thisMSSpwErrorHandler=0x0;}
 
   MSSpectralWindow& subTable() {return spwSubTable_p;}
 private:
