64a53c16936b2f3a6f770ddff1ccd0b11e4a8ed2
alxames <amesfoort@astron.nl>
2016-06-01 16:49:00 +0200

    Add -Wno-unknown-pragmas if OPENMP if OFF
    No need to wrap '#pragma omp ...' in ifdef anymore.
    If you wrap the include of omp.h, then it's still needed to also wrap omp_*() func calls.
    
    Remove now redundant ifdef _OPENMP wrapping in Utilities Sort code and in 2 Measures tests.

--- casa/Utilities/Sort.cc
+++ casa/Utilities/Sort.cc
@@ -346,10 +346,7 @@ uInt Sort::parSort (int nthr, uInt nrrec, uInt* inx) const
   int step = nrrec/nthr;
   for (int i=0; i<nthr; ++i) tinx[i] = i*step;
   tinx[nthr] = nrrec;
-  // Use ifdef to avoid compiler warning.
-#ifdef _OPENMP
 #pragma omp parallel for
-#endif
   for (int i=0; i<nthr; ++i) {
     int nparts = 1;
     index[tinx[i]] = tinx[i];
@@ -401,10 +398,7 @@ void Sort::merge (uInt* inx, uInt* tmp, uInt nrrec, uInt* index,
   // if the last part is in the same buffer.
   uInt* last = inx + index[np-1];
   while (np > 1) {
-  // Use ifdef to avoid compiler warning.
-#ifdef _OPENMP
 #pragma omp parallel for schedule(dynamic)
-#endif
     for (int i=0; i<np; i+=2) {
       if (i < np-1) {
         // Merge 2 subsequent parts of the array.
