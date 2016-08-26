6eacfcf04859b3a43449524342b83e4187aefa88
David Mehringer <dmehring@nrao.edu>
2016-08-26 09:33:59 -0400

    CAS-8918. Add first exposure times data structure to scans and subscans data structures

--- ms/MSOper/test/tMSMetaData.cc
+++ ms/MSOper/test/tMSMetaData.cc
@@ -1357,15 +1357,25 @@ void testIt(MSMetaData& md) {
             AlwaysAssert(res.size() == 1 && *res.begin() == 3, AipsError);
         }
         {
-            cout << "*** test getFirstExposureTimeMap()" << endl;
+            cout << "*** test getFirstExposureTimeMap() (deprecated)" << endl;
             vector<std::map<Int, Quantity> > mymap = md.getFirstExposureTimeMap();
             cout << "val " << mymap[0][30].getValue("s") << endl;
             cout << "val " << mymap[0][30] << endl;
-
             AlwaysAssert(near(mymap[0][30].getValue("s"), 1.152), AipsError);
             AlwaysAssert(near(mymap[10][17].getValue("s"), 1.008), AipsError)
-
             cout << "mymap " << mymap[10][17] << endl;
+        }
+        {
+            cout << "*** test getScanToFirstExposureTimeMap()" << endl;
+            map<ScanKey, MSMetaData::FirstExposureTimeMap> mymap
+                = md.getScanToFirstExposureTimeMap(False);
+            ScanKey scan;
+            scan.arrayID = 0;
+            scan.obsID = 0;
+            scan.scan = 30;
+            AlwaysAssert(near(mymap[scan][0].second.getValue("s"), 1.152), AipsError);
+            scan.scan = 17;
+            AlwaysAssert(near(mymap[scan][10].second.getValue("s"), 1.008), AipsError)
         }
         {
             cout << "*** test getUniqueFiedIDs()" << endl;
