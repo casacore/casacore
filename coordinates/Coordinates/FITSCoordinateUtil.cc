eb9a684f5485f6cc0b4c3b012320c720e2dec9ac
Dirk Petry <dpetry@eso.org>
2016-10-09 08:28:23 +0200

    Corrected indentation depth.

--- coordinates/Coordinates/FITSCoordinateUtil.cc
+++ coordinates/Coordinates/FITSCoordinateUtil.cc
@@ -1945,15 +1945,15 @@ namespace casacore { //# NAMESPACE CASACORE - BEGIN
 	// sanitize to avoid segfaults in fitshdr
 	Bool crlfwarned = False;
 	for(uInt i=0; i<strlen(pHeader)-1; i++){
-	  uInt headerchar = pHeader[i];
-	  if( (headerchar==10) || (headerchar==13) ){
-	    if( ! crlfwarned ){
-	      os << LogIO::WARN << "HEADER contains LF and/or CR characters!" << LogIO::POST;
-	      os << LogIO::WARN << "Will try to replace them by spaces ...." << LogIO::POST;
-	      crlfwarned = True;
+	    uInt headerchar = pHeader[i];
+	    if( (headerchar==10) || (headerchar==13) ){
+	        if( ! crlfwarned ){
+		    os << LogIO::WARN << "HEADER contains LF and/or CR characters!" << LogIO::POST;
+		    os << LogIO::WARN << "Will try to replace them by spaces ...." << LogIO::POST;
+		    crlfwarned = True;
+		}
+		pHeader[i] = 32;
 	    }
-	    pHeader[i] = 32;
-	  }
 	}
 
 	int nCards = strlen(pHeader) / 80;
@@ -1963,7 +1963,7 @@ namespace casacore { //# NAMESPACE CASACORE - BEGIN
 	int status = fitshdr (pHeader, nCards, nKeyIds, keyids, &nReject, &keys);
 	
 	if (status != 0) {
-	  throw(AipsError("Failed to extract non-coordinate cards from FITS header"));
+	    throw(AipsError("Failed to extract non-coordinate cards from FITS header"));
 	}
 	
 //
