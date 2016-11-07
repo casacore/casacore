0b3b091dce53eeff4119a96e797902eecdaa9669
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2016-11-07 12:43:30 +0100

    Travis OSX: Put dependencies of gcc in cache

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,13 +7,13 @@ if [ "$TRAVIS_OS_NAME" = osx ]; then
     cachefile="$HOME/.ccache/homebrew-cache.tar.gz"
     if [ -e "$cachefile" ]; then
         tar xf "$cachefile" --directory /usr/local/Cellar
-        brew link szip cfitsio wcslib fftw hdf5 ccache boost-python gcc
+        brew link szip cfitsio wcslib fftw hdf5 ccache boost-python gcc isl isl011 libmpc libmpc08
     else
         brew update >/dev/null
         brew tap homebrew/science
         brew install cfitsio wcslib fftw hdf5 ccache boost-python gcc
         ls /usr/local/Cellar
-        tar cfz "$cachefile" --directory /usr/local/Cellar szip cfitsio wcslib fftw hdf5 ccache boost-python gcc
+        tar cfz "$cachefile" --directory /usr/local/Cellar szip cfitsio wcslib fftw hdf5 ccache boost-python gcc isl isl011 libmpc libmpc08
     fi
     # 93M with gz, 85M with xz -9
     ls -lh "$cachefile"
