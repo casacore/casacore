1b6daa478def096c1651e8f13b90601efa9044e7
David Mehringer <dmehring@nrao.edu>
2017-06-07 14:20:30 -0400

    CAS-10016 create StatisticsAlgorithmFactory, which is a stats  algorithm configuration class that can be used as a common way to configure objects/applications that use the stats framework, thus reducing the need for the same or similar configuration code in multiple places.

---
+++ scimath/Mathematics/StatisticsAlgorithmFactory.h
@@ -0,0 +1,108 @@
+//# Copyright (C) 2000,2001
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This library is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU Library General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or (at your
+//# option) any later version.
+//#
+//# This library is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+//# License for more details.
+//#
+//# You should have received a copy of the GNU Library General Public License
+//# along with this library; if not, write to the Free Software Foundation,
+//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+
+#ifndef SCIMATH_STATSALGORITHMFACTORY_H
+#define SCIMATH_STATSALGORITHMFACTORY_H
+
+#include <casa/Utilities/CountedPtr.h>
+#include <scimath/Mathematics/FitToHalfStatisticsData.h>
+#include <scimath/Mathematics/NumericTraits.h>
+#include <scimath/Mathematics/StatisticsAlgorithm.h>
+#include <scimath/Mathematics/StatisticsData.h>
+
+namespace casacore {
+
+// Allows a common way for configuring and creating stats algorithm objects
+
+template <class AccumType, class DataIterator, class MaskIterator=const Bool *, class WeightsIterator=DataIterator>
+class StatisticsAlgorithmFactory {
+
+public:
+
+    struct FitToHalfData {
+        FitToHalfStatisticsData::CENTER center;
+        // fit to half data portion to use
+        FitToHalfStatisticsData::USE_DATA side;
+        // fit to half center value (only relevent if center=CVALUE)
+        AccumType centerValue;
+    };
+
+    struct ChauvenetData {
+        Double zScore;
+        Int maxIter;
+    };
+
+    // upon construction, the object is configured to use the classical stats algorithm
+    StatisticsAlgorithmFactory();
+
+    ~StatisticsAlgorithmFactory();
+
+    void configureClassical();
+
+    // configure to use fit to half algorithm.
+    void configureFitToHalf(
+            FitToHalfStatisticsData::CENTER centerType=FitToHalfStatisticsData::CMEAN,
+            FitToHalfStatisticsData::USE_DATA useData=FitToHalfStatisticsData::LE_CENTER,
+            AccumType centerValue=0
+    );
+
+    // configure to use hinges-fences algorithm
+    void configureHingesFences(Double f);
+
+    // configure to use Chauvenet's criterion
+    void configureChauvenet(Double zscore=-1, Int maxIterations=-1);
+
+    CountedPtr<StatisticsAlgorithm<CASA_STATP> > createStatsAlgorithm() const;
+
+    StatisticsData::ALGORITHM algorithm() const { return _algorithm; }
+
+    ChauvenetData chauvenetData() const;
+
+    Double hingesFencesFactor() const;
+
+    FitToHalfData fitToHalfData() const;
+
+    Record toRecord() const;
+
+    // create an object from a record
+    static StatisticsAlgorithmFactory<CASA_STATP> fromRecord(const Record& r);
+
+private:
+
+    StatisticsData::ALGORITHM _algorithm;
+    // hinges-fences f factor
+    Double _hf;
+    FitToHalfData _fitToHalfData;
+    ChauvenetData _chauvData;
+
+};
+
+}
+
+#ifndef CASACORE_NO_AUTO_TEMPLATES
+#include <casacore/scimath/Mathematics/StatisticsAlgorithmFactory.tcc>
+#endif
+
+#endif
