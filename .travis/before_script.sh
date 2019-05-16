d0b1948e7808feaf89284609a1e48e2a469e5c76
Gijs Molenaar <gijs@pythonic.nl>
2019-05-16 11:14:31 +0200

    fix path

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -12,7 +12,6 @@ if [ "$TRAVIS_OS_NAME" = osx ]; then
     if [ ! -f "$SOFA_ARCHIVE" ]; then
       wget http://www.iausofa.org/2015_0209_F/sofa_f-20150209_a.tar.gz -O $SOFA_ARCHIVE
     fi
-    tar -xzf $SOFA_ARCHIVE
     
     if [ ! -f "$MEASURES_ARCHIVE" ]; then
         wget ftp://ftp.astron.nl/outgoing/Measures/WSRT_Measures.ztar -O $MEASURES_ARCHIVE
@@ -23,7 +22,8 @@ if [ "$TRAVIS_OS_NAME" = osx ]; then
     mkdir -p build
     cd build
 
-   tar zxvf $MEASURES_ARCHIVE
+   tar zxvf ../$SOFA_ARCHIVE
+   tar zxvf ../$MEASURES_ARCHIVE
 
    ccache -M 80M
 
@@ -39,7 +39,7 @@ if [ "$TRAVIS_OS_NAME" = osx ]; then
         -DBOOST_PYTHON3_LIBRARY_NAME=python37 \
         -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH} \
         -DDATA_DIR=$PWD \
-        -DSOFA_ROOT_DIR=$HOME \
+        -DSOFA_ROOT_DIR=$PWD \
         -DCMAKE_INSTALL_PREFIX=${TRAVIS_BUILD_DIR}/installed
 else
     docker build . -f .travis/${DIST}_${CC}.docker -t casacore/${DIST}_${CC}
