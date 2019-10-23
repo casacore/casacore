7568353c3bd60d1344cd6d05021a9b568b8b9554
Rodrigo Tobar <rtobar@icrar.org>
2019-10-23 17:50:54 +0800

    Improving indentation
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- tables/DataMan/Adios2StMan.cc
+++ tables/DataMan/Adios2StMan.cc
@@ -225,7 +225,7 @@ Adios2StMan::impl::~impl()
 DataManager *Adios2StMan::impl::makeObject(const String &/*aDataManType*/,
                                      const Record &/*spec*/)
 {
-        return new Adios2StMan(itsMpiComm, itsAdiosEngineType,
+    return new Adios2StMan(itsMpiComm, itsAdiosEngineType,
                                itsAdiosEngineParams,
                                itsAdiosTransportParamsVec);
 }
