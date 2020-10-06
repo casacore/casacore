casacore/casacore

commit 7aeaaf16873e29abe362592aabef2661c7960173

Remove further unnecessary user-defined methods
The QVector class had a user-defined copy constructor that made the
generation of implicitly-defined copy assignment operator deprecated.
Removing the user-defined copy constructor and the user-defined
destructor, both of which do what their implicitly-defined version would
do, allows for the correct generation of the implicitly-defined copy
assignment operator.

While working on this I found that the Quantum and QBase classes also
had extra user-defined methods, so I removed them. Similarly, some of
the tables tests had unnecessary user-defined copy constructors that
resulted in the same type of warnings being issued.

Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

casa/Quanta/QBase.cc
@@ -35,9 +35,6 @@ namespace casacore { //# NAMESPACE CASACORE - BEGIN
 QBase::QBase() 
 : qUnit() {}
 
-QBase::QBase(const QBase &other) 
-: qUnit(other.qUnit) {}
-
 QBase::QBase(const Unit &s) 
 : qUnit(s) {}
 

casa/Quanta/QBase.h
@@ -84,8 +84,6 @@ public:
   //# Constructors
   // Default constructor, generates ""
   QBase();
-  // Copy constructor
-  QBase(const QBase &other);
   // Construct dimensioned QBase (e.g. 'km/Mpc')
   // <thrown>
   //   <li> AipsError if non-matching unit dimensions
@@ -96,10 +94,6 @@ public:
   
   // Destructor
   virtual ~QBase();
-  
-  //# Operators
-  // Assignment (copy)
-  QBase &operator=(const QBase &other);
   
   //# Member functions
   // Get units of QBase

casa/Quanta/QVector.h
@@ -73,10 +73,6 @@ template <class T> class QVector : public Quantum<Vector<T> > {
 	// to the same unit.
 	QVector(const Vector<Quantum<T> >& q);
 
-	// Copy constructor (deep copy)
-	QVector(const QVector& other);
-
-	~QVector();
 	// access single element
 	Quantum<T> operator[](uInt index) const;
 

casa/Quanta/QVector.tcc
@@ -57,10 +57,6 @@ template <class T> QVector<T>::QVector(const Vector<Quantum<T> >& q)
     this->setValue(copy);
 }
 
-template <class T> QVector<T>::QVector(const QVector<T>& other) : Quantum<Vector<T> >(other) {}
-
-template <class T> QVector<T>::~QVector() {}
-
 template <class T> Quantum<T> QVector<T>::operator[](uInt index) const {
 	return Quantum<T>(this->getValue()[index], this->getUnit());
 }

casa/Quanta/Quantum.h
@@ -276,8 +276,6 @@ template <class Qtype> class Quantum : public QBase{
   //# Constructors
   // Default constructor, generates '0'
   Quantum();
-  // Copy constructor (deep copy)
-  Quantum(const Quantum<Qtype> &other);
   // Construct undimensioned quantum (i.e. unit="")
   Quantum(const Qtype &factor);
   // Construct dimensioned quantum (e.g. '1.23 km/Mpc')
@@ -289,14 +287,6 @@ template <class Qtype> class Quantum : public QBase{
   // </group>
   // Construct quantum with unit copied from existing quantum
   Quantum(const Qtype &factor, const QBase &other);
-  
-  // Destructor
-  ~Quantum();
-
-  //# Operators
-  // Assignment (deep copy)
-  Quantum<Qtype> &operator=(const Quantum<Qtype> &other);
-  
   
   // Unary operations
   // <group>

casa/Quanta/Quantum.tcc
@@ -50,12 +50,6 @@ Quantum<Qtype>::Quantum() :
     QBase() { qVal = Qtype();}
 
 template <class Qtype>
-Quantum<Qtype>::Quantum(const Quantum<Qtype> &other) :
-    QBase(other) {
-  qVal = other.qVal;
-}
-
-template <class Qtype>
 Quantum<Qtype>::Quantum(const Qtype &factor) : 
   QBase() {
   qVal = factor;
@@ -73,19 +67,7 @@ Quantum<Qtype>::Quantum(const Qtype &factor, const QBase &other) :
   qVal = factor;
 }
 
-template <class Qtype>
-Quantum<Qtype>::~Quantum() {}
-
 //# Quantum operators
-
-template <class Qtype>
-Quantum<Qtype> &Quantum<Qtype>::operator=(const Quantum<Qtype> &other) {
-    if (this != &other) {
-      qVal=other.qVal;
-      qUnit=other.qUnit;
-    }
-    return *this;
-}
 
 template <class Qtype>
 const Quantum<Qtype> &Quantum<Qtype>::operator+() const{

tables/DataMan/test/dRetypedArrayEngine.h
@@ -69,7 +69,6 @@ class RetypedArrayEx1
 public:
     RetypedArrayEx1(): x_p(0), y_p(0) {}
     RetypedArrayEx1(float x, float y) : x_p(x), y_p(y) {}
-    RetypedArrayEx1(const RetypedArrayEx1& that): x_p(that.x_p), y_p(that.y_p) {}
     static String dataTypeId()
         { return "RetypedArrayEx1"; }
     static IPosition shape()

tables/DataMan/test/dVSCEngine.h
@@ -40,7 +40,6 @@ class VSCExample
 public:
     VSCExample(): x_p(0), y_p(0) {}
     VSCExample(Int x, float y, const String& z) : x_p(x), y_p(y), z_p(z) {}
-    VSCExample(const VSCExample& that): x_p(that.x_p), y_p(that.y_p), z_p(that.z_p) {}
     static String dataTypeId()
 	{ return "VSCExample"; }
     Int x() const

commit 45eaeaaee16a0ad25e28f48b0ad04de1b6070024

Add test around deep-copying of Vectors in Quantum
The semantics of the Quantum class are like any other value C++ class:
on copy construction and copy assignment a deep copy operation should
happen. This behavior was however inadvertently broken in 7aeaaf168, in
part because there was no such test running by default that would check
this behavior (there is tMSMetaData, but it requires extra data and
therefore is skipped automatically by the CI infrastructure).

This commit adds two new checks to the Quantum tests to double-check
that when Quantum objects are copied, their underlying Vector objects
are also deep copied. This will ensure that this behavior is checked
when tests are run, even if tMSMetaData doesn't execute.

These tests would have originally passed, are failing now due to the
changes introduced in 7aeaaf168.

Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

casa/Quanta/test/tQuantum.cc
@@ -308,6 +308,21 @@ try {
     	AlwaysAssert(near(q.getValue("MHz"), 99930.8, 1e-5), AipsError);
 
     }
+
+    // Test copy construction and copy assignment with Vector does deep copying
+    {
+        // Copy constructor
+        Quantum<Vector<Int>> original({1, 2, 3}, "m");
+        Quantum<Vector<Int>> copy_constructed(original);
+        copy_constructed.getValue()[0] = 100;
+        AlwaysAssert(original.getValue()[0] == 1, AipsError);
+
+        // Copy assignment
+        Quantum<Vector<Int>> copy_assigned;
+        copy_assigned = original;
+        copy_assigned.getValue()[0] = 100;
+        AlwaysAssert(original.getValue()[0] == 1, AipsError);
+    }
     cout << endl << "--------------------------" << endl;
     return 0;
 }