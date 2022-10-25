7abbba5ddcf1e4f662d4d55a640bfca87c8f0033
Ger van Diepen <diepen@astron.nl>
2022-10-25 20:05:38 +0200

    Add hash function to String

--- casa/BasicSL/String.h
+++ casa/BasicSL/String.h
@@ -1035,4 +1035,14 @@ inline ostream &operator<<(ostream &s, const String &x) {
 
 } //# NAMESPACE CASACORE - END
 
+
+// Define the hash function for String, so unordered_set<String> can be used.
+template<>
+struct std::hash<casacore::String>
+{
+  std::size_t operator()(casacore::String const& k) const noexcept
+    { return std::hash<std::string>()(k); }
+};
+
+
 #endif
