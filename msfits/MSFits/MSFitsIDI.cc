a8a6717a4eb0517522d2782a7542a29b257ac06b
Mark Kettenis <kettenis@jive.eu>
2023-02-21 11:41:24 +0100

    Revert "Use MODEL_COMPS table to determine zero-padding"
    This reverts commit 198571b0ca8f2382e2260f75c9a117f4827feb0f.

--- msfits/MSFits/MSFitsIDI.cc
+++ msfits/MSFits/MSFitsIDI.cc
@@ -291,7 +291,6 @@ void MSFitsIDI::readFITSFile(Bool& atEnd)
   // Correlator
   String correlat;
   Float vanVleck = 0.0;
-  Int zeroPad = 0;
 
   // Loop over all HDU in the FITS-IDI file
   Bool initFirstMain = True;
@@ -322,19 +321,12 @@ void MSFitsIDI::readFITSFile(Bool& atEnd)
 
     } else {
       // Process the FITS-IDI input from the position of this binary table
-      FITSIDItoMS1 bintab(infits, correlat, itsObsType, initFirstMain, vanVleck, zeroPad);
+      FITSIDItoMS1 bintab(infits, correlat, itsObsType, initFirstMain, vanVleck);
       initFirstMain = False;
       String hduName = bintab.extname();
       hduName = hduName.before(trailing);
       String tableName = itsMSOut;
       if (hduName != "") {
-	if (hduName == "MODEL_COMPS") {
-	  // Zero padding "factor"; a value of 0 implies no zero padding
-	  if (bintab.kw("ZERO_PAD")) {
-	    zeroPad = bintab.kw("ZERO_PAD")->asInt();
-	    os << LogIO::NORMAL << "ZeroPad: " << zeroPad << LogIO::POST;
-	  }
-	}
 	if (hduName != "UV_DATA") {
 	  tableName = tableName + "_tmp/" + hduName;
 	}
