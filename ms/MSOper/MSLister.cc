be65969210c79a217ef9ee5e5ea7258f9b1e2874
Ville <vsuorant@users.noreply.github.com>
2023-11-17 12:51:49 -0500

    Guard against log10(0) in MSLister.cc

--- ms/MSOper/MSLister.cc
+++ ms/MSOper/MSLister.cc
@@ -540,7 +540,12 @@ void MSLister::selectvis(const String& timerange,
 /// Calculate the max number of digits needed to print the values in an array
 template <class T> uInt maxDigitsToPrint(const Array<T> &values)
 {
-    return (uInt)max(1,(Int)rint(abs(log10(abs(max(values))))+0.5));
+    // Guard against log10(0).
+    if (max(values) == 0) {
+      return 1;
+    } else {
+      return (uInt)max(1,(Int)rint(abs(log10(abs(max(values))))+0.5));
+    }
 }
 
 void MSLister::listData(const int pageRows,
