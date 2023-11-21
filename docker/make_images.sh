a3a78ca7566f2369bb820ad5aa25659443068a81
Marcel Loose <loose@astron.nl>
2023-11-21 14:53:13 +0100

    Change name of docker images
    Changed the name of the generated docker images. It now contains the tag, instead of hard-coded `master`.

--- docker/make_images.sh
+++ docker/make_images.sh
@@ -1,8 +1,10 @@
-#!/bin/bash -ve
+#!/bin/bash -xe
 
 HERE=`dirname "$0"`
 cd $HERE/..
 
+tag=$(git describe --tags)
+
 for i in 37 38 39 310 311 312; do
-    docker build -f docker/py${i}_wheel.docker . -t quay.io/casacore/casacore:master_wheel${i}
+    docker build -f docker/py${i}_wheel.docker . -t quay.io/casacore/casacore:${tag}_wheel${i}
 done
