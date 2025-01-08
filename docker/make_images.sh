df6bf8c88a60f2b0d69da04d4bd17e3bfae4f44a
Tammo Jan Dijkema <dijkema@astron.nl>
2025-01-08 13:35:19 +0100

    Add 3.13, remove 3.7 and unicode stuff

--- docker/make_images.sh
+++ docker/make_images.sh
@@ -11,14 +11,12 @@ REPOSITORY=quay.io/casacore/casacore
 GIT_TAG=$(git describe --tags --dirty)
 
 py_major=3
-for py_minor in $(seq 7 12); do
+for py_minor in $(seq 8 13); do
     echo -e "\n\n******** Building wheel for python ${py_major}.${py_minor} ********\n"
     py_version=${py_major}${py_minor}
-    [ ${py_minor} -le 7 ] && py_unicode="m" || py_unicode=
     docker build \
         --build-arg PYMAJOR=${py_major} \
         --build-arg PYMINOR=${py_minor} \
-        --build-arg PYUNICODE=${py_unicode} \
         --build-arg THREADS=16 \
         --file ${SCRIPT_DIR}/py_wheel.docker \
         --tag ${REPOSITORY}:py${py_version}_${GIT_TAG} \
