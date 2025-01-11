77709ce3c4e08d898b3d9c7870246594aec645bd
André Offringa <offringa@astron.nl>
2025-01-11 11:04:25 +0100

    Make sure malloc.h gets included

--- casa/OS/Memory.cc
+++ casa/OS/Memory.cc
@@ -26,14 +26,9 @@
 //# MH 97/11/24 Stop attempt to use mallinfo from stdlib.h for HPUX
 #include <casacore/casa/aips.h>
 
-#if defined(__hpux__) && !defined(AIPS_NO_LEA_MALLOC)
-#define _STRUCT_MALLINFO
-#endif
-
 #include <casacore/casa/OS/Memory.h>
 #include <casacore/casa/OS/malloc.h>
 
-#if defined(AIPS_NO_LEA_MALLOC)
 #if defined(AIPS_DARWIN) || defined(AIPS_CRAY_PGI)
 #include <sys/time.h>
 #include <sys/resource.h>
@@ -42,7 +37,6 @@
 #endif
 #else
 #include <malloc.h>
-#endif
 #endif
 
 namespace casacore  { //#Begin namespace casa
