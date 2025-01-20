09661e4e8eefa4cdfc1c0b5dadc3c35696623bc8
André Offringa <offringa@gmail.com>
2025-01-20 15:28:11 +0100

    Review comment: document function

--- tables/AlternateMans/StokesIStManColumn.h
+++ tables/AlternateMans/StokesIStManColumn.h
@@ -31,12 +31,27 @@ void ExpandFromStokesI(T *data, size_t n) {
   }
 }
 
+/**
+ * Calculates for every set of 4 input values the Stokes-I values by
+ * doing out = 0.5 * (in_pp + in_qq), where pp/qq can be xx/yy or 
+ * ll/rr. If pq or qp is non-zero, an exception is thrown. 
+ * 
+ * If type T is bool, then out = in_pp || in_qq.
+ */
 template <typename T>
 inline T *TransformToStokesI(const T *input, char *buffer, size_t n) {
   for (size_t i = 0; i != n; ++i) {
     // Placement new is used, because the lifetime of type T needs
     // to be started.
     new (&buffer[i * sizeof(T)]) T((input[i * 4] + input[i * 4 + 3]) * T(0.5));
+    if(input[i*4+1] != T(0.0) || input[i*4+2] != T(0.0))
+      throw std::runtime_error(
+          "Stokes-I stman cannot store data for which the 2nd and 3rd "
+          "correlation are non-zero");
+    // While we could also check whether pp == qq, this is a bit more
+    // complicated because of rounding inaccuracies. The above check should
+    // catch the most crucial misuse of the stman, so a pp == qq check is not
+    // performed.
   }
   return reinterpret_cast<T *>(buffer);
 }
