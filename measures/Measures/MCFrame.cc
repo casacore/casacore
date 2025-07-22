51aca637073c1623fefacd16ad7f7d6f873a92c2
André Offringa <offringa@gmail.com>
2025-07-22 11:00:29 +0200

    MeasConverters don't need to be optional

--- measures/Measures/MCFrame.cc
+++ measures/Measures/MCFrame.cc
@@ -49,53 +49,53 @@ struct MCFrameImplementation {
   // Conversion to TDB time (due to some (for me) unsolvable dependency
   // errors)
   // not the proper MeasConvert* here)
-  std::optional<MeasConvert<MEpoch>> epConvTDB;
+  MeasConvert<MEpoch> epConvTDB;
   // TDB time
   std::optional<Double> epTDBp;
   // Conversion to UT1 time
-  std::optional<MeasConvert<MEpoch>> epConvUT1;
+  MeasConvert<MEpoch> epConvUT1;
   // UT1 time
   std::optional<Double> epUT1p;
   // Conversion to TT time
-  std::optional<MeasConvert<MEpoch>> epConvTT;
+  MeasConvert<MEpoch> epConvTT;
   // TT time
   std::optional<Double> epTTp;
   // Conversion to LAST time
-  std::optional<MeasConvert<MEpoch>> epConvLAST;
+  MeasConvert<MEpoch> epConvLAST;
   // LAST time
   std::optional<Double> epLASTp;
   // Conversion to ITRF longitude/latitude
-  std::optional<MeasConvert<MPosition>> posConvLong;
+  MeasConvert<MPosition> posConvLong;
   // Longitude
   Vector<Double> posLongp;
   // Position
   std::optional<MVPosition> posITRFp;
   // Conversion to geodetic longitude/latitude
-  std::optional<MeasConvert<MPosition>> posConvLongGeo;
+  MeasConvert<MPosition> posConvLongGeo;
   // Latitude
   Vector<Double> posLongGeop;
   // Position
   std::optional<MVPosition> posGeop;
   // Conversion to J2000
-  std::optional<MeasConvert<MDirection>> dirConvJ2000;
+  MeasConvert<MDirection> dirConvJ2000;
   // Longitude
   Vector<Double> j2000Longp;
   // J2000 coordinates
   std::optional<MVDirection> dirJ2000p;
   // Conversion to B1950
-  std::optional<MeasConvert<MDirection>> dirConvB1950;
+  MeasConvert<MDirection> dirConvB1950;
   // Longitude
   Vector<Double> b1950Longp;
   // B1950 coordinates
   std::optional<MVDirection> dirB1950p;
   // Conversion to apparent coordinates
-  std::optional<MeasConvert<MDirection>> dirConvApp;
+  MeasConvert<MDirection> dirConvApp;
   // Longitude
   Vector<Double> appLongp;
   // Apparent coordinates
   std::optional<MVDirection> dirAppp;
   // Conversion to LSR radial velocity
-  std::optional<MeasConvert<MRadialVelocity>> radConvLSR;
+  MeasConvert<MRadialVelocity> radConvLSR;
   // Radial velocity
   std::optional<Double> radLSRp;
 };
@@ -150,8 +150,7 @@ void MCFrame::resetComet() {
 Bool MCFrame::getTDB(Double &tdb, const MeasFrame& myf) {
   if (myf.epoch()) {
     if (!impl_->epTDBp) {
-      impl_->epTDBp = impl_->epConvTDB->operator()
-	(*dynamic_cast<const MVEpoch *const>(myf.epoch()->getData())).
+      impl_->epTDBp = impl_->epConvTDB(*dynamic_cast<const MVEpoch *const>(myf.epoch()->getData())).
 	getValue().get();
     }
     tdb = *impl_->epTDBp;
@@ -164,8 +163,7 @@ Bool MCFrame::getTDB(Double &tdb, const MeasFrame& myf) {
 Bool MCFrame::getUT1(Double &tdb, const MeasFrame& myf) {
   if (myf.epoch()) {
     if (!impl_->epUT1p) {
-      impl_->epUT1p = impl_->epConvUT1->operator()
-	(*dynamic_cast<const MVEpoch *const>(myf.epoch()->getData())).
+      impl_->epUT1p = impl_->epConvUT1(*dynamic_cast<const MVEpoch *const>(myf.epoch()->getData())).
 	getValue().get();
     }
     tdb = *impl_->epUT1p;
@@ -178,8 +176,7 @@ Bool MCFrame::getUT1(Double &tdb, const MeasFrame& myf) {
 Bool MCFrame::getTT(Double &tdb, const MeasFrame& myf) {
   if (myf.epoch()) {
     if (!impl_->epTTp) {
-      impl_->epTTp = impl_->epConvTT->operator()
-	(*dynamic_cast<const MVEpoch *const>(myf.epoch()->getData())).
+      impl_->epTTp = impl_->epConvTT(*dynamic_cast<const MVEpoch *const>(myf.epoch()->getData())).
 	getValue().get();
     }
     tdb = *impl_->epTTp;
@@ -192,8 +189,7 @@ Bool MCFrame::getTT(Double &tdb, const MeasFrame& myf) {
 Bool MCFrame::getLong(Double &tdb, const MeasFrame& myf) {
   if (myf.position()) {
     if (impl_->posLongp.empty()) {
-      impl_->posITRFp = impl_->posConvLong->operator()
-	(*dynamic_cast<const MVPosition *const>(myf.position()->getData())).
+      impl_->posITRFp = impl_->posConvLong(*dynamic_cast<const MVPosition *const>(myf.position()->getData())).
 	getValue();
       impl_->posLongp = impl_->posITRFp->get();
     }
@@ -207,8 +203,7 @@ Bool MCFrame::getLong(Double &tdb, const MeasFrame& myf) {
 Bool MCFrame::getLat(Double &tdb, const MeasFrame& myf) {
   if (myf.position()) {
     if (impl_->posLongp.empty()) {
-      impl_->posITRFp = impl_->posConvLong->operator()
-	(*dynamic_cast<const MVPosition *const>(myf.position()->getData())).
+      impl_->posITRFp = impl_->posConvLong(*dynamic_cast<const MVPosition *const>(myf.position()->getData())).
 	getValue();
       impl_->posLongp = impl_->posITRFp->get();
     }
@@ -222,8 +217,7 @@ Bool MCFrame::getLat(Double &tdb, const MeasFrame& myf) {
 Bool MCFrame::getLatGeo(Double &tdb, const MeasFrame& myf) {
   if (myf.position()) {
     if (impl_->posLongGeop.empty()) {
-      impl_->posGeop = impl_->posConvLongGeo->operator()
-        (*dynamic_cast<const MVPosition *const>(myf.position()->getData())).
+      impl_->posGeop = impl_->posConvLongGeo(*dynamic_cast<const MVPosition *const>(myf.position()->getData())).
         getValue();
       impl_->posLongGeop = impl_->posGeop->get();
     }
@@ -237,8 +231,7 @@ Bool MCFrame::getLatGeo(Double &tdb, const MeasFrame& myf) {
 Bool MCFrame::getITRF(MVPosition &tdb, const MeasFrame& myf) {
   if (myf.position()) {
     if (impl_->posLongp.empty()) {
-      impl_->posITRFp = impl_->posConvLong->operator()
-	(*dynamic_cast<const MVPosition *const>(myf.position()->getData())).
+      impl_->posITRFp = impl_->posConvLong(*dynamic_cast<const MVPosition *const>(myf.position()->getData())).
 	getValue();
       impl_->posLongp = impl_->posITRFp->get();
     }
@@ -252,8 +245,7 @@ Bool MCFrame::getITRF(MVPosition &tdb, const MeasFrame& myf) {
 Bool MCFrame::getRadius(Double &tdb, const MeasFrame& myf) {
   if (myf.position()) {
     if (impl_->posLongp.empty()) {
-      impl_->posITRFp = impl_->posConvLong->operator()
-	(*dynamic_cast<const MVPosition *const>(myf.position()->getData())).
+      impl_->posITRFp = impl_->posConvLong(*dynamic_cast<const MVPosition *const>(myf.position()->getData())).
 	getValue();
       impl_->posLongp = impl_->posITRFp->get();
     }
@@ -267,8 +259,7 @@ Bool MCFrame::getRadius(Double &tdb, const MeasFrame& myf) {
 Bool MCFrame::getLAST(Double &tdb, const MeasFrame& myf) {
   if (myf.epoch()) {
     if (!impl_->epLASTp) {
-      impl_->epLASTp = impl_->epConvLAST->operator()
-	(*dynamic_cast<const MVEpoch *const>(myf.epoch()->getData())).
+      impl_->epLASTp = impl_->epConvLAST(*dynamic_cast<const MVEpoch *const>(myf.epoch()->getData())).
 	getValue().get();
     }
     tdb = fmod(*impl_->epLASTp, 1.0);
@@ -287,8 +278,7 @@ Bool MCFrame::getLASTr(Double &tdb, const MeasFrame& myf) {
 Bool MCFrame::getJ2000Long(Double &tdb, const MeasFrame& myf) {
   if (myf.direction()) {
     if (impl_->j2000Longp.empty()) {
-      impl_->dirJ2000p = impl_->dirConvJ2000->operator()
-	(*dynamic_cast<const MVDirection *const>(myf.direction()->getData())).
+      impl_->dirJ2000p = impl_->dirConvJ2000(*dynamic_cast<const MVDirection *const>(myf.direction()->getData())).
 	getValue();
       impl_->j2000Longp = impl_->dirJ2000p->get();
     }
@@ -302,8 +292,7 @@ Bool MCFrame::getJ2000Long(Double &tdb, const MeasFrame& myf) {
 Bool MCFrame::getJ2000Lat(Double &tdb, const MeasFrame& myf) {
   if (myf.direction()) {
     if (impl_->j2000Longp.empty()) {
-      impl_->dirJ2000p = impl_->dirConvJ2000->operator()
-	(*dynamic_cast<const MVDirection *const>(myf.direction()->getData())).
+      impl_->dirJ2000p = impl_->dirConvJ2000(*dynamic_cast<const MVDirection *const>(myf.direction()->getData())).
 	getValue();
       impl_->j2000Longp = impl_->dirJ2000p->get();
     }
@@ -317,8 +306,7 @@ Bool MCFrame::getJ2000Lat(Double &tdb, const MeasFrame& myf) {
 Bool MCFrame::getJ2000(MVDirection &tdb, const MeasFrame& myf) {
   if (myf.direction()) {
     if (impl_->j2000Longp.empty()) {
-      impl_->dirJ2000p = impl_->dirConvJ2000->operator()
-	(*dynamic_cast<const MVDirection *const>(myf.direction()->getData())).
+      impl_->dirJ2000p = impl_->dirConvJ2000(*dynamic_cast<const MVDirection *const>(myf.direction()->getData())).
 	getValue();
       impl_->j2000Longp = impl_->dirJ2000p->get();
     }
@@ -332,8 +320,7 @@ Bool MCFrame::getJ2000(MVDirection &tdb, const MeasFrame& myf) {
 Bool MCFrame::getB1950Long(Double &tdb, const MeasFrame& myf) {
   if (myf.direction()) {
     if (impl_->b1950Longp.empty()) {
-      impl_->dirB1950p = impl_->dirConvB1950->operator()
-	(*dynamic_cast<const MVDirection *const>(myf.direction()->getData())).
+      impl_->dirB1950p = impl_->dirConvB1950(*dynamic_cast<const MVDirection *const>(myf.direction()->getData())).
 	getValue();
       impl_->b1950Longp = impl_->dirB1950p->get();
     }
@@ -347,8 +334,7 @@ Bool MCFrame::getB1950Long(Double &tdb, const MeasFrame& myf) {
 Bool MCFrame::getB1950Lat(Double &tdb, const MeasFrame& myf) {
   if (myf.direction()) {
     if (impl_->b1950Longp.empty()) {
-      impl_->dirB1950p = impl_->dirConvB1950->operator()
-	(*dynamic_cast<const MVDirection *const>(myf.direction()->getData())).
+      impl_->dirB1950p = impl_->dirConvB1950(*dynamic_cast<const MVDirection *const>(myf.direction()->getData())).
 	getValue();
       impl_->b1950Longp = impl_->dirB1950p->get();
     }
@@ -362,8 +348,7 @@ Bool MCFrame::getB1950Lat(Double &tdb, const MeasFrame& myf) {
 Bool MCFrame::getB1950(MVDirection &tdb, const MeasFrame& myf) {
   if (myf.direction()) {
     if (impl_->b1950Longp.empty()) {
-      impl_->dirB1950p = impl_->dirConvB1950->operator()
-	(*dynamic_cast<const MVDirection *const>(myf.direction()->getData())).
+      impl_->dirB1950p = impl_->dirConvB1950(*dynamic_cast<const MVDirection *const>(myf.direction()->getData())).
 	getValue();
       impl_->b1950Longp = impl_->dirB1950p->get();
     }
@@ -377,8 +362,7 @@ Bool MCFrame::getB1950(MVDirection &tdb, const MeasFrame& myf) {
 Bool MCFrame::getAppLong(Double &tdb, const MeasFrame& myf) {
   if (myf.direction()) {
     if (impl_->appLongp.empty()) {
-      impl_->dirAppp = impl_->dirConvApp->operator()
-	(*dynamic_cast<const MVDirection *const>(myf.direction()->getData())).
+      impl_->dirAppp = impl_->dirConvApp(*dynamic_cast<const MVDirection *const>(myf.direction()->getData())).
 	getValue();
       impl_->appLongp = impl_->dirAppp->get();
     }
@@ -392,8 +376,7 @@ Bool MCFrame::getAppLong(Double &tdb, const MeasFrame& myf) {
 Bool MCFrame::getAppLat(Double &tdb, const MeasFrame& myf) {
   if (myf.direction()) {
     if (impl_->appLongp.empty()) {
-      impl_->dirAppp = impl_->dirConvApp->operator()
-	(*dynamic_cast<const MVDirection *const>(myf.direction()->getData())).
+      impl_->dirAppp = impl_->dirConvApp(*dynamic_cast<const MVDirection *const>(myf.direction()->getData())).
 	getValue();
       impl_->appLongp = impl_->dirAppp->get();
     }
@@ -407,8 +390,7 @@ Bool MCFrame::getAppLat(Double &tdb, const MeasFrame& myf) {
 Bool MCFrame::getApp(MVDirection &tdb, const MeasFrame& myf) {
   if (myf.direction()) {
     if (impl_->appLongp.empty()) {
-      impl_->dirAppp = impl_->dirConvApp->operator()
-	(*dynamic_cast<const MVDirection *const>(myf.direction()->getData())).
+      impl_->dirAppp = impl_->dirConvApp(*dynamic_cast<const MVDirection *const>(myf.direction()->getData())).
 	getValue();
       impl_->appLongp = impl_->dirAppp->get();
     }
@@ -422,8 +404,7 @@ Bool MCFrame::getApp(MVDirection &tdb, const MeasFrame& myf) {
 Bool MCFrame::getLSR(Double &tdb, const MeasFrame& myf) {
   if (myf.radialVelocity()) {
     if (!impl_->radLSRp) {
-      impl_->radLSRp = impl_->radConvLSR->operator()
-	(*dynamic_cast<const MVRadialVelocity *const>(myf.radialVelocity()->
+      impl_->radLSRp = impl_->radConvLSR(*dynamic_cast<const MVRadialVelocity *const>(myf.radialVelocity()->
 						      getData())).
 	getValue();
     }
@@ -462,7 +443,6 @@ void MCFrame::makeEpoch(const MeasFrame& myf) {
   impl_->epTDBp.reset();
   impl_->epUT1p.reset();
   impl_->epTTp.reset();
-  impl_->epConvLAST.reset();
   impl_->epConvLAST = MEpoch::Convert(*(myf.epoch()),
 				   MEpoch::Ref(MEpoch::LAST, myf));
   impl_->epLASTp.reset();
@@ -474,7 +454,7 @@ void MCFrame::makeEpoch(const MeasFrame& myf) {
 void MCFrame::makePosition(const MeasFrame& myf) {
   static const MPosition::Ref REFLONG 
     = MPosition::Ref(MPosition::ITRF);
-  impl_->posConvLong = MPosition::Convert(*(myf.position()),
+  impl_->posConvLong = MPosition::Convert(*myf.position(),
 				       REFLONG);
   impl_->posLongp = casacore::Vector<Double>();
   impl_->posITRFp.reset();
@@ -482,7 +462,7 @@ void MCFrame::makePosition(const MeasFrame& myf) {
   impl_->radLSRp.reset();
   static const MPosition::Ref REFGEO
     = MPosition::Ref(MPosition::WGS84);
-  impl_->posConvLongGeo = MPosition::Convert(*(myf.position()),
+  impl_->posConvLongGeo = MPosition::Convert(*myf.position(),
 					  REFGEO);
   impl_->posLongGeop = casacore::Vector<Double>();
   impl_->posGeop.reset();
@@ -490,15 +470,15 @@ void MCFrame::makePosition(const MeasFrame& myf) {
 
 void MCFrame::makeDirection(const MeasFrame& myf) {
   static const MDirection::Ref REFJ2000 = MDirection::Ref(MDirection::J2000);
-  impl_->dirConvJ2000 = MDirection::Convert(*(myf.direction()),
+  impl_->dirConvJ2000 = MDirection::Convert(*myf.direction(),
 					 MDirection::Ref(MDirection::J2000,
 							 myf));
 
   static const MDirection::Ref REFB1950 = MDirection::Ref(MDirection::B1950);
-  impl_->dirConvB1950 = MDirection::Convert(*(myf.direction()),
+  impl_->dirConvB1950 = MDirection::Convert(*myf.direction(),
 					 MDirection::Ref(MDirection::B1950,
 							 myf));
-  impl_->dirConvApp = MDirection::Convert(*(myf.direction()),
+  impl_->dirConvApp = MDirection::Convert(*myf.direction(),
 				       MDirection::Ref(MDirection::APP,
 						       myf));
   impl_->j2000Longp = casacore::Vector<Double>();
@@ -513,7 +493,7 @@ void MCFrame::makeDirection(const MeasFrame& myf) {
 void MCFrame::makeRadialVelocity(const MeasFrame& myf) {
   static const MRadialVelocity::Ref REFLSR 
     = MRadialVelocity::Ref(MRadialVelocity::LSRK);
-  impl_->radConvLSR = MRadialVelocity::Convert(*(myf.radialVelocity()),
+  impl_->radConvLSR = MRadialVelocity::Convert(*myf.radialVelocity(),
 					    REFLSR);
   impl_->radLSRp.reset();
 }
