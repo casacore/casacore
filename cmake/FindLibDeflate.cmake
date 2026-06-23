0a19f5c3edc3cc299bd68213279bd4401077d58b
Marcel Loose <loose@astron.nl>
2026-06-23 17:40:58 +0200

    Reformat Find module
    Reformatted `FindLibDeflate.cmake` using `cmake-format`, ignoring the comment header.

--- cmake/FindLibDeflate.cmake
+++ cmake/FindLibDeflate.cmake
@@ -9,25 +9,31 @@
 
 if(NOT LibDeflate_FOUND)
 
-  find_path(LibDeflate_INCLUDE_DIR NAMES libdeflate.h 
+  find_path(
+    LibDeflate_INCLUDE_DIR
+    NAMES libdeflate.h
     HINTS ${LibDeflate_ROOT_DIR}/include)
-  find_library(LibDeflate_LIBRARY NAMES deflate libdeflate 
+  find_library(
+    LibDeflate_LIBRARY
+    NAMES deflate libdeflate
     HINTS ${LibDeflate_ROOT_DIR}/lib)
 
   if(LibDeflate_INCLUDE_DIR AND EXISTS "${LibDeflate_INCLUDE_DIR}/libdeflate.h")
-    file(STRINGS "${LibDeflate_INCLUDE_DIR}/libdeflate.h" _version_line 
-      REGEX "^#define[\t ]+LIBDEFLATE_VERSION_STRING[\t ]+\".*\"")
-    string(REGEX REPLACE "^.*LIBDEFLATE_VERSION_STRING[\t ]+\"([^\"]*)\".*$" "\\1" _version_string "${_version_line}")
+    file(STRINGS "${LibDeflate_INCLUDE_DIR}/libdeflate.h" _version_line
+         REGEX "^#define[\t ]+LIBDEFLATE_VERSION_STRING[\t ]+\".*\"")
+    string(REGEX REPLACE "^.*LIBDEFLATE_VERSION_STRING[\t ]+\"([^\"]*)\".*$"
+                         "\\1" _version_string "${_version_line}")
   endif()
-
-  set(LibDeflate_VERSION ${_version_string} CACHE STRING "Version of libdeflate found" FORCE)
+  set(LibDeflate_VERSION
+      ${_version_string}
+      CACHE STRING "Version of libdeflate found" FORCE)
 
   mark_as_advanced(LibDeflate_INCLUDE_DIR LibDeflate_LIBRARY LibDeflate_VERSION)
 
   include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(LibDeflate
-      REQUIRED_VARS LibDeflate_LIBRARY LibDeflate_INCLUDE_DIR
-      VERSION_VAR LibDeflate_VERSION
-  )
+  find_package_handle_standard_args(
+    LibDeflate
+    REQUIRED_VARS LibDeflate_LIBRARY LibDeflate_INCLUDE_DIR
+    VERSION_VAR LibDeflate_VERSION)
 
 endif(NOT LibDeflate_FOUND)
