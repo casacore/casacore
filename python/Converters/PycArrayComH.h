777efd4f331ae6bd290968bb1be7cf5282c855cd
André Offringa <offringa@astron.nl>
2026-09-19 15:07:50 +0200

    Format python module

--- python/Converters/PycArrayComH.h
+++ python/Converters/PycArrayComH.h
@@ -1,89 +1,80 @@
-//# PycArrayComH.h: Common code to convert an Array to/from a Python array
-//# Copyright (C) 2006
-//# Associated Universities, Inc. Washington DC, USA.
-//#
-//# This library is free software; you can redistribute it and/or modify it
-//# under the terms of the GNU Library General Public License as published by
-//# the Free Software Foundation; either version 2 of the License, or (at your
-//# option) any later version.
-//#
-//# This library is distributed in the hope that it will be useful, but WITHOUT
-//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
-//# License for more details.
-//#
-//# You should have received a copy of the GNU Library General Public License
-//# along with this library; if not, write to the Free Software Foundation,
-//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
-//#
-//# Correspondence concerning AIPS++ should be addressed as follows:
-//#        Internet email: casa-feedback@nrao.edu.
-//#        Postal address: AIPS++ Project Office
-//#                        National Radio Astronomy Observatory
-//#                        520 Edgemont Road
-//#                        Charlottesville, VA 22903-2475 USA
+// # PycArrayComH.h: Common code to convert an Array to/from a Python array
+// # Copyright (C) 2006
+// # Associated Universities, Inc. Washington DC, USA.
+// #
+// # This library is free software; you can redistribute it and/or modify it
+// # under the terms of the GNU Library General Public License as published by
+// # the Free Software Foundation; either version 2 of the License, or (at your
+// # option) any later version.
+// #
+// # This library is distributed in the hope that it will be useful, but WITHOUT
+// # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// # FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+// # License for more details.
+// #
+// # You should have received a copy of the GNU Library General Public License
+// # along with this library; if not, write to the Free Software Foundation,
+// # Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+// #
+// # Correspondence concerning AIPS++ should be addressed as follows:
+// #        Internet email: casa-feedback@nrao.edu.
+// #        Postal address: AIPS++ Project Office
+// #                        National Radio Astronomy Observatory
+// #                        520 Edgemont Road
+// #                        Charlottesville, VA 22903-2475 USA
 
-  // Check if the PyObject is an array object.
-  Bool PycArrayCheck (PyObject* obj_ptr);
+// Check if the PyObject is an array object.
+Bool PycArrayCheck(PyObject* obj_ptr);
 
-  // Check if the API is or can be imported.
-  // <group>
-  Bool isImported();
-  inline Bool canImport()
-    { return True; }
-  Bool importArray();
-  void loadAPI();
-  // </group>
+// Check if the API is or can be imported.
+// <group>
+Bool isImported();
+inline Bool canImport() { return True; }
+Bool importArray();
+void loadAPI();
+// </group>
 
-  // Convert the python array to a Casacore array in the ValueHolder.
-  // If copyData is True, the array data is always copied.
-  // Otherwise only if needed.
-  ValueHolder makeArray (PyObject* obj_ptr, Bool copyData);
+// Convert the python array to a Casacore array in the ValueHolder.
+// If copyData is True, the array data is always copied.
+// Otherwise only if needed.
+ValueHolder makeArray(PyObject* obj_ptr, Bool copyData);
 
-  // Copy/convert the array data as needed.
-  // Specializations are defined for complex and string.
-  // <group>
-  template <typename T> struct ArrayCopy
-  {
-    static void toPy (void* to, const T* from, size_t nr);
-    static void fromPy (T* to, const void* from, size_t nr);
-    static Array<T> toArray (const IPosition& shape,
-			     void* data, bool copy);
-  };
+// Copy/convert the array data as needed.
+// Specializations are defined for complex and string.
+// <group>
+template <typename T>
+struct ArrayCopy {
+  static void toPy(void* to, const T* from, size_t nr);
+  static void fromPy(T* to, const void* from, size_t nr);
+  static Array<T> toArray(const IPosition& shape, void* data, bool copy);
+};
 
-  template <> struct ArrayCopy<Complex>
-  {
-    static void toPy (void* to, const Complex* from, size_t nr);
-    static void fromPy (Complex* to, const void* from, size_t nr);
-    static Array<Complex> toArray (const IPosition& shape,
-				   void* data, bool copy);
-  };
+template <>
+struct ArrayCopy<Complex> {
+  static void toPy(void* to, const Complex* from, size_t nr);
+  static void fromPy(Complex* to, const void* from, size_t nr);
+  static Array<Complex> toArray(const IPosition& shape, void* data, bool copy);
+};
 
-  template <> struct ArrayCopy<DComplex>
-  {
-    static void toPy (void* to, const DComplex* from, size_t nr);
-    static void fromPy (DComplex* to, const void* from, size_t nr);
-    static Array<DComplex> toArray (const IPosition& shape,
-				    void* data, bool copy);
-  };
+template <>
+struct ArrayCopy<DComplex> {
+  static void toPy(void* to, const DComplex* from, size_t nr);
+  static void fromPy(DComplex* to, const void* from, size_t nr);
+  static Array<DComplex> toArray(const IPosition& shape, void* data, bool copy);
+};
 
-  template <> struct ArrayCopy<String>
-  {
-    static void toPy (void* to, const String* from, size_t nr);
-    static void fromPy (String* to, const void* from, size_t nr);
-    static Array<String> toArray (const IPosition& shape,
-				  void* data, bool copy);
-  };
-  // </group>
+template <>
+struct ArrayCopy<String> {
+  static void toPy(void* to, const String* from, size_t nr);
+  static void fromPy(String* to, const void* from, size_t nr);
+  static Array<String> toArray(const IPosition& shape, void* data, bool copy);
+};
+// </group>
 
-  Array<String> ArrayCopyStr_toArray (const IPosition& shape,
-				      void* data, size_t slen);
+Array<String> ArrayCopyStr_toArray(const IPosition& shape, void* data, size_t slen);
 
-  Array<String> ArrayCopyUnicode_toArray (const IPosition& shape,
-					  void* data, size_t slen);
+Array<String> ArrayCopyUnicode_toArray(const IPosition& shape, void* data, size_t slen);
 
-  // Convert a Casacore array to a Python array object.
-  template <typename T>
-  boost::python::object makePyArrayObject (casacore::Array<T> const& arr);
-
-
+// Convert a Casacore array to a Python array object.
+template <typename T>
+boost::python::object makePyArrayObject(casacore::Array<T> const& arr);
--- python/Converters/PycValueHolder.h
+++ python/Converters/PycValueHolder.h
@@ -1,99 +1,89 @@
-//# PycValueHolder.h: Class to convert a ValueHolder to/from Python
-//# Copyright (C) 2006
-//# Associated Universities, Inc. Washington DC, USA.
-//#
-//# This library is free software; you can redistribute it and/or modify it
-//# under the terms of the GNU Library General Public License as published by
-//# the Free Software Foundation; either version 2 of the License, or (at your
-//# option) any later version.
-//#
-//# This library is distributed in the hope that it will be useful, but WITHOUT
-//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
-//# License for more details.
-//#
-//# You should have received a copy of the GNU Library General Public License
-//# along with this library; if not, write to the Free Software Foundation,
-//# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
-//#
-//# Correspondence concerning AIPS++ should be addressed as follows:
-//#        Internet email: casa-feedback@nrao.edu.
-//#        Postal address: AIPS++ Project Office
-//#                        National Radio Astronomy Observatory
-//#                        520 Edgemont Road
-//#                        Charlottesville, VA 22903-2475 USA
-
+// # PycValueHolder.h: Class to convert a ValueHolder to/from Python
+// # Copyright (C) 2006
+// # Associated Universities, Inc. Washington DC, USA.
+// #
+// # This library is free software; you can redistribute it and/or modify it
+// # under the terms of the GNU Library General Public License as published by
+// # the Free Software Foundation; either version 2 of the License, or (at your
+// # option) any later version.
+// #
+// # This library is distributed in the hope that it will be useful, but WITHOUT
+// # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// # FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+// # License for more details.
+// #
+// # You should have received a copy of the GNU Library General Public License
+// # along with this library; if not, write to the Free Software Foundation,
+// # Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+// #
+// # Correspondence concerning AIPS++ should be addressed as follows:
+// #        Internet email: casa-feedback@nrao.edu.
+// #        Postal address: AIPS++ Project Office
+// #                        National Radio Astronomy Observatory
+// #                        520 Edgemont Road
+// #                        Charlottesville, VA 22903-2475 USA
 
 #ifndef PYRAP_PYCVALUEHOLDER_H
 #define PYRAP_PYCVALUEHOLDER_H
 
-//# Includes
+// # Includes
 
 // include first to avoid _POSIX_C_SOURCE redefined warnings
 #include <boost/python.hpp>
 #include <casacore/casa/Containers/ValueHolder.h>
 #include <casacore/casa/Utilities/DataType.h>
 
-namespace casacore { namespace python {
+namespace casacore {
+namespace python {
 
+// <summary>
+// A class to convert a ValueHolder to/from Python objects.
+// </summary>
 
-  // <summary>
-  // A class to convert a ValueHolder to/from Python objects.
-  // </summary>
+// <use visibility=export>
+// <reviewed reviewer="" date="" tests="">
+// </reviewed>
 
-  // <use visibility=export>
-  // <reviewed reviewer="" date="" tests="">
-  // </reviewed>
+// <synopsis>
+// </synopsis>
 
-  // <synopsis>
-  // </synopsis>
+struct casa_value_to_python {
+  static boost::python::object makeobject(ValueHolder const&);
+  static PyObject* convert(ValueHolder const& c) {
+    return boost::python::incref(makeobject(c).ptr());
+  }
+};
 
-  struct casa_value_to_python
-  {
-    static boost::python::object makeobject (ValueHolder const&);
-    static PyObject* convert (ValueHolder const& c)
-    {
-      return boost::python::incref(makeobject(c).ptr());
-    }
-  };
+struct casa_value_from_python {
+  casa_value_from_python() {
+    boost::python::converter::registry::push_back(&convertible, &construct,
+                                                  boost::python::type_id<ValueHolder>());
+  }
 
-  struct casa_value_from_python
-  {
-    casa_value_from_python()
-    {
-      boost::python::converter::registry::push_back(
-        &convertible,
-        &construct,
-        boost::python::type_id<ValueHolder>());
-    }
+  // Check if it is a type we can convert.
+  static void* convertible(PyObject* obj_ptr);
 
-    // Check if it is a type we can convert.
-    static void* convertible(PyObject* obj_ptr);
+  // Constructs a ValueHolder from a Python object.
+  static void construct(PyObject* obj_ptr,
+                        boost::python::converter::rvalue_from_python_stage1_data* data);
 
-    // Constructs a ValueHolder from a Python object.
-    static void construct(
-      PyObject* obj_ptr,
-      boost::python::converter::rvalue_from_python_stage1_data* data);
+  // Make a ValueHolder from all possible python data types.
+  static ValueHolder makeValueHolder(PyObject* obj_ptr);
 
-    // Make a ValueHolder from all possible python data types.
-    static ValueHolder makeValueHolder (PyObject* obj_ptr);
+  // Make a vector from a python sequence.
+  static ValueHolder toVector(PyObject* obj_ptr);
+  // Get (and check) the data type in a python sequence.
+  static DataType checkDataType(PyObject* obj_ptr);
+};
 
-    // Make a vector from a python sequence.
-    static ValueHolder toVector (PyObject* obj_ptr);
-    // Get (and check) the data type in a python sequence.
-    static DataType checkDataType (PyObject* obj_ptr);
-  };
+// Register the ValueHolder conversion.
+struct convert_casa_valueholder {
+  static void reg();
+  static bool _done;
+};
+inline void register_convert_casa_valueholder() { convert_casa_valueholder::reg(); }
 
-
-  // Register the ValueHolder conversion.
-  struct convert_casa_valueholder
-  {
-    static void reg();
-    static bool _done;
-  };
-  inline void register_convert_casa_valueholder()
-    { convert_casa_valueholder::reg(); }
-
-}}
+}  // namespace python
+}  // namespace casacore
 
 #endif
